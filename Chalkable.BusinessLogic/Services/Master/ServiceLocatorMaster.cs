9e67815fef6df9b423be0e7328a35f37f8da403c
Ievgenii Zasoba <geka.zasoba@gmail.com>
2013-06-17 16:16:35 +0300

    added background task table/da/service

--- Chalkable.BusinessLogic/Services/Master/ServiceLocatorMaster.cs
+++ Chalkable.BusinessLogic/Services/Master/ServiceLocatorMaster.cs
@@ -9,20 +9,24 @@ namespace Chalkable.BusinessLogic.Services.Master
         UserContext Context { get; }
         IUserService UserService { get; }
         ISchoolService SchoolService { get; }
+        IBackgroundTaskService BackgroundTaskService { get; }
     }
 
     public class ServiceLocatorMaster : ServiceLocator, IServiceLocatorMaster
     {
         private IUserService userService;
         private ISchoolService schoolService;
+        private IBackgroundTaskService backgroundTaskService;
         public ServiceLocatorMaster(UserContext context) : base(context)
         {
             userService = new UserService(this);
             schoolService = new SchoolService(this);
+            backgroundTaskService = new BackgroundTaskService(this);
         }
 
         public IUserService UserService { get { return userService; } }
         public ISchoolService SchoolService { get { return schoolService; } }
+        public IBackgroundTaskService BackgroundTaskService { get { return backgroundTaskService; } }
 
         public IServiceLocatorSchool SchoolServiceLocator(Guid schoolId, string schoolName, string schoolServerUrl)
         {
---
+++ Chalkable.Data.Master/DataAccess/BackgroundTaskDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.Common;
+using Chalkable.Data.Master.Model;
+
+namespace Chalkable.Data.Master.DataAccess
+{
+    public class BackgroundTaskDataAccess : DataAccessBase
+    {
+        public BackgroundTaskDataAccess(UnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public void Create(BackgroundTask task)
+        {
+            SimpleInsert(task);
+        }
+
+        public BackgroundTask GetTaskForProcessing()
+        {
+            using (var reader = ExecuteStoredProcedureReader("spGetBackgroundTaskForProcessing", new Dictionary<string, object>()))
+            {
+                return reader.ReadOrNull<BackgroundTask>();
+            }
+        }
+    }
+}
