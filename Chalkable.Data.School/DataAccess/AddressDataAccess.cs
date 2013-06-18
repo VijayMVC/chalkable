ad00dae5a364a5f4969b1ef62f76289a238f3f5d
Ievgenii Zasoba <geka.zasoba@gmail.com>
2013-06-18 13:21:17 +0300

    Added loging for background tasks

---
+++ Chalkable.Data.School/DataAccess/AddressDataAccess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.Common;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Data.School.DataAccess
+{
+    public class AddressDataAccess : DataAccessBase
+    {
+        public AddressDataAccess(UnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public void Create(Address address)
+        {
+            SimpleInsert(address);
+        }
+
+        public void Update(Address address)
+        {
+            SimpleUpdate(address);
+        }
+        
+        public void Delete(Guid id)
+        {
+            SimpleDelete<Address>(id);
+        } 
+
+        public IList<Address> GetAddresses()
+        {
+            return SelectMany<Address>();
+        } 
+        
+        public Address GetAddressById(Guid id)
+        {
+            var conds = new Dictionary<string, object>{{"Id", id}};
+            return SelectOne<Address>(conds);
+        }
+    }
+}
---
+++ Chalkable.Data.School/DataAccess/AnnouncementTypeDataAccess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.Common;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Data.School.DataAccess
+{
+    public class AnnouncementTypeDataAccess : DataAccessBase
+    {
+        public AnnouncementTypeDataAccess(UnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public AnnouncementType GetById(int id)
+        {
+            var conds = new Dictionary<string, object> {{"id", id}};
+            return SelectOne<AnnouncementType>(conds);
+        }
+        public IList<AnnouncementType> GetList()
+        {
+            return SelectMany<AnnouncementType>();
+        } 
+    }
+}
