64b28273dd8ffb52b78f4549f5717c6179f6679a
Andriy Machnew <amachnew@emp-game.com>
2013-06-20 15:07:23 +0300

    added schools activity

--- Chalkable.Web/app/chlk/controllers/BaseController.js
+++ Chalkable.Web/app/chlk/controllers/BaseController.js
@@ -15,6 +15,32 @@ NAMESPACE('chlk.controllers', function (){
                });
 
                this.getView().push(instance);
+           },
+
+           ria.async.Future, function validateResponse_() {
+               var head
+                   , me = this;
+
+               (head = new ria.async.Future)
+                   .catchError(function (error) {
+                       throw chlk.services.DataException('Failed to load data', error);
+                   })
+                   .then(function (data) {
+                       // TODO: check response here
+                       /*if (!data.isOkResponse())
+                        throw chlk.services.DataException('Failed to load data: ' + $L(data.getErrorCode()));*/
+
+                       return data;
+                   })
+                   .catchException(chlk.services.DataException, function (error) {
+                       this.BREAK(); // failed with exception, stop further processing
+
+                       console.error(error.toString());
+                       // todo: scoping !?
+                       //me.view.showAlertBox(error.getMessage());
+                   });
+
+               return head;
            }
 
    ])
--- Chalkable.Web/app/chlk/controllers/SysAdminController.js
+++ Chalkable.Web/app/chlk/controllers/SysAdminController.js
@@ -1,4 +1,7 @@
+REQUIRE('chlk.services.SchoolService');
 REQUIRE('chlk.controllers.BaseController');
+REQUIRE('chlk.activities.Schools');
+
 
 NAMESPACE('chlk.controllers', function (){
 
@@ -6,6 +9,20 @@ NAMESPACE('chlk.controllers', function (){
     CLASS(
         [ria.mvc.ControllerUri('index')],
         'SysAdminController', EXTENDS(chlk.controllers.BaseController), [
-            VOID, function indexAction() {}
-        ])
+
+        [ria.mvc.Inject],
+        chlk.services.SchoolService, 'schoolService',
+
+        VOID, function indexAction() {
+            var result = this.schoolService
+                .getSchools()
+                .attach(this.validateResponse_());
+            /* Put activity in stack and render when result is ready */
+            return this.PushView(chlk.activities.Schools, result);
+        },
+        VOID, function appsAction() {},
+        VOID, function settingsAction() {},
+        VOID, function signupsAction() {},
+        VOID, function fundsAction() {}
+    ])
 });
