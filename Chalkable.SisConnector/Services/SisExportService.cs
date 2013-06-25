116f8f02595f956e8763bde3867f0c0932c6a6e2
Andriy Machnew <amachnew@emp-game.com>
2013-06-25 13:16:18 +0300

    ?????????

---
+++ Chalkable.SisConnector/Services/SisExportService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Chalkable.BusinessLogic.Services;
+using Chalkable.BusinessLogic.Services.Master;
+using Chalkable.BusinessLogic.Services.School;
+
+namespace Chalkable.SisConnector.Services
+{
+    public abstract class SisExportService
+    {
+        protected IServiceLocatorMaster ServiceLocatorMaster { get; set; }
+        protected IServiceLocatorSchool ServiceLocatorSchool { get; set; }
+        protected Guid SchoolId { get; private set; }
+        protected int SisSchoolId { get; private set; }
+        protected BackgroundTaskService.BackgroundTaskLog Log { get; private set; }
+
+        protected SisExportService(Guid schoolId, int sisSchoolId, BackgroundTaskService.BackgroundTaskLog log)
+        {
+            ServiceLocatorMaster = ServiceLocatorFactory.CreateMasterSysAdmin();
+            var school = ServiceLocatorMaster.SchoolService.GetById(schoolId);
+            ServiceLocatorSchool = ServiceLocatorMaster.SchoolServiceLocator(school.Id, school.Name, school.TimeZone, school.ServerUrl);
+            SchoolId = schoolId;
+            SisSchoolId = sisSchoolId;
+            Log = log;
+        }
+
+        public abstract void ExportAttendances(int sisSchoolId);
+    }
+}
