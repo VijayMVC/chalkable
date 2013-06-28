7110fc4eef9641418a6c0d8cb4fc96bcb7757585
Andriy Machnew <amachnew@emp-game.com>
2013-06-28 12:28:33 +0300

    fixed templatepage

--- Chalkable.Web/app/chlk/activities/TemplatePage.js
+++ Chalkable.Web/app/chlk/activities/TemplatePage.js
@@ -7,33 +7,33 @@ NAMESPACE('chlk.activities', function () {
         [[String]],
         function PageClass(clazz) {});
 
-    var pageClass, wrapper = new ria.dom.Dom('#content-wrapper');
-
     /** @class chlk.activities.TemplatePage*/
     CLASS(
         'TemplatePage', EXTENDS(chlk.activities.TemplateActivity), [
             function $() {
                 BASE();
+                this._wrapper = new ria.dom.Dom('#content-wrapper');
             },
 
             OVERRIDE, VOID, function bind_() {
                 BASE();
                 var ref = ria.reflection.ReflectionFactory(this.getClass());
                 if (ref.isAnnotatedWith(chlk.activities.PageClass)){
-                    pageClass = ref.findAnnotation(chlk.activities.PageClass)[0].clazz;
+                    this._pageClass = ref.findAnnotation(chlk.activities.PageClass)[0].clazz;
                 }else{
-                    pageClass = null
+                    this._pageClass = null;
                 }
             },
 
             OVERRIDE, VOID, function onRender_(model){
                 BASE(model);
-                pageClass && wrapper.addClass(pageClass);
+
+                this._pageClass && this._wrapper.addClass(this._pageClass);
             },
 
             OVERRIDE, VOID, function onStop_() {
                 BASE();
-                pageClass && wrapper.removeClass(pageClass);
+                this._pageClass && this._wrapper.removeClass(this._pageClass);
             }
         ]);
 });
