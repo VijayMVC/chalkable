77d6c43cf0ac62d040be6e2f75efbfe05138381b
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2013-07-08 20:05:41 +0300

    added scheduleSectionController

---
+++ Chalkable.Web/Controllers/ScheduleSectionController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Chalkable.Common;
+using Chalkable.Data.Common.Enums;
+using Chalkable.Data.Master.Model;
+using Chalkable.Data.School.Model;
+using Chalkable.Web.ActionFilters;
+using Chalkable.Web.Models;
+
+namespace Chalkable.Web.Controllers
+{
+    public class ScheduleSectionController : ChalkableController
+    {
+        [AuthorizationFilter]
+        public ActionResult List(Guid markingPeriodId)
+        {
+            var res = SchoolLocator.ScheduleSectionService.GetSections(markingPeriodId);
+            return Json(res.Select(ScheduleSectionViewData.Create));
+        }
+
+        //[AuthorizationFilter("System Admin, AdminGrade, AdminEdit, AdminView, Teacher", Preference.API_DESCR_SCHEDULE_SECTION_LIST_FOR_MARKING_PERIODS, true, CallType.Get, new[] { AppPermissionType.Schedule })]
+        //public ActionResult ListForMarkingPeriods(GuidList markingPeriodIds)
+        //{
+        //    var markingPeriod = SchoolLocator.MarkingPeriodService.GetMarkingPeriodById(markingPeriodIds[0]);
+        //    var canGetSectoions = SchoolLocator.ScheduleSectionService.CanGetSection(markingPeriodIds);
+        //    var sections = canGetSectoions ? SchoolLocator.ScheduleSectionService.GetSections(markingPeriodIds) : new List<ScheduleSection>();
+        //    var canchange = canGetSectoions && SchoolLocator.ScheduleSectionService.CanDeleteSections(markingPeriodIds);
+        //    return Json(SectionsForMPsViewData.Create(sections, markingPeriod, canGetSectoions, canchange));
+        //}
+
+        [AuthorizationFilter("System Admin, AdminGrade, AdminEdit")]
+        public ActionResult ChangeSections(StringList sections, GuidList markingPeriodIds)
+        {
+            SchoolLocator.ScheduleSectionService.ReBuildSections(sections, markingPeriodIds);
+            return Json(true);
+        }
+    }
+}
