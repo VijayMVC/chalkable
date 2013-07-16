157969c370c846a77c491da379223ae8c9f28da4
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2013-07-16 16:15:11 +0300

    addded categoryService and category Controller

---
+++ Chalkable.Web/Models/CategoryVeiwData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Chalkable.Data.Master.Model;
+
+namespace Chalkable.Web.Models
+{
+    public class CategoryVeiwData
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        private CategoryVeiwData() { }
+
+        public static CategoryVeiwData Create(Category category)
+        {
+           return new CategoryVeiwData
+            {
+                Id = category.Id,
+                Name = category.Name,
+                Description = category.Description
+            };
+        }
+        public static IList<CategoryVeiwData> Create(IList<Category> categories)
+        {
+            return categories.Select(Create).ToList();
+        }
+    }
+}
