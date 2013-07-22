732f71e01217f6fb68c6837f14427b28fdb0fb8f
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2013-07-22 20:01:28 +0300

    fix ChalkableDepartment table

--- SQL/ChalkableMaster/1008 - added ChalkableDepartment table.sql
+++ SQL/ChalkableMaster/1008 - added ChalkableDepartment table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE ChalkableDepartment
 (
-	[Id] uniqueidentifier NOT NULL,
+	[Id] uniqueidentifier NOT NULL primary key,
 	[Name] [nvarchar](255) NOT NULL,
 	[Keywords] [nvarchar](max) NOT NULL
 )
