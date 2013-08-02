8b860502ab27f1be62fc9ff566de0bd83e5fb366
Ostap Stasyuk <ostasyuk@emp-game.com>
2013-08-02 17:24:14 +0300

    added BgTasks types

--- Chalkable.Web/app/chlk/models/bgtasks/BgTaskType.js
+++ Chalkable.Web/app/chlk/models/bgtasks/BgTaskType.js
@@ -8,7 +8,9 @@ NAMESPACE('chlk.models.bgtasks', function () {
             CREATE_EMPTY_SCHOOL: 0,
             SIS_DATA_IMPORT:1,
             BACKUP_DATABASES: 2,
-            RESTORE_DATABASES: 3
+            RESTORE_DATABASES: 3,
+            DATABASE_UPDATE: 4,
+            CREATE_DEMO_SCHOOL: 5
         });
 
 
@@ -22,6 +24,8 @@ NAMESPACE('chlk.models.bgtasks', function () {
               this._types[chlk.models.bgtasks.BgTaskTypeEnum.SIS_DATA_IMPORT] = "SIS data import";
               this._types[chlk.models.bgtasks.BgTaskTypeEnum.BACKUP_DATABASES] = "Backup datatabases";
               this._types[chlk.models.bgtasks.BgTaskTypeEnum.RESTORE_DATABASES] = "Restore databases";
+              this._types[chlk.models.bgtasks.BgTaskTypeEnum.DATABASE_UPDATE] = "Database Update";
+              this._types[chlk.models.bgtasks.BgTaskTypeEnum.CREATE_DEMO_SCHOOL] = "Create Demo School";
             },
             String, function toString(){
                 return this._types[this.getTypeId()];
--- Chalkable.Web/app/chlk/models/people/Person.js
+++ Chalkable.Web/app/chlk/models/people/Person.js
@@ -1,5 +1,6 @@
 REQUIRE('chlk.models.common.ChlkDate');
 REQUIRE('chlk.models.people.User');
+REQUIRE('chlk.models.people.Phone');
 
 NAMESPACE('chlk.models.people', function () {
     "use strict";
@@ -8,6 +9,7 @@ NAMESPACE('chlk.models.people', function () {
     CLASS(
         'Person', EXTENDS(chlk.models.people.User), [
             chlk.models.common.ChlkDate, 'birthDate',
+            ArrayOf(chlk.models.people.Phone), 'phones',
             String, 'salutation'
         ]);
 });
