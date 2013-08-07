f423c300ba1fad78b53024c197005fc41bbe6307
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2013-08-07 16:48:18 +0300

    added DeveloperSchoolTestContext

---
+++ Chalkable.Tests/Services/Master/ApplicationServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Chalkable.Tests.Services.Master
+{
+    public class ApplicationServiceTest : MasterServiceTestBase
+    {
+        [Test]
+        public void CreateTest()
+        {
+            
+        }
+
+        [Test]
+        public void UpdateTest()
+        {
+            
+        }
+    }
+}
