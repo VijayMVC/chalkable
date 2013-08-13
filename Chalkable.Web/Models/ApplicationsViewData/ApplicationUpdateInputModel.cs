411ce5171160ee43669c4dc93999dcfc6fe3ab4e
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2013-08-13 15:56:17 +0300

    fix in ApplicationUpdateInputModel

--- Chalkable.Web/Models/ApplicationsViewData/ApplicationUpdateInputModel.cs
+++ Chalkable.Web/Models/ApplicationsViewData/ApplicationUpdateInputModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Chalkable.BusinessLogic.Model;
+using Chalkable.Common;
 using Chalkable.Data.Common.Enums;
 
 namespace Chalkable.Web.Models.ApplicationsViewData
@@ -12,15 +13,54 @@ namespace Chalkable.Web.Models.ApplicationsViewData
         public Guid ApplicationId { get; set; }
         public bool ForSubmit { get; set; }
 
-        public IList<int> Permissions
+        public IntList Permissions
         {
-            get { return PermissionIds.Select(x => (int) x).ToList(); }
+            get
+            {
+                var res = new IntList();
+                res.AddRange(PermissionIds.Select(x => (int)x).ToList());
+                return res;
+            }
             set { PermissionIds = value.Select(x => (AppPermissionType)x).ToList(); }
         }
 
+        public new IntList GradeLevels
+        {
+            get
+            {
+                var res = new IntList();
+                res.AddRange(base.GradeLevels);
+                return res;
+            }
+            set { base.GradeLevels = value; }
+        }
+        public new GuidList Categories
+        {
+            get
+            {
+                var res = new GuidList();
+                res.AddRange(base.Categories);
+                return res;
+            }
+            set { base.Categories = value; }
+        }
+        public new GuidList PicturesId
+        {
+            get
+            {
+                var res = new GuidList();
+                res.AddRange(base.PicturesId);
+                return res;
+            }
+            set { base.PicturesId = value; }
+        }
+        
         public ApplicationUpdateInputModel()
         {
-            Permissions = new List<int>();
+            Permissions = new IntList();
+            GradeLevels = new IntList();
+            Categories = new GuidList();
+            PicturesId = new GuidList();
         }
     }
 }
