2208c6dee867be1dc7628083cf503e9ae6ac3e60
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2013-08-13 16:31:21 +0300

    fix in ApplicationUpdateInputModel

--- Chalkable.Web/Models/ApplicationsViewData/ApplicationUpdateInputModel.cs
+++ Chalkable.Web/Models/ApplicationsViewData/ApplicationUpdateInputModel.cs
@@ -13,54 +13,16 @@ namespace Chalkable.Web.Models.ApplicationsViewData
         public Guid ApplicationId { get; set; }
         public bool ForSubmit { get; set; }
 
-        public IntList Permissions
+        public IList<int> Permissions
         {
-            get
-            {
-                var res = new IntList();
-                res.AddRange(PermissionIds.Select(x => (int)x).ToList());
-                return res;
-            }
+            get { return PermissionIds.Select(x => (int) x).ToList(); }
             set { PermissionIds = value.Select(x => (AppPermissionType)x).ToList(); }
         }
 
-        public new IntList GradeLevels
-        {
-            get
-            {
-                var res = new IntList();
-                res.AddRange(base.GradeLevels);
-                return res;
-            }
-            set { base.GradeLevels = value; }
-        }
-        public new GuidList Categories
-        {
-            get
-            {
-                var res = new GuidList();
-                res.AddRange(base.Categories);
-                return res;
-            }
-            set { base.Categories = value; }
-        }
-        public new GuidList PicturesId
-        {
-            get
-            {
-                var res = new GuidList();
-                res.AddRange(base.PicturesId);
-                return res;
-            }
-            set { base.PicturesId = value; }
-        }
-        
+       
         public ApplicationUpdateInputModel()
         {
             Permissions = new IntList();
-            GradeLevels = new IntList();
-            Categories = new GuidList();
-            PicturesId = new GuidList();
         }
     }
 }
