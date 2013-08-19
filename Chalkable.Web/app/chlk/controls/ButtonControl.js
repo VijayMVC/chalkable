3f4672e1595f3ef4fc3c7a37e52b4a1a60b7131c
Andriy Machnew <amachnew@emp-game.com>
2013-08-19 15:55:48 +0300

    fixes to button

--- Chalkable.Web/app/chlk/controls/ButtonControl.js
+++ Chalkable.Web/app/chlk/controls/ButtonControl.js
@@ -8,6 +8,19 @@ NAMESPACE('chlk.controls', function () {
             OVERRIDE, VOID, function onCreate_() {
                 BASE();
                 ASSET('~/assets/jade/controls/button.jade')(this);
+            },
+
+            [[Object]],
+            Object, function processAttrs(attributes){
+                if(attributes.disabled)
+                    if(Array.isArray(attributes.class)) {
+                        attributes.class = attributes.class || [];
+                        attributes.class.push('disabled');
+                    }else{
+                        attributes.class = attributes.class + ' disabled';
+                    }
+
+                return attributes;
             }
         ]);
 });
