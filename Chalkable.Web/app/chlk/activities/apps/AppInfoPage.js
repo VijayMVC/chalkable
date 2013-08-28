e7a3a83880e3b28b1cf4461d7ae1a8bce80e53df
Andriy Machnew <amachnew@emp-game.com>
2013-08-28 16:46:48 +0300

    fixed pricing block

--- Chalkable.Web/app/chlk/activities/apps/AppInfoPage.js
+++ Chalkable.Web/app/chlk/activities/apps/AppInfoPage.js
@@ -26,6 +26,20 @@ NAMESPACE('chlk.activities.apps', function () {
             VOID, function toggleAppPaymentInfo(node, event){
                 var appPricing = this.dom.find('.prices');
                 appPricing.toggleClass(HIDDEN_CLASS, node.checked());
+            },
+
+            [ria.mvc.DomEventBind('click', 'input[name=schoolFlatRateEnabled]')],
+            [[ria.dom.Dom, ria.dom.Event]],
+            VOID, function toggleSchoolFlatRate(node, event){
+                var schoolFlatRate = this.dom.find('.school-flat-rate');
+                schoolFlatRate.toggleClass(HIDDEN_CLASS, !node.checked());
+            },
+
+            [ria.mvc.DomEventBind('click', 'input[name=classFlatRateEnabled]')],
+            [[ria.dom.Dom, ria.dom.Event]],
+            VOID, function toggleClassFlatRate(node, event){
+                var classFlatRate = this.dom.find('.class-flat-rate');
+                classFlatRate.toggleClass(HIDDEN_CLASS, !node.checked());
             }
         ]);
 });
