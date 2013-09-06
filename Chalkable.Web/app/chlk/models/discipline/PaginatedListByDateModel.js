c825ae55e0e95b8934795eaadf143b36dbcf2a18
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2013-09-06 16:55:03 +0300

    fix in discipline

---
+++ Chalkable.Web/app/chlk/models/discipline/PaginatedListByDateModel.js
@@ -0,0 +1,22 @@
+REQUIRE('chlk.models.common.PaginatedList');
+REQUIRE('chlk.models.common.ChlkDate');
+
+NAMESPACE('chlk.models.discipline', function(){
+    "use strict";
+
+    /** @class chlk.templates.discipline.PaginatedListByDateModel*/
+
+    CLASS('PaginatedListByDateModel', [
+
+        chlk.models.common.PaginatedList, 'items',
+        chlk.models.common.ChlkDate, 'date',
+
+        [[chlk.models.common.PaginatedList, chlk.models.common.ChlkDate]],
+        function $(items_, date_){
+            if(items_)
+                this.setItems(items_);
+            if(date_)
+                this.setDate(date_);
+        }
+    ]);
+});
