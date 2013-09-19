4585977d10c4b5351beab82c2b3f94e9a0d35046
Ostap Stasyuk <ostasyuk@emp-game.com>
2013-09-20 00:48:48 +0300

    updated admin attendance page

--- Chalkable.Web/app/chlk/activities/attendance/AdminAttendanceSummaryPage.js
+++ Chalkable.Web/app/chlk/activities/attendance/AdminAttendanceSummaryPage.js
@@ -8,5 +8,33 @@ NAMESPACE('chlk.activities.attendance', function () {
         [ria.mvc.DomAppendTo('#main')],
         [chlk.activities.lib.PageClass('attendance')],
         [ria.mvc.TemplateBind(chlk.templates.attendance.AdminAttendanceSummaryTpl)],
-        'AdminAttendanceSummaryPage', EXTENDS(chlk.activities.lib.TemplatePage), [ ]);
+        'AdminAttendanceSummaryPage', EXTENDS(chlk.activities.lib.TemplatePage), [
+
+            [ria.mvc.DomEventBind('click', '.attendance-top-box:not(.active)')],
+                [[ria.dom.Dom, ria.dom.Event]],
+                VOID, function boxClick(node, event) {
+                    var index = node.getData('index');
+                    this.dom.find('.attendance-top-box.active').removeClass('active');
+                    this.dom.find('.students-statistic.active').removeClass('active');
+                    this.dom.find('.students-statistic.page-' + index).addClass('active');
+                    node.addClass('active');
+                    var left = node.getAttr('left');
+                    var text = node.getAttr('text');
+                    var slider = this.dom.find('.slider-container');
+                    slider.setCss('left', left);
+                    slider.find('.active').setHTML(text);
+            },
+
+            [ria.mvc.DomEventBind('click', '.more-students')],
+                [[ria.dom.Dom, ria.dom.Event]],
+                VOID, function moreStudentsClick(node, event) {
+                    var hiddenBlock = this.dom.find('.hidden-students-block.no-used:eq(0)');
+                    var height = hiddenBlock.find('.hidden-students-block-2').height();
+                    hiddenBlock
+                        .setCss('height', height)
+                        .removeClass('no-used');
+                    if(hiddenBlock.hasClass('last-block'))
+                        node.parent('.more-container').hide();
+            }
+        ]);
 });
