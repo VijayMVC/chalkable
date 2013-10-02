3c72262b9305699eec60a2786c6c1a83ae43ca11
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2013-10-02 13:03:29 +0300

    fixed attendance calendar

--- Chalkable.Web/app/chlk/templates/calendar/attendance/StudentAttendanceCalendarBodyTpl.js
+++ Chalkable.Web/app/chlk/templates/calendar/attendance/StudentAttendanceCalendarBodyTpl.js
@@ -22,7 +22,18 @@ NAMESPACE('chlk.templates.calendar.attendance', function (){
             chlk.models.common.ChlkDate, 'minDate',
 
             [ria.templates.ModelPropertyBind],
-            ArrayOf(chlk.models.calendar.attendance.StudentAttendanceCalendarMonthItem), 'calendarItems'
+            ArrayOf(chlk.models.calendar.attendance.StudentAttendanceCalendarMonthItem), 'calendarItems',
 
+            [[Number]],
+            String, function getColorByAttendanceType(attendanceType){
+                var attTypeEnums = chlk.models.attendance.AttendanceTypeEnum;
+                switch (attendanceType){
+                    case attTypeEnums.PRESENT.valueOf() : return 'green';
+                    case attTypeEnums.ABSENT.valueOf() : return 'red';
+                    case attTypeEnums.EXCUSED.valueOf() : return 'blue';
+                    case attTypeEnums.LATE.valueOf() : return 'darkOrange';
+                    default : return 'black';
+                }
+            }
         ]);
 });
