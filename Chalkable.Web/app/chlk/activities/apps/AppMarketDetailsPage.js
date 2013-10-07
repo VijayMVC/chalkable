cf5b4a2703a9fc1b18a8a8a0bb77f3218f25a813
Andriy Machnew <amachnew@emp-game.com>
2013-10-07 19:39:38 +0300

    fixed market details

--- Chalkable.Web/app/chlk/activities/apps/AppMarketDetailsPage.js
+++ Chalkable.Web/app/chlk/activities/apps/AppMarketDetailsPage.js
@@ -9,5 +9,18 @@ NAMESPACE('chlk.activities.apps', function () {
         [chlk.activities.lib.PageClass('app-market')],
         [ria.mvc.TemplateBind(chlk.templates.apps.AppMarketDetails)],
         'AppMarketDetailsPage', EXTENDS(chlk.activities.lib.TemplatePage), [
+
+            [ria.mvc.DomEventBind('click', '.write-review-btn')],
+            [[ria.dom.Dom, ria.dom.Event]],
+            VOID, function toggleReviewArea(node, event){
+                this.dom.find('.add-review').toggleClass('x-hidden');
+            },
+
+            [ria.mvc.DomEventBind('click', '.cancel-btn')],
+            [[ria.dom.Dom, ria.dom.Event]],
+            VOID, function cancelReview(node, event){
+                this.dom.find('.add-review').addClass('x-hidden');
+                //todo: reset ratings here also
+            }
         ]);
 });
