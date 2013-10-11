27fa2184aec156da2d8f1ee6a1000e0a05f998d8
Ostap Stasyuk <ostasyuk@emp-game.com>
2013-10-11 16:27:11 +0300

    fixed announcement saving (changing types and redirect to view)

---
+++ Chalkable.Data.School/DataAccess/ClassDisciplineTypeDataAccess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.Common;
+using Chalkable.Data.Common.Orm;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Data.School.DataAccess
+{
+    public class ClassDisciplineTypeDataAccess : DataAccessBase<ClassDisciplineType>
+    {
+        public ClassDisciplineTypeDataAccess(UnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public void  Delete(Guid classDisciplineId, Guid? disciplineType)
+        {
+            var condition = new AndQueryCondition {{ClassDisciplineType.CLASS_DISCIPLINE_REF, classDisciplineId}};
+            if(disciplineType.HasValue)
+                condition.Add(ClassDisciplineType.DISCIPLINE_TYPE_REF, disciplineType);
+            SimpleDelete<ClassDisciplineType>(condition);
+        }
+    }
+}
---
+++ Chalkable.Web/Controllers/GradingController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Chalkable.BusinessLogic.Services;
+using Chalkable.Common.Exceptions;
+using Chalkable.Data.Common.Enums;
+using Chalkable.Data.Master.Model;
+using Chalkable.Data.School.DataAccess;
+using Chalkable.Data.School.Model;
+using Chalkable.Web.ActionFilters;
+using Chalkable.Web.Logic;
+using Chalkable.Web.Models;
+
+namespace Chalkable.Web.Controllers
+{
+    [RequireHttps, TraceControllerFilter]
+    public class GradingController : ChalkableController
+    {
+        [AuthorizationFilter("Teacher")]
+        public ActionResult TeacherSummary(Guid teacherId)
+        {
+            var schoolYearId = GetCurrentSchoolYearId();
+            var gradingStats = SchoolLocator.GradingStatisticService.GetStudentsGradePerClass(teacherId, schoolYearId);
+            gradingStats = gradingStats.Where(x => x.Avg.HasValue).ToList();
+            var classes = SchoolLocator.ClassService.GetClasses(null, null, teacherId);
+            return Json(GradingTeacherClassSummaryViewData.Create(gradingStats, classes), 6);
+        }
+
+        [AuthorizationFilter("Teacher", Preference.API_DESCR_GRADING_CLASS_SUMMARY, true, CallType.Get, new[] { AppPermissionType.Grade, AppPermissionType.Class })]
+        public ActionResult ClassSummary(Guid classId)
+        {
+            if (!SchoolLocator.Context.SchoolId.HasValue)
+                throw new UnassignedUserException();
+            var teacherId = Context.UserId;
+            return Json(ClassLogic.GetGradingSummary(SchoolLocator, classId, GetCurrentSchoolYearId(), teacherId), 7);
+        }
+
+        [AuthorizationFilter("AdminGrade, AdminEdit, AdminView, Teacher, Student", Preference.API_DESCR_GRADING_CLASS_SUMMARY, true, CallType.Get, new[] { AppPermissionType.Grade, AppPermissionType.Class })]
+        public ActionResult ItemGradingStat(Guid announcementId)
+        {
+            var ann = SchoolLocator.AnnouncementService.GetAnnouncementById(announcementId);
+            var studentAnns = SchoolLocator.StudentAnnouncementService.GetStudentAnnouncements(announcementId);
+            var mapper = SchoolLocator.GradingStyleService.GetMapper();
+            return Json(ItemGradigStatViewData.Create(studentAnns, ann, mapper));
+        }
+
+        [AuthorizationFilter("AdminGrade, AdminEdit, AdminView, Teacher, Student")]
+        public ActionResult StudentSummary(Guid studentId, Guid? classId)
+        {
+            var announcements = SchoolLocator.AnnouncementService.GetAnnouncements(30, true);
+            var mp = SchoolLocator.MarkingPeriodService.GetMarkingPeriodByDate(Context.NowSchoolTime.Date, true);
+            var gradingStats = SchoolLocator.GradingStatisticService.GetStudentGradePerDate(studentId, mp.Id, classId);
+            return Json(GradingStudentSummaryViewData.Create(announcements, gradingStats));
+        }
+
+        //TODO: duplicate part of announcement/read data. for API compatibility only
+        [AuthorizationFilter("Teacher", Preference.API_DESCR_GRADE_LIST_ITEMS, true, CallType.Get, new[] { AppPermissionType.Grade, AppPermissionType.Announcement })]
+        public ActionResult ItemGradesList(Guid announcementId)
+        {
+            var annView = SchoolLocator.AnnouncementService.GetAnnouncementDetails(announcementId);
+            var attachmentsInfo = AttachmentLogic.PrepareAttachmentsInfo(annView.AnnouncementAttachments);
+            var res = StudentAnnouncementLogic.ItemGradesList(SchoolLocator, annView, attachmentsInfo);
+            return Json(res);
+        }
+
+        public ActionResult ApplyAutoGrade(Guid announcementId)
+        {
+            SchoolLocator.StudentAnnouncementService.ResolveAutoGrading(announcementId, true);
+            return ItemGradesList(announcementId);
+        }
+
+        public ActionResult ApplyManualGrade(Guid announcementId)
+        {
+            SchoolLocator.StudentAnnouncementService.ResolveAutoGrading(announcementId, false);
+            return ItemGradesList(announcementId);
+        }
+
+        //TODO: do we need this in API still?
+        [RequireRequestValue("studentAnnouncementId")]
+        [AuthorizationFilter("Teacher", Preference.API_DESCR_GRADE_UPDATE_ITEM, true, CallType.Get, new[] { AppPermissionType.Grade, AppPermissionType.Announcement })]
+        public ActionResult UpdateItem(Guid studentAnnouncementId, int? gradeValue, string extraCredits, string comment, bool dropped)
+        {
+            var studentAnn = SchoolLocator.StudentAnnouncementService.SetGrade(studentAnnouncementId, gradeValue, extraCredits, comment, dropped, (int)GradingStyleEnum.Numeric100);
+            return PrepareStudentAnnouncementResponse(studentAnn);
+        }
+
+        [AuthorizationFilter("Teacher ,AdminGrade, Student", Preference.API_DESCR_SET_AUTO_GRADE, true, CallType.Get, new[] { AppPermissionType.Grade, AppPermissionType.Announcement })]
+        public ActionResult SetAutoGrade(Guid studentAnnouncementId, int value, Guid applicationId)
+        {
+            var res = SchoolLocator.StudentAnnouncementService.SetAutoGrade(studentAnnouncementId, value, applicationId);
+            return PrepareStudentAnnouncementResponse(res);
+        }
+
+        private ActionResult PrepareStudentAnnouncementResponse(StudentAnnouncement studentAnn)
+        {
+            var studentAnnsInfo = SchoolLocator.StudentAnnouncementService.GetStudentAnnouncements(studentAnn.AnnouncementRef);
+            var res = studentAnnsInfo.First(x => x.Id == studentAnn.Id);
+            var attachments = SchoolLocator.AnnouncementAttachmentService.GetAttachments(res.AnnouncementRef, 0, 1000).ToList();
+            var attc = AttachmentLogic.PrepareAttachmentsInfo(attachments.Where(x => x.PersonRef == res.Id).ToList());
+            return Json(StudentAnnouncementViewData.Create(res, attc));
+        }
+    }
+}
