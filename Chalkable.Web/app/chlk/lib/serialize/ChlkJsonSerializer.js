de3151fdbfd1720525291c150baee6a1a3880373
Andriy Machnew <amachnew@emp-game.com>
2013-10-15 16:26:21 +0300

    fixed chlk json serializer, sync with ria v545

--- Chalkable.Web/app/chlk/lib/serialize/ChlkJsonSerializer.js
+++ Chalkable.Web/app/chlk/lib/serialize/ChlkJsonSerializer.js
@@ -7,12 +7,12 @@ NAMESPACE('chlk.lib.serialize', function () {
     /** @class chlk.lib.serialize.ChlkJsonSerializer*/
     CLASS('ChlkJsonSerializer', EXTENDS(ria.serialize.JsonSerializer), [
 
-        OVERRIDE, Object, function deserialize(raw, clazz) {
+        OVERRIDE, Object, function deserialize(raw, clazz, instance_) {
             if (ria.__API.isClassConstructor(clazz)) {
                 if (raw === null || raw === undefined || raw == '' && chlk.models.common.ChlkDate == clazz)
                     return null;
             }
-            return BASE(raw, clazz);
+            return BASE(raw, clazz, instance_);
         }
     ])
 });
