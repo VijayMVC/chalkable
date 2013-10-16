68c42212b0986aa79f35cf28efeb06e0af970300
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2013-10-16 16:37:08 +0300

    fixed profile apps page

---
+++ Chalkable.Tests/Services/School/AttendanceReasonServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.School.Model;
+using Chalkable.Tests.Services.TestContext;
+using NUnit.Framework;
+
+namespace Chalkable.Tests.Services.School
+{
+    public class AttendanceReasonServiceTest : BaseSchoolServiceTest
+    {
+        [Test]
+        public void AttReasonTest()
+        {
+            AssertForDeny(sl => sl.AttendanceReasonService.Add(AttendanceTypeEnum.Excused, "code1", "reason 1"), SchoolTestContext,
+                SchoolContextRoles.FirstTeacher | SchoolContextRoles.FirstStudent | SchoolContextRoles.AdminViewer | SchoolContextRoles.FirstParent);
+
+            var attReason = SchoolTestContext.AdminGradeSl.AttendanceReasonService.Add(AttendanceTypeEnum.Excused, "code1", "reason 1");
+            Assert.AreEqual(attReason.Code, "code1");
+            Assert.AreEqual(attReason.AttendanceType, AttendanceTypeEnum.Excused);
+            Assert.AreEqual(attReason.Description, "reason 1");
+
+            Assert.AreEqual(SchoolTestContext.AdminGradeSl.AttendanceReasonService.List().Count, 1);
+            Assert.AreEqual(SchoolTestContext.FirstTeacherSl.AttendanceReasonService.List().Count, 1);
+
+            AssertForDeny(sl => sl.AttendanceReasonService.Edit(attReason.Id, AttendanceTypeEnum.Excused, "code1", "reason 1"), SchoolTestContext,
+                SchoolContextRoles.FirstTeacher | SchoolContextRoles.FirstStudent | SchoolContextRoles.FirstParent | SchoolContextRoles.AdminViewer);
+
+            attReason = SchoolTestContext.AdminGradeSl.AttendanceReasonService.Edit(attReason.Id, AttendanceTypeEnum.Late, "code2", "reason 2");
+            Assert.AreEqual(attReason.Code, "code2");
+            Assert.AreEqual(attReason.AttendanceType, AttendanceTypeEnum.Late);
+            Assert.AreEqual(attReason.Description, "reason 2");
+
+            AssertForDeny(sl => sl.AttendanceReasonService.Delete(attReason.Id), SchoolTestContext, SchoolContextRoles.FirstTeacher
+                | SchoolContextRoles.FirstStudent | SchoolContextRoles.AdminViewer | SchoolContextRoles.FirstParent);
+
+            SchoolTestContext.AdminGradeSl.AttendanceReasonService.Delete(attReason.Id);
+        }
+    }
+}
---
+++ Chalkable.Web/Models/DisciplinesViewData/StudentDisciplineDetailedViewData.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Chalkable.Common;
+using Chalkable.Data.School.Model;
+using Chalkable.Web.Models.PersonViewDatas;
+
+namespace Chalkable.Web.Models.DisciplinesViewData
+{
+    public class StudentDisciplineDetailedViewData : ShortPersonViewData
+    {
+        public IList<StudentDisciplineBoxViewData> DisciplineBoxes { get; set; }
+        public MarkingPeriodViewData MarkingPeriod { get; set; }
+        public string Summary { get; set; }
+
+        protected StudentDisciplineDetailedViewData(PersonDetails student) : base(student) { }
+
+        //TODO: refactor this code later 
+        public static StudentDisciplineDetailedViewData Create(PersonDetails student, IList<ClassDisciplineDetails> disciplines, MarkingPeriod markingPeriod)
+        {
+            var classdiscTypes = new List<ClassDisciplineTypeDetails>();
+            foreach (var discipline in disciplines)
+            {
+                classdiscTypes.AddRange(discipline.DisciplineTypes);
+            } 
+            var disciplinesDictionary = classdiscTypes.OrderBy(x => x.DisciplineType.Name)
+                                                      .GroupBy(x => x.DisciplineTypeRef)
+                                                      .ToDictionary(x => x.Key, y => y.ToList());
+           
+            string summary = string.Format("{0} is doing great. No discipline issues.", student.FirstName);
+            string className;
+            if(disciplinesDictionary.Count > 0)
+            {
+                var classesDictionary = disciplines.GroupBy(x => x.Class.Id).ToDictionary(x => x.Key, x => x.ToList());
+                if (classesDictionary.Count > 0)
+                {
+                    var classDisciplines = classesDictionary.OrderByDescending(x => x.Value.Count).First().Value;
+                    className = classDisciplines.First().Class.Name;
+                    summary = string.Format(ChlkResources.STUDENT_IS_HAVING_DISCIPLINARY_ISSUES, student.FirstName, className);
+                }
+            }
+            var disciplineBoxes = new List<StudentDisciplineBoxViewData>();
+            foreach (var disciplinesList in disciplinesDictionary)
+            {
+                disciplineBoxes.Add(StudentDisciplineBoxViewData.Create(disciplinesList.Value.Count, disciplinesList.Value.Select(x=>x.ClassDiscipline).ToList(), disciplinesList.Value.First().DisciplineType));
+            }
+            var res = new StudentDisciplineDetailedViewData(student)
+                          {
+                              MarkingPeriod = MarkingPeriodViewData.Create(markingPeriod),
+                              Summary = summary,
+                              DisciplineBoxes = disciplineBoxes
+                          };
+            return res;
+        }
+    }
+
+
+    public class StudentDisciplineBoxViewData : HoverBoxesViewData<StudentDisciplineHoverViewData>
+    {
+        public string Name { get; set; }
+        public bool IsPassing { get; set; }
+        public static StudentDisciplineBoxViewData Create(int typeCount, List<ClassDisciplineDetails> disciplines, DisciplineType disciplineType)
+        {
+            var isPassing = false; //TODO: calc when is passing
+            var disciplinesDictionary = disciplines.OrderBy(x => x.Class.Name).GroupBy(x => x.Class.Id).ToDictionary(x => x.Key, x => x.ToList());
+
+            return new StudentDisciplineBoxViewData
+            {
+                Hover = StudentDisciplineHoverViewData.Create(disciplinesDictionary),
+                IsPassing = isPassing,
+                Title = typeCount.ToString(),
+                Name = disciplineType.Name
+            };
+        }
+    }
+
+    public class StudentDisciplineHoverViewData
+    {
+        public int Value { get; set; }
+        public string ClassName { get; set; }
+        public const int HOVER_DATA_COUNT = 4;
+        private const string OTHER = "Other";
+
+        public static StudentDisciplineHoverViewData Create(List<ClassDisciplineDetails> disciplines)
+        {
+
+            var res = new StudentDisciplineHoverViewData
+            {
+                Value = disciplines.Sum(x=>x.DisciplineTypes.Count),
+                ClassName = disciplines.First().Class.Name
+            };
+            return res;
+        }
+        public static IList<StudentDisciplineHoverViewData> Create(Dictionary<Guid, List<ClassDisciplineDetails>> disciplinesDictionary)
+        {
+            var res = disciplinesDictionary.Select(x => Create(x.Value)).OrderByDescending(x => x.Value)
+                .ToList();
+            if (res.Count > HOVER_DATA_COUNT)
+            {
+                var rest = res.Skip(HOVER_DATA_COUNT).Sum(x => x.Value);
+
+                res = res.Take(HOVER_DATA_COUNT).ToList();
+                res.Add(new StudentDisciplineHoverViewData
+                {
+                    ClassName = OTHER,
+                    Value = rest
+                });
+            }
+            return res;
+        }
+    }
+
+}
