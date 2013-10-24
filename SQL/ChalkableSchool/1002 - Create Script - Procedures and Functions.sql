dea778e53e1e207c3031ea2870454b14f8696f0b
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2013-10-24 17:17:10 +0300

    added DeleteAnnouncement procedure added ReorderAnnouncements procedure

--- SQL/ChalkableSchool/1002 - Create Script - Procedures and Functions.sql
+++ SQL/ChalkableSchool/1002 - Create Script - Procedures and Functions.sql
@@ -1100,3 +1100,114 @@ commit
 exec spGetAnnouncementDetails @announcementId, @personId, @callerRole, @schoolId
 
 GO
+
+
+
+create procedure [dbo].[spReorderAnnouncements] @schoolYearId int, @clasAnnType int, 
+												@ownerId int, @classId int
+as
+with AnnView as
+               (
+                select a.Id, Row_Number() over(order by a.Expires, a.[Created]) as [Order]  
+                from Announcement a
+                join Class c on c.Id = a.ClassRef
+				where c.SchoolYearRef = @schoolYearId and a.ClassAnnouncementTypeRef = @clasAnnType 
+                      and a.PersonRef = @ownerId and [State] = 1 and a.ClassRef = @classId
+               )
+update Announcement
+set [Order] = AnnView.[Order]
+from AnnView 
+where AnnView.Id = Announcement.Id
+select  1
+GO
+
+CREATE PROCEDURE [dbo].[spDeleteAnnouncement] @id int, @personId int, @classId int,
+												@state int, @classAnnouncementTypeId int
+AS
+
+declare @ann table
+(
+	Id int,
+	SchoolYearId int,
+	AnnouncementTypeId int,
+	PersonId int,
+	[State] int,
+	ClassId int
+)
+
+insert into @ann(Id, SchoolYearId, AnnouncementTypeId, PersonId, [State], ClassId)
+select a.Id, c.SchoolYearRef, a.ClassAnnouncementTypeRef, a.PersonRef, a.State, a.ClassRef
+from Announcement a
+join Class c on c.Id = a.ClassRef
+where (@id is null or a.Id = @id)
+	and (@personId is null or PersonRef = @personId)
+	and (@classId is null or a.ClassRef = @classId)
+	and (@classAnnouncementTypeId is null or ClassAnnouncementTypeRef = @classAnnouncementTypeId)
+	and (a.ClassRef is not null or a.ClassAnnouncementTypeRef is null  or a.[State] = 0)
+	and (@state is null or a.[State] = @state)
+
+delete from AnnouncementReminder where AnnouncementRef in (select Id from @ann)
+/*DELETE Attachment*/
+delete from AnnouncementAttachment where AnnouncementRef in (select Id from @ann)
+/*DELETE AnnouncementApplication*/
+delete from StudentAnnouncement
+where AnnouncementRef in (select Id from @ann)
+/*DELETE AnnouncementRecipient*/
+delete from AnnouncementRecipient
+where AnnouncementRef in (select Id from @ann)
+/*DELETE AnnouncementRecipientData*/
+delete from AnnouncementRecipientData
+where AnnouncementRef in (select Id from @ann)
+
+delete from [Notification]
+where AnnouncementRef in (select Id from @ann)
+
+/*DELETE ANOUNCEMENTQNA*/
+delete from AnnouncementQnA
+where AnnouncementRef in (select Id from @ann)
+
+/*DELETE AnnouncementApplication*/
+delete from AnnouncementApplication
+where AnnouncementRef in (select Id from @ann)
+
+/*DELETE Announcement*/
+delete from Announcement where Id in (select Id from @ann)
+
+
+declare @schoolYearId int
+
+declare  AnnCursor cursor for
+select SchoolYearId, AnnouncementTypeId, PersonId,  ClassId
+from @ann
+
+open AnnCursor
+fetch next from AnnCursor
+into @schoolYearId, @classAnnouncementTypeId, @personId, @classId
+
+declare @oldSchoolYear int, @oldannouncementTypeId int,
+@oldPersonId int, @oldClassId int
+
+while @@FETCH_STATUS = 0
+begin
+
+if(@schoolYearId <> @oldSchoolYear or @classAnnouncementTypeId <> @oldannouncementTypeId
+	or @personId <> @oldPersonId or @classId <> @oldClassId
+	or (@oldSchoolYear is null and @oldannouncementTypeId is null
+	and @oldPersonId is null and @oldClassId is null))
+begin
+/*Reordering Process*/
+exec spReorderAnnouncements @schoolYearId, @classAnnouncementTypeId, @personId, @classId
+
+set @oldannouncementTypeId = @classAnnouncementTypeId
+set @oldClassId = @classId
+set @oldPersonId = @personId
+set @oldSchoolYear = @schoolYearId
+
+end
+fetch next from AnnCursor
+into @schoolYearId, @classAnnouncementTypeId, @personId, @classId
+end
+CLOSE AnnCursor;
+DEALLOCATE AnnCursor;
+
+GO
