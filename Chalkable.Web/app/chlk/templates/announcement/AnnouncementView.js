ed7af2198390689e85a06cf53a8d6a5dce811f76
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2013-11-18 15:26:32 +0200

    fixed sstudent/announcementView  dropped

--- Chalkable.Web/app/chlk/templates/announcement/AnnouncementView.js
+++ Chalkable.Web/app/chlk/templates/announcement/AnnouncementView.js
@@ -14,6 +14,16 @@ NAMESPACE('chlk.templates.announcement', function () {
                 return this.getStudentAnnouncements().getItems().filter(function(item){
                     return item.getState() == 0;
                 }).length;
+            },
+
+            Number, function getStudentAnnGradingStyle(){return this.getStudentAnnouncements().getGradingStyle();},
+            chlk.models.grading.Mapping, function getMapping(){return this.getStudentAnnouncements().getMapping()},
+
+            String, function getGradeLetter(){
+                return GradingStyler.getLetterByGrade(this.getGrade(), this.getMapping(), this.getStudentAnnGradingStyle());
+            },
+            Number, function getGradeNumber(){
+                return GradingStyler.getGradeNumberValue(this.getGrade(), this.getMapping(), this.getStudentAnnGradingStyle());
             }
         ])
 });
