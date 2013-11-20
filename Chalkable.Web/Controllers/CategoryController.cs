df0996f935dd1017d2c7bce9ad50af342ccc024b
Andriy Machnew <amachnew@emp-game.com>
2013-11-20 18:53:35 +0200

    added person budget

---
+++ Chalkable.Web/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Mvc;
+using Chalkable.Web.ActionFilters;
+using Chalkable.Web.Models;
+
+namespace Chalkable.Web.Controllers
+{
+    [RequireHttps, TraceControllerFilter]
+    public class CategoryController : ChalkableController
+    {
+        [Authorize]
+        public ActionResult List(int? start, int? count)
+        {
+            var categories = MasterLocator.CategoryService.ListCategories(start ?? 0, count ?? 10);
+            return Json(categories.Transform(CategoryViewData.Create));
+        }
+        [Authorize]
+        public ActionResult GetInfo(Guid categoryId)
+        {
+            var category = MasterLocator.CategoryService.GetById(categoryId);
+            return Json(CategoryViewData.Create(category));
+        }
+
+        [AuthorizationFilter("SysAdmin")]
+        public ActionResult Add(string name, string description)
+        {
+            var category = MasterLocator.CategoryService.Add(name, description);
+            return Json(CategoryViewData.Create(category));
+        }
+        [AuthorizationFilter("SysAdmin")]
+        public ActionResult Edit(Guid categoryId, string name, string description)
+        {
+            var category = MasterLocator.CategoryService.Edit(categoryId, name, description);
+            return Json(CategoryViewData.Create(category));
+        }
+        [AuthorizationFilter("SysAdmin")]
+        public ActionResult Delete(Guid categoryId)
+        {
+            MasterLocator.CategoryService.Delete(categoryId);
+            return List(null, null);
+        }
+
+    }
+}
---
+++ Chalkable.Web/app/chlk/services/DisciplineService.js
@@ -0,0 +1,65 @@
+REQUIRE('chlk.services.BaseService');
+REQUIRE('ria.async.Future');
+REQUIRE('chlk.models.common.ChlkDate');
+REQUIRE('chlk.models.discipline.DisciplineSummary');
+REQUIRE('chlk.models.discipline.Discipline');
+REQUIRE('chlk.models.discipline.SetDisciplineListModel');
+REQUIRE('chlk.models.id.SchoolPersonId');
+REQUIRE('chlk.models.id.SchoolYearId');
+REQUIRE('chlk.models.id.ClassPeriodId');
+REQUIRE('chlk.models.id.ClassPersonId');
+REQUIRE('chlk.models.discipline.StudentDisciplineSummary');
+//REQUIRE('chlk.models.developer.DeveloperInfo');
+
+NAMESPACE('chlk.services', function(){
+    "use strict";
+    /** @class chlk.services.DisciplineService*/
+    CLASS(
+        'DisciplineService', EXTENDS(chlk.services.BaseService), [
+            [[chlk.models.common.ChlkDate, Number, Number]],
+            ria.async.Future, function list(date_, start_, count_){
+                return this.getPaginatedList('Discipline/List.json', chlk.models.discipline.DisciplineSummary,{
+                    date: date_,
+                    start: start_,
+                    count: count_
+                });
+            },
+
+            [[chlk.models.id.SchoolYearId, chlk.models.id.SchoolPersonId, chlk.models.common.ChlkDate]],
+            ria.async.Future, function listStudentDiscipline(schoolYearId_, schoolPersonId, date_){
+                return this.get('Discipline/ListStudentDiscipline.json', ArrayOf(chlk.models.discipline.Discipline),{
+                    schoolYearId: schoolYearId_ ? schoolYearId_.valueOf() : null,
+                    schoolPersonId: schoolPersonId.valueOf(),
+                    date: date_ ? date_.valueOf() : null
+                });
+            },
+
+            [[chlk.models.id.ClassId, chlk.models.common.ChlkDate, Number, Number]],
+            ria.async.Future, function getClassDisciplines(classId, date_, start_,count_){
+                return this.get('Discipline/ClassList.json', ArrayOf(chlk.models.discipline.Discipline),{
+                    classId: classId && classId.valueOf(),
+                    date: date_ && date_.toStandardFormat(),
+                    start: start_,
+                    count: count_
+                });
+            },
+
+            [[chlk.models.discipline.SetDisciplineListModel]],
+            ria.async.Future, function setDisciplines(disciplines){
+                return this.post('Discipline/SetClassDiscipline.json', Boolean,{
+                    disciplines: disciplines.getPostData()
+                });
+            },
+
+            [[chlk.models.id.SchoolPersonId, chlk.models.id.MarkingPeriodId]],
+            ria.async.Future, function getStudentDisciplineSummary(personId, markingPeriodId){
+                return this.get('Discipline/StudentDisciplineSummary.json', chlk.models.discipline.StudentDisciplineSummary ,{
+                    personId: personId && personId.valueOf(),
+                    markingPeriodId: markingPeriodId && markingPeriodId.valueOf()
+                });
+            }
+
+        ]
+    );
+});
+
