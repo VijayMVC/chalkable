70eeb495e0f0c983321f909202eda35f0bc5f75c
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-01-29 18:42:21 +0200

    fixed AnnouncementService

---
+++ Chalkable.Web/Controllers/FundController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Chalkable.Common.Exceptions;
+using Chalkable.Web.ActionFilters;
+using Chalkable.Web.Models;
+
+namespace Chalkable.Web.Controllers
+{
+    [RequireHttps, TraceControllerFilter]
+    public class FundController : ChalkableController
+    {
+        [AuthorizationFilter("AdminGrade, AdminEdit, AdminView")]
+        public ActionResult GetAppBudgetBalance()
+        {
+            return FakeJson("~/fakeData/appBudget.json");
+            //if (!Context.SchoolId.HasValue)
+            //    throw new UnassignedUserException();
+            //var schoolId = Context.SchoolId.Value;
+            //var toSchoolPayment = (double)MasterLocator.FundService.GetToSchoolPayment(schoolId);
+            //var paymentforApp = (double)MasterLocator.FundService.GetPaymentForApps(schoolId);
+            //return Json(BudgetBalanceViewData.Craete(toSchoolPayment, paymentforApp));
+        }
+
+        //TODO: implementation for those methods 
+        [AuthorizationFilter("Teacher, Student, AdminGrade, AdminEdit, AdminView")]
+        public ActionResult GetPersonBudgetBalance(int personId)
+        {
+            return Json(new {balance = 0});
+        }
+
+        [AuthorizationFilter("Teacher, Student, Parent")]
+        public ActionResult PersonFunds()
+        {
+            return FakeJson("~/fakeData/personFunds.json");
+        }
+
+        [AuthorizationFilter("Teacher, Student, Parent")]
+        public ActionResult AddCredit(decimal amount, string cardNumber,int month,int year, int cvcNumber, string cardType)
+        {
+            var res = cardNumber.Replace(" ", "") != "1324982345234523";
+            return Json(res);
+        }
+
+        [AuthorizationFilter("Teacher, Student, Parent")]
+        public ActionResult AddViaPayPal(decimal amount)
+        {
+            return Json(true);
+        }
+        
+        [AuthorizationFilter("Teacher, Student, Parent")]
+        public ActionResult GetCreditCardInfo(bool needCardInfo)
+        {
+            return needCardInfo ? FakeJson("~/fakeData/creditCardData.json") : Json(null);
+        }
+
+        [AuthorizationFilter("Teacher, Student, Parent")]
+        public ActionResult DeleteCreditCardInfo()
+        {
+            return Json(true);
+        }
+    }
+}
