b91935eed0a5a65ed8b3656bb74755416b50f1dd
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-02-04 15:56:56 +0200

    fixed for CHLK-2576 Homepage - email/pw input too much padding

---
+++ Chalkable.Data.School/Model/ClassDiscipline.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.Common;
+
+namespace Chalkable.Data.School.Model
+{
+    public class ClassDiscipline
+    {
+        public Guid Id { get; set; }
+        public const string CLASS_PERSON_REF_FIELD = "ClassPersonRef";
+        public Guid ClassPersonRef { get; set; }
+        public const string CLASS_PERIOD_REF_FIELD = "ClassPeriodRef";
+        public Guid ClassPeriodRef { get; set; }
+        public const string DATE_FIELD = "Date";
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class ClassDisciplineDetails : ClassDiscipline
+    {
+        public Person Student { get; set; }
+
+        private ClassPerson classPerson;
+        [DataEntityAttr]
+        public ClassPerson ClassPerson
+        {
+            get { return classPerson; }
+            set
+            {
+                classPerson = value;
+                //if (value != null && value.Id != Guid.Empty)
+                //    ClassPersonRef = value.Id;
+            }
+        }
+
+        private ClassPeriod classPeriod;
+        [DataEntityAttr]
+        public ClassPeriod ClassPeriod
+        {
+            get { return classPeriod; }
+            set
+            {
+                classPeriod = value;
+                //if (value != null && value.Id != Guid.Empty)
+                //    ClassPeriodRef = value.Id;
+            }
+        }
+        [DataEntityAttr]
+        public Class Class { get; set; }
+
+        public IList<ClassDisciplineTypeDetails> DisciplineTypes { get; set; } 
+    }
+}
---
+++ Chalkable.Web/Models/SysAdminViewData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Chalkable.Common;
+using Chalkable.Data.Master.Model;
+
+namespace Chalkable.Web.Models
+{
+    public class SysAdminViewData
+    {
+        public Guid Id { get; set; }
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public RoleViewData Role { get; set; }
+
+        public static SysAdminViewData Create(User user)
+        {
+            return new SysAdminViewData
+                {
+                    Id = user.Id,
+                    DisplayName = user.Login,
+                    Email = user.Login,
+                    Role = RoleViewData.Create(CoreRoles.SUPER_ADMIN_ROLE)
+                };
+        }
+    }
+}
