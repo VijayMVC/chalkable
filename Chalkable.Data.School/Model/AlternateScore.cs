3c805636a6264a6e7b4fd203819f5ce0ef9b16ee
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-02-06 18:27:04 +0200

    added alphaGrade, alterbateScore tables added AlphaGradeDataAccess and AlternateScoreDataAccess

---
+++ Chalkable.Data.School/Model/AlternateScore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chalkable.Data.School.Model
+{
+    public class AlternateScore
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IncludeInAverage { get; set; }
+        public decimal PercentOfMaximumScore { get; set; }
+    }
+}
