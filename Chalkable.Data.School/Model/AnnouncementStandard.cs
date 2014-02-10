0194ccc871fcfda14b363eee3c36c2c76d8c5605
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-02-10 12:57:00 +0200

    added AlphaGrade and AlternateScore into session in UI

---
+++ Chalkable.Data.School/Model/AnnouncementStandard.cs
@@ -0,0 +1,20 @@
+﻿using Chalkable.Data.Common;
+
+namespace Chalkable.Data.School.Model
+{
+    public class AnnouncementStandard
+    {
+        public const string ANNOUNCEMENT_REF_FIELD = "AnnouncementRef";
+        public const string STANDARD_REF_FIELD = "StandardRef";
+
+        [PrimaryKeyFieldAttr]
+        public int AnnouncementRef { get; set; }
+        [PrimaryKeyFieldAttr]
+        public int StandardRef { get; set; }
+    }
+
+    public class AnnouncementStandardDetails : AnnouncementStandard
+    {
+        public Standard Standard { get; set; }
+    }
+}
---
+++ Chalkable.Data.School/Model/Standard.cs
@@ -0,0 +1,19 @@
+﻿using Chalkable.Data.Common;
+
+namespace Chalkable.Data.School.Model
+{
+    public class Standard
+    {
+        public const string ID_FIELD = "Id";
+
+        [PrimaryKeyFieldAttr]
+        public int Id { get; set; }
+        public int? ParentStandardRef { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int StandardSubjectRef { get; set; }
+        public int? LowerGradeLevelRef { get; set; }
+        public int? UpperGradeLevelRef { get; set; }
+        public bool IsActive { get; set; } 
+    }
+}
