eaac80c9f3b785ae1d0221983274301dfb0dd1aa
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-02-10 15:39:21 +0200

    added ScoreConnector added ScoreToStudentAnnouncementMapper fixed modelMappers

---
+++ Chalkable.BusinessLogic/Mapping/ModelMappers/MapperFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Common;
+using Chalkable.Common.Exceptions;
+using Chalkable.Data.School.Model;
+using Chalkable.StiConnector.Connectors.Model;
+
+namespace Chalkable.BusinessLogic.Mapping.ModelMappers
+{
+    public class MapperFactory
+    {
+        private static IDictionary<Pair<Type, Type>, IMapper> _customMappers;
+
+        static MapperFactory()
+        {
+            BuildMapperDictionary();
+        }
+
+        private static void BuildMapperDictionary()
+        {
+            _customMappers = new Dictionary<Pair<Type, Type>, IMapper>
+                    {
+                        {new Pair<Type, Type>(typeof(Announcement), typeof(Activity)), new ActivityToAnnouncementMapper()},
+                        {new Pair<Type, Type>(typeof(AnnouncementComplex), typeof(Activity)), new ActivityToAnnouncementMapper()},
+                        {new Pair<Type, Type>(typeof(AnnouncementDetails), typeof(Activity)), new ActivityToAnnouncementMapper()},
+                        {new Pair<Type, Type>(typeof(Activity), typeof(AnnouncementComplex)), new AnnouncementComplexToActivityMapper()},
+                        {new Pair<Type, Type>(typeof(Activity), typeof(AnnouncementDetails)), new AnnouncementComplexToActivityMapper()},
+                        {new Pair<Type, Type>(typeof(ActivityAttachment), typeof(AnnouncementAttachment)), new AnnouncementAttToActivityAttMapper()},
+                        {new Pair<Type, Type>(typeof(AnnouncementAttachment), typeof(ActivityAttachment)), new StiAttachmentToAnnouncementAttMapper()},
+                        {new Pair<Type, Type>(typeof(AnnouncementAttachment), typeof(StiAttachment)), new StiAttachmentToAnnouncementAttMapper()},
+                    };
+        }
+        public static IMapper GetMapper<TReturn, TSource>()
+        {
+            var typesObj = new Pair<Type, Type>(typeof(TReturn), typeof(TSource));
+            if (!_customMappers.ContainsKey(typesObj))
+                throw new ChalkableException("There are no mapper with such source and return types");
+            return _customMappers[typesObj];
+        }
+    }
+}
---
+++ Chalkable.BusinessLogic/Mapping/ModelMappers/ScoreToStudentAnnMapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.School.Model;
+using Chalkable.StiConnector.Connectors.Model;
+
+namespace Chalkable.BusinessLogic.Mapping.ModelMappers
+{
+    public class ScoreToStudentAnnMapper : BaseMapper<StudentAnnouncement,Score>
+    {
+        protected override void InnerMap(StudentAnnouncement returnObj, Score sourceObj)
+        {
+            returnObj.Comment = sourceObj.Comment;
+            returnObj.Dropped = sourceObj.Dropped;
+            returnObj.GradeValue = (int?) sourceObj.NumericScore;
+            returnObj.PersonRef = sourceObj.StudentId;
+        }
+    }
+}
