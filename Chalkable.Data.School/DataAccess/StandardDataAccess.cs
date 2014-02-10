1154153285a459de11da13d4de706cc5227d2012
Andrew Machnew <amachnew@emp-game.com>
2014-02-10 16:42:11 +0200

    moved date moethods to chlkdate

---
+++ Chalkable.Data.School/DataAccess/StandardDataAccess.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.Common;
+using Chalkable.Data.Common.Orm;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Data.School.DataAccess
+{
+    public class StandardDataAccess : DataAccessBase<Standard, int>
+    {
+        public StandardDataAccess(UnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public override void Delete(int key)
+        {
+            var q = new DbQuery(new List<DbQuery>
+                {
+                    Orm.SimpleDelete<AnnouncementStandard>(new AndQueryCondition { {AnnouncementStandard.STANDARD_REF_FIELD, key} }),
+                    Orm.SimpleDelete<ClassStandard>(new AndQueryCondition {{ClassStandard.STANDARD_REF_FIELD, key}}),
+                    Orm.SimpleDelete<Standard>(new AndQueryCondition {{Standard.ID_FIELD, key}})
+                });
+            ExecuteNonQueryParametrized(q.Sql.ToString(), q.Parameters);
+        }
+    }
+
+    public class StandardSubjectDataAccess : DataAccessBase<StandardSubject, int>
+    {
+        public StandardSubjectDataAccess(UnitOfWork unitOfWork)
+            : base(unitOfWork)
+        {
+        }
+    }
+
+    public class ClassStandardDataAccess: DataAccessBase<ClassStandard, int>
+    {
+        public ClassStandardDataAccess(UnitOfWork unitOfWork)
+            : base(unitOfWork)
+        {
+        }
+    }
+
+    public class AnnouncementStandardDataAccess : DataAccessBase<AnnouncementStandard, int>
+    {
+        public AnnouncementStandardDataAccess(UnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/services/DeveloperService.js
@@ -0,0 +1,41 @@
+REQUIRE('chlk.services.BaseInfoService');
+REQUIRE('ria.async.Future');
+REQUIRE('chlk.models.id.SchoolPersonId');
+REQUIRE('chlk.models.developer.DeveloperInfo');
+
+NAMESPACE('chlk.services', function () {
+    "use strict";
+
+    /** @class chlk.services.DeveloperService*/
+    CLASS(
+        'DeveloperService', EXTENDS(chlk.services.BaseInfoService), [
+            [[chlk.models.id.SchoolPersonId]],
+            ria.async.Future, function getInfo(id) {
+                return this.get('Developer/DeveloperInfo.json', chlk.models.developer.DeveloperInfo, {
+                    developerId: id.valueOf()
+                });
+            },
+            [[chlk.models.id.SchoolPersonId, String, String ,String]],
+            ria.async.Future, function saveInfo(id, name, webSite, email) {
+                return this
+                    .post('Developer/UpdateInfo.json', chlk.models.developer.DeveloperInfo, {
+                        developerId: id.valueOf(),
+                        name: name,
+                        webSiteLink: webSite,
+                        email: email
+                    })
+                    .then(function(result){
+                        this.userInfoChange.notify([result.getName()]);
+                        return result;
+                    }, this);
+            },
+
+            [[chlk.models.id.SchoolPersonId, String]],
+            ria.async.Future, function updatePaymentInfo(id ,email){
+                //todo: response from server
+                var mdl = new chlk.models.developer.PayPalInfo();
+                mdl.setEmail(email);
+                return ria.async.DeferredData(mdl);
+            }
+        ])
+});
