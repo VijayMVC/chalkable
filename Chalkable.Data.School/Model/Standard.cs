3cbd82af2f12991c4535ff2044f17d24d4b8e317
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-02-12 15:12:01 +0200

    added StandardController added GetStandard to StandardController

--- Chalkable.Data.School/Model/Standard.cs
+++ Chalkable.Data.School/Model/Standard.cs
@@ -5,6 +5,10 @@ namespace Chalkable.Data.School.Model
     public class Standard
     {
         public const string ID_FIELD = "Id";
+        public const string STANDARD_SUBJECT_ID_FIELD = "StandardSubjectRef";
+        public const string LOWER_GRADE_LEVEL_REF_FIELD = "LowerGradeLevelRef";
+        public const string UPPER_GRADE_LEVEL_REF_FIELD = "UpperGradeLevelRef";
+
 
         [PrimaryKeyFieldAttr]
         public int Id { get; set; }
