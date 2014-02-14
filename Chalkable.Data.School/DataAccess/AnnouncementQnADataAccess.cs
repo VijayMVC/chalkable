c8ee1f59d4ce04d1a945ac99ba7414b9a5d2a0ff
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-02-14 19:01:24 +0200

    fixed GetStudentAnnouncements

---
+++ Chalkable.Data.School/DataAccess/AnnouncementQnADataAccess.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Chalkable.Data.Common;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Data.School.DataAccess
+{
+    public class AnnouncementQnADataAccess : DataAccessBase<AnnouncementQnA, int>
+    {
+        public AnnouncementQnADataAccess(UnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+
+        private const string GET_ANNOUNCEMENT_QNA_PROCEDURE = "spGetAnnouncementsQnA";
+        private const string CALLER_ID_PARAM = "callerId";
+        private const string ASKER_ID_PARAM = "askerId";
+        private const string ANSWERER_ID_PARAM = "answererId";
+        private const string ANNOUNCEMENT_ID_PARAM = "announcementId";
+        private const string ANNOUNCEMENT_QNA_ID_PARAM = "announcementQnAId";
+        
+        public AnnouncementQnAQueryResult GetAnnouncementQnA(AnnouncementQnAQuery query)
+        {
+            var parameter = new Dictionary<string, object>
+                {
+                    {ANNOUNCEMENT_QNA_ID_PARAM, query.Id},
+                    {ANNOUNCEMENT_ID_PARAM, query.AnnouncementId},
+                    {ASKER_ID_PARAM, query.AskerId},
+                    {ANSWERER_ID_PARAM, query.AnswererId},
+                    {CALLER_ID_PARAM, query.CallerId}
+                };
+            using (var reader = ExecuteStoredProcedureReader(GET_ANNOUNCEMENT_QNA_PROCEDURE, parameter))
+            {
+                var res = ReadAnnouncementQnAComplexes(reader);
+                return new AnnouncementQnAQueryResult 
+                    {
+                        AnnouncementQnAs = res,
+                        Query = query
+                    };
+            }
+        }
+
+        public static IList<AnnouncementQnAComplex> ReadAnnouncementQnAComplexes(DbDataReader reader)
+        {
+            var res = new List<AnnouncementQnAComplex>();
+            while (reader.Read())
+            {
+                res.Add(ReadAnnouncementQnAComplex(reader));
+            }
+            return res;
+        }
+        public static AnnouncementQnAComplex ReadAnnouncementQnAComplex(DbDataReader reader)
+        {
+            var annQnA = reader.Read<AnnouncementQnAComplex>();
+            annQnA.Asker = ReadAnnouncementQnAPerson(reader, "Asker");
+            annQnA.Answerer = ReadAnnouncementQnAPerson(reader, "Answerer");
+            return annQnA;
+        }
+        private static Person ReadAnnouncementQnAPerson(DbDataReader reader, string prefix)
+        {
+            var template = prefix + "{0}";
+            return new Person
+                {
+                    Id = SqlTools.ReadInt32(reader, string.Format(template, Person.ID_FIELD)),
+                    FirstName = SqlTools.ReadStringNull(reader, string.Format(template, Person.FIRST_NAME_FIELD)),
+                    LastName = SqlTools.ReadStringNull(reader, string.Format(template, Person.LAST_NAME_FIELD)),
+                    Gender = SqlTools.ReadStringNull(reader, string.Format(template, Person.GENDER_FIELD)),
+                    RoleRef = SqlTools.ReadInt32(reader, string.Format(template, Person.ROLE_REF_FIELD))
+                };
+        } 
+    }
+
+    public class AnnouncementQnAQuery
+    {
+        public int? Id { get; set; }
+        public int? AskerId { get; set; }
+        public int? AnswererId { get; set; }
+        public int? AnnouncementId { get; set; }
+        public int? CallerId { get; set; }
+    }
+
+    public class AnnouncementQnAQueryResult
+    {
+        public AnnouncementQnAQuery Query { get; set; }
+        public IList<AnnouncementQnAComplex> AnnouncementQnAs { get; set; }
+    }
+}
---
+++ Chalkable.StiConnector/Connectors/ActivityConnector.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Http;
+using Chalkable.Common;
+using Chalkable.StiConnector.Connectors.Model;
+
+namespace Chalkable.StiConnector.Connectors
+{
+    public class ActivityConnector : ConnectorBase
+    {
+        private const string END_DATE_PARAM = "endDate";
+        private const string START_DATE_PARAM = "startDate";
+        private const string START_PARAM = "start";
+        private const string END_PARAM = "end";
+
+        private string urlFormat;
+        public ActivityConnector(ConnectorLocator locator) : base(locator)
+        {
+            urlFormat = BaseUrl + "Chalkable/activities/{0}";
+        }
+
+        public Activity GetActivity(int id)
+        {
+            string url = string.Format(urlFormat, id);
+            return Call<Activity>(url);
+        }
+
+        public IList<Activity> GetActivities(int sectionId, int? start, int? end, DateTime? endDate = null, DateTime? startDate = null)
+        {
+            var url = string.Format(BaseUrl + "chalkable/sections/{0}/activities", sectionId);
+            var optionalParams = new NameValueCollection();
+            if(endDate.HasValue)
+                optionalParams.Add(END_DATE_PARAM, endDate.Value.ToString(Constants.DATE_FORMAT));
+            if(startDate.HasValue)
+                optionalParams.Add(START_DATE_PARAM, startDate.Value.ToString(Constants.DATE_FORMAT));
+            var res =  Call<IList<Activity>>(url, optionalParams);
+            if (start.HasValue)
+                res = res.Skip(start.Value).ToList();
+            if (end.HasValue)
+                res = res.Take(end.Value).ToList();
+            return res;
+        } 
+
+        public IList<Activity> GetTeacherActivities(int acadSessionId, int teacherId, int? start = null, int? end = null, DateTime? endDate = null, DateTime? startDate = null)
+        {
+            var url = string.Format(BaseUrl + "Chalkable/{0}/teachers/{1}/activities", acadSessionId, teacherId);
+            var optinalParams = new NameValueCollection();
+            if(start.HasValue)
+                optinalParams.Add(START_PARAM, start.Value.ToString());
+            if(end.HasValue)
+                optinalParams.Add(END_PARAM, end.Value.ToString());
+            if(startDate.HasValue)
+                optinalParams.Add(START_DATE_PARAM, startDate.Value.ToString(Constants.DATE_FORMAT));
+            if(endDate.HasValue)
+                optinalParams.Add(END_DATE_PARAM, endDate.Value.ToString(Constants.DATE_FORMAT));
+            return Call<IList<Activity>>(url, optinalParams);
+        }
+
+        public IList<Activity> GetStudentAcivities(int acadSessionId, int studentId, int? start = null, int? end = null, DateTime? endDate = null, DateTime? startDate = null)
+        {
+            var url = string.Format(BaseUrl + "Chalkable/{0}/students/{1}/activities", acadSessionId, studentId);
+            var optinalParams = new NameValueCollection();
+            if (start.HasValue)
+                optinalParams.Add(START_PARAM, start.Value.ToString());
+            if (end.HasValue)
+                optinalParams.Add(END_PARAM, end.Value.ToString());
+            if (startDate.HasValue)
+                optinalParams.Add(START_DATE_PARAM, startDate.Value.ToString(Constants.DATE_FORMAT));
+            if (endDate.HasValue)
+                optinalParams.Add(END_DATE_PARAM, endDate.Value.ToString(Constants.DATE_FORMAT));
+            return Call<IList<Activity>>(url, optinalParams);
+        } 
+
+        public void DeleteActivity(int id)
+        {
+            Delete(string.Format(urlFormat, id));           
+        }
+        public Activity CreateActivity(int sectionId, Activity activity)
+        {
+            return Post(string.Format(urlFormat, sectionId), activity);
+        }
+        public void UpdateActivity(int id, Activity activity)
+        {
+            Put(string.Format(urlFormat, id), activity);
+        }
+    }
+}
