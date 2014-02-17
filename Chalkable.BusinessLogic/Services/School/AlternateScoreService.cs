e0a5eca84b519dcb11c6af5776478859aad58471
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-02-17 15:19:50 +0200

    fixed UI for announcement saving

---
+++ Chalkable.BusinessLogic/Services/School/AlternateScoreService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.BusinessLogic.Security;
+using Chalkable.Common.Exceptions;
+using Chalkable.Data.School.DataAccess;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.School
+{
+    public interface IAlternateScoreService
+    {
+        void AddAlternateScores(IList<AlternateScore> alternateScores);
+        void Delete(int id);
+        IList<AlternateScore> GetAlternateScores();
+    }
+
+    public class AlternateScoreService : SchoolServiceBase, IAlternateScoreService
+    {
+        public AlternateScoreService(IServiceLocatorSchool serviceLocator) : base(serviceLocator)
+        {
+        }
+
+        public void AddAlternateScores(IList<AlternateScore> alternateScores)
+        {
+            if(!BaseSecurity.IsSysAdmin(Context))
+                throw new ChalkableSecurityException();
+            
+            using (var uow = Update())
+            {
+                new AlternateScoreDataAccess(uow).Insert(alternateScores);
+                uow.Commit();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            if (!BaseSecurity.IsSysAdmin(Context))
+                throw new ChalkableSecurityException();
+            using (var uow = Update())
+            {
+                new AlternateScoreDataAccess(uow).Delete(id);
+                uow.Commit();
+            }
+        }
+
+        public IList<AlternateScore> GetAlternateScores()
+        {
+            using (var uow = Read())
+            {
+                return new AlternateScoreDataAccess(uow).GetAll();
+            }
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/models/search/SearchItem.js
@@ -0,0 +1,35 @@
+REQUIRE('chlk.models.common.ChlkDate');
+REQUIRE('chlk.models.course.Course');
+
+
+NAMESPACE('chlk.models.search', function () {
+
+    "use strict";
+    /** @class chlk.models.search.SearchItem*/
+    CLASS(
+        'SearchItem', [
+            String, 'id',
+            String, 'description',
+            String, 'gender',
+            [ria.serialize.SerializeProperty('searchtype')],
+            Number, 'searchType',
+            [ria.serialize.SerializeProperty('roleid')],
+            Number, 'roleId',
+            chlk.models.course.Course, 'course',
+            [ria.serialize.SerializeProperty('announcementid')],
+            String, 'announcementId',
+            [ria.serialize.SerializeProperty('announcementtype')],
+            Number, 'announcementType',
+            [ria.serialize.SerializeProperty('isadminannouncement')],
+            Boolean, 'adminAnnouncement',
+
+            READONLY, Number, 'chalkableAnnouncementType',
+            Number, function getChalkableAnnouncementType(){
+                var res = this.getAnnouncementType();
+                if(res) return res;
+                if(this.isAdminAnnouncement()) return chlk.models.announcement.AnnouncementTypeEnum.ADMIN.valueOf();
+                return chlk.models.announcement.AnnouncementTypeEnum.ANNOUNCEMENT.valueOf();
+            }
+        ]);
+});
+
