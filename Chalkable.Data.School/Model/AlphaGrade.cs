7e0a1829787d2adc34c4ac489a2646a71bfaee95
Andrew Machnew <amachnew@emp-game.com>
2014-02-17 19:00:21 +0200

    added access control headers

---
+++ Chalkable.Data.School/Model/AlphaGrade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chalkable.Data.School.Model
+{
+    public class AlphaGrade
+    {
+        public const string SCHOOL_ID_FIELD = "SchoolRef";
+
+        public int Id { get; set; }
+        public int SchoolRef { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Chalkable.Web/app/chlk/models/discipline/Discipline.js
@@ -0,0 +1,43 @@
+REQUIRE('chlk.models.discipline.DisciplineType');
+REQUIRE('chlk.models.period.Period');
+REQUIRE('chlk.models.id.SchoolPersonId');
+REQUIRE('chlk.models.id.ClassPeriodId');
+REQUIRE('chlk.models.id.ClassPersonId');
+
+NAMESPACE('chlk.models.discipline', function(){
+    "use strict";
+
+    /** @class chlk.models.discipline.Discipline*/
+    CLASS('Discipline', [
+
+        [ria.serialize.SerializeProperty('studentid')],
+        chlk.models.id.SchoolPersonId, 'studentId',
+
+        chlk.models.people.ShortUserInfo, 'student',
+
+        [ria.serialize.SerializeProperty('classpersonid')],
+        chlk.models.id.ClassPersonId, 'classPersonId',
+
+        [ria.serialize.SerializeProperty('classperiodid')],
+        chlk.models.id.ClassPeriodId, 'classPeriodId',
+
+        [ria.serialize.SerializeProperty('teacherid')],
+        chlk.models.id.SchoolPersonId, 'teacherId',
+
+        [ria.serialize.SerializeProperty('classname')],
+        String, 'className',
+
+        [ria.serialize.SerializeProperty('disciplinetypes')],
+        ArrayOf(chlk.models.discipline.DisciplineType), 'disciplineTypes',
+
+        chlk.models.common.ChlkDate, 'date',
+
+        chlk.models.period.Period, 'period',
+
+        String, 'description',
+
+        String, 'summary',
+
+        Boolean, 'editable'
+    ]);
+});
