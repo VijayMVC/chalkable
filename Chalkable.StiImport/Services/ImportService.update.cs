cc9ec32f6702a465fd9ec47e8b67d4afc6fbc4d8
Ievgenii Zasoba <geka.zasoba@gmail.com>
2014-03-20 15:47:54 +0200

    Removed AddressInfo model class. Added gender and grading period sync. Started update sync implementation.

---
+++ Chalkable.StiImport/Services/ImportService.update.cs
@@ -0,0 +1,182 @@
+﻿using System.Linq;
+using Chalkable.BusinessLogic.Services.School;
+using Chalkable.StiConnector.SyncModel;
+
+namespace Chalkable.StiImport.Services
+{
+    public partial class ImportService
+    {
+        private void ProcessUpdate()
+        {
+            UpdateSchools();
+            UpdateAddresses();
+            UpdatePersons();
+            UpdateSchoolPersons();
+            UpdatePhones();
+            UpdateGradeLevels();
+            UpdateSchoolYears();
+            UpdateStudentSchoolYears();
+            UpdateMarkingPeriods();
+            UpdateGradingPeriods();
+            UpdateDayTypes();
+            UpdateDays();
+            UpdateRooms();
+            UpdateCourses();
+            UpdateStandards();
+            UpdateMarkingPeriodClasses();
+            UpdateClassAnnouncementTypes();
+            UpdatePeriods();
+            UpdateClassPeriods();
+            UpdateClassPersons();
+            UpdateAttendanceReasons();
+            UpdateAlphaGrades();
+            UpdateAlternateScores();
+        }
+
+        private void UpdateSchools()
+        {
+            var schools = context.GetSyncResult<School>().Updated.Select(x=>new Data.School.Model.School
+                {
+                    Id = x.SchoolID,
+                    IsActive = x.IsActive,
+                    IsPrivate = x.IsPrivate,
+                    Name = x.Name
+                }).ToList();
+            ServiceLocatorSchool.SchoolService.Edit(schools);
+        }
+
+        private void UpdateAddresses()
+        {
+            var addresses = context.GetSyncResult<Address>().Updated.Select(x=>new Data.School.Model.Address
+                {
+                    AddressLine1 = x.AddressLine1,
+                    AddressLine2 = x.AddressLine2,
+                    AddressNumber = x.AddressNumber,
+                    City = x.City,
+                    Country = x.Country,
+                    CountyId = x.CountryID,
+                    Id = x.AddressID,
+                    Latitude = x.Latitude,
+                    Longitude = x.Longitude,
+                    PostalCode = x.PostalCode
+                }).ToList();
+            ServiceLocatorSchool.AddressService.Edit(addresses);
+        }
+
+        private void UpdatePersons()
+        {
+            var genders = context.GetSyncResult<Gender>().All.ToDictionary(x => x.GenderID);
+            var persons = context.GetSyncResult<Person>().Updated.Select(x => new PersonInfo
+                {
+                    Id = x.PersonID,
+                    Active = true,
+                    AddressRef = x.PhysicalAddressID,
+                    BirthDate = x.DateOfBirth,
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    Gender = x.GenderID.HasValue ? genders[x.GenderID.Value].Code : "U"
+                }).ToList();
+            ServiceLocatorSchool.PersonService.Edit(persons);
+        }
+
+        private void UpdateSchoolPersons()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdatePhones()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdateGradeLevels()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdateSchoolYears()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdateStudentSchoolYears()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdateMarkingPeriods()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdateGradingPeriods()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdateDayTypes()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdateDays()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdateRooms()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdateCourses()
+        {
+            // throw new NotImplementedException();
+        }
+
+        private void UpdateStandards()
+        {
+            // throw new NotImplementedException();
+        }
+
+        private void UpdateMarkingPeriodClasses()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdateClassAnnouncementTypes()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdatePeriods()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdateClassPeriods()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdateClassPersons()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdateAttendanceReasons()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdateAlphaGrades()
+        {
+            // throw new NotImplementedException();
+        }
+
+        private void UpdateAlternateScores()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ Chalkable.Web/Models/GradingStandardGridViewData.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Chalkable.BusinessLogic.Model;
+using Chalkable.Data.School.Model;
+using Chalkable.Web.Models.AnnouncementsViewData;
+
+namespace Chalkable.Web.Models
+{
+    public class StandardGradingGridViewData
+    {
+        public int? Avg { get; set; }
+        public IList<GradeStudentViewData> Students { get; set; }
+        public MarkingPeriodViewData MarkingPeriod { get; set; }
+        public IList<StandardGradingViewData> StandardGradings { get; set; } 
+
+        public static StandardGradingGridViewData Create(MarkingPeriod markingPeriod, IList<GradingStandardInfo> gradingStandardInfos)
+        {
+            var res = new StandardGradingGridViewData {MarkingPeriod = MarkingPeriodViewData.Create(markingPeriod)};
+            var students = gradingStandardInfos.GroupBy(x => x.Student).Select(x => x.Key).ToList();
+            res.Students = students.Select(GradeStudentViewData.Create).ToList();
+            res.StandardGradings = StandardGradingViewData.Create(gradingStandardInfos);
+            if (res.StandardGradings.Count > 0)
+                res.Avg = (int?) res.StandardGradings.Average(x => x.NumericAvg);
+            return res;
+        }
+    }
+
+    public class StandardGradingViewData
+    {
+        public IList<StandardGradingItemViewData> Items { get; set; }
+        public int? NumericAvg { get; set; }
+        public string AlphaGradeNameAvg { get; set; }
+        public AnnouncementStandardViewData Standard { get; set; }
+
+        public static IList<StandardGradingViewData> Create(IList<GradingStandardInfo> gradingStandards)
+        {
+            var gradigDic = gradingStandards.GroupBy(x => x.Standard.Id).ToDictionary(x => x.Key, x => x.ToList());
+            var res = new List<StandardGradingViewData>();
+            foreach (var kv in gradigDic)
+            {
+                var gradingSt = kv.Value.First();
+                res.Add(new StandardGradingViewData
+                    {
+                        Standard = AnnouncementStandardViewData.Create(gradingSt.Standard),
+                        NumericAvg = (int?)kv.Value.Average(x => x.NumericGrade),
+                        Items = kv.Value.Select(StandardGradingItemViewData.Create).ToList()
+                    });
+            }
+            return res;
+        }
+    }
+
+    public class StandardGradingItemViewData
+    {
+        public int StudentId { get; set; }
+        public int StandardId { get; set; }
+        public int? GradeId { get; set; }
+        public string GradeValue { get; set; }
+        public int GradingPeriodId { get; set; }
+
+        public static StandardGradingItemViewData Create(GradingStandardInfo gradingStandard)
+        {
+            return new StandardGradingItemViewData
+                {
+                    StudentId = gradingStandard.StudentId,
+                    GradeId = gradingStandard.AlphaGradeId,
+                    GradeValue = gradingStandard.AlphaGradeName,
+                    StandardId = gradingStandard.Standard.Id,
+                    GradingPeriodId = gradingStandard.GradingPeriodId
+                };
+        }
+    }
+}
