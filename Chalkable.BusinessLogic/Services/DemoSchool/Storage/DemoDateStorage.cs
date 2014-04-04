1d8d0de0aa52c64835db00e7bb791c7b287cf97d
Andrew Machnew <amachnew@emp-game.com>
2014-04-04 04:11:48 +0300

    fixed demodatestorage

--- Chalkable.BusinessLogic/Services/DemoSchool/Storage/DemoDateStorage.cs
+++ Chalkable.BusinessLogic/Services/DemoSchool/Storage/DemoDateStorage.cs
@@ -1,45 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Chalkable.Data.School.DataAccess;
 using Chalkable.Data.School.Model;
 
 namespace Chalkable.BusinessLogic.Services.DemoSchool.Storage
 {
-    public class DemoDateStorage
+    public class DemoDateStorage:BaseDemoStorage<int ,Date>
     {
+        private int index = 0;
+
+        public DemoDateStorage(DemoStorage storage) : base(storage)
+        {
+        }
+
         public Date GetDateOrNull(DateQuery dateQuery)
         {
-            throw new System.NotImplementedException();
+            return GetDatesFiltered(dateQuery).FirstOrDefault();
         }
 
         public DateTime GetDbDateTime()
         {
-            throw new NotImplementedException();
+            return DateTime.Now;
         }
 
         public IList<Date> GetDates(DateQuery dateQuery)
         {
-            throw new NotImplementedException();
+            return GetDatesFiltered(dateQuery);
         }
 
         public void Add(IList<Date> days)
         {
-            throw new NotImplementedException();
+            foreach (var date in days)
+            {
+                Add(date);
+            }
         }
 
         public void Delete(DateQuery query)
         {
-            throw new NotImplementedException();
+            var dates = GetDatesFiltered(query);
+
+
+            foreach (var date in dates)
+            {
+                var item = data.First(x => x.Value == date);
+                data.Remove(item.Key);
+            }
+        }
+
+        private List<Date> GetDatesFiltered(DateQuery query)
+        {
+            var dates = data.Select(x => x.Value);
+
+            if (query.SchoolYearId.HasValue)
+                dates = dates.Where(x => x.SchoolYearRef == query.SchoolYearId);
+            if (query.FromDate.HasValue)
+                dates = dates.Where(x => x.Day >= query.FromDate);
+            if (query.ToDate.HasValue)
+                dates = dates.Where(x => x.Day <= query.ToDate);
+            if (query.SchoolDaysOnly)
+                dates = dates.Where(x => x.IsSchoolDay);
+
+
+            //todo: add selection by marking periods
+
+            if (query.DayType.HasValue)
+                dates = dates.Where(x => x.DayType.Id == query.DayType);
+
+            return dates.ToList();
         }
 
         public void Add(Date days)
         {
-            throw new NotImplementedException();
+            data.Add(index ++, days);
         }
 
         public void Update(IList<Date> dates)
         {
-            throw new NotImplementedException();
+            foreach (var date in dates)
+            {
+                var item = data.First(x => x.Value == date);
+                data[item.Key] = date;
+            }
         }
     }
 }
