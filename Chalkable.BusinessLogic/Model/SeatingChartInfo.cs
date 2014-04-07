f6638485cf9632e4a1b9047be4ab60e88a3b89ac
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-04-07 17:12:54 +0300

    fixed seatingChart update

--- Chalkable.BusinessLogic/Model/SeatingChartInfo.cs
+++ Chalkable.BusinessLogic/Model/SeatingChartInfo.cs
@@ -9,7 +9,7 @@ namespace Chalkable.BusinessLogic.Model
     {
         public byte Columns { get; set; }
         public byte Rows { get; set; }
-        public IList<IList<SeatInfo>> SeatsList { get; set; }
+        public IList<IList<SeatInfo>> SeatingList { get; set; }
         public int ClassId { get; set; }
 
         public static SeatingChartInfo Create(SeatingChart seatingChart)
@@ -19,7 +19,7 @@ namespace Chalkable.BusinessLogic.Model
                     Columns = seatingChart.Columns,
                     Rows = seatingChart.Rows,
                     ClassId = seatingChart.SectionId,
-                    SeatsList = new List<IList<SeatInfo>>()
+                    SeatingList = new List<IList<SeatInfo>>()
                 };
             var index = 0;
             for (byte row = 1; row <= res.Rows; row++)
@@ -34,7 +34,7 @@ namespace Chalkable.BusinessLogic.Model
                     seats.Add(seatInfo);
                     index++;
                 }
-                res.SeatsList.Add(seats);
+                res.SeatingList.Add(seats);
             }
             return res;
         }
