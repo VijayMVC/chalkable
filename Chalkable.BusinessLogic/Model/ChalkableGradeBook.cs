17f219610cb85dbbef74af170b696ebcd99d9cb0
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-04-09 19:54:02 +0300

    added StudentAvarages and ClassOptions to gradeBook

--- Chalkable.BusinessLogic/Model/ChalkableGradeBook.cs
+++ Chalkable.BusinessLogic/Model/ChalkableGradeBook.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Chalkable.Data.School.Model;
+using Chalkable.StiConnector.Connectors.Model;
 
 namespace Chalkable.BusinessLogic.Model
 {
@@ -13,6 +14,58 @@ namespace Chalkable.BusinessLogic.Model
         public int Avg { get; set; }
         public IList<Person> Students { get; set; }
         public IList<AnnouncementDetails> Announcements { get; set; } 
+        public IList<ChalkableStudentAverage> Averages { get; set; }
+        public ChalkableClassOptions Options { get; set; }
+    }
+
+    public class ChalkableStudentAverage
+    {
+        public int AverageId { get; set; }
+        public string AverageName { get; set; }
+        public decimal? AvgValue { get; set; }
+        public int StudentId { get; set; }
+        public AlphaGrade AlphaGrade { get; set; }
+        public bool IsGradingPeriodAverage { get; set; }
+
+        public static ChalkableStudentAverage Create(StudentAverages studentAverages)
+        {
+            var res = new ChalkableStudentAverage
+                {
+                    AverageId = studentAverages.AverageId,
+                    AverageName = studentAverages.AverageName,
+                    AvgValue = studentAverages.Score,
+                    StudentId = studentAverages.StudentId,
+                    IsGradingPeriodAverage = studentAverages.IsGradingPeriodAverage
+                };
+            if (studentAverages.AlphaGradeId.HasValue)
+            {
+                res.AlphaGrade = new AlphaGrade
+                    {
+                        Id = studentAverages.AlphaGradeId.Value,
+                        Name = studentAverages.AlphaGradeName
+                    };
+            }
+            return res;
+        }
+    }
+
+    public class ChalkableClassOptions
+    {
+        public bool DisplayAlphaGrades { get; set; }
+        public bool DisplayStudentAverage { get; set; }
+        public bool DisplayTotalPoints { get; set; }
+        public bool IncludeWithdrawnStudents { get; set; }
+
+        public static ChalkableClassOptions Create(ClassroomOption classroomOption)
+        {
+            return new ChalkableClassOptions
+                {
+                    DisplayAlphaGrades = classroomOption.DisplayAlphaGrades,
+                    DisplayStudentAverage = classroomOption.DisplayStudentAverage,
+                    DisplayTotalPoints = classroomOption.DisplayTotalPoints,
+                    IncludeWithdrawnStudents = classroomOption.IncludeWithdrawnStudents
+                };
+        }
     }
 
     public class ClassGradingSummary
