3cc08c03948b3090f6ce8212594f65124a4829fa
Andrew Machnew <amachnew@emp-game.com>
2014-04-10 14:36:28 +0300

    updated ria

---
+++ Chalkable.BusinessLogic/Security/UserSecurity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Chalkable.BusinessLogic.Services;
+using Chalkable.Data.Master.Model;
+
+namespace Chalkable.BusinessLogic.Security
+{
+    public static class UserSecurity
+    {
+        public static bool CanCreate(UserContext context, Guid? districtId)
+        {
+            return BaseSecurity.IsSysAdmin(context) ||
+                   (BaseSecurity.IsAdminEditor(context) && context.DistrictId == districtId);
+        }
+
+        public static bool CanModify(UserContext context, User user)
+        {
+            var districtId = user.DistrictRef;
+            return CanCreate(context, districtId) || context.UserId == user.Id;
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/templates/grading/ShortGradingClassSummaryGridAvgsTpl.js
@@ -0,0 +1,30 @@
+REQUIRE('chlk.models.grading.ShortGradingClassSummaryGridItems');
+REQUIRE('chlk.templates.ChlkTemplate');
+
+NAMESPACE('chlk.templates.grading', function () {
+    "use strict";
+    /** @class chlk.templates.grading.ShortGradingClassSummaryGridAvgsTpl*/
+    CLASS(
+        [ria.templates.TemplateBind('~/assets/jade/activities/grading/TeacherClassGradingGridSummaryAvgs.jade')],
+        [ria.templates.ModelBind(chlk.models.grading.ShortGradingClassSummaryGridItems)],
+        'ShortGradingClassSummaryGridAvgsTpl', EXTENDS(chlk.templates.ChlkTemplate), [
+
+            [ria.templates.ModelPropertyBind],
+            ArrayOf(chlk.models.grading.StudentWithAvg), 'students',
+
+            [ria.templates.ModelPropertyBind],
+            ArrayOf(chlk.models.announcement.ShortAnnouncementViewData), 'gradingItems',
+
+            [ria.templates.ModelPropertyBind],
+            chlk.models.common.NameId, 'gradingPeriod',
+
+            [ria.templates.ModelPropertyBind],
+            Number, 'avg',
+
+            [ria.templates.ModelPropertyBind],
+            Number, 'rowIndex',
+
+            [ria.templates.ModelPropertyBind],
+            Boolean, 'autoUpdate'
+        ]);
+});
