e14186351685c823440e79c1c4f5fe50f843ee62
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-04-17 15:47:31 +0300

    added fill all functionaliti to Announcement View

---
+++ Chalkable.BusinessLogic/Services/Master/PictureServices/PersonPictureService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.BusinessLogic.Security;
+using Chalkable.Common;
+using Chalkable.Common.Exceptions;
+
+namespace Chalkable.BusinessLogic.Services.Master.PictureServices
+{
+    public class PersonPictureService : PictureService
+    {
+        public PersonPictureService(IServiceLocatorMaster serviceLocator)
+            : base(serviceLocator)
+        {
+            supportedSizes.Add(new PictureSize { Height = 24, Width = 24 });
+            supportedSizes.Add(new PictureSize { Height = 40, Width = 40 });
+            supportedSizes.Add(new PictureSize { Height = 47, Width = 47 });
+            supportedSizes.Add(new PictureSize { Height = 64, Width = 64 });
+            supportedSizes.Add(new PictureSize { Height = 128, Width = 128 });
+            supportedSizes.Add(new PictureSize { Height = 256, Width = 256 });
+        }
+
+        public override void UploadPicture(Guid id, byte[] content)
+        {
+            if (!(BaseSecurity.IsAdminEditor(Context)
+                || (Context.Role == CoreRoles.TEACHER_ROLE && Context.UserId == id)))
+                throw new ChalkableSecurityException();
+            base.UploadPicture(id, content);
+        }
+        public override void DeletePicture(Guid id)
+        {
+            if (!(BaseSecurity.IsAdminEditor(Context)
+                || (Context.Role == CoreRoles.TEACHER_ROLE && Context.UserId == id)))
+                throw new ChalkableSecurityException();
+            base.DeletePicture(id);
+        }
+    }
+}
---
+++ Chalkable.Tests/Services/ServiceTestBase.Assert.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.BusinessLogic.Services;
+using Chalkable.BusinessLogic.Services.School;
+using Chalkable.Tests.Services.School;
+using NUnit.Framework;
+
+namespace Chalkable.Tests.Services
+{
+    public partial class ServiceTestBase
+    {
+        protected static void AssertException<TExc>(Action action) where TExc : Exception
+        {
+            bool wasException = false;
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+                if (ex is TExc)
+                    wasException = true;
+            }
+            Assert.IsTrue(wasException);
+        }
+
+        protected static void AssertAreEqual<T>(T expected, T actual) where T : new()
+        {
+            var type = typeof(T);
+            var fields = type.GetProperties().Where(x => !x.PropertyType.IsClass && !x.PropertyType.IsInterface).ToList();
+            foreach (var propertyInfo in fields)
+            {
+                Assert.AreEqual(propertyInfo.GetValue(expected), propertyInfo.GetValue(actual));
+            }
+        }
+
+        protected static void AssertAreEqual<T>(IList<T> expected, IList<T> actual) where T : new()
+        {
+            if (expected == null && actual == null)
+                return;
+            if (expected == null)
+                throw new Exception("Expected list is null but actual isn't");
+            if (actual == null)
+                throw new Exception("Actual list is null but expected isn't");
+            if (expected.Count != actual.Count)
+                throw new Exception(string.Format("Different size of lists. expected {0} actual {1}", expected.Count, actual.Count));
+
+            var type = typeof(T);
+            var isObject = (type.IsClass || type.IsInterface) && typeof(Guid) != type && typeof(Guid?) != type;
+            for (int i = 0; i < expected.Count; i++)
+            {
+                if (isObject)
+                {
+                    AssertAreEqual(expected[i], actual[i]);
+                }
+                else
+                {
+                    Assert.AreEqual(expected[i], actual[i]);
+                }
+            }
+        }
+
+        public static int GetNewId<TModel>(IList<TModel> models, Func<TModel, int> action)
+        {
+            return models.Count > 0 ? models.Max(action) + 1 : 1;
+        }
+
+        public static int GetNewId<TModel>(IServiceLocatorSchool distirctLocator, Func<IServiceLocatorSchool, IList<TModel>> serviceMethod, Func<TModel, int> action)
+        {
+            var sysLocator = ServiceLocatorFactory.CreateMasterSysAdmin()
+                .SchoolServiceLocator(distirctLocator.Context.DistrictId.Value, null);
+            return GetNewId(serviceMethod(sysLocator), action);
+        }
+
+
+    }
+}
