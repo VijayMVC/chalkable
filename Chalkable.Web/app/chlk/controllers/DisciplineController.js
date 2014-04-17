9b91f63cfee7921d9387a454491da041fb4e8c97
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-04-17 15:37:12 +0300

    add maintain discipline permission

--- Chalkable.Web/app/chlk/controllers/DisciplineController.js
+++ Chalkable.Web/app/chlk/controllers/DisciplineController.js
@@ -81,10 +81,11 @@ NAMESPACE('chlk.controllers', function(){
                     ])
                     .attach(this.validateResponse_())
                     .then(function(result){
-                        var classes = this.classService.getClassesForTopBar(false);
+                        var classes = this.getClassForDisciplines_();
                         var classBarData = new chlk.models.classes.ClassesForTopBar(classes);
                         return new chlk.models.discipline.ClassDisciplinesViewData(
-                            classBarData, classId_, result[0], result[1], date_, true
+                            classBarData, classId_, result[0], result[1], date_, true,
+                            this.hasUserPermission_(chlk.models.people.UserPermissionEnum.MAINTAIN_CLASSROOM_DISCIPLINE)
                         );
                     }, this);
 
