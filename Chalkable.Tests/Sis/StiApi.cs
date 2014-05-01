ef013b3cafa9176ccba2b7cbcb11a6e61e93c8bf
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-05-01 18:56:40 +0300

    Merge

--- Chalkable.Tests/Sis/StiApi.cs
+++ Chalkable.Tests/Sis/StiApi.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using Chalkable.StiConnector.Connectors;
 using Chalkable.StiConnector.SyncModel;
 using NUnit.Framework;
@@ -31,32 +32,11 @@ namespace Chalkable.Tests.Sis
             //var cl = ConnectorLocator.Create("administrator", "1234qwer", "http://localhost/");
             var cl = ConnectorLocator.Create("administrator", "Ee9E(#UQe/5(G$U", "http://sandbox.sti-k12.com/chalkable/api/");
             //var cl = ConnectorLocator.Create("administrator", "qwertyui1234", "http://208.83.95.80:8210/");
-        
-            var r = cl.SyncConnector.GetDiff(typeof(ClassroomOption), null) as SyncResult<ClassroomOption>;
-            foreach (var item in r.All)
-            {
-                Debug.WriteLine(item.SectionID+" " +item.StandardsCalculationMethod);
-            }
 
-            var r1 = cl.SyncConnector.GetDiff(typeof(GradingScaleRange), null) as SyncResult<GradingScaleRange>;
-            foreach (var item in r1.All)
-            {
-                Debug.WriteLine(item.GradingScaleID + " " + item.AveragingEquivalent);
-            }
+            var r = cl.SyncConnector.GetDiff(typeof(Student), null) as SyncResult<Student>;
+            var s = r.All.Where(x => x.StudentID == 30).First();
+            Debug.WriteLine(s.HasMedicalAlert);
 
-            var r2 = cl.SyncConnector.GetDiff(typeof(GradingScale), null) as SyncResult<GradingScale>;
-            foreach (var item in r2.All)
-            {
-                Debug.WriteLine(item.GradingScaleID + " " + item.Name);
-            }
-
-            var r3 = cl.SyncConnector.GetDiff(typeof(GradingComment), null) as SyncResult<GradingComment>;
-            foreach (var item in r3.All)
-            {
-                Debug.WriteLine(item.GradingCommentID + " " + item.Code);
-            }
-            
-            
         }
     }
 }
