194965305fa3a22c98143f58112ce6d024731c26
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-05-12 18:15:58 +0300

    fixed item title saving

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/Storage/DemoAnnonucementRecipientStorage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool.Storage
+{
+    public class DemoAnnouncementRecipientStorage:BaseDemoStorage<int ,AnnouncementRecipient>
+    {
+        public DemoAnnouncementRecipientStorage(DemoStorage storage) : base(storage)
+        {
+        }
+
+        public void Add(List<AnnouncementRecipient> annRecipients)
+        {
+            foreach (var announcementRecipient in annRecipients)
+            {
+                if (!data.ContainsKey(announcementRecipient.Id))
+                    data[announcementRecipient.Id] = announcementRecipient;
+            }
+            
+        }
+
+        public void DeleteByAnnouncementId(int id)
+        {
+            var annRep = data.Where(x => x.Value.AnnouncementRef == id).Select(x => x.Key).ToList();
+            Delete(annRep);
+        }
+
+        public IList<AnnouncementRecipient> GetList(int announcementId)
+        {
+            return data.Where(x => x.Value.AnnouncementRef == announcementId).Select(x => x.Value).ToList();
+        }
+
+
+        public override void Setup()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
