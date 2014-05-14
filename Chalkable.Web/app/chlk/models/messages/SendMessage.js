6e1e8bc1a4e1c24e42db110db550015d5796e015
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-05-14 08:19:54 -0600

    fixed deleteAction

---
+++ Chalkable.Web/app/chlk/models/messages/SendMessage.js
@@ -0,0 +1,17 @@
+REQUIRE('chlk.models.common.ChlkDate');
+REQUIRE('chlk.models.id.SchoolPersonId');
+REQUIRE('chlk.models.people.User');
+
+
+NAMESPACE('chlk.models.messages', function () {
+
+    "use strict";
+    /** @class chlk.models.messages.SendMessage*/
+    CLASS(
+        'SendMessage', [
+            chlk.models.id.SchoolPersonId, 'recipientId',
+            String, 'subject',
+            String, 'body'
+        ]);
+});
+
