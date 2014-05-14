29b935f1164836ab2ef9dfe3a407c8bbb122fc6b
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-05-14 22:09:38 +0300

    removed DatePicker from ListWeekCalendar

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/Storage/sti/DemoStiAttendanceStorage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.StiConnector.Connectors.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool.Storage.sti
+{
+    public class DemoStiAttendanceStorage:BaseDemoStorage<int, SectionAttendance>
+    {
+        public DemoStiAttendanceStorage(DemoStorage storage)
+            : base(storage)
+        {
+        }
+
+        public SectionAttendance GetSectionAttendance(DateTime date, int classId)
+        {
+            var ds = date.ToString("yyyy-MM-dd");
+
+            if (data.Count(x => x.Value.SectionId == classId && x.Value.Date == ds) == 0)
+            {
+                return new SectionAttendance()
+                {
+                    SectionId = classId,
+                    Date = ds,
+                    StudentAttendance = new List<StudentSectionAttendance>()
+                    {
+                        new StudentSectionAttendance()
+                        {
+                            Date = ds,
+                            SectionId = classId,
+                            StudentId = 1196
+                        }
+                    }
+                };
+            }
+           return data.First(x => x.Value.SectionId == classId && x.Value.Date == ds).Value;
+        }
+
+        public override void Setup()
+        {
+           
+        }
+
+        public void SetSectionAttendance(int schoolYearId, DateTime date, int classId, SectionAttendance sa)
+        {
+            var ds = date.ToString("yyyy-MM-dd");
+            if (data.Count(x => x.Value.SectionId == classId && x.Value.Date == ds) == 0)
+            {
+                data.Add(GetNextFreeId(), sa);
+            }
+            var item = data.First(x => x.Value.SectionId == classId && x.Value.Date == ds).Key;
+            sa.IsPosted = true;
+            data[item] = sa;
+        }
+    }
+}
