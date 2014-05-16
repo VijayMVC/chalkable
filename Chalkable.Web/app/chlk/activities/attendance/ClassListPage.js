060c39430e731953c1a58bae392aaad506b44153
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-05-16 19:24:57 +0300

    fixed for
    Teacher: Attendance > class selected. Need provide popup for attendance.....

--- Chalkable.Web/app/chlk/activities/attendance/ClassListPage.js
+++ Chalkable.Web/app/chlk/activities/attendance/ClassListPage.js
@@ -1,6 +1,7 @@
 REQUIRE('chlk.activities.lib.TemplatePage');
 REQUIRE('chlk.templates.attendance.ClassAttendanceTpl');
 REQUIRE('chlk.templates.attendance.ClassList');
+REQUIRE('chlk.templates.common.InfoMsg');
 
 NAMESPACE('chlk.activities.attendance', function () {
     "use strict";
@@ -23,10 +24,22 @@ NAMESPACE('chlk.activities.attendance', function () {
                 this._reasons = null;
                 this._classAttendances = null;
                 this._currentStudentAtt = null;
+                this._needPopUp = false;
+            },
+
+            function datePickerHandler(event){
+                var target = new ria.dom.Dom(event.target);
+                if(target.is('.ui-state-default'))
+                    if(!this.tryToLeave(target)){
+                        event.preventDefault();
+                        event.stopPropagation();
+                        return false;
+                    }
             },
 
             [ria.mvc.PartialUpdateRule(chlk.templates.attendance.ClassAttendanceTpl)],
             VOID, function doUpdateItem(tpl, model, msg_) {
+                this._needPopUp = true;
                 this.dom.find('.keyboard-suggestion').hide();
                 var container = this.dom.find('.container-' + model.getStudent().getId().valueOf());
                 container.empty();
@@ -80,7 +93,6 @@ NAMESPACE('chlk.activities.attendance', function () {
                     row_.find('input[name=type]').setValue(this._typesEnum.PRESENT.valueOf());
               //      row_.find('form').trigger('submit');
                 }else{
-                    console.info('rowSelect');
                     this.showDropDown();
                 }
             },
@@ -195,10 +207,79 @@ NAMESPACE('chlk.activities.attendance', function () {
                 }
             },
 
+            [[ria.dom.Dom, Object]],
+            function showLeavePopUp(node, needChange_){
+                var buttons = [{
+                    text: Msg.Leave_anyways,
+                    color: chlk.models.common.ButtonColor.RED.valueOf(),
+                    attributes: {
+                        clazz: 'leave-button'
+                    }
+                }, {
+                    text: Msg.Go_Back,
+                    color: chlk.models.common.ButtonColor.GREEN.valueOf(),
+                    attributes: {
+                        clazz: 'go-back'
+                    }
+                }];
+                var buttonsModel = [];
+                var serializer = new chlk.lib.serialize.ChlkJsonSerializer();
+                buttons.forEach(function(item){
+                    buttonsModel.push(serializer.deserialize(item, chlk.models.common.Button));
+                });
+                var model = new chlk.models.common.InfoMsg(Msg.Click_post_to_save, Msg.Whoa.capitalize(), buttonsModel, 'attendance-leave');
+                var tpl = new chlk.templates.common.InfoMsg();
+                tpl.assign(model);
+                tpl.renderTo(new ria.dom.Dom('#chlk-dialogs'));
+                new ria.dom.Dom('#chlk-overlay, #chlk-dialogs').removeClass('x-hidden');
+                var that = this;
+
+                new ria.dom.Dom('.leave-button').on('click', function(){
+                    that._needPopUp = false;
+                    that.removeLeavePopUp();
+                    node.trigger('click');
+                });
+                new ria.dom.Dom('.go-back').on('click', function(){
+                    that.removeLeavePopUp();
+                });
+            },
+
+            function removeLeavePopUp(){
+                new ria.dom.Dom('#chlk-overlay, #chlk-dialogs').addClass('x-hidden');
+                new ria.dom.Dom('#chlk-overlay').setHTML('');
+            },
+
             OVERRIDE, VOID, function onRender_(model){
                 BASE(model);
+                this._needPopUp = false;
                 this._reasons = model.getReasons();
                 this._classAttendances = model.getItems();
+                var that = this;
+                new ria.dom.Dom('.page').on('click.leave', '.action-link:not(.class-button)', function(node, event){
+                    return that.tryToLeave(node);
+                });
+
+                this.dom.find('.class-button:not(.pressed)').on('click.leave', function(node, event){
+                    return that.tryToLeave(node);
+                });
+
+                document.addEventListener('click', this.datePickerHandler, true);
+            },
+
+            OVERRIDE, VOID, function onStop_() {
+                BASE();
+                new ria.dom.Dom('.page').off('click.leave', '.action-link:not(.class-button)');
+                this.dom.find('.class-button:not(.pressed)').off('click.leave');
+                document.removeEventListener('click', this.datePickerHandler, true);
+            },
+
+            [[ria.dom.Dom, Boolean]],
+            function tryToLeave(node, needChange_){
+                if(this._needPopUp && !node.parent('.set-attendance-list-form').exists()){
+                    this.showLeavePopUp(node, needChange_);
+                    return false;
+                }
+                return true;
             },
 
             [ria.mvc.DomEventBind('click', '.current-attendance')],
@@ -278,6 +359,7 @@ NAMESPACE('chlk.activities.attendance', function () {
                 jQuery(node.valueOf()[0]).text('SAVING');
                 var attendancesNode = this.dom.find('input[name="attendancesJson"]');
                 attendancesNode.setValue(JSON.stringify(this.getAttendances_()));
+                this._needPopUp = false;
             }
         ]);
 });
