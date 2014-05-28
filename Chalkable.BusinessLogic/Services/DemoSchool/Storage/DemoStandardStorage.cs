ddbb58d29746a61c04eaf508020bcf19b70679aa
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-05-28 04:16:46 -0600

    fixed BaseApp

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/Storage/DemoStandardStorage.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Chalkable.BusinessLogic.Services.DemoSchool.Common;
+using Chalkable.Data.School.DataAccess;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool.Storage
+{
+    public class DemoStandardStorage:BaseDemoIntStorage<Standard>
+    {
+        public DemoStandardStorage(DemoStorage storage) : base(storage, x => x.Id)
+        {
+        }
+
+        public void AddStandards(IList<Standard> standards)
+        {
+            Add(standards);
+        }
+
+        public IList<Standard> GetStandarts(StandardQuery query)
+        {
+            var standards = data.Select(x => x.Value);
+            if (query.StandardSubjectId.HasValue)
+                standards = standards.Where(x => x.StandardSubjectRef == query.StandardSubjectId);
+            if (query.GradeLavelId.HasValue)
+                standards =
+                    standards.Where(
+                        x => query.GradeLavelId <= x.LowerGradeLevelRef && query.GradeLavelId >= x.UpperGradeLevelRef);
+            if (!query.AllStandards || query.ParentStandardId.HasValue)
+                standards = standards.Where(x => x.ParentStandardRef == query.ParentStandardId);
+
+            if (query.ClassId.HasValue)
+            {
+                var classStandarts = Storage.ClasStandardStorage.GetAll(query.ClassId).Select(x => x.StandardRef);
+                standards = standards.Where(x => classStandarts.Contains(x.Id));
+            }
+            if (query.CourseId.HasValue)
+            {
+                var classStandarts = Storage.ClasStandardStorage.GetAll(query.CourseId).Select(x => x.StandardRef);
+                standards = standards.Where(x => classStandarts.Contains(x.Id));
+            }
+
+            return standards.ToList();
+        }
+
+        public override void Setup()
+        {
+            AddStandards(new List<Standard>
+            {
+                new Standard
+                {
+                    Id = DemoSchoolConstants.MathStandard1,
+                    IsActive = true,
+                    Name = "Math 1",
+                    LowerGradeLevelRef = DemoSchoolConstants.GradeLevel10,
+                    UpperGradeLevelRef = DemoSchoolConstants.GradeLevel10,
+                    StandardSubjectRef = DemoSchoolConstants.ScienceStandardSubject
+                },
+
+                new Standard
+                {
+                    Id = DemoSchoolConstants.MathStandard2,
+                    IsActive = true,
+                    Name = "Math 2",
+                    LowerGradeLevelRef = DemoSchoolConstants.GradeLevel11,
+                    UpperGradeLevelRef = DemoSchoolConstants.GradeLevel11,
+                    StandardSubjectRef = DemoSchoolConstants.ScienceStandardSubject
+                },
+
+                new Standard
+                {
+                    Id = DemoSchoolConstants.MathStandard3,
+                    IsActive = true,
+                    Name = "Math 3",
+                    LowerGradeLevelRef = DemoSchoolConstants.GradeLevel12,
+                    UpperGradeLevelRef = DemoSchoolConstants.GradeLevel12,
+                    StandardSubjectRef = DemoSchoolConstants.ScienceStandardSubject
+                },
+
+            });  
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/models/grading/ShortGradingClassSummaryGridItems.js
@@ -0,0 +1,72 @@
+REQUIRE('chlk.models.grading.StudentWithAvg');
+REQUIRE('chlk.models.common.NameId');
+REQUIRE('chlk.models.announcement.ShortAnnouncementViewData');
+REQUIRE('chlk.models.grading.StudentAverageInfo');
+REQUIRE('chlk.models.schoolYear.GradingPeriod');
+REQUIRE('chlk.models.id.StandardId');
+REQUIRE('chlk.models.id.AnnouncementTypeGradingId');
+
+NAMESPACE('chlk.models.grading', function () {
+    "use strict";
+
+    /** @class chlk.models.grading.ShortGradingClassSummaryGridItems*/
+    CLASS(
+        'ShortGradingClassSummaryGridItems', [
+            ArrayOf(chlk.models.grading.StudentWithAvg), 'students',
+
+            [ria.serialize.SerializeProperty('gradingitems')],
+            ArrayOf(chlk.models.announcement.ShortAnnouncementViewData), 'gradingItems',
+
+            [ria.serialize.SerializeProperty('gradingperiod')],
+            chlk.models.schoolYear.GradingPeriod, 'gradingPeriod',
+
+            chlk.models.id.StandardId, 'standardId',
+
+            chlk.models.id.AnnouncementTypeGradingId, 'categoryId',
+
+            Number, 'avg',
+
+            Number, 'rowIndex',
+
+            Boolean, 'autoUpdate',
+
+            [ria.serialize.SerializeProperty('displayalphagrades')],
+            Boolean , 'ableDisplayAlphaGrades',
+
+            [ria.serialize.SerializeProperty('displaystudentaverage')],
+            Boolean , 'ableDisplayStudentAverage',
+
+            [ria.serialize.SerializeProperty('displaytotalpoints')],
+            Boolean , 'ableDisplayTotalPoints',
+
+            [ria.serialize.SerializeProperty('totalavarages')],
+            ArrayOf(chlk.models.grading.StudentAverageInfo), 'studentAverages',
+
+            [ria.serialize.SerializeProperty('totalpoints')],
+            ArrayOf(Number), 'totalPoints',
+
+            function getTooltipText(){
+                return (this.getAvg() != null ? Msg.Avg + " " + this.getAvg() : 'No grades yet');
+            },
+
+            [[chlk.models.grading.StudentAverageInfo]],
+            String, function displayAvgName(studentAverage){
+                return studentAverage && studentAverage.isGradingPeriodAverage()
+                    ? Msg.Avg : studentAverage.getAverageName();
+            },
+
+            String, function displayGrade(grade){
+                return grade ? grade.toFixed(2) : '';
+            },
+
+            [[chlk.models.grading.ShortStudentAverageInfo, Boolean, Boolean]],
+            String, function displayAvgGradeValue(averageInfo, isAbleDisplayAlphaGrades_, original_){
+                var alphaGrade = original_ ? averageInfo.getCalculatedAlphaGrade() : averageInfo.getAlphaGrade();
+                var res = this.displayGrade(original_ ? averageInfo.getCalculatedAvg() : averageInfo.getNumericAvg());
+                if(res && isAbleDisplayAlphaGrades_ && alphaGrade && alphaGrade.trim() != ''){
+                    res += '(' + alphaGrade + ')';
+                }
+                return res;
+            }
+        ]);
+});
