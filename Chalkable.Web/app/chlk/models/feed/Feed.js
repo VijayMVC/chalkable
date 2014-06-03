8cc8c46fcf9f414ecae077985cf1f382ed70c5b2
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-06-03 10:52:44 -0600

    fixed feedItem on Ui

--- Chalkable.Web/app/chlk/models/feed/Feed.js
+++ Chalkable.Web/app/chlk/models/feed/Feed.js
@@ -9,19 +9,19 @@ NAMESPACE('chlk.models.feed', function () {
     CLASS(
         'Feed', EXTENDS(chlk.models.common.PageWithClasses), [
             [[ArrayOf(chlk.models.announcement.Announcement), chlk.models.classes.ClassesForTopBar, Boolean, Number]],
-            function $(items_, classes_, completeOnly_, newNotificationCount_){
+            function $(items_, classes_, importantOnly_, newNotificationCount_){
                 BASE(classes_);
                 if(items_)
                     this.setItems(items_);
-                if(completeOnly_)
-                    this.setCompleteOnly(completeOnly_);
+                if(importantOnly_)
+                    this.setImportantOnly(importantOnly_);
                 if(newNotificationCount_)
                     this.setNewNotificationCount(newNotificationCount_);
             },
 
             ArrayOf(chlk.models.announcement.Announcement), 'items',
 
-            Boolean, 'completeOnly',
+            Boolean, 'importantOnly',
 
             Number, 'importantCount',
 
