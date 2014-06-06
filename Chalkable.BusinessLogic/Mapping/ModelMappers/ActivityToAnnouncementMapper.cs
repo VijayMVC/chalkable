c75529aa995851b7f64385f7a196235edc82ad6c
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-06-06 17:03:41 +0300

    fixed for CHLK-2642 Attendance > All: Add data for each class to chart

---
+++ Chalkable.BusinessLogic/Mapping/ModelMappers/ActivityToAnnouncementMapper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.School.Model;
+using Chalkable.StiConnector.Connectors.Model;
+
+namespace Chalkable.BusinessLogic.Mapping.ModelMappers
+{
+    public class ActivityToAnnouncementMapper : BaseMapper<Announcement, Activity>
+    {
+        protected override void InnerMap(Announcement ann, Activity activity)
+        {
+            ann.Content = activity.Description;
+            ann.MaxScore = activity.MaxScore;
+            ann.ClassRef = activity.SectionId;
+            ann.Expires = activity.Date;
+            ann.MayBeDropped = activity.MayBeDropped;
+            ann.WeightAddition = activity.WeightAddition;
+            ann.WeightMultiplier = activity.WeightMultiplier;
+            ann.Dropped = activity.IsDropped;
+            ann.ClassAnnouncementTypeRef = activity.CategoryId;
+            ann.VisibleForStudent = activity.DisplayInHomePortal;
+            ann.Title = activity.Name;
+            ann.MayBeExempt = activity.MayBeExempt;
+            ann.IsScored = activity.IsScored;
+            ann.Complete = activity.Complete;
+            if (ann is AnnouncementComplex && activity.Attachments != null)
+            {
+                var annC = ann as AnnouncementComplex;
+                annC.AttachmentsCount = activity.Attachments.Count();
+                //annC.Starred = activity.Starred;
+            }
+            if (ann is AnnouncementDetails)
+            {
+                var annDetails = ann as AnnouncementDetails;
+                if (activity.Attachments != null && activity.Attachments.Any())
+                {
+
+                    if (annDetails.AnnouncementAttachments == null)
+                        annDetails.AnnouncementAttachments = new List<AnnouncementAttachment>();
+                    foreach (var att in activity.Attachments)
+                    {
+                        var annAtt = annDetails.AnnouncementAttachments.FirstOrDefault(x => x.SisAttachmentId == att.AttachmentId);
+                        if (annAtt == null)
+                        {
+                            annAtt = new AnnouncementAttachment
+                            {
+                                AnnouncementRef = annDetails.Id,
+                                SisAttachmentId = att.AttachmentId
+                            };
+                            annDetails.AnnouncementAttachments.Add(annAtt);
+                        }
+                        MapperFactory.GetMapper<AnnouncementAttachment, ActivityAttachment>().Map(annAtt, att);
+                    }
+                }
+                if (annDetails.AnnouncementStandards == null)
+                    annDetails.AnnouncementStandards = new List<AnnouncementStandardDetails>();
+                if (activity.Standards != null && activity.Standards.Any())
+                {
+                    foreach (var activityStandard in activity.Standards)
+                    {
+                        var annStandard = annDetails.AnnouncementStandards.FirstOrDefault(x => x.StandardRef == activityStandard.Id);
+                        if (annStandard == null)
+                        {
+                            annStandard = new AnnouncementStandardDetails { AnnouncementRef = annDetails.Id };
+                            annDetails.AnnouncementStandards.Add(annStandard);
+                        }
+                        annStandard.StandardRef = activityStandard.Id;
+                        if (annStandard.Standard == null)
+                            annStandard.Standard = new Standard();
+                        annStandard.Standard.Id = activityStandard.Id;
+                        annStandard.Standard.Name = activityStandard.Name;
+                    }
+                }
+            }
+            
+        }
+    }
+
+}
---
+++ Chalkable.BusinessLogic/Services/DemoSchool/Common/DemoSchoolConstants.cs
@@ -0,0 +1,72 @@
+﻿namespace Chalkable.BusinessLogic.Services.DemoSchool.Common
+{
+    class DemoSchoolConstants
+    {
+        
+
+        public const int MathStandard1 = 1;
+        public const int MathStandard2 = 2;
+        public const int MathStandard3 = 3;
+
+        public const int ScienceStandardSubject = 3;
+
+        public const int AlgebraAcademicAchievementId = 0;
+        public const int AlgebraAcademicPracticeId = 1;
+
+        public const int GeometryAcademicAchievementId = 2;
+        public const int GeometryAcademicPracticeId = 3;
+
+        public const int SchoolId = 1;
+        public const int CurrentSchoolYearId = 1;
+
+        public const int FirstMarkingPeriodId = 1;
+        public const int SecondMarkingPeriodId = 2;
+
+        public const int GradingPeriodQ1 = 1;
+        public const int GradingPeriodQ2 = 2;
+        public const int GradingPeriodQ3 = 3;
+        public const int GradingPeriodQ4 = 4;
+
+        public const int GradeLevel1 = 1;
+        public const int GradeLevel2 = 2;
+        public const int GradeLevel3 = 3;
+        public const int GradeLevel4 = 4;
+        public const int GradeLevel5 = 5;
+        public const int GradeLevel6 = 6;
+        public const int GradeLevel7 = 7;
+        public const int GradeLevel8 = 8;
+        public const int GradeLevel9 = 9;
+        public const int GradeLevel10 = 10;
+        public const int GradeLevel11 = 11;
+        public const int GradeLevel12 = 12;
+
+
+
+        public const int FirstPeriodId = 1;
+        public const int SecondPeriodId = 2;
+
+        public const int TeacherId = 1195;
+        public const int AdminGradeId = 1197;
+        public const int AdminEditId = 1198;
+        public const int AdminViewId = 1199;
+        public const int Student1 = 1;
+        public const int Student2 = 2;
+        public const int Student3 = 3;
+        public const int Student4 = 4;
+        public const int Student5 = 5;
+        public const int Student6 = 6;
+        public const int Student7 = 7;
+        public const int Student8 = 8;
+        public const int Student9 = 9;
+        public const int Student10 = 10;
+
+
+        public const int AlgebraClassId = 1;
+        public const int GeometryClassId = 2;
+        public const int PhysicsClassId = 3;
+        public const int PreCalculusClassId = 4;
+
+
+
+    }
+}
