fb8e17c2d97c00155adb75d399e734a0d321a643
Andrew Machnew <amachnew@emp-game.com>
2014-06-09 12:35:59 +0300

    fixed demo change password

---
+++ Chalkable.Data.School/Model/School.cs
@@ -0,0 +1,13 @@
+﻿using Chalkable.Data.Common;
+
+namespace Chalkable.Data.School.Model
+{
+    public class School
+    {
+        [PrimaryKeyFieldAttr]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsPrivate { get; set; }
+    }
+}
---
+++ Chalkable.Web/app/chlk/models/common/PaginatedList.js
@@ -0,0 +1,29 @@
+NAMESPACE('chlk.models.common', function () {
+    "use strict";
+
+    /** @class chlk.models.common.PaginatedList */
+    CLASS(
+        'PaginatedList', [
+            [[Function]],
+            function $(itemClass) {
+                BASE();
+                this.itemClass = itemClass;
+            },
+
+            READONLY, Function, 'itemClass',
+
+            ArrayOf(Object), 'items',
+            Number, 'pageIndex',
+            Number, 'pageSize',
+            Number, 'totalCount',
+            Number, 'actualCount',
+            Number, 'totalPages',
+            Boolean, 'hasNextPage',
+            Boolean, 'hasPreviousPage',
+
+            VOID, function setItems(values) {
+                VALIDATE_ARG('value', [ArrayOf(this.itemClass)], values);
+                this.items = values;
+            }
+        ])
+})
