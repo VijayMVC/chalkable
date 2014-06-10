e24fa17cd8a740e0bf9f77c18df097def06502ee
Andrew Machnew <amachnew@emp-game.com>
2014-06-10 15:17:36 +0300

    fixed demo set grade

--- Chalkable.BusinessLogic/Services/DemoSchool/Storage/DemoAlphaGradeStorage.cs
+++ Chalkable.BusinessLogic/Services/DemoSchool/Storage/DemoAlphaGradeStorage.cs
@@ -11,34 +11,12 @@ namespace Chalkable.BusinessLogic.Services.DemoSchool.Storage
 
         public IList<AlphaGrade> GetForClassStandarts(int classId)
         {
-            /*
-             * var dbQuery = new DbQuery();
-            dbQuery.Sql.AppendFormat(@"select AlphaGrade.* from AlphaGrade
-                                       join GradingScaleRange on GradingScaleRange.[{0}] = AlphaGrade.[{1}]
-                                       join ClassroomOption on ClassroomOption.[{2}] = GradingScaleRange.[{3}]"
-                                     , GradingScaleRange.ALPHA_GRADE_REF_FIELD, AlphaGrade.ID_FIELD
-                                     , ClassroomOption.STANDARD_GRADING_SCALE_REF_FIELD,
-                                     GradingScaleRange.GRADING_SCALE_REF_FIELD);
-            var conds = new AndQueryCondition {{ClassroomOption.ID_FIELD, classId}};
-            conds.BuildSqlWhere(dbQuery, "ClassroomOption");
-             */
-            //throw new NotImplementedException();
-            return new List<AlphaGrade>();
+            return GetAll();
         }
 
         public IList<AlphaGrade> GetForClass(int classId)
         {
-            /*var sql = @"select AlphaGrade.* from AlphaGrade
-                        join GradingScaleRange on GradingScaleRange.[{0}] = AlphaGrade.[{1}]
-                        join Class on Class.[{2}] = GradingScaleRange.[{3}]";
-            var dbQuery = new DbQuery();
-            dbQuery.Sql.AppendFormat(sql, GradingScaleRange.ALPHA_GRADE_REF_FIELD, AlphaGrade.ID_FIELD
-                                     , Class.GRADING_SCALE_REF_FIELD, GradingScaleRange.GRADING_SCALE_REF_FIELD);
-            var conds = new AndQueryCondition {{Class.ID_FIELD, classId}};
-            conds.BuildSqlWhere(dbQuery, "Class");
-            return ReadMany<AlphaGrade>(dbQuery);*/
-            //throw new NotImplementedException();
-            return new List<AlphaGrade>();
+            return GetAll();
         }
     }
 }
