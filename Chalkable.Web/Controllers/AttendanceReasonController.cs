cd970a0bbd9e5af733ac62b36ce8d74b051ca732
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-06-11 15:51:23 +0300

    fixed for When teacher puts a grade for student in firefox he will see half "grade box"

---
+++ Chalkable.Web/Controllers/AttendanceReasonController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Chalkable.Data.Common.Enums;
+using Chalkable.Data.Master.Model;
+using Chalkable.Web.ActionFilters;
+using Chalkable.Web.Models;
+using Chalkable.Web.Models.AttendancesViewData;
+
+namespace Chalkable.Web.Controllers
+{
+    [RequireHttps, TraceControllerFilter]
+    public class AttendanceReasonController : ChalkableController
+    {
+        [AuthorizationFilter("SysAdmin, AdminGrade, AdminEdit, AdminView, Teacher", Preference.API_DESCR_ATTENDANCE_REASON_LIST, true, CallType.Post, new[] {AppPermissionType.Attendance})]
+        public ActionResult List()
+        {
+            return Json(AttendanceReasonViewData.Create(SchoolLocator.AttendanceReasonService.List()));
+        }
+    }
+}
