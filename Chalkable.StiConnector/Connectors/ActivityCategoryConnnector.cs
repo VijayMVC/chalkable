7c0394962314d7e9f5b0ccc6eac21317e16fa65f
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-06-12 06:37:19 -0600

    fixed create announcement

---
+++ Chalkable.StiConnector/Connectors/ActivityCategoryConnnector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.StiConnector.Connectors.Model;
+
+
+namespace Chalkable.StiConnector.Connectors
+{
+    public class ActivityCategoryConnnector : ConnectorBase
+    {
+        public ActivityCategoryConnnector(ConnectorLocator locator) : base(locator)
+        {
+        }
+
+        public IList<ActivityCategory> GetBySectionIds(IList<int> sectionIds)
+        {
+            if(sectionIds.Count == 1)
+                return Call<IList<ActivityCategory>>(string.Format("{0}sections/{1}/activities/categories", BaseUrl, sectionIds.First()));
+   
+            var nvc = new NameValueCollection();
+            for (int i = 0; i < sectionIds.Count; i++)
+                nvc.Add(string.Format("sectionIds[{0}]", i), sectionIds[i].ToString());   
+            return Call<IList<ActivityCategory>>(string.Format("{0}activities/categories", BaseUrl), nvc);
+        }
+
+        public ActivityCategory GetById(int activityCategoryId)
+        {
+            return Call<ActivityCategory>(string.Format("{0}activities/categories/{1}", BaseUrl, activityCategoryId));
+        }
+    }
+}
