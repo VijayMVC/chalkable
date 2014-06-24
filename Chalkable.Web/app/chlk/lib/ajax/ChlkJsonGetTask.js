694a0b0bdbca53bf02e7d3278277949e9896eee3
Ievgenii Zasoba <geka.zasoba@gmail.com>
2014-06-24 21:05:47 +0300

    added gzip support to sync API

---
+++ Chalkable.Web/app/chlk/lib/ajax/ChlkJsonGetTask.js
@@ -0,0 +1,23 @@
+REQUIRE('ria.ajax.JsonGetTask');
+REQUIRE('chlk.lib.exception.NotAuthorizedException');
+
+NAMESPACE('chlk.lib.ajax', function () {
+    "use strict";
+
+    /** @class chlk.lib.ajax.ChlkJsonGetTask */
+    CLASS(
+        'ChlkJsonGetTask', EXTENDS(ria.ajax.JsonGetTask), [
+            function $(url) {
+                BASE(url);
+            },
+
+            OVERRIDE, VOID, function transferComplete_(evt) {
+                if (this._xhr.getResponseHeader('REQUIRES_AUTH') != null)   {
+                    this._completer.completeError(chlk.lib.exception.NotAuthorizedException());
+                    return;
+                }
+
+                BASE(evt);
+            }
+        ]);
+});
