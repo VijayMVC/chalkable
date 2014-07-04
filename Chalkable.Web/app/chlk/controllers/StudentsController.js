c0ff6033fbfd26f224a14713cbe09fcf9d4d1ba4
Andrew Machnew <amachnew@emp-game.com>
2014-07-04 16:33:14 +0300

    fixed student controller

--- Chalkable.Web/app/chlk/controllers/StudentsController.js
+++ Chalkable.Web/app/chlk/controllers/StudentsController.js
@@ -77,7 +77,7 @@ NAMESPACE('chlk.controllers', function (){
                         .getStudents(classId_, null, !isStudent && isMy, true, 0, 10)
                         .attach(this.validateResponse_());
                 }
-                result.then(function(users){
+                result = result.then(function(users){
                         var usersModel = this.prepareUsersModel(users, 0, true);
                         var classes = this.classService.getClassesForTopBar(true);
                         var topModel = new chlk.models.classes.ClassesForTopBar(classes, classId_);
