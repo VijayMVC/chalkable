b6b3a3d81088d0178bdec6505719b90bb383e8e2
Andrew Machnew <amachnew@emp-game.com>
2014-07-07 12:59:45 +0300

    fixed for CHLK-2674

---
+++ Chalkable.Data.School/Model/StudentSchoolYear.cs
@@ -0,0 +1,32 @@
+﻿using Chalkable.Data.Common;
+
+namespace Chalkable.Data.School.Model
+{
+    public class StudentSchoolYear
+    {
+        public const string GRADE_LEVEL_REF_FIELD = "GradeLevelRef";
+        public const string STUDENT_FIELD_REF_FIELD = "StudentRef";
+        public const string SCHOOL_YEAR_REF_FIELD = "SchoolYearRef";
+        public const string ENROLLMENT_STATUS_FIELD = "EnrollmentStatus";
+        
+        [PrimaryKeyFieldAttr]
+        public int SchoolYearRef { get; set; }
+        public int GradeLevelRef { get; set; }
+        [PrimaryKeyFieldAttr]
+        public int StudentRef { get; set; }
+        public StudentEnrollmentStatusEnum EnrollmentStatus { get; set; }
+        [NotDbFieldAttr]
+        public GradeLevel GradeLevel { get; set; }
+       
+        public bool IsEnrolled
+        {
+            get { return EnrollmentStatus == StudentEnrollmentStatusEnum.CurrentlyEnrolled; }
+        }
+    }
+
+    public enum StudentEnrollmentStatusEnum
+    {
+        CurrentlyEnrolled = 0,
+        PreviouslyEnrolled = 1
+    }
+}
---
+++ Chalkable.Web/Tools/CompilerHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Configuration;
+
+namespace Chalkable.Web.Tools
+{
+    public class CompilerHelper
+    {
+        public static bool IsDebug
+        {
+            get
+            {
+                var res = false;
+#if DEBUG
+                res = true;
+#endif
+                return res;
+            }
+        }
+
+        public static string ScriptsRoot
+        {
+            get
+            {
+                var res = ConfigurationManager.AppSettings["ScriptsRoot"];
+                var includeVersion = bool.Parse(ConfigurationManager.AppSettings["ScriptsRootIncludeVersion"]);
+                if (includeVersion)
+                    res += Version;
+                return res;
+            }
+        }
+        
+        private const string VERSION = "private-build";
+        public static string Version
+        {
+            get
+            {
+                return VERSION.Replace('.', '-');    
+            }
+        }
+
+    }
+}
