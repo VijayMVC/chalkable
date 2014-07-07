9057bd048ebca1f0ea258e6ccae363a1a5dd138c
Ievgenii Zasoba <geka.zasoba@gmail.com>
2014-07-07 17:14:40 +0300

    rearranged insertion in School and Master DB for persons

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/Storage/DemoGradingScaleStorage.cs
@@ -0,0 +1,11 @@
+﻿using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool.Storage
+{
+    public class DemoGradingScaleStorage: BaseDemoIntStorage<GradingScale>
+    {
+        public DemoGradingScaleStorage(DemoStorage storage) : base(storage, x => x.Id)
+        {
+        } 
+    }
+}
