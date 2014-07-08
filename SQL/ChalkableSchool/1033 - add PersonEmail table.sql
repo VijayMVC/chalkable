6ef2808f96450b59c3b7d981e776741cf284be8b
Ievgenii Zasoba <geka.zasoba@gmail.com>
2014-07-09 02:49:57 +0300

    Fixed double sync

--- SQL/ChalkableSchool/1033 - add PersonEmail table.sql
+++ SQL/ChalkableSchool/1033 - add PersonEmail table.sql
@@ -11,3 +11,12 @@ go
 ALTER TABLE PersonEmail
 ADD CONSTRAINT PK_PersonRef_EmailAddress PRIMARY KEY (PersonRef, EmailAddress)
 go
+
+CREATE TYPE [dbo].[TPersonEmail] AS TABLE (
+	PersonRef int not null,
+	EmailAddress nvarchar(128) not null,
+	[Description] nvarchar(max) not null,
+	IsListed bit,
+	IsPrimary bit
+)
+go
