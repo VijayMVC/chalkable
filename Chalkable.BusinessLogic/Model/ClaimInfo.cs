9cc291a5e1738250270f47f381be12e92d352016
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-07-10 06:09:29 -0600

    fixed vwPerson

---
+++ Chalkable.BusinessLogic/Model/ClaimInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.StiConnector.Connectors.Model;
+
+namespace Chalkable.BusinessLogic.Model
+{
+    public class ClaimInfo
+    {
+        public const string VIEW_LOOKUP = "View Lookup";
+        public const string VIEW_CLASSROOM = "View Classroom";
+        public const string VIEW_CLASSROOM_ADMIN = "View Classroom (Admin)";
+
+        public string Type { get; set; }
+        public IEnumerable<string> Values { get; set; }
+
+
+        public static IList<ClaimInfo> Create(IList<Claim> claims)
+        {
+            return claims.Select(claim => new ClaimInfo
+                {
+                    Type = claim.Type,
+                    Values = claim.Values
+                }).ToList();
+        } 
+
+        public static bool HasPermission(IList<ClaimInfo> claimInfos, IList<string> claimsValues)
+        {
+            return claimInfos.Any(claim => claimsValues.All(value => claim.Values.Contains(value)));
+        }
+    }
+}
