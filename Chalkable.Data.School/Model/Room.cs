9c84fd726374693b6ca7d1c993577e6ce31f1cab
Andrew Machnew <amachnew@emp-game.com>
2014-07-14 16:54:02 +0300

    fixed demo student grades

---
+++ Chalkable.Data.School/Model/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.Common;
+
+namespace Chalkable.Data.School.Model
+{
+    public class Room
+    {
+        [PrimaryKeyFieldAttr]
+        public int Id { get; set; }
+        public string RoomNumber { get; set; }
+        public string Description { get; set; }
+        public string Size { get; set; }
+        public int? Capacity { get; set; }
+        public string PhoneNumber { get; set; }
+        public int SchoolRef { get; set; }
+    }
+}
