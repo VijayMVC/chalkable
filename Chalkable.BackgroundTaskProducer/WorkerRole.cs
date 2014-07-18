2cad34a4abe80f4934390177085ca99b836c0a9d
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-07-18 02:56:18 -0600

    fixed progress report comments

---
+++ Chalkable.BackgroundTaskProducer/WorkerRole.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using Chalkable.BackgroundTaskProducer.Producers;
+using Chalkable.Data.Master.Model;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.Diagnostics.Management;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace Chalkable.BackgroundTaskProducer
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        public override void Run()
+        {
+            Trace.WriteLine("BackgroundTaskProducer entry point called", "Information");
+            var cp = new CompositeProducer();
+            //cp.AddProducer("Process Reminders Producer", new AllDistrictsProducer("ProcessReminderProducer", BackgroundTaskTypeEnum.ProcessReminder));
+            //cp.AddProducer("Attendance Notification Producer", new AllDistrictsProducer("AttendanceNotificationProducer", BackgroundTaskTypeEnum.AttendanceNotification));
+            //cp.AddProducer("Teacher Attendance Notification Producer", new AllDistrictsProducer("TeacherAttendanceNotification", BackgroundTaskTypeEnum.TeacherAttendanceNotification));
+            cp.AddProducer("Sis Import Producer", new AllDistrictsProducer("SisImportProducer", BackgroundTaskTypeEnum.SisDataImport));
+            
+            while (true)
+            {
+                try
+                {
+                    Thread.Sleep(20000);
+                    Trace.TraceInformation("Producing cycle is starting");
+                    cp.Produce();
+                    Trace.TraceInformation("Producing cycle is ended");
+                }
+                catch (Exception ex)
+                {
+                    while (ex != null)
+                    {
+                        Trace.TraceInformation("Exception during task producing");
+                        Trace.TraceInformation(ex.Message);
+                        Trace.TraceInformation(ex.StackTrace);
+                        ex = ex.InnerException;
+                    }
+                    
+                }
+                
+            }
+        }
+
+        public override bool OnStart()
+        {
+            // Set the maximum number of concurrent connections 
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // For information on handling configuration changes
+            // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+            ConfigureDiagnostics();
+            return base.OnStart();
+        }
+
+        private void ConfigureDiagnostics()
+        {
+            String wadConnectionString = "Microsoft.WindowsAzure.Plugins.Diagnostics.ConnectionString";
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue(wadConnectionString));
+
+            RoleInstanceDiagnosticManager roleInstanceDiagnosticManager =
+                cloudStorageAccount.CreateRoleInstanceDiagnosticManager(RoleEnvironment.DeploymentId, RoleEnvironment.CurrentRoleInstance.Role.Name, RoleEnvironment.CurrentRoleInstance.Id);
+            DiagnosticMonitorConfiguration diagnosticMonitorConfiguration = roleInstanceDiagnosticManager.GetCurrentConfiguration();
+            diagnosticMonitorConfiguration.Directories.ScheduledTransferPeriod = TimeSpan.FromMinutes(5d);
+            diagnosticMonitorConfiguration.Logs.ScheduledTransferPeriod = TimeSpan.FromSeconds(30);
+            diagnosticMonitorConfiguration.Logs.ScheduledTransferLogLevelFilter = LogLevel.Verbose;
+            diagnosticMonitorConfiguration.WindowsEventLog.DataSources.Add("Application!*");
+            diagnosticMonitorConfiguration.WindowsEventLog.DataSources.Add("System!*");
+            diagnosticMonitorConfiguration.WindowsEventLog.ScheduledTransferPeriod = TimeSpan.FromMinutes(5d);
+            PerformanceCounterConfiguration performanceCounterConfiguration = new PerformanceCounterConfiguration();
+            performanceCounterConfiguration.CounterSpecifier = @"\Processor(_Total)\% Processor Time";
+            performanceCounterConfiguration.SampleRate = TimeSpan.FromSeconds(10d);
+            diagnosticMonitorConfiguration.PerformanceCounters.DataSources.Add(performanceCounterConfiguration);
+            diagnosticMonitorConfiguration.PerformanceCounters.ScheduledTransferPeriod = TimeSpan.FromMinutes(1d);
+            roleInstanceDiagnosticManager.SetCurrentConfiguration(diagnosticMonitorConfiguration);
+        }
+    }
+}
---
+++ Chalkable.BusinessLogic/Services/DemoSchool/Storage/DemoDateStorage.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Chalkable.BusinessLogic.Services.DemoSchool.Common;
+using Chalkable.Data.School.DataAccess;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool.Storage
+{
+    public class DemoDateStorage:BaseDemoIntStorage<Date>
+    {
+        public DemoDateStorage(DemoStorage storage) : base(storage, null, true)
+        {
+        }
+
+        public Date GetDateOrNull(DateQuery dateQuery)
+        {
+            return GetDatesFiltered(dateQuery).FirstOrDefault();
+        }
+
+        public DateTime GetDbDateTime()
+        {
+            return DateTime.Now;
+        }
+
+        public IList<Date> GetDates(DateQuery dateQuery)
+        {
+            return GetDatesFiltered(dateQuery);
+        }
+
+        public void Delete(DateQuery query)
+        {
+            var dates = GetDatesFiltered(query);
+
+
+            foreach (var date in dates)
+            {
+                var item = data.First(x => x.Value == date);
+                data.Remove(item.Key);
+            }
+        }
+
+        private List<Date> GetDatesFiltered(DateQuery query)
+        {
+            var dates = data.Select(x => x.Value);
+
+            if (query.SchoolYearId.HasValue)
+                dates = dates.Where(x => x.SchoolYearRef == query.SchoolYearId);
+            if (query.FromDate.HasValue)
+                dates = dates.Where(x => x.Day >= query.FromDate);
+            if (query.ToDate.HasValue)
+                dates = dates.Where(x => x.Day <= query.ToDate);
+            if (query.SchoolDaysOnly)
+                dates = dates.Where(x => x.IsSchoolDay);
+
+
+            if (query.MarkingPeriodId.HasValue)
+            {
+
+                var mp = Storage.MarkingPeriodStorage.GetById(query.MarkingPeriodId.Value);
+                dates = dates.Where(x => mp.StartDate <= x.Day && mp.EndDate >= x.Day);
+            }
+
+            if (query.DayType.HasValue)
+                dates = dates.Where(x => x.DayType.Id == query.DayType);
+
+            return dates.ToList();
+        }
+
+        private void AddMonth(int year, int month)
+        {
+            var daysCount = DateTime.DaysInMonth(year, month);
+
+            for (var i = 0; i < daysCount; ++i)
+            {
+                var typeRef = 0;
+
+                var day = new DateTime(year, month, i + 1);
+
+                if (day.DayOfWeek == DayOfWeek.Thursday || day.DayOfWeek == DayOfWeek.Tuesday)
+                    typeRef = 20;
+                if (day.DayOfWeek == DayOfWeek.Monday)
+                    typeRef = 19;
+                if (day.DayOfWeek == DayOfWeek.Wednesday || day.DayOfWeek == DayOfWeek.Friday)
+                    typeRef = 21;
+                data.Add(GetNextFreeId(), new Date
+                {
+                    Day = day,
+                    SchoolYearRef = DemoSchoolConstants.CurrentSchoolYearId,
+                    SchoolRef = DemoSchoolConstants.SchoolId,
+                    IsSchoolDay = day.DayOfWeek != DayOfWeek.Thursday && day.DayOfWeek != DayOfWeek.Friday,
+                    DayTypeRef = typeRef
+                });
+            }
+        }
+
+        public void AddDates()
+        {
+            var currentYear = DateTime.Now.Year;
+            for(var i = 1; i <= 12; ++i)
+                AddMonth(currentYear, i);
+        }
+    }
+}
