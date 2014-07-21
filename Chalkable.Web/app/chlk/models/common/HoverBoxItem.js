5a91ab2ab37c245d77f8e31358b5a2d8b4e2d727
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-07-21 06:07:55 -0600

    fixed class discipline page

---
+++ Chalkable.Web/app/chlk/models/common/HoverBoxItem.js
@@ -0,0 +1,11 @@
+NAMESPACE('chlk.models.common', function () {
+    "use strict";
+
+    /** @class chlk.models.common.HoverBoxItem*/
+    CLASS(
+        'HoverBoxItem', [
+            String, 'summary',
+
+            Number, 'total'
+        ]);
+});
