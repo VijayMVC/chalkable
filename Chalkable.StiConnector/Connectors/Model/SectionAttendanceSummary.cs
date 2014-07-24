8ce414935feb73fd106588312d82ac779cba731f
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-07-24 17:01:15 +0300

    fixed for CHLK-2829 Teacher. Teacher will see spinner picture in an item, that was added by a student

---
+++ Chalkable.StiConnector/Connectors/Model/SectionAttendanceSummary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Chalkable.StiConnector.Connectors.Model
+{
+    public class SectionAttendanceSummary
+    {
+        public IEnumerable<DailySectionAttendanceSummary> Days { get; set; }
+        public int SectionId { get; set; }
+        public IEnumerable<StudentSectionAttendanceSummary> Students { get; set; }
+    }
+
+    public class DailySectionAttendanceSummary
+    {
+        public decimal Absences { get; set; }
+        public DateTime Date { get; set; }
+        public int SectionId { get; set; }
+        public int Tardies { get; set; }
+    }
+
+    public class StudentSectionAttendanceSummary
+    {
+        public decimal Absences { get; set; }
+        public int SectionId { get; set; }
+        public int StudentId { get; set; }
+        public int Tardies { get; set; }
+    }
+}
---
+++ Chalkable.Web/Models/ClassesViewData/ClassScheduleViewData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Chalkable.Data.School.Model;
+using Chalkable.Web.Models.CalendarsViewData;
+
+namespace Chalkable.Web.Models.ClassesViewData
+{
+    public class ClassScheduleViewData
+    {
+        public ClassViewData Class { get; set; }
+        public IList<AnnouncementDayCalendarViewData> Schedule { get; set; }
+        private ClassScheduleViewData() { }
+
+
+        public static ClassScheduleViewData Create(ClassDetails classDetails, IList<AnnouncementDayCalendarViewData> schedule)
+        {
+            var res = new ClassScheduleViewData
+            {
+                Class = ClassViewData.Create(classDetails),
+                Schedule = schedule
+            };
+            return res;
+        }
+    }
+}
