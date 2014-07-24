e1ea1df9fc495536e1ccecb2147f0112ddfdbcca
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-07-24 03:46:34 -0600

    fixed GetPersons

--- Chalkable.Web/Logic/PersonLogic.cs
+++ Chalkable.Web/Logic/PersonLogic.cs
@@ -23,8 +23,13 @@ namespace Chalkable.Web.Logic
                     RoleId = string.IsNullOrEmpty(roleName) ? default(int?) : CoreRoles.GetByName(roleName).Id,
                     Start = start ?? 0,
                     Count = count ?? DEFAULT_COUNT,
-                    SortType = byLastName.HasValue && byLastName.Value ? SortTypeEnum.ByLastName : SortTypeEnum.ByFirstName
+                    SortType = byLastName.HasValue && byLastName.Value ? SortTypeEnum.ByLastName : SortTypeEnum.ByFirstName,
                 };
+            if (classId.HasValue)
+            {
+                var classRoomOption = locator.ClassroomOptionService.GetClassOption(classId.Value);
+                query.IsEnrolled = classRoomOption != null && !classRoomOption.IncludeWithdrawnStudents ? true : default(bool?);
+            }
             var res = locator.PersonService.GetPaginatedPersons(query);
             return res.Transform(PersonViewData.Create);
         }
