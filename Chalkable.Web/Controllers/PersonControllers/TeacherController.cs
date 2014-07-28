0ea53a3bdea27b4a2a23d1fce718256be6284afd
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-07-28 08:33:23 -0600

    fixed CHLK-2810 Teacher. When teacher puts a grade for an item with a standard,                         this grade won't be on the "Standards" tab (in Inow),                         untill teacher does "Edit"-> "Save" (in chalkable)

---
+++ Chalkable.Web/Controllers/PersonControllers/TeacherController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web.Mvc;
+using Chalkable.Common;
+using Chalkable.Data.Common.Enums;
+using Chalkable.Data.Master.Model;
+using Chalkable.Web.ActionFilters;
+using Chalkable.Web.Logic;
+using Chalkable.Web.Models.PersonViewDatas;
+
+namespace Chalkable.Web.Controllers.PersonControllers
+{
+    [RequireHttps, TraceControllerFilter]
+    public class TeacherController : PersonController
+    {
+        [AuthorizationFilter("AdminGrade, AdminEdit, AdminView, Teacher, Student", Preference.API_DESCR_TEACHER_SUMMARY, true, CallType.Get, new[] { AppPermissionType.User })]
+        public ActionResult Summary(int personId)
+        {
+            var teacher = SchoolLocator.PersonService.GetPerson(personId);
+            var room = SchoolLocator.RoomService.WhereIsPerson(personId, SchoolLocator.Context.NowSchoolTime);
+            return Json(TeacherSummaryViewData.Create(teacher, room));
+        }
+
+        [AuthorizationFilter("AdminGrade, AdminEdit, AdminView, Teacher, Student")]
+        public ActionResult Info(int personId)
+        {
+            var res = GetInfo(personId, TeacherInfoViewData.Create);
+            return Json(res);
+        }
+
+        /*[AuthorizationFilter("AdminGrade, AdminEdit, Teacher")]
+        public ActionResult UpdateInfo(AdminTeacherInputModel model)
+        {
+            var teacher = UpdateTeacherOrAdmin(model);
+            //MixPanelService.ChangedEmail(SchoolLocator.Context., email);
+            return Json(GetInfo(teacher.Id, TeacherInfoViewData.Create));
+        }*/
+
+
+        [AuthorizationFilter("AdminGrade, AdminEdit, AdminView, Teacher, Student")]
+        public ActionResult GetTeachers(string filter, int? start, int? count, int? classId, bool? byLastName, bool? onlyMyTeachers)
+        {
+            var role = CoreRoles.TEACHER_ROLE.Name;
+            return Json(PersonLogic.GetPersons(SchoolLocator, start, count, byLastName, filter, role, classId, null, null, onlyMyTeachers));
+        }
+    }
+}
