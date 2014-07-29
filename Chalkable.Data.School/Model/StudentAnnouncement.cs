f38a3b146e253ed322fe9fb27f120e1e78cd1d32
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-07-29 03:18:17 -0600

    added to AnnouncementComplex DepartmentId

---
+++ Chalkable.Data.School/Model/StudentAnnouncement.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Chalkable.Data.Common;
+
+namespace Chalkable.Data.School.Model
+{
+
+    public enum StudentAnnouncementStateEnum
+    {
+        Auto = 0,
+        None = 1,
+        Manual = 2
+    }
+
+    public class StudentAnnouncement
+    {
+        public int AnnouncementId { get; set; }
+        public string AnnouncementTitle { get; set; }
+        public int ActivityId { get; set; }
+        public int StudentId { get; set; }
+        public string Comment { get; set; }
+        public string ExtraCredit { get; set; }
+        public bool Dropped { get; set; }
+        
+        public decimal? NumericScore { get; set; }
+        public string ScoreValue { get; set; }
+        public int? AlternateScoreId { get; set; }
+        public int? AlphaGradeId { get; set; }
+        public bool Exempt { get; set; }
+        public bool Incomplete { get; set; }
+        public bool Late { get; set; }
+        public string AbsenceCategory { get; set; }
+        public bool Absent { get; set; }
+        public bool Withdrawn { get; set; }
+        public bool OverMaxScore { get; set; }
+
+        public bool IsUnexcusedAbsent
+        {
+            get { return Absent && !string.IsNullOrEmpty(AbsenceCategory) && AbsenceCategory.ToLower() == "u"; }
+        }
+        
+        //TODO : remove this later
+        public StudentAnnouncementStateEnum State { get { return StudentAnnouncementStateEnum.Manual; } }
+
+        public bool IsGraded
+        {
+            get { return !string.IsNullOrEmpty(ScoreValue) || Late || Incomplete || Exempt || Dropped; }
+        }
+    }
+
+    public class StudentAnnouncementDetails : StudentAnnouncement
+    {
+        public int ClassId { get; set; }
+        public Person Student { get; set; }
+    }
+
+    public class StudentAnnouncementGrade : StudentAnnouncement
+    {
+        private AnnouncementComplex announcement;
+
+        public AnnouncementComplex Announcement
+        {
+            get { return announcement; }
+            set
+            {
+                announcement = value;
+                if (announcement != null)
+                    AnnouncementId = announcement.Id;
+            }
+        }
+    }
+}
