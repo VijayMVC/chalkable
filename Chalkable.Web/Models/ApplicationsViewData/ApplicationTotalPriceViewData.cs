d75a771b7d6d915af977e0dc23b9a7e94b1a8b50
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-07-29 08:28:14 -0600

    fixed CHLK-2877 Seating Chart: change button text

---
+++ Chalkable.Web/Models/ApplicationsViewData/ApplicationTotalPriceViewData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Chalkable.BusinessLogic.Model;
+using Chalkable.Data.Master.Model;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Web.Models.ApplicationsViewData
+{
+    public class ApplicationTotalPriceViewData : ApplicationPriceViewData
+    {
+        public decimal TotalPrice { get; set; }
+        public int TotalPersonsCount { get; set; }
+
+        private ApplicationTotalPriceViewData(Application application, ApplicationTotalPriceInfo priceInfo): base(application)
+        {
+            TotalPrice = priceInfo.TotalPrice;
+            TotalPersonsCount = priceInfo.TotalCount;
+                // priceInfo.ApplicationInstallCountInfo.FirstOrDefault(x => x.Type == PersonsFroAppInstallTypeEnum.Total).Count.Value;
+        }
+        public static ApplicationTotalPriceViewData Create(Application application, ApplicationTotalPriceInfo priceInfo)
+        {
+            return new ApplicationTotalPriceViewData(application, priceInfo);
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/templates/classes/ClassInfoTpl.js
@@ -0,0 +1,27 @@
+REQUIRE('chlk.templates.profile.ClassProfileTpl');
+REQUIRE('chlk.models.classes.ClassProfileInfoViewData');
+
+NAMESPACE('chlk.templates.classes', function () {
+
+    /** @class chlk.templates.classes.ClassInfoTpl*/
+    CLASS(
+        [ria.templates.TemplateBind('~/assets/jade/activities/class/ClassInfo.jade')],
+        [ria.templates.ModelBind(chlk.models.classes.ClassProfileInfoViewData)],
+        'ClassInfoTpl', EXTENDS(chlk.templates.profile.ClassProfileTpl), [
+
+            chlk.models.classes.ClassInfo, function getClassInfo(){return this.getClazz();},
+
+            Object, function prepareDetailedInfo(){
+                var model = this.getClassInfo();
+                var res = [
+                    {title: 'TEACHER', value: model.getTeacher().getDisplayName()}
+                ];
+                if(model.getRoom())
+                    res.push({title: 'ROOM NUMBER', value: model.getRoom().getRoomNumber()});
+                if(model.getDepartment())
+                    res.push({title: 'DEPARTMENT', value: model.getDepartment().getName()});
+                res.push({title: 'GRADE', value: model.getGradeLevel().getFullText()});
+                return res;
+            }
+        ])
+});
