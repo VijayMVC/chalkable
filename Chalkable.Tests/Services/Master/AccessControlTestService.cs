9a48acda7da6b20be21824eaef5a746f94d3582d
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-07-30 06:54:15 -0600

    fixed CHLK-2887 Week Calendar sidebar - add class name in tooltip

---
+++ Chalkable.Tests/Services/Master/AccessControlTestService.cs
@@ -0,0 +1,40 @@
+﻿using Chalkable.BusinessLogic.Services.Master;
+using WindowsAzure.Acs.Oauth2;
+
+namespace Chalkable.Tests.Services.Master
+{
+    public class AccessControlTestService : MasterServiceBase, IAccessControlService
+    {
+        public AccessControlTestService(IServiceLocatorMaster serviceLocator) : base(serviceLocator)
+        {
+        }
+
+
+        public string GetAccessToken(string accessTokenUrl, string redirectUrl, string clientId, string clientSecret, string userName,
+                                     int? schoolYear, string scope)
+        {
+            return null;
+        }
+
+        public string GetAuthorizationCode(string clientId, string userName, int? schoolYear, string scope = null)
+        {
+            return null;
+        }
+
+        public ApplicationRegistration GetApplication(string clientId)
+        {
+            return null;
+        }
+
+        public bool RegisterApplication(string clientId, string appSecretKey, string appUrl, string appName)
+        {
+            return true;
+        }
+
+        public void RemoveApplication(string clientId)
+        {
+            return;
+        }
+
+    }
+}
