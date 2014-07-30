195655205aa7ffde80b152bf37565c623f071185
Andrew Machnew <amachnew@emp-game.com>
2014-07-30 15:11:03 +0300

    fixed for CHLK-2843

---
+++ Chalkable.Data.School/Model/Address.cs
@@ -0,0 +1,28 @@
+﻿using Chalkable.Data.Common;
+
+namespace Chalkable.Data.School.Model
+{
+    public class Address
+    {
+        public const string ID_FIELD = "Id";
+        [PrimaryKeyFieldAttr]
+        public int Id { get; set; }
+        public string AddressNumber { get; set; }
+        public string StreetNumber { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public int? CountyId { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+    }
+
+    public enum AddressType
+    {
+        Home,
+        Work
+    }
+}
