918dff03bea7b97fe97529094ebbe352f1e945e8
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-08-11 08:52:26 -0600

    fixed CHLK-2980 Notifications: If recipient is teacher,  text should be "Byron Bowen" not "Mr. Bowen"

---
+++ Chalkable.Tests/Services/Master/EmailTestService.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.BusinessLogic.Model;
+using Chalkable.BusinessLogic.Services.Master;
+using Chalkable.Common;
+using Chalkable.Common.Exceptions;
+using Chalkable.Data.Master.Model;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Tests.Services.Master
+{
+    public class EmailTestService : MasterServiceBase, IEmailService
+    {
+
+        private IDictionary<EmailResultTypeEnum, EmailResultTestModel> emailResult = new Dictionary<EmailResultTypeEnum, EmailResultTestModel>();
+
+        public EmailTestService(IServiceLocatorMaster serviceLocator) : base(serviceLocator)
+        {
+        }
+
+        private Pair<string, string> PrepareEmailToSysAdmin()
+        {
+            var sysEMail = PreferenceService.GetTyped<EmailInfo>(Preference.SYSTEM_EMAIL);
+            var sysadminMail = ServiceLocator.UserService.GetSysAdmin();
+            return new Pair<string, string>(sysEMail.Email, sysadminMail.Login);
+        }
+        private void PreperinResult(EmailResultTypeEnum type, EmailResultTestModel result)
+        {
+            if (!emailResult.ContainsKey(type))
+                emailResult.Add(type, result);
+            emailResult[type] = result;
+        }
+
+        public EmailResultTestModel GetEmailResult(EmailResultTypeEnum typeResult)
+        {
+            if (!emailResult.ContainsKey(typeResult))
+                throw new ChalkableException();
+            return emailResult[typeResult];
+        }
+
+        public void SendInviteToPerson(Person person, string confirmationKey, string message, string messageTemplate)
+        {
+            return;
+        }
+
+        public void SendChangedEmailToPerson(Person person, string newEmail)
+        {
+            return;
+        }
+
+        public void SendResettedPasswordToPerson(Person person, string confirmationKey)
+        {
+            return;
+        }
+
+        public void SendResettedPasswordToDeveloper(Developer developer, string confirmationKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendNotificationToPerson(Person person, string message)
+        {
+            return;
+        }
+
+        public void SendMailToFriend(string fromMail, string toMail, string message, string subject = null)
+        {
+            return;
+        }
+
+        public void SendApplicationEmailToDeveloper(Application application)
+        {
+            return;
+        }
+
+        public void SendApplicationEmailToSysadmin(Application application)
+        {
+            return;
+        }
+    }
+
+
+    public class EmailResultTestModel
+    {
+        public const string DEFUAL_MESSAGE = "new mail was send";
+
+        public string FromPerson { get; set; }
+        public string ToPerson { get; set; }
+        public string Message { get; set; }
+
+        //public IDictionary<ImportTypeEnum, CsvContainer> ImportContainers { get; set; }
+        public EmailResultTestModel()
+        {
+            Message = DEFUAL_MESSAGE;
+        }
+    }
+
+    public enum EmailResultTypeEnum
+    {
+        SetupDataUploaded = 1,
+        RegistrationToSysAdmins,
+        SchoolRegistrationBegin,
+        SchoolSetupFirst,
+        SchoolSetupSecond,
+        InviteToPerson,
+        NotificationToPerson,
+        ActionLink,
+        ResettedPasswordToPerson,
+        PurchaseOrderConfirmationToPerson,
+        MailToFrined,
+        ApplicationEmailToSysAdmin,
+        ApplicationEmailToDeveloper
+    }
+}
---
+++ Chalkable.Web/Controllers/StaticContentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Mvc;
+using Chalkable.BusinessLogic.Services.Master.PictureServices;
+using Chalkable.Common.Exceptions;
+using Chalkable.Web.Tools;
+
+namespace Chalkable.Web.Controllers
+{
+    public class StaticContentController : ChalkableController
+    {
+        public ActionResult GetRootUrl()
+        {
+            #if DEBUG
+                return Json(new { url = Request.Url.GetLeftPart(UriPartial.Authority) + Url.Content("~/") });
+            #else
+                return Json(new {url = CompilerHelper.ScriptsRoot});
+#endif
+        }
+
+        public ActionResult GetPictureRootUrl()
+        {
+            if (Context == null || Context.DistrictId == null)
+                throw new ChalkableException("User is not authorized");
+            return Json(new {url = PictureService.GetPicturesRelativeAddress() + Context.DistrictId});
+        }
+    }
+}
