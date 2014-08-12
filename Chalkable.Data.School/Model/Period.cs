a996a5414a77bc253646cb9017dde458e750d202
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-08-12 15:44:26 +0300

    fixed for CHLK-2709 Grid: Update Total Points column dynamically

---
+++ Chalkable.Data.School/Model/Period.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.Common;
+
+namespace Chalkable.Data.School.Model
+{
+    public class Period
+    {
+        public const string ID_FIELD = "Id";
+        public const string START_TIME_FIELD = "StartTime";
+        public const string END_TIME_FIELD = "EndTime";
+        public const string SCHOOL_YEAR_REF = "SchoolYearRef";
+        public const string ORDER_FIELD = "Order";
+        [PrimaryKeyFieldAttr]
+        public int Id { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+        public int SchoolYearRef { get; set; }
+        public int Order { get; set; }
+        public int SchoolRef { get; set; }
+    }
+}
---
+++ Chalkable.Tests/Services/School/BaseSchoolServiceTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.BusinessLogic.Services;
+using Chalkable.BusinessLogic.Services.Master;
+using Chalkable.BusinessLogic.Services.School;
+using Chalkable.Common;
+using Chalkable.Data.Common;
+using Chalkable.Data.Master.DataAccess;
+using Chalkable.Data.Master.Model;
+using Chalkable.Tests.Services.Master;
+using Chalkable.Tests.Services.TestContext;
+using NUnit.Framework;
+
+namespace Chalkable.Tests.Services.School
+{
+    public partial class BaseSchoolServiceTest : ServiceTestBase
+    {
+
+        [SetUp]
+        public void SetUp()
+        {
+            CreateMasterDb();
+            InitBaseData();
+        }
+
+        public DistrictTestContext DistrictTestContext { get; private set; }
+        public SchoolTestContext FirstSchoolContext { get { return DistrictTestContext.FirstSchoolContext; } }
+        public SchoolTestContext SecondSchoolContext { get { return DistrictTestContext.SecondSchoolContext; } }
+
+        public IServiceLocatorSchool SysAdminFirstSchoolLocator{ get; private set; }
+        public IServiceLocatorSchool SysAdminSecondSchoolLocator { get; private set; }
+        public IServiceLocatorMaster SysAdminMasterLocator
+        {
+            get { return SysAdminFirstSchoolLocator.ServiceLocatorMaster; }
+        }
+        
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        protected void InitBaseData()
+        {
+            DistrictTestContext = CreateDistrictTestContext();
+            var sysLocator = ServiceLocatorFactory.CreateMasterSysAdmin();
+            SysAdminFirstSchoolLocator = CreateSysAdminSchoolLocator(sysLocator, DistrictTestContext.FirstSchoolContext);
+            SysAdminSecondSchoolLocator = CreateSysAdminSchoolLocator(sysLocator, DistrictTestContext.SecondSchoolContext);
+        }
+        
+        private IServiceLocatorSchool CreateSysAdminSchoolLocator(IServiceLocatorMaster sysLocator, SchoolTestContext schoolContext)
+        {
+            var context = sysLocator.SchoolServiceLocator(schoolContext.School.Id).Context;
+            return new BaseSchoolServiceLocatorTest(new BaseMasterServiceLocatorTest(context));
+        }
+    }
+}
