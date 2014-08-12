eadac112c0c461cc283744f2a343bcc0a35c0a48
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-08-12 13:01:12 +0300

    fixed for CHLK-3001 Search: remove query from field after action is taken

---
+++ Chalkable.Tests/Common/SettingsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Common;
+using NUnit.Framework;
+
+namespace Chalkable.Tests.Common
+{
+    [TestFixture]
+    public class SettingsTest
+    {
+        [Test]
+        public void Test()
+        {
+            Debug.WriteLine(Settings.MasterConnectionString);
+            Debug.WriteLine(Settings.SchoolConnectionStringTemplate);
+            Debug.WriteLine(Settings.Configuration.SchoolTemplateDataBase);
+            Debug.WriteLine(Settings.Configuration.Servers.AllKeys.JoinString(",")); 
+        }
+
+        [Test]
+        public void PasswordTest()
+        {
+            string password = "5MQVuZf$xbKbn7NYsG78";
+            var bytes = Encoding.UTF8.GetBytes(password);
+            var hash = new MD5CryptoServiceProvider().ComputeHash(bytes);
+            var b64 = Convert.ToBase64String(hash);
+            Debug.WriteLine(b64);
+        }
+    }
+}
---
+++ Chalkable.Web/Models/AlternateScoreViewData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Web.Models
+{
+    public class AlternateScoreViewData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+   
+        public static AlternateScoreViewData Create(AlternateScore alternateScore)
+        {
+            return new AlternateScoreViewData
+                {
+                    Id = alternateScore.Id,
+                    Name = alternateScore.Name,
+                    Description = alternateScore.Description
+                };
+        }
+        public static IList<AlternateScoreViewData> Create(IList<AlternateScore> alternateScores)
+        {
+            return alternateScores.Select(Create).ToList();
+        } 
+    }
+}
