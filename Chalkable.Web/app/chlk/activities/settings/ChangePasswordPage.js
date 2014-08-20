3f40fe12388034bf204769206b0e9b6a8504e07a
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-08-20 08:35:09 -0600

    fixed password reset

--- Chalkable.Web/app/chlk/activities/settings/ChangePasswordPage.js
+++ Chalkable.Web/app/chlk/activities/settings/ChangePasswordPage.js
@@ -9,5 +9,14 @@ NAMESPACE('chlk.activities.settings', function () {
         [ria.mvc.DomAppendTo('#main')],
         [chlk.activities.lib.PageClass('profile')],
         [ria.mvc.TemplateBind(chlk.templates.settings.ChangePasswordTpl)],
-        'ChangePasswordPage', EXTENDS(chlk.activities.lib.TemplatePage), []);
+        'ChangePasswordPage', EXTENDS(chlk.activities.lib.TemplatePage), [
+
+
+            [ria.mvc.DomEventBind('click', '#changePasswordLink')],
+            [[ria.dom.Dom, ria.dom.Event]],
+            VOID, function submitClick(node, event){
+                node.addClass('x-hidden');
+                node.parent().parent().find('#changePasswordForm').removeClass('x-hidden');
+            }
+        ]);
 });
