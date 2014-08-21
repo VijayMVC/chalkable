2c6957c41f0b76e0c64cf26f0b5ed6b6d1709bc7
Andrew Machnew <amachnew@emp-game.com>
2014-08-21 18:14:34 +0300

    fixed CHLK-3077 Developer. App Info. When developer clicks "Download Template", the page will crash

---
+++ Chalkable.StiConnector/Connectors/ActivityAttachmentsConnector.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Chalkable.StiConnector.Connectors.Model;
+
+namespace Chalkable.StiConnector.Connectors
+{
+    public class ActivityAttachmentsConnector : ConnectorBase
+    {
+        private string urlFormat;
+        public ActivityAttachmentsConnector(ConnectorLocator locator) : base(locator)
+        {
+            urlFormat = BaseUrl + "Chalkable/activities/{0}/attachments";
+        }
+
+        public ActivityAttachment CreateActivityAttachments(int activityId, ActivityAttachment activityAttachment)
+        {
+            return Post(string.Format(urlFormat, activityId), activityAttachment);
+        }
+
+        public void Delete(int activityId, int id)
+        {
+            Delete(string.Format(urlFormat + "/{1}", activityId, id));
+        }
+
+        public void Update(int activityId, int id, ActivityAttachment activityAttachment)
+        {
+            Put(string.Format(urlFormat + "/{1}", activityId, id), activityAttachment);
+        }
+
+        public IList<ActivityAttachment> GetAttachments(int activityId)
+        {
+            return Call<IList<ActivityAttachment>>(string.Format(urlFormat, activityId));
+        } 
+       
+        public ActivityAttachment GetAttachment(int activityId, int id)
+        {
+            return Call<ActivityAttachment>(string.Format(urlFormat + "/{1}", activityId, id));
+        }
+    }
+}
