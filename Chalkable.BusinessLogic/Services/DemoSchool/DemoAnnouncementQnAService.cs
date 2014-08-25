5ed1e9d439b3ab5cd06b85390964ad7393593c49
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-08-25 09:55:22 -0600

    fixed StandardGrids

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/DemoAnnouncementQnAService.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Chalkable.BusinessLogic.Security;
+using Chalkable.BusinessLogic.Services.DemoSchool.Storage;
+using Chalkable.BusinessLogic.Services.School;
+using Chalkable.Common.Exceptions;
+using Chalkable.Data.School.DataAccess;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool
+{
+    public class DemoAnnouncementQnAService :DemoSchoolServiceBase, IAnnouncementQnAService
+    {
+        public DemoAnnouncementQnAService(IServiceLocatorSchool serviceLocator, DemoStorage storage) : base(serviceLocator ,storage)
+        {
+        }
+
+        public AnnouncementQnA AskQuestion(int announcementId, string question)
+        {
+            if (!(Context.UserLocalId.HasValue && Context.SchoolId.HasValue))
+                throw new UnassignedUserException();
+
+            var ann = ServiceLocator.AnnouncementService.GetAnnouncementById(announcementId);
+
+            var annQnA = new AnnouncementQnAComplex
+            {
+                AnnouncementRef = announcementId,
+                AskerRef = Context.UserLocalId.Value,
+                ClassRef = ann.ClassRef,
+                Question = question,
+                QuestionTime = Context.NowSchoolTime,
+                State = AnnouncementQnAState.Asked,
+                Asker = Storage.PersonStorage.GetById(Context.UserLocalId.Value),
+                Answerer = Storage.PersonStorage.GetById(ann.PrimaryTeacherRef)
+            };
+            Storage.AnnouncementQnAStorage.Add(annQnA);
+            annQnA = Storage.AnnouncementQnAStorage.GetAnnouncementQnA(new AnnouncementQnAQuery
+            {
+                AnnouncementId = announcementId,
+                CallerId = annQnA.AskerRef
+            }).AnnouncementQnAs.OrderByDescending(x => x.Id).First();
+            ServiceLocator.NotificationService.AddAnnouncementNotificationQnToAuthor(annQnA.Id, ann.Id);
+            return annQnA;
+        }
+
+        private bool CanEditAnswer(AnnouncementQnAComplex announcementQnA)
+        {
+            return BaseSecurity.IsSysAdmin(Context)
+                   || ((!announcementQnA.AnswererRef.HasValue && Storage.ClassTeacherStorage.Exists(announcementQnA.ClassRef, Context.SchoolLocalId.Value))
+                        || announcementQnA.AnswererRef == Context.UserLocalId);
+        }
+
+        public AnnouncementQnA Answer(int announcementQnAId, string question, string answer)
+        {
+            var annQnA = GetAnnouncementQnA(announcementQnAId);
+            if (!CanEditAnswer(annQnA))
+                throw new ChalkableSecurityException();
+
+            annQnA.State = AnnouncementQnAState.Answered;
+            annQnA.Question = question;
+            annQnA.Answer = answer;
+            annQnA.AnsweredTime = Context.NowSchoolTime;
+            Storage.AnnouncementQnAStorage.Update(annQnA);
+            ServiceLocator.NotificationService.AddAnnouncementNotificationAnswerToPerson(annQnA.Id, annQnA.AnnouncementRef);
+            return annQnA;
+        }
+
+        public AnnouncementQnA EditAnswer(int announcementQnAId, string answer)
+        {
+            var annQnA = GetAnnouncementQnA(announcementQnAId);
+            if (!CanEditAnswer(annQnA))
+                throw new ChalkableSecurityException();
+
+            annQnA.Answer = answer;
+            annQnA.AnsweredTime = Context.NowSchoolTime;
+            Storage.AnnouncementQnAStorage.Update(annQnA);
+            return annQnA;
+        }
+
+        public AnnouncementQnA EditQuestion(int announcementQnAId, string question)
+        {
+            var annQnA = GetAnnouncementQnA(announcementQnAId);
+            if (!CanEditAnswer(annQnA))
+                throw new ChalkableSecurityException();
+
+
+            annQnA.Question = question;
+            annQnA.QuestionTime = Context.NowSchoolTime;
+            Storage.AnnouncementQnAStorage.Update(annQnA);
+            return annQnA;
+        }
+
+        public void Delete(int announcementQnAId)
+        {
+            var annQnA = GetAnnouncementQnA(announcementQnAId);
+            if (!CanEditAnswer(annQnA))
+                throw new ChalkableSecurityException();
+            Storage.AnnouncementQnAStorage.Delete(annQnA.Id);
+        }
+
+        public AnnouncementQnA MarkUnanswered(int announcementQnAId)
+        {
+            var annQnA = GetAnnouncementQnA(announcementQnAId);
+            if (!CanEditAnswer(annQnA))
+                throw new ChalkableSecurityException();
+
+            annQnA.State = AnnouncementQnAState.Unanswered;
+            Storage.AnnouncementQnAStorage.Update(annQnA);
+            return annQnA;
+        }
+
+        public AnnouncementQnAComplex GetAnnouncementQnA(int announcementQnAId)
+        {
+            return GetAnnouncmentQnAs(new AnnouncementQnAQuery {Id = announcementQnAId}).AnnouncementQnAs.First();
+        }
+
+        private AnnouncementQnAQueryResult GetAnnouncmentQnAs(AnnouncementQnAQuery query)
+        {
+            query.CallerId = Context.UserLocalId;
+            return Storage.AnnouncementQnAStorage.GetAnnouncementQnA(query);
+        }
+
+        public IList<AnnouncementQnAComplex> GetAnnouncementQnAs(int announcementId)
+        {
+            var query = new AnnouncementQnAQuery {AnnouncementId = announcementId};
+            return GetAnnouncmentQnAs(query).AnnouncementQnAs;
+        }
+    }
+}
---
+++ Chalkable.BusinessLogic/Services/DemoSchool/Storage/sti/DemoStiDisciplineStorage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Chalkable.BusinessLogic.Services.DemoSchool.Common;
+using Chalkable.StiConnector.Connectors.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool.Storage.sti
+{
+    public class DemoStiDisciplineStorage:BaseDemoIntStorage<DisciplineReferral>
+    {
+        public DemoStiDisciplineStorage(DemoStorage storage) : base(storage, x => x.Id, true)
+        {
+        }
+
+        public IList<DisciplineReferral> GetList(int classId, DateTime date)
+        {
+            return data.Where(x => x.Value.SectionId == classId && x.Value.Date == date).Select(x => x.Value).ToList();
+        }
+
+        public IList<DisciplineReferral> GetList(DateTime date)
+        {
+            return data.Where(x => x.Value.Date == date).Select(x => x.Value).ToList();
+        }
+
+        public DisciplineReferral Create(DisciplineReferral stiDiscipline)
+        {
+            Add(stiDiscipline);
+            return stiDiscipline;
+        }
+    }
+}
