3e80d732538def258997f035ec85906f3e4ae7c2
Andrew Machnew <amachnew@emp-game.com>
2014-08-25 18:34:14 +0300

    fixed CHLK-3076 Developer. App info page. Need to add placeholder text

---
+++ Chalkable.Web/app/chlk/services/BgTaskService.js
@@ -0,0 +1,40 @@
+REQUIRE('chlk.services.BaseService');
+REQUIRE('ria.async.Future');
+REQUIRE('chlk.models.bgtasks.BgTask');
+REQUIRE('chlk.models.bgtasks.BgTaskLog');
+REQUIRE('chlk.models.id.BgTaskId');
+
+
+
+
+NAMESPACE('chlk.services', function () {
+    "use strict";
+
+    /** @class chlk.services.BgTaskService*/
+    CLASS(
+        'BgTaskService', EXTENDS(chlk.services.BaseService), [
+            [[Number, Number]],
+            ria.async.Future, function getTasks(pageIndex_, state_) {
+                return this.getPaginatedList('BackgroundTask/GetTasks.json', chlk.models.bgtasks.BgTask, {
+                    start: pageIndex_,
+                    state: state_
+                });
+            },
+            [[chlk.models.id.BgTaskId, Number, Number]],
+            ria.async.Future, function getLogs(id, start_, count_) {
+                return this.getPaginatedList('BackgroundTask/GetTaskLogs.json', chlk.models.bgtasks.BgTaskLog, {
+                    taskId: id.valueOf(),
+                    start: start_,
+                    count: count_
+                });
+            },
+            [[chlk.models.id.BgTaskId]],
+            ria.async.Future, function deleteTask(id) {
+                return this.get('BackgroundTask/Delete.json', null
+                    , {
+                            taskId: id.valueOf()
+                      }
+                );
+            }
+        ])
+});
