e7b1370322558ad44d841a55d4f47fbaf3aad4ae
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-08-27 16:00:24 +0300

    fixed Announcement View

---
+++ Chalkable.BusinessLogic/Model/StudentCommentInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Model
+{
+    public class StudentCommentInfo
+    {
+        public Person Student { get; set; }
+        public string Comment { get; set; }
+    }
+}
---
+++ Chalkable.StiConnector/SyncModel/SpEdStatus.cs
@@ -0,0 +1,8 @@
+namespace Chalkable.StiConnector.SyncModel
+{
+    public class SpEdStatus
+    {
+        public int SpEdStatusID { get; set; }
+        public string Name { get; set; }
+    }
+}
