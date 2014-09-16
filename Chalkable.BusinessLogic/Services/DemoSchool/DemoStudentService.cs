8e9ea42efa223fb3410cfe01539e2e657cf47ce3
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-09-16 18:24:37 +0300

    updates for CHLK-3205 Teacher. Buttons are not highlighted

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/DemoStudentService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.BusinessLogic.Services.DemoSchool.Storage;
+using Chalkable.BusinessLogic.Services.School;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool
+{
+    public class DemoStudentService : DemoSchoolServiceBase, IStudentService
+    {
+        public DemoStudentService(IServiceLocatorSchool serviceLocator, DemoStorage demoStorage) : base(serviceLocator, demoStorage)
+        {
+        }
+
+        public void AddStudents(IList<Student> students)
+        {
+            Storage.DemoStudentStorage.Add(students);
+        }
+
+        public void EditStudents(IList<Student> students)
+        {
+            Storage.DemoStudentStorage.Update(students);
+        }
+
+        public void DeleteStudents(IList<Student> students)
+        {
+            Storage.DemoStudentStorage.Delete(students);
+        }
+
+        public void AddStudentSchools(IList<StudentSchool> studentSchools)
+        {
+            Storage.DemoStudentSchoolStorage.Add(studentSchools);
+        }
+
+        public void EditStudentSchools(IList<StudentSchool> studentSchools)
+        {
+            Storage.DemoStudentSchoolStorage.Update(studentSchools);
+        }
+
+        public void DeleteStudentSchools(IList<StudentSchool> studentSchools)
+        {
+            Storage.DemoStudentSchoolStorage.Delete(studentSchools);
+        }
+    }
+}
---
+++ Chalkable.BusinessLogic/Services/DemoSchool/Storage/DemoStudentSchoolStorage.cs
@@ -0,0 +1,11 @@
+﻿using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool.Storage
+{
+    public class DemoStudentSchoolStorage : BaseDemoIntStorage<StudentSchool>
+    {
+        public DemoStudentSchoolStorage(DemoStorage storage) : base(storage, null, true)
+        {
+        }
+    }
+}
