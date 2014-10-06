15a1197ee8d24b4f0d7df30f936c0f430db8eef2
Andrew Machnew <amachnew@emp-game.com>
2014-10-06 16:29:46 +0300

    fixed CHLK-3181 [55 148] Teacher Testing Item ?? - Progress report ? entering individual student comment should ?suggest? from the look up bank as you type

---
+++ Chalkable.BusinessLogic/Services/Master/ServiceLocatorMaster.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Chalkable.BusinessLogic.Services.Master.PictureServices;
+using Chalkable.BusinessLogic.Services.School;
+
+namespace Chalkable.BusinessLogic.Services.Master
+{
+    public interface IServiceLocatorMaster : IServiceLocator
+    {
+        IServiceLocatorSchool SchoolServiceLocator(Guid districtId, int? schoolLocalId);
+        UserContext Context { get; }
+        IUserService UserService { get; }
+        ISchoolService SchoolService { get; }
+        IDistrictService DistrictService { get; }
+        IBackgroundTaskService BackgroundTaskService { get; }
+        IPreferenceService PreferenceService { get; }
+        IChalkableDepartmentService ChalkableDepartmentService { get; }
+        IPersonPictureService PersonPictureService { get; }
+        IPictureService CourseIconService { get; }
+        IPictureService DepartmentIconService { get; }
+        IPictureService FundRequestPictureService { get; }
+        IPictureService ApplicationPictureService { get; }
+        IApplicationService ApplicationService { get; }
+        ICategoryService CategoryService { get; }
+        IApplicationUploadService ApplicationUploadService { get; }
+        IAccessControlService AccessControlService { get; }
+        IEmailService EmailService { get; }
+        IFundService FundService { get; }
+        IDeveloperService DeveloperService { get; }
+        IDbService DbService { get; }
+    }
+
+    public class ServiceLocatorMaster : ServiceLocator, IServiceLocatorMaster
+    {
+        private IUserService userService;
+        private ISchoolService schoolService;
+        private IDistrictService districtService;
+        private IBackgroundTaskService backgroundTaskService;
+        private IPreferenceService preferenceService;
+        private IChalkableDepartmentService chalkableDepartmentService;
+        private IPersonPictureService personPictureService;
+        private IPictureService courseIconService;
+        private IPictureService departmentIconService;
+        private IPictureService fundRequestPictureService;
+        private IPictureService applicationPictureService;
+        private IApplicationService applicationService;
+        private ICategoryService categoryService;
+        private IAccessControlService accessControlService;
+        private IApplicationUploadService applicationUploadService;
+        private IEmailService emailService;
+        private IFundService fundService;
+        private IDeveloperService developerService;
+        private IDbService dbService;
+
+        public ServiceLocatorMaster(UserContext context) : base(context)
+        {
+            userService = new UserService(this);
+            schoolService = new SchoolService(this);
+            backgroundTaskService = new BackgroundTaskService(this);
+            preferenceService = new PreferenceService(this);
+            chalkableDepartmentService = new ChalkableDepartmentService(this);
+            personPictureService = new PersonPictureService(this);
+            courseIconService = new CourseIconService(this);
+            departmentIconService = new DepartmentIconService(this);
+            districtService = new DistrictService(this);
+            applicationService = new ApplicationService(this);
+            categoryService = new CategoryService(this);
+            accessControlService = new AccessControlService(this);
+            applicationUploadService = new ApplicationUploadService(this);
+            emailService = new EmailService(this);
+            fundService = new FundService(this);
+            fundRequestPictureService = new FundRequestPictureService(this);
+            developerService = new DeveloperService(this);
+            applicationPictureService = new ApplicationPictureService(this);
+            dbService = new DbService(Context != null ? Context.MasterConnectionString : null);
+        }
+
+        public IUserService UserService { get { return userService; } }
+        public ISchoolService SchoolService { get { return schoolService; } }
+        public IDistrictService DistrictService { get { return districtService; } }
+        public IBackgroundTaskService BackgroundTaskService { get { return backgroundTaskService; } }
+        public IPreferenceService PreferenceService { get { return preferenceService; } }
+        public IChalkableDepartmentService ChalkableDepartmentService { get { return chalkableDepartmentService; } }
+        public IPersonPictureService PersonPictureService { get { return personPictureService; } }
+        public IPictureService CourseIconService { get { return courseIconService; } }
+        public IPictureService DepartmentIconService { get { return departmentIconService; } }
+        public IApplicationService ApplicationService { get { return applicationService; } }
+        public ICategoryService CategoryService { get { return categoryService; } }
+        public IApplicationUploadService ApplicationUploadService { get { return applicationUploadService; } }
+        public IAccessControlService AccessControlService
+        {
+            get { return accessControlService; }
+            protected set { accessControlService = value; }
+        }
+        public IEmailService EmailService
+        {
+            get { return emailService; } 
+            protected set { emailService = value; }
+        }
+
+        public IFundService FundService { get { return fundService; } }
+        public IDeveloperService DeveloperService { get { return developerService; } }
+        public IPictureService FundRequestPictureService { get { return fundRequestPictureService; } }
+        public IPictureService ApplicationPictureService { get { return applicationPictureService; } }
+
+        public virtual IServiceLocatorSchool SchoolServiceLocator(Guid districtId, int? schoolLocalId)
+        {
+            if (Context.DistrictId != districtId || Context.SchoolLocalId != schoolLocalId)
+            {
+                var district = DistrictService.GetByIdOrNull(districtId);
+                var developer = DeveloperService.GetDeveloperByDictrict(district.Id);
+                var developerId = developer != null ? developer.Id : (Guid?)null;
+                Context.SwitchSchool(district.Id, district.Name, district.TimeZone, schoolLocalId, district.ServerUrl, developerId);
+            }
+            var serviceLocator = new ServiceLocatorSchool(this);
+            return serviceLocator;
+        }
+
+        public IDbService DbService
+        {
+            get { return dbService; } 
+            protected set { dbService = value; }
+        }
+        
+    }
+}
---
+++ Chalkable.Web/app/chlk/activities/attendance/SummaryPage.js
@@ -0,0 +1,167 @@
+REQUIRE('chlk.activities.lib.TemplatePage');
+REQUIRE('chlk.templates.attendance.SummaryPage');
+
+NAMESPACE('chlk.activities.attendance', function () {
+
+    var interval;
+
+    /** @class chlk.activities.attendance.SummaryPage */
+    CLASS(
+        [ria.mvc.DomAppendTo('#main')],
+        [ria.mvc.TemplateBind(chlk.templates.attendance.SummaryPage)],
+        'SummaryPage', EXTENDS(chlk.activities.lib.TemplatePage), [
+            OVERRIDE, VOID, function onRender_(model){
+                BASE(model);
+                this.dom.addClass('refreshed');
+                var panelToShow = this.dom.find('.absent-late-page[data-page=1]');
+                this.startInterval(panelToShow);
+            },
+
+            [[ria.dom.Dom, Boolean]],
+            VOID, function changeLineOpacity(node, needOpacity){
+                var color = needOpacity ? 'rgba(193,193,193,0.5)' : node.getData('color');
+                var index = node.getData('index');
+                var chart = this.dom.find('.main-chart:visible').getData('chart');
+                chart.series[index].graph && chart.series[index].graph.attr({ stroke: color });
+                chart.series[index].update(this.getSerieConfigs_(!needOpacity, color));
+            },
+
+            [[Boolean, String]],
+            function getSerieConfigs_(enabled, color_){
+                return enabled ? {
+                    marker : {
+                        enabled: true,
+                        symbol: 'circle',
+                        radius: 3,
+                        fillColor: '#ffffff',
+                        lineWidth: 2,
+                        lineColor: color_,
+                        states: {
+                            hover: {
+                                radius: 6,
+                                lineWidth: 2,
+                                enabled: true
+                            }
+                        }
+                    },
+                    color: color_,
+                    zIndex: 10,
+                    enableMouseTracking: true
+                } : {
+                    marker : {
+                        enabled: false,
+                        states: {
+                            hover: {
+                                enabled: false
+                            }
+                        }
+                    },
+                    color: "#c1c1c1",
+                    zIndex: 1,
+                    enableMouseTracking: true
+                };
+            },
+
+            [ria.mvc.DomEventBind('mouseover mouseleave', '.legend-item')],
+            [[ria.dom.Dom, ria.dom.Event]],
+            VOID, function itemTypeHover(node, event){
+                if(!node.hasClass('hovered')){
+                    var needOpacity = event.type == 'mouseleave';
+                    this.changeLineOpacity(node, needOpacity);
+                }
+            },
+
+            [ria.mvc.DomEventBind('click', '.legend-item')],
+            [[ria.dom.Dom, ria.dom.Event]],
+            VOID, function itemTypeClick(node, event){
+                var wasHovered = node.hasClass('hovered');
+                this.dom.find('.legend-item.hovered').removeClass('hovered').removeClass('fixed-hovered');
+                if(!wasHovered)
+                    node.addClass('hovered').addClass('fixed-hovered');
+            },
+
+            [ria.mvc.DomEventBind('seriemouseover seriemouseleave', '.main-chart')],
+            [[ria.dom.Dom, ria.dom.Event, Object, Object]],
+            VOID, function chartHover(node, event, chart_, hEvent_){
+                var needOpacity = event.type == 'seriemouseleave';
+                var item = node.parent('.chart-container').find('.legend-item[data-index=' + chart_.index + ']:visible');
+                if(!item.hasClass('fixed-hovered')){
+                    if(needOpacity)
+                        item.removeClass('hovered');
+                    else
+                        item.addClass('hovered');
+                    this.changeLineOpacity(item, needOpacity);
+                }
+
+            },
+
+            function startInterval(panelToShow){
+                clearInterval(interval);
+                var grid = panelToShow.find('.students-container');
+                var pageHeight = document.documentElement.clientHeight;
+                var baseContentHeight = grid.height();
+                var scrollPosition;
+                var contentHeight = baseContentHeight + grid.offset().top;
+
+                interval = setInterval(function(grid){
+                    scrollPosition = window.pageYOffset;
+                    var hiddenBlock = panelToShow.find('.hidden-students-block.height-0:eq(0)');
+                    if(hiddenBlock.exists()){
+                        if((contentHeight - pageHeight - scrollPosition) < 400){
+                            var height = hiddenBlock.find('.hidden-students-block-2').height();
+                            var div = new ria.dom.Dom('<div class="horizontal-loader"></div>');
+                            grid.appendChild(div);
+                            setTimeout(function(div){
+                                div.remove();
+                                hiddenBlock.find('.horizontal-loader').remove();
+                                hiddenBlock
+                                    .setCss('height', height)
+                                    .removeClass('height-0');
+                            }.bind(this, div), 400);
+                            contentHeight += baseContentHeight;
+                        }
+                    }
+                    else{
+                        clearInterval(interval);
+                    }
+                }.bind(this, grid), 500);
+            },
+
+            [ria.mvc.DomEventBind('click', '.absent-late-button:not(.pressed)')],
+            [[ria.dom.Dom, ria.dom.Event]],
+            VOID, function buttonClick(node, event){
+                this.dom.find('.absent-late-button.pressed').removeClass('pressed');
+                node.addClass('pressed');
+                var page = node.getData('page');
+                var panelToShow = this.dom.find('.absent-late-page[data-page=' + page + ']');
+                panelToShow.show();
+                this.dom.find('.absent-late-page[data-page!=' + page + ']').hide();
+                this.dom.find('.hidden-students-block').setCss('height', '0').addClass('height-0');
+
+                this.startInterval(panelToShow);
+            },
+
+            [ria.mvc.DomEventBind('mouseleave', '.student-with-picture')],
+            [[ria.dom.Dom, ria.dom.Event]],
+            VOID, function studentHover1(node, event){
+                var popUp = node.find('.chlk-pop-up-container');
+                popUp.hide();
+            },
+
+            [ria.mvc.DomEventBind('mousemove', '.alerts')],
+            [[ria.dom.Dom, ria.dom.Event]],
+            VOID, function studentHover2(node, event){
+                var popUp = node.parent('.student-with-picture').find('.chlk-pop-up-container');
+                popUp.hide();
+            },
+
+            [ria.mvc.DomEventBind('mousemove', '.student-with-picture .avatar-wrapper>div')],
+            [[ria.dom.Dom, ria.dom.Event]],
+            VOID, function studentHover3(node, event){
+                var popUp = node.parent('.student-with-picture').find('.chlk-pop-up-container');
+                popUp.show();
+                var left = ((node.width() - popUp.width()) / 2) - 8 + 'px';
+                popUp.setCss('left', left);
+            }
+        ]);
+});
