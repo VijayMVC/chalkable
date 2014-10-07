43244dc71c29b048c99a594764a1aa0415350643
Ievgenii Zasoba <geka.zasoba@gmail.com>
2014-10-07 12:01:23 +0300

    Removed exception stack traces from release configuration

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/Models/AnnouncementComplete.cs
@@ -0,0 +1,9 @@
+﻿namespace Chalkable.BusinessLogic.Services.DemoSchool.Models
+{
+    public class AnnouncementComplete
+    {
+        public int AnnouncementId { get; set; }
+        public int UserId { get; set; }
+        public bool Complete { get; set; }
+    }
+}
---
+++ SQL/ChalkableSchool/1057 - Added indexes for performance purpose.sql
@@ -0,0 +1,8 @@
+Create Index IX_MarkingPeriodClass_Class on MarkingPeriodClass(ClassRef)
+GO
+Create Index IX_MarkingPeriodClass_MarkingPeriod on MarkingPeriodClass(MarkingPeriodRef)
+GO
+Create Index IX_ClassPeriod_Class on ClassPeriod(ClassRef)
+GO
+Create Index IX_ClassPeriod_Period on ClassPeriod(PeriodRef)
+GO
