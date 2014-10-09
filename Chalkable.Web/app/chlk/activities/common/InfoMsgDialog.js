d71ffa10b2d73e847d47900a037a8579182bd0d9
Ievgenii Zasoba <geka.zasoba@gmail.com>
2014-10-09 12:05:55 +0300

    removed redudndant table

---
+++ Chalkable.Web/app/chlk/activities/common/InfoMsgDialog.js
@@ -0,0 +1,27 @@
+REQUIRE('chlk.activities.lib.TemplateDialog');
+REQUIRE('chlk.templates.common.InfoMsg');
+
+NAMESPACE('chlk.activities.common', function () {
+     /** @class chlk.activities.common.InfoMsgDialog */
+     CLASS(
+        [ria.mvc.DomAppendTo('#chlk-dialogs')],
+        [ria.mvc.TemplateBind(chlk.templates.common.InfoMsg)],
+        [ria.mvc.ActivityGroup('InfoMessage')],
+        'InfoMsgDialog', EXTENDS(chlk.activities.lib.TemplateDialog), [
+            [[Object]],
+            OVERRIDE, VOID, function onRender_(data) {
+                 BASE(data);
+                 var node = this.dom.find('.centered');
+                 node.setCss('margin-top', -parseInt(node.height()/2, 10) + 'px');
+                 node.setCss('margin-left', -parseInt(node.width()/2, 10) + 'px');
+             },
+
+
+            [ria.mvc.DomEventBind('click', 'button, a')],
+            [[ria.dom.Dom, ria.dom.Event]],
+            VOID, function onBtnClick(node, event) {
+                this.close();
+                event.preventDefault();
+            }
+         ]);
+ });
