70a1ceac6cb86ef6daaf1cac49ba9e12475c08b1
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-10-13 16:08:25 +0300

    fixed for CHLK-3300 [55 569] Chalkable > Attendance graph not filtering correctly, supposed to be highlighting the selected section, but also retains the last few selections

---
+++ Chalkable.Web/Models/PersonViewDatas/StudentCommentViewData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Chalkable.BusinessLogic.Model;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Web.Models.PersonViewDatas
+{
+    public class StudentCommentViewData : ShortPersonViewData
+    {
+        public string Comment { get; set; }
+
+        protected StudentCommentViewData(Person person) : base(person)
+        {
+        }
+
+        public static IList<StudentCommentViewData> Create(IList<StudentCommentInfo> studentComments)
+        {
+            return studentComments.Select(x => new StudentCommentViewData(x.Student) {Comment = x.Comment}).ToList();
+        }
+    }
+}
