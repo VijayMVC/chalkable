6862c42f6fe5012d6cfd049fff28195c5de4d128
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-10-13 18:56:39 +0300

    fixed for CHLK-3024 Grading > ALL : position "All" button in center always

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/DemoAlternateScoreService.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Chalkable.BusinessLogic.Security;
+using Chalkable.BusinessLogic.Services.DemoSchool.Storage;
+using Chalkable.BusinessLogic.Services.School;
+using Chalkable.Common.Exceptions;
+using Chalkable.Data.School.DataAccess;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool
+{
+    public class DemoAlternateScoreService : DemoSchoolServiceBase, IAlternateScoreService
+    {
+        public DemoAlternateScoreService(IServiceLocatorSchool serviceLocator, DemoStorage storage) : base(serviceLocator, storage)
+        {
+        }
+
+        public void AddAlternateScores(IList<AlternateScore> alternateScores)
+        {
+            if(!BaseSecurity.IsSysAdmin(Context))
+                throw new ChalkableSecurityException();
+
+            Storage.AlternateScoreStorage.Add(alternateScores);
+        }
+
+        public void EditAlternateScores(IList<AlternateScore> alternateScores)
+        {
+            if (!BaseSecurity.IsSysAdmin(Context))
+                throw new ChalkableSecurityException();
+
+
+            Storage.AlternateScoreStorage.Update(alternateScores);
+        }
+
+        public void Delete(int id)
+        {
+            if (!BaseSecurity.IsSysAdmin(Context))
+                throw new ChalkableSecurityException();
+            Storage.AlternateScoreStorage.Delete(id);
+            
+        }
+
+        public void Delete(IList<int> ids)
+        {
+            if (!BaseSecurity.IsSysAdmin(Context))
+                throw new ChalkableSecurityException();
+            Storage.AlternateScoreStorage.Delete(ids);
+        }
+
+        public IList<AlternateScore> GetAlternateScores()
+        {
+            return Storage.AlternateScoreStorage.GetAll();
+        }
+
+
+        public AlternateScore GetAlternateScore(int id)
+        {
+            return Storage.AlternateScoreStorage.GetById(id);
+        }
+    }
+}
