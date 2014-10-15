b9f0b3fdf0bd901ef0ec93559b7132b56fcecd99
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-10-15 19:10:44 +0300

    fixed for CHLK-3222 Teacher. Attendance. When teacher puts attendance for the first student and presses "Enter", the second student won't be selected.

---
+++ Chalkable.BusinessLogic/Security/NotificationSecurity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.BusinessLogic.Services;
+using Chalkable.Common;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Security
+{
+    public static class NotificationSecurity
+    {
+        public static bool CanCreateAnnouncementNotification(Announcement announcement, UserContext context)
+        {
+            return BaseSecurity.IsSysAdmin(context) || announcement.IsOwner;
+        }
+
+        public static bool CanCreateSimpleNotification(Guid personId, UserContext context)
+        {
+            return BaseSecurity.IsAdminOrTeacher(context);
+        }
+
+        public static bool CanModify(Notification notification, UserContext context)
+        {
+            return BaseSecurity.IsSysAdmin(context) || notification.PersonRef == context.PersonId;
+        }
+    }
+}
