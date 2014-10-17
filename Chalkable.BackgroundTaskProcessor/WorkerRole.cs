c4c74d11d82d1250e2a4b19288194a95ad5e20c9
Volodymyr Iatsyshyn <viatsyshyn@emp-game.com>
2014-10-17 17:54:11 +0300

    fix for CHLK-3260: fixed crash when marking perios has ended but accademic session is not

---
+++ Chalkable.BackgroundTaskProcessor/WorkerRole.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using Chalkable.Common;
+using Chalkable.Web.Tools;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.Diagnostics.Management;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Mindscape.Raygun4Net;
+
+
+namespace Chalkable.BackgroundTaskProcessor
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        public override void Run()
+        {
+            // This is a sample worker implementation. Replace with your logic.
+            Trace.WriteLine("Chalkable.BackgroundTaskProcessor entry point called", "Information");
+            var processor = new TaskProcessor();
+            var delay = Settings.TaskProcessorDelay;
+            var raygunClient = new RaygunClient();
+            raygunClient.ApplicationVersion = CompilerHelper.Version;
+            while (true)
+            {
+                if (delay <= 0)
+                {
+                    Thread.Sleep(7200000);
+                    Trace.TraceError("Task processing is turned off");
+                    continue;
+                }
+                try
+                {
+                    Thread.Sleep(delay);
+                    processor.Process();
+                }
+                catch (Exception ex)
+                {
+#if !DEBUG
+                    try
+                    {
+                        raygunClient.SendInBackground(ex);
+                    }
+                    catch (Exception){}
+#else
+                    Debug.Fail(ex.Message);
+#endif
+                    while (ex != null)
+                    {
+                        Trace.TraceError(ex.Message);
+                        Trace.TraceError(ex.StackTrace);
+                        ex = ex.InnerException;
+                    }
+                }
+            }
+        }
+
+        public override bool OnStart()
+        {
+            // Set the maximum number of concurrent connections 
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // For information on handling configuration changes
+            // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+            ConfigureDiagnostics();
+            
+            return base.OnStart();
+        }
+
+        private void ConfigureDiagnostics()
+        {
+            String wadConnectionString = "Microsoft.WindowsAzure.Plugins.Diagnostics.ConnectionString";
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue(wadConnectionString));
+
+            RoleInstanceDiagnosticManager roleInstanceDiagnosticManager =
+                cloudStorageAccount.CreateRoleInstanceDiagnosticManager(RoleEnvironment.DeploymentId, RoleEnvironment.CurrentRoleInstance.Role.Name, RoleEnvironment.CurrentRoleInstance.Id);
+            DiagnosticMonitorConfiguration diagnosticMonitorConfiguration = roleInstanceDiagnosticManager.GetCurrentConfiguration();
+            diagnosticMonitorConfiguration.Directories.ScheduledTransferPeriod = TimeSpan.FromMinutes(5d);
+            diagnosticMonitorConfiguration.Logs.ScheduledTransferPeriod = TimeSpan.FromSeconds(30);
+            diagnosticMonitorConfiguration.Logs.ScheduledTransferLogLevelFilter = LogLevel.Verbose;
+            diagnosticMonitorConfiguration.WindowsEventLog.DataSources.Add("Application!*");
+            diagnosticMonitorConfiguration.WindowsEventLog.DataSources.Add("System!*");
+            diagnosticMonitorConfiguration.WindowsEventLog.ScheduledTransferPeriod = TimeSpan.FromMinutes(5d);
+            PerformanceCounterConfiguration performanceCounterConfiguration = new PerformanceCounterConfiguration();
+            performanceCounterConfiguration.CounterSpecifier = @"\Processor(_Total)\% Processor Time";
+            performanceCounterConfiguration.SampleRate = TimeSpan.FromSeconds(10d);
+            diagnosticMonitorConfiguration.PerformanceCounters.DataSources.Add(performanceCounterConfiguration);
+            diagnosticMonitorConfiguration.PerformanceCounters.ScheduledTransferPeriod = TimeSpan.FromMinutes(1d);
+            roleInstanceDiagnosticManager.SetCurrentConfiguration(diagnosticMonitorConfiguration);
+        }
+    }
+}
---
+++ Chalkable.Web/Models/ApplicationsViewData/ApplicationRatingViewData.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Chalkable.Common;
+using Chalkable.Data.Master.Model;
+using Chalkable.Data.School.Model;
+using Chalkable.Web.Models.PersonViewDatas;
+
+namespace Chalkable.Web.Models.ApplicationsViewData
+{
+    public class ApplicationRatingViewData
+    {
+        public IList<ApplicationRatingByRoleViewData> RatingByRoles { get; set; }
+        public IList<ApplicationRatingByPersonViewData> RatingByPerson { get; set; }
+        public double Avg { get; set; }
+        private ApplicationRatingViewData() { }
+
+        public static ApplicationRatingViewData Create(IList<ApplicationRating> ratings, IList<Person> persons)
+        {
+            //TODO: it could be user id from staff or student
+            ratings = ratings.Where(x => persons.Any(y => y.UserId == x.User.SisUserId)).ToList();
+            persons = persons.Where(x => ratings.Any(y => y.User.SisUserId == x.UserId)).ToList();
+
+            var ratingsbyrole = persons.GroupBy(x => x.RoleRef)
+                                       .ToDictionary(x => CoreRoles.GetById(x.Key),
+                                                     x => ratings.Where(y => x.Any(z => z.Id == y.User.SisUserId)).ToList());
+            var res = new ApplicationRatingViewData
+            {
+                RatingByPerson = ApplicationRatingByPersonViewData.Create(ratings, persons),
+                RatingByRoles = ApplicationRatingByRoleViewData.Create(ratingsbyrole)
+            };
+            res.Avg = res.RatingByPerson.Count != 0 ? res.RatingByPerson.Average(x => x.Rating) : 0;
+            return res;
+        }
+    }
+
+
+
+    public class ApplicationRatingByPersonViewData
+    {
+        public ShortPersonViewData Person { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }
+
+        private ApplicationRatingByPersonViewData() { }
+
+        public static ApplicationRatingByPersonViewData Create(ApplicationRating rating, IList<Person> persons)
+        {
+            var res = new ApplicationRatingByPersonViewData
+            {
+                Rating = rating.Rating,
+                Review = rating.Review,
+                Person = ShortPersonViewData.Create(persons.First(x=>x.Id == rating.User.SisUserId)) 
+            };
+            return res;
+        }
+
+        public static IList<ApplicationRatingByPersonViewData> Create(IList<ApplicationRating> ratings, IList<Person> persons)
+        {
+            return ratings.Select(x => Create(x, persons)).ToList();
+        }
+    }
+
+    public class ApplicationRatingByRoleViewData
+    {
+
+        public RoleViewData Role { get; set; }
+        public double AvgRating { get; set; }
+        public int PersonCount { get; set; }
+        private ApplicationRatingByRoleViewData() { }
+
+
+        public static IList<ApplicationRatingByRoleViewData> Create(IDictionary<CoreRole, List<ApplicationRating>> ratingsbyrole)
+        {
+            var res = new List<ApplicationRatingByRoleViewData>();
+            foreach (var rolerating in ratingsbyrole)
+            {
+                var ratings = rolerating.Value.Select(x => x.Rating).ToList();
+                res.Add(new ApplicationRatingByRoleViewData
+                {
+                    Role = RoleViewData.Create(rolerating.Key),
+                    AvgRating = CallsAvgRating(ratings),
+                    PersonCount = rolerating.Value.Count
+                });
+            }
+            return res;
+        }
+
+        private static double CallsAvgRating(IList<int> ratings)
+        {
+            int res = ratings.Sum();
+            return res == 0 ? 0 : res / ratings.Count;
+        }
+    }
+}
