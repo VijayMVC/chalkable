e37412a7d32498ece39ebc212c65cf44274885ed
Andrew Machnew <amachnew@emp-game.com>
2014-10-21 20:52:46 +0300

    fixed demo announcement storage

---
+++ Chalkable.Web/Models/HoverBoxesViewData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chalkable.Web.Models
+{
+    public class HoverBoxesViewData<T>
+    {
+        public string Title { get; set; }
+        public IList<T> Hover { get; set; }
+
+        protected const int MAX_HOVER_LIST_NUMBER = 4;
+    }
+}
