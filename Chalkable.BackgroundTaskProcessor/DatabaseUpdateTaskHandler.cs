3449fbafb6a196db568befafc1f626529c9d6ec7
Ievgenii Zasoba <geka.zasoba@gmail.com>
2014-10-21 13:04:23 +0300

    Remove DB backup/restore from db update task bc we're using point in time backups

--- Chalkable.BackgroundTaskProcessor/DatabaseUpdateTaskHandler.cs
+++ Chalkable.BackgroundTaskProcessor/DatabaseUpdateTaskHandler.cs
@@ -11,46 +11,14 @@ namespace Chalkable.BackgroundTaskProcessor
 {
     public class DatabaseUpdateTaskHandler : ITaskHandler
     {
-        private bool BackupAll(DateTime now, BackgroundTaskService.BackgroundTaskLog log)
-        {
-            var backuper = new BackupTaskHandler(true);
-            var time = now.Ticks;
-            return backuper.Handle(new BackgroundTask
-                {
-                    Created = now,
-                    Scheduled = now,
-                    Type = BackgroundTaskTypeEnum.BackupDatabases,
-                    Data = (new DatabaseBackupRestoreTaskData(time, true)).ToString()
-                }, log);
-        }
-
-        private bool RestoreAll(DateTime now, BackgroundTaskService.BackgroundTaskLog log)
-        {
-            var restorer = new BackupTaskHandler(false);
-            var time = now.Ticks;
-            return restorer.Handle(new BackgroundTask
-                {
-                    Created = now,
-                    Scheduled = now,
-                    Type = BackgroundTaskTypeEnum.BackupDatabases,
-                    Data = (new DatabaseBackupRestoreTaskData(time, true)).ToString()
-                }, log);
-        }
-
         public bool Handle(BackgroundTask task, BackgroundTaskService.BackgroundTaskLog log)
         {
-            var now = DateTime.UtcNow;
-            if (!BackupAll(now, log))
-            {
-                log.LogError("Database wasn't updated because backup fails");
-                return false;
-            }
             bool res = true;
             try
             {
                 var data = task.GetData<DatabaseUpdateTaskData>();
                 var sl = ServiceLocatorFactory.CreateMasterSysAdmin();
-                var schools = sl.DistrictService.GetDistricts();
+                var districts = sl.DistrictService.GetDistricts();
                 foreach (var updateSql in data.Sqls)
                 {
                     if (updateSql.RunOnMaster)
@@ -72,9 +40,7 @@ namespace Chalkable.BackgroundTaskProcessor
                             }
                         }
                         var runer = new AllSchoolRunner<string>();
-                        if (
-                            !runer.Run(schools, updateSql.Sql, log, ExecSql,
-                                       task1 => AllSchoolRunner<string>.TaskStatusEnum.Completed))
+                        if (!runer.Run(districts, updateSql.Sql, log, ExecSql, task1 => AllSchoolRunner<string>.TaskStatusEnum.Completed))
                         {
                             res = false;
                             break;
@@ -92,11 +58,6 @@ namespace Chalkable.BackgroundTaskProcessor
                     ex = ex.InnerException;
                 }
                 res = false;
-            }
-            if (!res)
-            {
-                if (!RestoreAll(now, log))
-                    log.LogError("Database wasn't restored after update fails");
             }
             return res;
 
