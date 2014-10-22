731322e77f7d3e8aa894f97e1ace2ecd858228a6
Andrew Machnew <amachnew@emp-game.com>
2014-10-22 13:04:10 +0300

    fixed CHLK-3338 Demo. Teacher. Attendance. When a teacher chooses the past date, the page will crash

---
+++ Chalkable.Web/Controllers/BaseAnnouncementController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Chalkable.Common;
+using Chalkable.Data.School.Model;
+using Chalkable.Web.Logic;
+using Chalkable.Web.Models.AnnouncementsViewData;
+
+namespace Chalkable.Web.Controllers
+{
+    public class AnnouncementBaseController : ChalkableController
+    {
+        protected AnnouncementViewData PrepareFullAnnouncementViewData(int announcementId, bool needsAllAttachments = true, bool isRead = false)
+        {
+            var annDetails = SchoolLocator.AnnouncementService.GetAnnouncementDetails(announcementId);
+            if (annDetails.SisActivityId.HasValue)
+            {
+                annDetails.StudentAnnouncements = SchoolLocator.StudentAnnouncementService.GetStudentAnnouncements(announcementId);
+                annDetails.GradingStudentsCount = annDetails.StudentAnnouncements.Count(x=>x.IsGraded);
+            }
+            var teachersIds = SchoolLocator.ClassService.GetClassTeachers(annDetails.ClassRef, null).Select(x=>x.PersonRef).ToList();
+            var attInfo = AttachmentLogic.PrepareAttachmentsInfo(annDetails.AnnouncementAttachments, teachersIds);
+            var annViewData = AnnouncementDetailedViewData.Create(annDetails, SchoolLocator.GradingStyleService.GetMapper(), Context.PersonId.Value, attInfo);
+            annViewData.Applications = ApplicationLogic.PrepareAnnouncementApplicationInfo(SchoolLocator, MasterLocator, announcementId);
+            annViewData.ApplicationsCount = annViewData.Applications.Count;
+            if (annViewData.Applications.Count > 0)
+            {
+                annViewData.ApplicationName = annViewData.Applications.Count == 1
+                                  ? annViewData.Applications.First().Name
+                                  : annViewData.Applications.Count.ToString();
+            }
+
+            if (isRead && annDetails.State == AnnouncementState.Created)
+            {
+                var ids = new HashSet<Guid>();
+                IList<string> appNames = new List<string>();  
+                //TODO : think about appNames 
+                //foreach (var sa in annDetails.StudentAnnouncements)
+                //{
+                //    var appRef = sa.ApplicationRef;
+                //    if (appRef.HasValue && !ids.Contains(appRef.Value))
+                //    {
+                //        var app = applications.First(x => x.Id == appRef);
+                //        ids.Add(appRef.Value);
+                //        appNames.Add(app.Name);
+                //    }
+                //}
+                var stAnnouncements = annDetails.StudentAnnouncements;
+                annViewData.autoGradeApps = appNames;
+                if (SchoolLocator.Context.Role == CoreRoles.STUDENT_ROLE)
+                    annViewData.Dropped = stAnnouncements.Count > 0 && stAnnouncements[0].Dropped;
+
+                if (stAnnouncements.Count > 0 && annDetails.GradableType)
+                    annViewData.StudentAnnouncements = StudentAnnouncementLogic.ItemGradesList(SchoolLocator, annDetails, attInfo);
+            }
+            if(!isRead)
+                annViewData.CanAddStandard = SchoolLocator.AnnouncementService.CanAddStandard(announcementId);
+            return annViewData;
+        }
+    }
+}
---
+++ SQL/ChalkableSchool/1047 - changed type for Person table.sql
@@ -0,0 +1,23 @@
+drop type TPerson
+go
+
+CREATE TYPE [dbo].[TPerson] AS TABLE(
+	[Id] [int] NOT NULL,
+	[FirstName] [nvarchar](255) NOT NULL,
+	[LastName] [nvarchar](255) NOT NULL,
+	[BirthDate] [datetime2](7) NULL,
+	[Gender] [nvarchar](255) NULL,
+	[Salutation] [nvarchar](255) NULL,
+	[Active] [bit] NOT NULL,
+	[LastPasswordReset] [datetime2](7) NULL,
+	[FirstLoginDate] [datetime2](7) NULL,
+	[LastMailNotification] [datetime2](7) NULL,
+	[AddressRef] [int] NULL,
+	[HasMedicalAlert] [bit] NOT NULL,
+	[IsAllowedInetAccess] [bit] NOT NULL,
+	[SpecialInstructions] [nvarchar](1024) NOT NULL,
+	[SpEdStatus] [nvarchar](256) NULL,
+	[PhotoModifiedDate] [datetime2](7) NULL,
+	[UserId] int null
+)
+GO
