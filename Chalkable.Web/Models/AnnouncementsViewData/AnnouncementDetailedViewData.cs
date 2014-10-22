f174d0837dad6a9631d26011964dd63d8cabb25a
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-10-22 18:14:29 +0300

    fixed for CHLK-3339 Student Profile. When teacher views a student profile, alert will be cutt off

---
+++ Chalkable.Web/Models/AnnouncementsViewData/AnnouncementDetailedViewData.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Chalkable.BusinessLogic.Mapping;
+using Chalkable.Data.School.Model;
+using Chalkable.Web.Logic;
+using Chalkable.Web.Models.ApplicationsViewData;
+using Chalkable.Web.Models.PersonViewDatas;
+
+namespace Chalkable.Web.Models.AnnouncementsViewData
+{
+    public class AnnouncementDetailedViewData : AnnouncementGradeViewData
+    {
+        public IList<AnnouncementAttachmentViewData> AnnouncementAttachments { get; set; }
+        public IList<AnnouncementQnAViewData> AnnouncementQnAs { get; set; }
+        public IList<AnnouncementApplicationViewData> Applications { get; set; }
+        public IList<AnnouncementStandardViewData> Standards { get; set; }
+        public StudentAnnouncementsViewData StudentAnnouncements { get; set; }
+        public IList<String> autoGradeApps { get; set; }
+
+        public ShortPersonViewData Owner { get; set; }
+        public bool Exempt { get; set; }
+        
+
+        private AnnouncementDetailedViewData(AnnouncementDetails announcementDetails, IList<StudentAnnouncement> studentAnnouncements, IGradingStyleMapper mapper, int currentSchoolPersonId)
+            : base(announcementDetails, studentAnnouncements, mapper, null)
+        {
+            if (announcementDetails.AnnouncementQnAs != null)
+                AnnouncementQnAs = AnnouncementQnAViewData.Create(announcementDetails.AnnouncementQnAs);
+
+            if(announcementDetails.Owner != null)
+                Owner = ShortPersonViewData.Create(announcementDetails.Owner);
+            if(announcementDetails.AnnouncementStandards != null)
+                Standards = AnnouncementStandardViewData.Create(announcementDetails.AnnouncementStandards);
+            if (announcementDetails.AnnouncementApplications == null) return;
+            //TODO: applicationViewData
+            //Applications = new List<AnnouncementApplicationViewData>();
+            //foreach (var announcementApplication in announcementDetails.AnnouncementApplications)
+            //{
+            //    Applications.Add(AnnouncementApplicationViewData.Create(announcementApplication, currentSchoolPersonId));
+            //}
+            Exempt = studentAnnouncements.Count > 0 && studentAnnouncements.All(x => x.Exempt);
+        }
+
+        private AnnouncementDetailedViewData(AnnouncementComplex announcement, bool? wasAnnouncementTypeGraded)
+            : base(announcement, wasAnnouncementTypeGraded)
+        {
+        }
+
+        public static AnnouncementDetailedViewData CreateEmpty(AnnouncementComplex announcement, bool? wasAnnouncementTypeGraded = null)
+        {
+            return new AnnouncementDetailedViewData(announcement, wasAnnouncementTypeGraded);
+        }
+
+        public static AnnouncementDetailedViewData Create(AnnouncementDetails announcementDetails, IGradingStyleMapper mapper, int currentSchoolPersonId)
+        {
+            var studentAnnouncements = announcementDetails.StudentAnnouncements.Select(x => new StudentAnnouncement
+            {
+                AnnouncementId = x.AnnouncementId,
+                StudentId = x.StudentId,
+                Comment = x.Comment,
+                Dropped = x.Dropped,
+                ExtraCredit = x.ExtraCredit,
+                ScoreValue = x.ScoreValue,
+                NumericScore = x.NumericScore,
+                IsWithdrawn = x.Student.IsWithdrawn
+            }).ToList();
+            return new AnnouncementDetailedViewData(announcementDetails, studentAnnouncements, mapper, currentSchoolPersonId);
+        }
+
+        public static AnnouncementDetailedViewData Create(AnnouncementDetails announcementDetails, IGradingStyleMapper mapper,
+            int currentSchoolPersonId, IList<AnnouncementAttachmentInfo> attachmentInfos)
+        {
+            var res = Create(announcementDetails, mapper, currentSchoolPersonId);
+            res.AnnouncementAttachments = AnnouncementAttachmentViewData.Create(attachmentInfos, currentSchoolPersonId);
+            return res;
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/services/ClassService.js
@@ -0,0 +1,112 @@
+REQUIRE('chlk.services.BaseService');
+REQUIRE('chlk.services.CalendarService');
+REQUIRE('ria.async.Future');
+REQUIRE('chlk.models.classes.ClassForTopBar');
+REQUIRE('chlk.models.classes.ClassForWeekMask');
+REQUIRE('chlk.models.id.ClassId');
+REQUIRE('chlk.models.classes.ClassSummary');
+REQUIRE('chlk.models.classes.ClassInfo');
+REQUIRE('chlk.models.classes.ClassSchedule');
+REQUIRE('chlk.models.classes.ClassGradingViewData');
+
+REQUIRE('chlk.models.common.ChlkDate');
+
+NAMESPACE('chlk.services', function () {
+    "use strict";
+
+    /** @class chlk.services.ClassService*/
+    CLASS(
+        'ClassService', EXTENDS(chlk.services.BaseService), [
+
+            [[Boolean, Boolean]],
+            Array, function getClassesForTopBar(withAll_, forCurrentMp_) {
+                var res = this.getContext().getSession().get(withAll_ ? ChlkSessionConstants.CLASSES_TO_FILTER_WITH_ALL : ChlkSessionConstants.CLASSES_TO_FILTER);
+
+                if(forCurrentMp_){
+                    var mpId = this.getContext().getSession().get(ChlkSessionConstants.MARKING_PERIOD).getId();
+                    res = res.filter(function(item){
+                        return item.getName() == 'All' || item.getMarkingPeriodsId().indexOf(mpId) > -1;
+                    })
+                }
+
+                return res;
+            },
+
+            [[chlk.models.id.ClassId]],
+            chlk.models.classes.ClassForTopBar, function getClassById(classId) {
+                var classes = this.getContext().getSession().get(ChlkSessionConstants.CLASSES_TO_FILTER), res;
+                classes.forEach(function(item){
+                    if(item.getId() == classId)
+                        res = item;
+                });
+                return res;
+            },
+
+            //TODO: refactor
+            [[chlk.models.id.ClassId]],
+            chlk.models.classes.ClassForWeekMask, function getClassAnnouncementInfo(id){
+                var res = window.classesInfo[id.valueOf()];
+                if(!res) {
+                    var cls = chlk.models.classes.ClassForWeekMask();
+                    cls.setAlphaGrades([]);
+                    cls.setMask([]);
+                    cls.setClassId(id);
+                    cls.setTypesByClass([]);
+                    cls.setAlphaGradesForStandards([]);
+                    return cls;
+                }
+                res.classId = id.valueOf();
+                res = new chlk.lib.serialize.ChlkJsonSerializer().deserialize(res, chlk.models.classes.ClassForWeekMask);
+                return res;
+            },
+
+            [[chlk.models.id.ClassId]],
+            ria.async.Future, function getSummary(classId) {
+                return this.get('Class/ClassSummary.json', chlk.models.classes.ClassSummary, {
+                    classId: classId.valueOf()
+                });
+            },
+            [[chlk.models.id.ClassId]],
+            ria.async.Future, function getInfo(classId){
+                return this.get('Class/ClassInfo.json', chlk.models.classes.ClassInfo,{
+                    classId: classId && classId.valueOf()
+                });
+            },
+
+            [[chlk.models.id.ClassId]],
+            ria.async.Future, function getGrading(classId){
+                return this.get('Class/ClassGrading.json', chlk.models.classes.ClassGradingViewData,{
+                    classId: classId && classId.valueOf()
+                })
+            },
+
+            [[chlk.models.id.ClassId, chlk.models.common.ChlkDate]],
+            ria.async.Future, function getSchedule(classId, date_){
+                return this.get('Class/ClassSchedule.json', chlk.models.classes.ClassSchedule,{
+                    classId: classId && classId.valueOf(),
+                    date: date_ && date_.toString('mm-dd-yy')
+                })
+                .then(function(data){
+                    this.getContext().getService(chlk.services.CalendarService)
+                        .saveDayCalendarDataInSession(data.getCalendarDayItems());
+                    return data;
+                }, this);
+            },
+
+            [[chlk.models.id.ClassId]],
+            ria.async.Future, function getAttendance(classId){
+                return this.get('Class/ClassAttendance.json', chlk.models.classes.ClassAttendanceSummary,{
+                    classId: classId && classId.valueOf()
+                })
+            },
+
+            [[chlk.models.id.ClassId]],
+            ria.async.Future, function getAppsInfo(classId){
+                return this.get('Class/ClassApps.json', chlk.models.classes.ClassApps,{
+                    classId: classId && classId.valueOf()
+                })
+            }
+
+
+        ])
+});
