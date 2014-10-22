47fff4cf1699b6645045103bde1f9d5b70fd41d9
Andrew Machnew <amachnew@emp-game.com>
2014-10-22 16:46:10 +0300

    fixed CHLK-3231 Demo. Teacher. When a teacher attaches a new file after discarding action, previous file will be attached to an item

---
+++ Chalkable.Web/app/chlk/models/grading/GradingTeacherClassSummaryViewData.js
@@ -0,0 +1,27 @@
+REQUIRE('chlk.models.grading.StudentGradingViewData');
+
+NAMESPACE('chlk.models.grading', function () {
+    "use strict";
+    /** @class chlk.models.grading.GradingTeacherClassSummaryViewData*/
+    CLASS(
+        'GradingTeacherClassSummaryViewData', [
+            [ria.serialize.SerializeProperty('class')],
+            chlk.models.classes.Class, 'clazz',
+
+            ArrayOf(chlk.models.grading.StudentGradingViewData), 'well',
+
+            ArrayOf(chlk.models.grading.StudentGradingViewData), 'trouble',
+
+            [ria.serialize.SerializeProperty('allstudents')],
+            ArrayOf(chlk.models.grading.StudentGradingViewData), 'allStudents',
+
+            function getUpdatedTrouble(){
+                var res = [], item;
+                for(var i = 0; i < 5; i++){
+                    item = this.getTrouble()[i] || new chlk.models.grading.StudentGradingViewData();
+                    res.unshift(item);
+                }
+                return res;
+            }
+        ]);
+});
