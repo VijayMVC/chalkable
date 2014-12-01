4ef590036bc46735d71e8310fc69082a8c5f8311
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2014-12-01 09:35:00 -0700

    rewrited attendance and activities api

--- Chalkable.StiConnector/Connectors/Model/PostedAttendance.cs
+++ Chalkable.StiConnector/Connectors/Model/PostedAttendance.cs
@@ -8,11 +8,20 @@ namespace Chalkable.StiConnector.Connectors.Model
 {
     public class PostedAttendance
     {
-        public DateTime Date { get; set; }
-
+        public bool AttendancePosted { get; set; }
+        public TimeSpan EndTime { get; set; }
+        /// <summary>
+        /// Id of the section
+        /// </summary>
         public int SectionId { get; set; }
-
-        public TimeSpan Time { get; set; }
-
+        public TimeSpan StartTime { get; set; }
+        /// <summary>
+        /// Id of the time slot the section meets
+        /// </summary>
+        public int TimeSlotId { get; set; }
+        /// <summary>
+        /// Id of the time slot variation, if one exists
+        /// </summary>
+        public int? TimeSlotVariationId { get; set; }
     }
 }
