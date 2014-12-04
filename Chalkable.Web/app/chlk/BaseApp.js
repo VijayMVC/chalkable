03752874962e5fac1252a82ca41edec14e515d89
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-12-04 16:06:38 +0200

    added alerts popup

--- Chalkable.Web/app/chlk/BaseApp.js
+++ Chalkable.Web/app/chlk/BaseApp.js
@@ -261,7 +261,7 @@ NAMESPACE('chlk', function (){
                             tpl.assign(model);
                             tooltip.html(tpl.render());
                             tooltip.show();
-                            var top = offset.top - (tooltip.height() - node.height()) / 2;
+                            var top = offset.top - (tooltip.height() - node.height()) / 2 - 20;
                             tooltip.css('left', offset.left + node.width() + 20)
                                 .css('top', top > 0 ? top : 0);
                             if(top < 0)
