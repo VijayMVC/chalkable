fa8bddc6af0190b7306101d4223bffb32dd2ed9f
Volodymyr Iatsyshyn <viatsyshyn@emp-game.com>
2014-12-05 11:50:54 +0200

    rebase

--- Chalkable.Web/app/chlk/activities/apps/AddCCStandardDialog.js
+++ Chalkable.Web/app/chlk/activities/apps/AddCCStandardDialog.js
@@ -11,32 +11,61 @@ NAMESPACE('chlk.activities.apps', function(){
         [ria.mvc.TemplateBind(chlk.templates.apps.AddCCStandardsTpl)],
         'AddCCStandardDialog', EXTENDS(chlk.activities.lib.TemplateDialog),[
 
+
+            [[ria.dom.Dom, Array]],
+            function removeColumnId_(column, standardIds){
+                var columnId = column.getData('id') || '';
+                var standardIdIndex = columnId ? standardIds.indexOf(columnId.toString()) : - 1;
+                if (standardIdIndex > -1) {
+                    standardIds.splice(standardIdIndex, 1);
+                }
+                return standardIds;
+            },
+
             [ria.mvc.DomEventBind('click', '.column-cell')],
             [[ria.dom.Dom, ria.dom.Event]],
             Boolean, function cellClick(node, event){
                 if(node.hasClass('active'))
                     return false;
-                node.parent('.column').find('.column-cell.active').removeClass('active');
+
+                var standardIdsNode = this.dom.find('.standard-ids');
+                var value = standardIdsNode.getValue();
+                var standardIds = value ? value.split(',') : [];
+
+                var prevColumn = node.parent('.column').find('.column-cell.active');
+                prevColumn.removeClass('active');
+
+                standardIds = this.removeColumnId_(prevColumn, standardIds);
+
                 node.addClass('active');
                 var id = node.getData('id') || '';
-                var standardsIdsNode = this.dom.find('.standard-ids');
-                var value = standardsIdsNode.getValue();
-                var standardIds = value ? value.split(',') : [];
-                //this.dom.find('input[name=standardid]').setValue(id);
-                var btnC = this.dom.find('.add-standard-btn');
-                var btn = btnC.find('button');
-                if(id && standardIds.indexOf(id.toString()) == -1){
-                    btnC.removeClass('disabled');
-                    btnC.removeAttr('disabled');
-                    btn.removeAttr('disabled');
+                var idIndex = id ? standardIds.indexOf(id.toString()) : - 1;
+                var btnAddContainer = this.dom.find('.add-standard-btn');
+                var btn = btnAddContainer.find('button');
+
+                var childNodes = node.parent('td').find('~ td');
+                var childColumns = childNodes
+                    .find('.column-cell')
+                    .forEach(function(column){
+                        standardIds = this.removeColumnId_(column, standardIds);
+                    }.bind(this));
+
+                childNodes.remove();
+
+                if(idIndex == -1 && id != '')
                     standardIds.push(id);
-                    standardsIdsNode.setValue(standardIds.join(','));
-                }else{
-                    btnC.addClass('disabled');
-                    btnC.setAttr('disabled', true);
+
+                if (standardIds.length == 0){
+                    btnAddContainer.addClass('disabled');
+                    btnAddContainer.setAttr('disabled', true);
                     btn.setAttr('disabled', true);
+                }else{
+                    btnAddContainer.removeClass('disabled');
+                    btnAddContainer.removeAttr('disabled');
+                    btn.removeAttr('disabled');
                 }
-                node.parent('td').find('~ td').remove();
+
+                standardIdsNode.setValue(standardIds.join(','));
                 return true;
             },
 
