186100eb8286426eaeb12fb745f3c1ab04e03f5a
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-12-08 18:40:12 +0200

    rebase

---
+++ Chalkable.StiConnector/Connectors/Model/MissingAssignmentsParams.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chalkable.StiConnector.Connectors.Model
+{
+    public class MissingAssignmentsParams
+    {
+        public int AcadSessionId { get; set; }
+        public int[] AlternateScoreIds { get; set; }
+        public bool AlternateScoresOnly { get; set; }
+        public bool ConsiderZerosAsMissingGrades { get; set; }
+        public DateTime EndDate { get; set; }
+        public int IdToPrint { get; set; }
+        public bool IncludeWithdrawn { get; set; }
+        public bool OnePerPage { get; set; }
+        public int OrderBy { get; set; }
+        public int? SectionId { get; set; }
+        public DateTime StartDate { get; set; }
+        public int? StudentId { get; set; }
+        public bool SuppressStudentName { get; set; }
+        public int UserId { get; set; }
+    }
+}
