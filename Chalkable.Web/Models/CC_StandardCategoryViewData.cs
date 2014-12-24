7163221f61cd2a448703d2cf30409744c2524fb7
Ostap Stasyuk <ostasyuk@emp-game.com>
2014-12-24 17:41:10 +0200

    fixed for CHLK-3572 Teacher. Chalkable doesn't display "withdrawn indicator".

---
+++ Chalkable.Web/Models/CC_StandardCategoryViewData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Chalkable.Data.Master.Model;
+
+namespace Chalkable.Web.Models
+{
+    public class CCStandardCategoryViewData
+    {
+        public Guid Id { get; set; }
+        public Guid? ParentCategoryId { get; set; }
+        public string Name { get; set; }
+
+        public static CCStandardCategoryViewData Create(CC_StandardCategory standardCategory)
+        {
+            return new CCStandardCategoryViewData
+                {
+                    Id = standardCategory.Id,
+                    ParentCategoryId = standardCategory.ParentCategoryRef,
+                    Name = standardCategory.Name
+                };
+        }
+
+        public static IList<CCStandardCategoryViewData> Create(IList<CC_StandardCategory> standardCategories)
+        {
+            return standardCategories.Select(Create).ToList();
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/templates/classes/Class.js
@@ -0,0 +1,38 @@
+REQUIRE('chlk.templates.ChlkTemplate');
+REQUIRE('chlk.models.classes.Class');
+
+NAMESPACE('chlk.templates.classes', function () {
+
+    /** @class chlk.templates.classes.Class*/
+    CLASS(
+        [ria.templates.TemplateBind('~/assets/jade/activities/profile/StudentClassItem.jade')],
+        [ria.templates.ModelBind(chlk.models.classes.Class)],
+        'Class', EXTENDS(chlk.templates.ChlkTemplate), [
+            [ria.templates.ModelPropertyBind],
+            chlk.models.id.DepartmentId, 'departmentId',
+
+            [ria.templates.ModelPropertyBind],
+            String, 'description',
+
+            [ria.templates.ModelPropertyBind],
+            chlk.models.grading.GradeLevel, 'gradeLevel',
+
+            [ria.templates.ModelPropertyBind],
+            chlk.models.id.ClassId, 'id',
+
+            [ria.templates.ModelPropertyBind],
+            ArrayOf(chlk.models.id.MarkingPeriodId), 'markingPeriodsId',
+
+            [ria.templates.ModelPropertyBind],
+            String, 'name',
+
+            [ria.templates.ModelPropertyBind],
+            String, 'classNumber',
+
+            [ria.templates.ModelPropertyBind],
+            String, 'fullClassName',
+
+            [ria.templates.ModelPropertyBind],
+            chlk.models.people.User, 'teacher'
+        ])
+});
