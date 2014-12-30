99e67ba19c794aed9c58cb945916295bd734c941
Ievgenii Zasoba <geka.zasoba@gmail.com>
2014-12-30 13:18:36 +0200

    Fixed staff search

---
+++ Chalkable.Web/app/chlk/models/settings/DeveloperSettings.js
@@ -0,0 +1,12 @@
+REQUIRE('chlk.models.id.SchoolPersonId');
+REQUIRE('chlk.models.id.AppId');
+NAMESPACE('chlk.models.settings', function () {
+    "use strict";
+    /** @class chlk.models.settings.DeveloperSettings*/
+    CLASS(
+        'DeveloperSettings', [
+            chlk.models.id.SchoolPersonId, 'developerId',
+            chlk.models.id.AppId, 'currentAppId',
+            String, 'currentAppName'
+        ]);
+});
---
+++ SQL/ChalkableMaster/1004 - add last sync field to district.sql
@@ -0,0 +1,3 @@
+Alter Table District
+	Add LastSync DateTime2
+GO
