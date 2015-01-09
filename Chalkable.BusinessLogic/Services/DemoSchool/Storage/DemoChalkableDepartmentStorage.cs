b60a60308b42c27152fbbdd9e0c7d018925d5a14
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-01-09 03:50:42 -0700

    fixed missingassignments report

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/Storage/DemoChalkableDepartmentStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Chalkable.Data.Master.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool.Storage
+{
+    public class DemoChalkableDepartmentStorage:BaseDemoGuidStorage<ChalkableDepartment>
+    {
+        public DemoChalkableDepartmentStorage(DemoStorage storage) : base(storage, x => x.Id)
+        {
+        }
+     
+        public ChalkableDepartment GetByIdOrNull(Guid id)
+        {
+            return data.ContainsKey(id) ? data[id] : null;
+        }
+    }
+}
