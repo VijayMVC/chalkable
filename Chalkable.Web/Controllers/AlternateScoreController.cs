09f1292e41c20c9380881998c6da225195db3a4a
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-01-14 17:26:25 +0200

    fixed for CHLK-3648 [56640] Comprehensive Progress report: The order of the listed students should be consistent with the Grades screen.
    only for ordering by name

---
+++ Chalkable.Web/Controllers/AlternateScoreController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Chalkable.Web.ActionFilters;
+using Chalkable.Web.Models;
+
+namespace Chalkable.Web.Controllers
+{
+    public class AlternateScoreController : ChalkableController
+    {
+        [AuthorizationFilter("AdminGrade, AdminEdit, AdminView, Teacher, Student")]
+        public ActionResult List()
+        {
+            var scores = AlternateScoreViewData.Create(SchoolLocator.AlternateScoreService.GetAlternateScores());
+            return Json(scores, 3);
+        }
+
+        
+    }
+}
