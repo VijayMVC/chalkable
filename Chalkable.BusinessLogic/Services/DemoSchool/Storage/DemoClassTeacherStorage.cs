d182381ed1ee81bb3a013de6cd0203d790933b2e
Andrew Machnew <amachnew@emp-game.com>
2015-01-14 18:09:09 +0200

    fixed developer profile page styling

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/Storage/DemoClassTeacherStorage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.BusinessLogic.Services.DemoSchool.Common;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool.Storage
+{
+    public class DemoClassTeacherStorage : BaseDemoIntStorage<ClassTeacher>
+    {
+        public DemoClassTeacherStorage(DemoStorage storage) : base(storage, null, true)
+        {
+        }
+
+        public IList<ClassTeacher> GetClassTeachers(int? classId, int? teacherId)
+        {
+            var res = data.Select(x => x.Value);
+            if (classId.HasValue)
+                res = res.Where(x => x.ClassRef == classId.Value);
+            if (teacherId.HasValue)
+                res = res.Where(x => x.PersonRef == teacherId.Value);
+            return res.ToList();
+        }
+
+        public bool Exists(int? classId, int? teacherId)
+        {
+            return GetClassTeachers(classId, teacherId).Count > 0;
+        }
+
+    }
+}
