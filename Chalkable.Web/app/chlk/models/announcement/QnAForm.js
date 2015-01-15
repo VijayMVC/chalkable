44bde41a62e56a7b237bac0d580d805e5cb28254
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-01-15 17:40:40 +0200

    fixed for CHLK-3654 [56657] Scores Feed View - Attachment indicator has been removed

---
+++ Chalkable.Web/app/chlk/models/announcement/QnAForm.js
@@ -0,0 +1,16 @@
+REQUIRE('chlk.models.id.AnnouncementQnAId');
+REQUIRE('chlk.models.id.AnnouncementId');
+
+NAMESPACE('chlk.models.announcement', function () {
+    "use strict";
+    /** @class chlk.models.announcement.QnAForm*/
+    CLASS(
+        'QnAForm', [
+            chlk.models.id.AnnouncementId, 'announcementId',
+            chlk.models.id.AnnouncementQnAId, 'id',
+            String, 'question',
+            String, 'answer',
+            String, 'updateType'
+        ]);
+});
+
