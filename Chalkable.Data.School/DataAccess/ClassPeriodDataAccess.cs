4b79112fc7191c39d7dca6d5f61477c25b3d10cd
Volodymyr Iatsyshyn <viatsyshyn@emp-game.com>
2015-01-15 12:27:24 +0200

    fixed class bar group title

---
+++ Chalkable.Data.School/DataAccess/ClassPeriodDataAccess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Chalkable.Common;
+using Chalkable.Data.Common;
+using Chalkable.Data.Common.Orm;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Data.School.DataAccess
+{
+
+    public class ClassPeriodDataAccess : BaseSchoolDataAccess<ClassPeriod>
+    {
+        public ClassPeriodDataAccess(UnitOfWork unitOfWork, int? schoolId)
+            : base(unitOfWork, schoolId)
+        {
+        }
+
+        public void FullDelete(int periodId, int classId, int dayTypeId)
+        {
+            var conds = new AndQueryCondition();
+            conds.Add(ClassPeriod.CLASS_REF_FIELD, classId);
+            conds.Add(ClassPeriod.PERIOD_REF_FIELD, periodId);
+            conds.Add(ClassPeriod.DAY_TYPE_REF_FIELD, dayTypeId);
+            SimpleDelete(conds);
+        }
+
+        public Class CurrentClassForTeacher(int schoolYearId, int personId, DateTime date, int time)
+        {
+            IDictionary<string, object> ps = new Dictionary<string, object>
+            {
+                {"@schoolYearId", schoolYearId},
+                {"@teacherId", personId},
+                {"@date", date},
+                {"@time", time},
+            };
+            using (var reader = ExecuteStoredProcedureReader("spCurrentClassForTeacher", ps))
+            {
+                return reader.ReadOrNull<Class>();
+            }
+        }
+    }
+}
