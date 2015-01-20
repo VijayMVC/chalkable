4a61419ceb6062f45897bbcb0cc3c11c456bcf50
Andrew Machnew <amachnew@emp-game.com>
2015-01-20 16:50:56 +0200

    fixed quick app install dialog

---
+++ Chalkable.Web/app/chlk/models/student/StudentProfileDisciplineViewData.js
@@ -0,0 +1,31 @@
+REQUIRE('chlk.models.people.UserProfileViewData');
+REQUIRE('chlk.models.discipline.StudentDisciplineSummary');
+REQUIRE('chlk.models.calendar.discipline.StudentDisciplineMonthCalendar');
+REQUIRE('chlk.models.schoolYear.MarkingPeriod');
+
+NAMESPACE('chlk.models.student', function(){
+    "use strict";
+
+    /**@class chlk.models.student.StudentProfileDisciplineViewData*/
+    CLASS('StudentProfileDisciplineViewData',EXTENDS(chlk.models.people.UserProfileViewData.OF(chlk.models.discipline.StudentDisciplineSummary)), [
+
+        chlk.models.calendar.discipline.StudentDisciplineMonthCalendar, 'disciplineCalendar',
+        ArrayOf(chlk.models.schoolYear.MarkingPeriod), 'markingPeriods',
+
+        chlk.models.discipline.StudentDisciplineSummary, function getSummaryInfo(){
+            return this.getUser();
+        },
+
+        [[chlk.models.common.Role,
+            chlk.models.discipline.StudentDisciplineSummary
+            , chlk.models.calendar.discipline.StudentDisciplineMonthCalendar
+            , ArrayOf(chlk.models.schoolYear.MarkingPeriod)
+            , ArrayOf(chlk.models.people.Claim)
+        ]],
+        function $(role, summaryInfo, disciplineCalendar, markingPeriods, claims_){
+            BASE(role, summaryInfo, claims_);
+            this.setDisciplineCalendar(disciplineCalendar);
+            this.setMarkingPeriods(markingPeriods);
+        }
+    ]);
+});
