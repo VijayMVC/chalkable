71c720e43b4c8ecb65b59516786068e43acb74ed
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-01-21 18:31:08 +0200

    updated Grading Stat chart

---
+++ Chalkable.Web/Controllers/GradingPeriodController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Chalkable.Web.ActionFilters;
+using Chalkable.Web.Models;
+
+namespace Chalkable.Web.Controllers
+{
+    [RequireHttps, TraceControllerFilter]
+    public class GradingPeriodController : ChalkableController
+    {
+        [AuthorizationFilter("SysAdmin, AdminGrade, AdminEdit, AdminView, Teacher, Student")]
+        public ActionResult List(int? schoolYearId)
+        {
+            schoolYearId = schoolYearId ?? GetCurrentSchoolYearId();
+            var res = SchoolLocator.GradingPeriodService.GetGradingPeriodsDetails(schoolYearId.Value);
+            return Json(res.Select(GradingPeriodViewData.Create));
+        }
+    }
+}
