7bb5b9d1e4e39c76b98f7bd170a51904c937ca92
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-01-23 04:15:30 -0700

    fixed Get Student For install

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/Storage/DemoStandardSubjectStorage.cs
@@ -0,0 +1,11 @@
+﻿using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool.Storage
+{
+    public class DemoStandardSubjectStorage:BaseDemoIntStorage<StandardSubject>
+    {
+        public DemoStandardSubjectStorage(DemoStorage storage) : base(storage, x => x.Id)
+        {
+        }
+    }
+}
