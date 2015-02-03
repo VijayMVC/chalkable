9960b119aeef3339a650164221b932000167223d
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-02-03 15:59:40 +0200

    fixed for CHLK-3736 Teacher. Grades. Teacher can't download reports, when 'start date' and 'end date' are the same

---
+++ Chalkable.Web/Models/AnnouncementsViewData/AnnouncementStandardViewData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Web.Models.AnnouncementsViewData
+{
+
+    public class AnnouncementStandardViewData
+    {
+        public int StandardId { get; set; }
+        public int? ParentStandardId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int StandardSubjectId { get; set; }
+        public string CCStandardCode { get; set; }
+        public Guid? AcademicBenchmarkId { get; set; }
+        
+        protected AnnouncementStandardViewData(){}
+
+        protected AnnouncementStandardViewData(Standard standard)
+        {
+            StandardId = standard.Id;
+            Description = standard.Description;
+            Name = standard.Name;
+            ParentStandardId = standard.ParentStandardRef;
+            StandardSubjectId = standard.StandardSubjectRef;
+            AcademicBenchmarkId = standard.AcademicBenchmarkId;
+            //CCStandardCode = standard.CCStandardCode;//TODO: get from AB to CC mapping
+        }
+
+        public static AnnouncementStandardViewData Create(Standard standard)
+        {
+            return new AnnouncementStandardViewData(standard);
+        }
+
+        public static IList<AnnouncementStandardViewData> Create(IList<Standard> standards)
+        {
+            return standards.Select(Create).ToList();
+        }
+        public static IList<AnnouncementStandardViewData> Create(IList<AnnouncementStandardDetails> standardDetailses)
+        {
+            return standardDetailses.Select(x => Create(x.Standard)).ToList();
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/templates/apps/MiniQuizDialog.js
@@ -0,0 +1,32 @@
+REQUIRE('chlk.templates.ChlkTemplate');
+
+REQUIRE('chlk.models.apps.MiniQuizViewData');
+
+NAMESPACE('chlk.templates.apps', function () {
+
+    /** @class chlk.templates.apps.MiniQuizDialog*/
+    CLASS(
+        [ria.templates.TemplateBind('~/assets/jade/activities/apps/mini-quiz-dialog.jade')],
+        [ria.templates.ModelBind(chlk.models.apps.MiniQuizViewData)],
+        'MiniQuizDialog', EXTENDS(chlk.templates.ChlkTemplate), [
+            [ria.templates.ModelPropertyBind],
+            ArrayOf(chlk.models.standard.Standard), 'standards',
+
+            [ria.templates.ModelPropertyBind],
+            chlk.models.id.StandardId, 'currentStandardId',
+
+            [ria.templates.ModelPropertyBind],
+            chlk.models.apps.Application, 'applicationInfo',
+
+            [ria.templates.ModelPropertyBind],
+            ArrayOf(chlk.models.apps.Application), 'installedApplications',
+
+            [ria.templates.ModelPropertyBind],
+            ArrayOf(chlk.models.apps.Application), 'recommendedApplications',
+
+            chlk.models.standard.Standard, function getCurrentStandard() {
+                var c = this.getCurrentStandardId();
+                return this.standards.filter(function (_) { return _.getStandardId() == c; })[0];
+            }
+        ])
+});
