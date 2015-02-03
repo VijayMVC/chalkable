29a25ff887c74bd861319dd98fb46211555f5ae2
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-02-03 17:02:24 +0200

    css transition fixes

---
+++ Chalkable.StiConnector/Connectors/LinkConnector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using Chalkable.StiConnector.Connectors.Model;
+
+namespace Chalkable.StiConnector.Connectors
+{
+    public class LinkConnector : ConnectorBase
+    {
+        public LinkConnector(ConnectorLocator locator)
+            : base(locator)
+        {
+        }
+        public bool Link(Guid key)
+        {
+            var url = string.Format("{0}chalkable/linkstatus?LinkKey={1}", BaseUrl, key);
+            return Call<string>(url).Equals("active", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public void CompleteSync(int schoolId)
+        {
+            var url = string.Format("{0}chalkable/school/{1}", BaseUrl, schoolId);
+            Post<object, object>(url, new { Id = schoolId, IsSyncComplete = true}, null, HttpMethod.Put);
+        }
+    }
+}
---
+++ Chalkable.Web/Models/ChalkableApiExplorerViewData/ParamViewData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chalkable.Web.Models.ChalkableApiExplorerViewData
+{
+    public class ParamViewData
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Description { get; set; }
+        public bool IsNullable { get; set; }
+        public ParamType ParamType { get; set; }
+
+        public static ParamViewData Create(Param param)
+        {
+            return new ParamViewData
+            {
+                Name = param.Name,
+                Value = param.Value,
+                Description = param.Description,
+                IsNullable = param.IsNullable,
+                ParamType = param.ParamType
+            };
+        }
+    }
+
+}
