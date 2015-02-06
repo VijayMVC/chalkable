066a25b2deb1abb19e67fd42dff1871ca4999e11
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-02-06 16:56:59 +0200

    fixed for CHLK-3749 [56939] When saving a newly created item in Chalkable, there's nothing to indicate whether or not it's saving, and user receives error if clicking again

--- Chalkable.Web/app/chlk/activities/lib/PendingActionDialog.js
+++ Chalkable.Web/app/chlk/activities/lib/PendingActionDialog.js
@@ -5,11 +5,11 @@ NAMESPACE('chlk.activities.lib', function () {
 
     /**@class chlk.activities.lib.PendingActionDialog*/
     CLASS(
-        [ria.mvc.DomAppendTo('#main-wrapper')],
+        [ria.mvc.DomAppendTo('#main')],
         [ria.mvc.ActivityGroup('InfoMessage')],
         'PendingActionDialog', EXTENDS(ria.mvc.DomActivity), [
             OVERRIDE, ria.dom.Dom, function onDomCreate_() {
-                return new ria.dom.Dom().fromHTML('<div class="pending-action"><div class="horizontal-loader"></div></div>');
+                return new ria.dom.Dom().fromHTML('<div class="loading-page"></div>');
             }
         ]);
 });
