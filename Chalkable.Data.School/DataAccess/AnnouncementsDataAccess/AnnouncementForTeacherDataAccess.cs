b1e828a5fc402d438021b82d031721392ebf5bc1
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-02-12 13:14:23 +0200

    added tooltips with numeric values for Study Center

---
+++ Chalkable.Data.School/DataAccess/AnnouncementsDataAccess/AnnouncementForTeacherDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Chalkable.Common;
+using Chalkable.Data.Common;
+using System.Linq;
+
+namespace Chalkable.Data.School.DataAccess.AnnouncementsDataAccess
+{
+    public class AnnouncementForTeacherDataAccess : AnnouncementDataAccess
+    {
+        public AnnouncementForTeacherDataAccess(UnitOfWork unitOfWork, int? schoolId) : base(unitOfWork, schoolId)
+        {
+        }
+        private const string GET_TEACHER_ANNOUNCEMENTS = "spGetTeacherAnnouncements";
+        private const string GRADED_ONLY_PARAM = "gradedOnly";
+        private const string ALL_SCHOOL_ITEMS_PARAM = "allSchoolItems";
+
+        public override AnnouncementQueryResult GetAnnouncements(AnnouncementsQuery query)
+        {
+            var parameters = new Dictionary<string, object>
+                {
+                    {GRADED_ONLY_PARAM, query.GradedOnly},
+                    {ALL_SCHOOL_ITEMS_PARAM, query.AllSchoolItems},
+                    {"@sisActivitiesIds", query.SisActivitiesIds != null ? query.SisActivitiesIds.Select(x => x.ToString()).JoinString(",") : null}
+                };
+            return GetAnnouncementsComplex(GET_TEACHER_ANNOUNCEMENTS, parameters, query);
+        }
+        protected override void BuildConditionForGetSimpleAnnouncement(Common.Orm.DbQuery dbQuery, int role, int callerId)
+        {
+            dbQuery.Sql.Append(" and ");
+            dbQuery.Sql.Append(@" (Announcement.ClassRef in (select ClassTeacher.ClassRef from ClassTeacher where ClassTeacher.PersonRef = @callerId))");
+            dbQuery.Parameters.Add("callerId", callerId);
+            dbQuery.Parameters.Add("@roleId", role);
+        }
+    }
+}
---
+++ Chalkable.Data.School/DataAccess/AutoGradeDataAccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.Common;
+using Chalkable.Data.Common.Orm;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Data.School.DataAccess
+{
+    public class AutoGradeDataAccess : DataAccessBase<AutoGrade, int>
+    {
+        public AutoGradeDataAccess(UnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public AutoGrade GetAutoGrade(int announcementApplicationId, int studentId)
+        {
+           var res = Orm.SimpleSelect<AutoGrade>(new AndQueryCondition
+                {
+                    {AutoGrade.ANNOUNCEMENT_APPLICATION_REF_FIELD, announcementApplicationId},
+                    {AutoGrade.STUDENT_REF_FIELD, studentId}
+                });
+            return ReadOne<AutoGrade>(res);
+        }
+    }
+}
