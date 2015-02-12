43d47c592953b868af296b6efda722c654b22fc1
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-02-12 16:43:43 +0200

    fixed for CHLK-3771 [57039] Study Center - Explorer. Selecting to Show More standards for a section does not allow you to shrink the list.

---
+++ Chalkable.BusinessLogic/Services/Master/CommonCoreStandardService.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Chalkable.BusinessLogic.Security;
+using Chalkable.Common.Exceptions;
+using Chalkable.Data.Common.Orm;
+using Chalkable.Data.Master.DataAccess;
+using Chalkable.Data.Master.Model;
+
+namespace Chalkable.BusinessLogic.Services.Master
+{
+    public interface ICommonCoreStandardService
+    {
+        void AddStandards(IList<CommonCoreStandard> commonCoreStandards);
+        void AddStandardsCategories(IList<CommonCoreStandardCategory> standardCategories);
+        void AddABToCCMapping(IList<ABToCCMapping> abtoCcMappings);
+
+        IList<ABToCCMapping> GetABToCCMappings(Guid? academicBenchmarkId, Guid? ccStandardId); 
+        IList<CommonCoreStandard> GetStandards(Guid? standardCategoryId = null, Guid? parentStandardId = null, bool allStandards = true);
+        IList<CommonCoreStandardCategory> GetCCStandardCategories();
+        CommonCoreStandard GetStandardByABId(Guid academicBenchmarkId);
+        IList<CommonCoreStandard> GetStandardsByABIds(IList<Guid> academicBenchmarkIds);
+        IList<CommonCoreStandard> GetStandards(string filter);
+    }
+
+    public class CommonCoreStandardService : MasterServiceBase, ICommonCoreStandardService
+    {
+        public CommonCoreStandardService(IServiceLocatorMaster serviceLocator) : base(serviceLocator)
+        {
+        }
+
+        public IList<CommonCoreStandard> GetStandards(Guid? standardCategoryId, Guid? parentStandardId, bool allStandards = true)
+        {
+            var conds = new AndQueryCondition();
+            if (standardCategoryId.HasValue)
+                conds.Add(CommonCoreStandard.STANDARD_CATEGORY_REF_FIELD, standardCategoryId);
+            if(!allStandards || parentStandardId.HasValue)
+                conds.Add(CommonCoreStandard.PARENT_STANDARD_REF_FIELD, parentStandardId);
+            return DoRead(u => new CommonCoreStandardDataAccess(u).GetAll(conds));
+        }
+
+        public IList<CommonCoreStandardCategory> GetCCStandardCategories()
+        {
+            return DoRead(u => new CC_StandardCategoryDataAccess(u).GetAll());
+        }
+
+
+        public CommonCoreStandard GetStandardByABId(Guid academicBenchmarkId)
+        {
+            var abIds = new List<Guid> {academicBenchmarkId};
+            return GetStandardsByABIds(abIds).FirstOrDefault();
+        }
+
+        public IList<CommonCoreStandard> GetStandardsByABIds(IList<Guid> academicBenchmarkIds)
+        {
+            if(academicBenchmarkIds.Count == 0) return new List<CommonCoreStandard>();
+            return DoRead(uow => new CommonCoreStandardDataAccess(uow).GetByABIds(academicBenchmarkIds));
+        }
+
+
+        public IList<CommonCoreStandard> GetStandards(string filter)
+        {
+            if(string.IsNullOrEmpty(filter)) return new List<CommonCoreStandard>();
+
+            return DoRead(uow => new CommonCoreStandardDataAccess(uow).GetByFilter(filter));
+        }
+
+        public void AddStandards(IList<CommonCoreStandard> commonCoreStandards)
+        {
+            //if (!BaseSecurity.IsSysAdmin(Context))
+            //    throw new ChalkableSecurityException();
+            DoUpdate(uow=> new CommonCoreStandardDataAccess(uow).Insert(commonCoreStandards));
+        }
+
+        public void AddStandardsCategories(IList<CommonCoreStandardCategory> standardCategories)
+        {
+            //if (!BaseSecurity.IsSysAdmin(Context))
+            //    throw new ChalkableSecurityException();
+
+            DoUpdate(uow => new CC_StandardCategoryDataAccess(uow).Insert(standardCategories));
+        }
+
+        public void AddABToCCMapping(IList<ABToCCMapping> abtoCcMappings)
+        {
+            //if (!BaseSecurity.IsSysAdmin(Context))
+            //    throw new ChalkableSecurityException();
+            DoUpdate(uow => new ABToCCMappingDataAccess(uow).Insert(abtoCcMappings));
+        }
+
+
+        public IList<ABToCCMapping> GetABToCCMappings(Guid? academicBenchmarkId, Guid? ccStandardId)
+        {
+            var conds = new AndQueryCondition();
+            if(academicBenchmarkId.HasValue)
+                conds.Add(ABToCCMapping.ACADEMIC_BENCHMARK_ID_FIELD, academicBenchmarkId);
+            if(ccStandardId.HasValue)
+                conds.Add(ABToCCMapping.CC_STANADARD_REF_FIELD, ccStandardId);
+            return DoRead(uow => new ABToCCMappingDataAccess(uow).GetAll(conds));
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/services/StandardService.js
@@ -0,0 +1,69 @@
+REQUIRE('chlk.services.BaseService');
+REQUIRE('ria.async.Future');
+
+REQUIRE('chlk.models.id.ClassId');
+REQUIRE('chlk.models.id.StandardSubjectId');
+REQUIRE('chlk.models.id.StandardId');
+
+REQUIRE('chlk.models.standard.CommonCoreStandard');
+REQUIRE('chlk.models.standard.CCStandardCategory');
+REQUIRE('chlk.models.id.CCStandardCategoryId');
+
+NAMESPACE('chlk.services', function () {
+    "use strict";
+
+    /** @class chlk.services.StandardService*/
+    CLASS(
+        'StandardService', EXTENDS(chlk.services.BaseService), [
+
+            [[chlk.models.id.ClassId]],
+            ria.async.Future, function getSubjects(classId_) {
+                return this.get('Standard/GetStandardSubject.json', ArrayOf(chlk.models.standard.StandardSubject), {
+                    classId: classId_ && classId_.valueOf()
+                });
+            },
+
+            [[chlk.models.id.ClassId, chlk.models.id.StandardSubjectId, chlk.models.id.StandardId]],
+            ria.async.Future, function getStandards(classId_, subjectId_, standardId_) {
+                return this.get('Standard/GetStandards.json', ArrayOf(chlk.models.standard.Standard), {
+                    classId: classId_ && classId_.valueOf(),
+                    subjectId: subjectId_ && subjectId_.valueOf(),
+                    parentStandardId: standardId_ && standardId_.valueOf()
+                });
+            },
+
+            [[ArrayOf(chlk.models.id.StandardId)]],
+            ria.async.Future, function getStandardsList(ids) {
+                return this.get('Standard/GetStandardsByIds.json', ArrayOf(chlk.models.standard.Standard), {
+                    ids: this.arrayToIds(ids)
+                }).then(function(model){
+                    var res = [];
+                    ids.forEach(function(id){
+                        res.push(model.filter(function(item){return item.getStandardId() == id})[0]);
+                    });
+                    return res;
+                });
+            },
+
+            [[String]],
+            ria.async.Future, function searchStandards(filter) {
+                return this.get('Standard/SearchStandards.json', ArrayOf(chlk.models.standard.Standard), {
+                    filter: filter
+                });
+            },
+
+            [[chlk.models.id.CCStandardCategoryId, chlk.models.id.CommonCoreStandardId, Boolean]],
+            ria.async.Future, function getCommonCoreStandards(standardCategoryId_, parentStandardId_, allStandards_) {
+                return this.get('Standard/GetCommonCoreStandards.json', ArrayOf(chlk.models.standard.CommonCoreStandard), {
+                    standardCategoryId: standardCategoryId_ && standardCategoryId_.valueOf(),
+                    parentStandardId: parentStandardId_ && parentStandardId_.valueOf(),
+                    allStandards: allStandards_
+                });
+            },
+
+            ria.async.Future, function getCCStandardCategories(){
+                return this.get('Standard/GetCommonCoreStandardCategories.json', ArrayOf(chlk.models.standard.CCStandardCategory), {
+                });
+            }
+        ])
+});
