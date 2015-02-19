656de247ef1a2396d5ac0a2124ce22edc75c0a49
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-02-19 16:21:27 +0200

    fixed for CHLK-3792 [57115] Study Center Teacher Explorer - Students name needs to be listed as the other links have it listed

---
+++ Chalkable.Web/app/chlk/templates/district/DistrictDialog.js
@@ -0,0 +1,28 @@
+REQUIRE('chlk.templates.ChlkTemplate');
+
+REQUIRE('chlk.models.district.District');
+REQUIRE('chlk.models.id.DistrictId');
+
+NAMESPACE('chlk.templates.district', function () {
+
+    /** @class chlk.templates.district.DistrictDialog*/
+    CLASS(
+        [ria.templates.TemplateBind('~/assets/jade/activities/district/district-dialog.jade')],
+        [ria.templates.ModelBind(chlk.models.district.District)],
+        'DistrictDialog', EXTENDS(chlk.templates.ChlkTemplate), [
+            [ria.templates.ModelPropertyBind],
+            chlk.models.id.DistrictId, 'id',
+            [ria.templates.ModelPropertyBind],
+            String, 'name',
+            [ria.templates.ModelPropertyBind],
+            String, 'sisUrl',
+            [ria.templates.ModelPropertyBind],
+            String, 'dbName',
+            [ria.templates.ModelPropertyBind],
+            String, 'sisUserName',
+            [ria.templates.ModelPropertyBind],
+            String, 'sisPassword',
+            [ria.templates.ModelPropertyBind],
+            Number, 'sisSystemType'
+        ])
+});
