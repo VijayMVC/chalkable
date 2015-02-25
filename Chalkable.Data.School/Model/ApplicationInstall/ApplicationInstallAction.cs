689078d1444de3a99ee2baccb9301ad907a2f20f
Volodymyr Iatsyshyn <viatsyshyn@emp-game.com>
2015-02-25 18:13:42 +0200

    implemented FixedTop dialogs annotation refactored mini quiz added FixedToo to AppInstall and QuickAppInstall

---
+++ Chalkable.Data.School/Model/ApplicationInstall/ApplicationInstallAction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.Common;
+
+namespace Chalkable.Data.School.Model.ApplicationInstall
+{
+    public class ApplicationInstallAction
+    {
+        public const string ID_FIELD = "Id";
+        public const string OWNER_REF_FIELD = "OwnerRef";
+        public const string APPLICATION_REF_FIELD = "ApplicationRef";
+
+        [IdentityFieldAttr]
+        [PrimaryKeyFieldAttr]
+        public int Id { get; set; }
+        public int OwnerRef { get; set; }
+        public int? PersonRef { get; set; }
+        public Guid ApplicationRef { get; set; }
+        public string Description { get; set; }
+
+        [NotDbFieldAttr]
+        public IList<ApplicationInstall> ApplicationInstalls { get; set; } 
+    }
+}
---
+++ Chalkable.Web/app/chlk/templates/school/Schools.js
@@ -0,0 +1,19 @@
+REQUIRE('chlk.models.school.SchoolListViewData');
+REQUIRE('chlk.models.school.School');
+REQUIRE('chlk.models.district.District');
+REQUIRE('chlk.models.id.DistrictId');
+
+NAMESPACE('chlk.templates.school', function () {
+
+    /** @class chlk.templates.school.Schools*/
+    CLASS(
+        [ria.templates.TemplateBind('~/assets/jade/activities/school/Schools.jade')],
+        [ria.templates.ModelBind(chlk.models.school.SchoolListViewData)],
+        'Schools', EXTENDS(chlk.templates.ChlkTemplate), [
+
+            [ria.templates.ModelPropertyBind],
+            chlk.models.common.PaginatedList, 'paginatedSchools',
+            [ria.templates.ModelPropertyBind],
+            chlk.models.id.DistrictId, 'districtId'
+        ])
+});
