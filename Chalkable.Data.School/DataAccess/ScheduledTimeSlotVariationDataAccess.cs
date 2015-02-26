145e9b0d44719455ef736cce7d7e7a83970581ff
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-02-26 18:34:22 +0200

    fixed Announcement model and Dialogs updating

---
+++ Chalkable.Data.School/DataAccess/ScheduledTimeSlotVariationDataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Chalkable.Data.Common;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Data.School.DataAccess
+{
+    public class ScheduledTimeSlotVariationDataAccess : BaseSchoolDataAccess<ScheduledTimeSlotVariation>
+    {
+        public ScheduledTimeSlotVariationDataAccess(UnitOfWork unitOfWork, int? localSchoolId) : base(unitOfWork, localSchoolId)
+        {
+        }
+
+        public void Delete(IList<ScheduledTimeSlotVariation> scheduledTimeSlotVariations)
+        {
+            SimpleDelete(scheduledTimeSlotVariations);
+        }
+    }
+}
