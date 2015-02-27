db1f3cb832b12192b3627733a6f83b2017000bc2
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-02-27 09:44:07 -0700

    added CateforyDropped, AvgDropped, AnnouncementDropped to Score model

---
+++ Chalkable.Web/app/chlk/models/discipline/DisciplineType.js
@@ -0,0 +1,11 @@
+REQUIRE('chlk.models.id.DisciplineTypeId');
+
+NAMESPACE('chlk.models.discipline', function(){
+    "use strict";
+
+    /** @class chlk.models.discipline.DisciplineType*/
+    CLASS('DisciplineType', [
+         chlk.models.id.DisciplineTypeId, 'id',
+         String, 'name'
+    ]);
+});
