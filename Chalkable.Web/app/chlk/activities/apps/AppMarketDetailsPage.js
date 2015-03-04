fad43a37736810c645ffeffa99e2df848ce4767d
Volodymyr Iatsyshyn <viatsyshyn@emp-game.com>
2015-03-04 17:39:46 +0200

    fix for CHLK-3761

--- Chalkable.Web/app/chlk/activities/apps/AppMarketDetailsPage.js
+++ Chalkable.Web/app/chlk/activities/apps/AppMarketDetailsPage.js
@@ -16,6 +16,7 @@ NAMESPACE('chlk.activities.apps', function () {
 
             OVERRIDE, VOID, function onRender_(model){
                 BASE(model);
+
                 jQuery('textarea').autoResize({
                     limit: 9999,
                     animateDuration : 100,
@@ -24,6 +25,8 @@ NAMESPACE('chlk.activities.apps', function () {
                         node.scrollTo('10000px');
                     }
                 });
+
+                this.refreshInlineTabsState_();
             },
 
             [[Object, String]],
@@ -34,6 +37,8 @@ NAMESPACE('chlk.activities.apps', function () {
                     this.dom.find('.side-panel')
                             .find('input[value=' + model.getApplicationRating().getAverage() + ']').setAttr('checked', true);
                 }
+
+                this.refreshInlineTabsState_();
             },
 
             [ria.mvc.DomEventBind('click', '.write-review-btn')],
@@ -43,18 +48,9 @@ NAMESPACE('chlk.activities.apps', function () {
                 this.dom.find('.rating-hint').hide();
             },
 
-            [ria.mvc.DomEventBind('click', '.cancel-btn')],
-            [[ria.dom.Dom, ria.dom.Event]],
-            VOID, function cancelReview(node, event){
-                this.dom.find('.add-review').addClass('x-hidden');
-                this.dom.find('.rating-hint').hide();
-                this.dom.find('.review-text').setValue('');
-                this.dom.find('[name=newRating]:checked').setAttr('checked', false);
-            },
-
             [ria.mvc.DomEventBind('click', '.submit-review-btn')],
             [[ria.dom.Dom, ria.dom.Event]],
-            VOID, function submitReview(node, event){
+            Boolean, function submitReview(node, event){
                 var rating = this.dom.find('[name=newRating]:checked').getValue();
                 if (!rating){
                     this.dom.find('.rating-hint').show();
@@ -62,11 +58,11 @@ NAMESPACE('chlk.activities.apps', function () {
                 }
             },
 
-            [ria.mvc.DomEventBind('click', '.review-link')],
+            /*[ria.mvc.DomEventBind('click', '.review-link')],
             [[ria.dom.Dom, ria.dom.Event]],
             VOID, function toggleRoleReviews(node, event){
                  this.dom.find('.rating-roles').toggleClass('x-hidden');
-            },
+            },*/
 
             OVERRIDE, VOID, function onPause_() {
                 BASE();
@@ -76,6 +72,36 @@ NAMESPACE('chlk.activities.apps', function () {
             OVERRIDE, VOID, function onResume_() {
                 BASE();
                 this.dom.find('iframe[wmode]').removeClass('x-hidden');
+            },
+
+            VOID, function refreshInlineTabsState_() {
+                this.dom.find('.inline-tabs').forEach(function ($tab) {
+                    var activeTab = $tab.find('.tab.active[data-tab-content]').first();
+
+                    if (!activeTab.exists())
+                        activeTab = $tab.find('.tab[data-tab-content]').first();
+
+                    if (activeTab.exists()) {
+                        this.onTabClick(activeTab, null);
+                    }
+                }.bind(this))
+            },
+
+            [ria.mvc.DomEventBind('click', '.tab[data-tab-content]:not(.active)')],
+            [[ria.dom.Dom, ria.dom.Event]],
+            Boolean, function onTabClick($node, event) {
+                var $parent = $node.parent().parent();
+
+                Assert($parent.hasClass('inline-tabs'));
+
+                var content = $node.getData('tab-content');
+                $parent.find('.active')
+                    .filter(function ($node) { return $node.getData('tab-content') != content; })
+                    .removeClass('active');
+
+                $parent.find('[data-tab-content=' + content + ']').addClass('active');
+
+                return false;
             }
         ]);
 });
