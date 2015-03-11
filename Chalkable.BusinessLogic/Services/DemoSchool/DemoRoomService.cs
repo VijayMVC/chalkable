86ace7044410c19dc19e596e3f58e35db35a909c
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-03-11 18:26:02 +0200

    fixed DatePicker control

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/DemoRoomService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Chalkable.BusinessLogic.Security;
+using Chalkable.BusinessLogic.Services.DemoSchool.Storage;
+using Chalkable.BusinessLogic.Services.School;
+using Chalkable.Common;
+using Chalkable.Common.Exceptions;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool
+{
+    
+    public class DemoRoomService : DemoSchoolServiceBase, IRoomService
+    {
+        public DemoRoomService(IServiceLocatorSchool serviceLocator, DemoStorage storage) : base(serviceLocator, storage)
+        {
+        }
+
+        public Room AddRoom(int id, int schoolId, string roomNumber, string description, string size, int? capacity, string phoneNumber)
+        {
+            if (!BaseSecurity.IsDistrict(Context))
+                throw new ChalkableSecurityException();
+            var room = new Room
+            {
+                Id = id,
+                SchoolRef = schoolId,
+                Capacity = capacity,
+                Description = description,
+                PhoneNumber = phoneNumber,
+                RoomNumber = roomNumber,
+                Size = size,
+            };
+            Storage.RoomStorage.Add(room);
+            return room;
+
+        }
+
+        public void AddRooms(IList<Room> rooms)
+        {
+            if (!BaseSecurity.IsDistrict(Context))
+                throw new ChalkableSecurityException();
+            Storage.RoomStorage.AddRooms(rooms);
+        }
+
+        public Room EditRoom(int id, string roomNumber, string description, string size, int? capacity, string phoneNumber)
+        {
+            if (!BaseSecurity.IsDistrict(Context))
+                throw new ChalkableSecurityException();
+            return Storage.RoomStorage.Edit(id, roomNumber, description, size, capacity, phoneNumber);
+
+        }
+
+        public void EditRooms(IList<Room> rooms)
+        {
+            if (!BaseSecurity.IsDistrict(Context))
+                throw new ChalkableSecurityException();
+            Storage.RoomStorage.Update(rooms);
+        }
+
+        public void DeleteRooms(IList<int> ids)
+        {
+            if (!BaseSecurity.IsDistrict(Context))
+                throw new ChalkableSecurityException();
+            Storage.RoomStorage.Delete(ids);
+        }
+
+        public PaginatedList<Room> GetRooms(int start = 0, int count = Int32.MaxValue)
+        {
+            return Storage.RoomStorage.GetAll(start, count);
+            
+        }
+
+        public Room WhereIsPerson(int personId, DateTime dateTime)
+        {
+            var c = ServiceLocator.ClassPeriodService.CurrentClassForTeacher(personId, dateTime);
+            return c.RoomRef.HasValue ? GetRoomById(c.RoomRef.Value) : null;
+        }
+
+        public Room GetRoomById(int id)
+        {
+            return Storage.RoomStorage.GetById(id);
+        }
+    }
+}
