0996bf2cef7ec17b79a3c84437da8458c709b13a
Volodymyr Iatsyshyn <viatsyshyn@emp-game.com>
2015-03-11 16:07:35 +0200

    fixed CHLK-3853

---
+++ Chalkable.BusinessLogic/Mapping/ModelMappers/DisciplineReferralToClassDisciplineMapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.School.Model;
+using Chalkable.StiConnector.Connectors.Model;
+
+namespace Chalkable.BusinessLogic.Mapping.ModelMappers
+{
+    public class DisciplineReferralToClassDisciplineMapper : BaseMapper<ClassDiscipline, DisciplineReferral>
+    {
+        protected override void InnerMap(ClassDiscipline returnObj, DisciplineReferral sourceObj)
+        {
+            returnObj.Id = sourceObj.Id;
+            returnObj.ClassId = sourceObj.SectionId;
+            returnObj.StudentId = sourceObj.StudentId;
+            returnObj.Date = sourceObj.Date;
+            returnObj.Description = sourceObj.Note;
+            if (sourceObj.Infractions != null)
+                returnObj.Infractions = sourceObj.Infractions.Select(x => new Data.School.Model.Infraction
+                    {
+                        Id = x.Id,
+                        Name = x.Name
+                    }).ToList();
+        }
+    }
+}
