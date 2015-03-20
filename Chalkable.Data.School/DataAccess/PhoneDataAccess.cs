f647e3a62cca7bdf62c70fb69c20cab41462fc48
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-03-20 08:24:41 -0600

    fixed CHLK-3874 Teacher > Class Profile > Explorer. "Explorer" tab won't be selected(will have grey color), when teacher clicks on it.

---
+++ Chalkable.Data.School/DataAccess/PhoneDataAccess.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Chalkable.Data.Common;
+using Chalkable.Data.Common.Orm;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Data.School.DataAccess
+{
+    public class PhoneDataAccess : DataAccessBase<Phone, int>
+    {
+        public PhoneDataAccess(UnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public void Update(Phone phone, QueryCondition condition)
+        {
+            SimpleUpdate(phone, condition);
+        }
+
+        public IList<Person> GetUsersByPhone(string phone)
+        {
+            var phoneTName = "Phone";
+            var query = new DbQuery();
+            query.Sql.AppendFormat(@"select [{1}].* from [{0}] 
+                                     join [{1}] on [{1}].[{2}] = [{0}].[{3}]"
+                , phoneTName, "Person", Person.ID_FIELD, Phone.PERSON_REF_FIELD);
+            var conds = new AndQueryCondition {{Phone.DIGIT_ONLY_VALUE_FIELD, phone}};
+            conds.BuildSqlWhere(query, phoneTName);
+            return ReadMany<Person>(query);
+        } 
+
+        public Phone GetPhone(int personId, string digitOnlyValue)
+        {
+            var conds = new AndQueryCondition();
+            conds.Add(Phone.PERSON_REF_FIELD, personId);
+            conds.Add(Phone.DIGIT_ONLY_VALUE_FIELD, digitOnlyValue);
+            return GetAll(conds).First();
+        }
+
+        public void Delete(Phone phone)
+        {
+            SimpleDelete(phone);
+        }
+        public void Delete(IList<Phone> phones)
+        {
+            SimpleDelete(phones);
+        }
+    }
+}
---
+++ Chalkable.Web/Controllers/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Chalkable.Data.Common.Enums;
+using Chalkable.Data.Master.Model;
+using Chalkable.Web.ActionFilters;
+using Chalkable.Web.Logic;
+using Chalkable.Web.Models;
+
+namespace Chalkable.Web.Controllers
+{
+    [RequireHttps, TraceControllerFilter]
+    public class SearchController : ChalkableController
+    {
+        [AuthorizationFilter("AdminGrade, AdminEdit, AdminView, Teacher, Student", Preference.API_DESCR_SEARCH_SEARCH, true, CallType.Get, new[] { AppPermissionType.User })]  // - System Admin
+        public ActionResult Search(string query)
+        {
+            IDictionary<SearchTypeEnum, Object> searchRes = new Dictionary<SearchTypeEnum, Object>();
+            query = query.ToLower();
+            searchRes.Add(SearchTypeEnum.Persons, SchoolLocator.PersonService.SearchPersons(query, true, 0, 30));
+            if (Context.SCEnabled)
+                searchRes.Add(SearchTypeEnum.Applications, MasterLocator.ApplicationService.GetApplications(null, null, query, null, null));
+            searchRes.Add(SearchTypeEnum.Announcements, SchoolLocator.AnnouncementService.GetAnnouncements(query));
+            var attachments = SchoolLocator.AnnouncementAttachmentService.GetAttachments(query);
+            searchRes.Add(SearchTypeEnum.Attachments, AttachmentLogic.PrepareAttachmentsInfo(attachments, MasterLocator.CrocodocService));
+            searchRes.Add(SearchTypeEnum.Classes, SchoolLocator.ClassService.SearchClasses(query));
+            return Json(SearchViewData.Create(searchRes));
+        }
+    }
+}
