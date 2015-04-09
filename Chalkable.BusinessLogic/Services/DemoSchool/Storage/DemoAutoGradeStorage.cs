ea3635c640754790471153c8515a48f1df2d11a5
Ievgenii Zasoba <geka.zasoba@gmail.com>
2015-04-09 20:49:07 +0300

    Fixed procedure create script

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/Storage/DemoAutoGradeStorage.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool.Storage
+{
+    public class DemoAutoGradeStorage:BaseDemoIntStorage<AutoGrade>
+    {
+        public DemoAutoGradeStorage(DemoStorage storage)
+            : base(storage, null, true)
+        {
+        }
+
+
+        public void SetAutoGrade(AutoGrade autograde)
+        {
+           var item = data.First(
+                x => x.Value.AnnouncementApplicationRef == autograde.AnnouncementApplicationRef && autograde.StudentRef == x.Value.StudentRef);
+
+            data[item.Key] = autograde;
+        }
+    }
+}
