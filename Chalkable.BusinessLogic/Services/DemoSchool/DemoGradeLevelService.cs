0c3ba463820bac34abf02040ffe8060866da159e
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-04-22 07:24:44 -0600

    create StudentContact, ContactRelationship tables

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/DemoGradeLevelService.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Chalkable.BusinessLogic.Security;
+using Chalkable.BusinessLogic.Services.DemoSchool.Storage;
+using Chalkable.BusinessLogic.Services.School;
+using Chalkable.Common.Exceptions;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool
+{
+  
+    public class DemoGradeLevelService : DemoSchoolServiceBase, IGradeLevelService
+    {
+        public DemoGradeLevelService(IServiceLocatorSchool serviceLocator, DemoStorage storage) : base(serviceLocator, storage)
+        {
+        }
+
+        public IList<GradeLevel> GetGradeLevels()
+        {
+            return Storage.GradeLevelStorage.GetAll();
+        }
+
+        public void Add(IList<GradeLevel> gradeLevels)
+        {
+            if (!BaseSecurity.IsDistrict(Context))
+                throw new ChalkableSecurityException();
+
+            Storage.GradeLevelStorage.Add(gradeLevels);
+        }
+
+        public void Delete(IList<GradeLevel> gradeLevels)
+        {
+            if (!BaseSecurity.IsDistrict(Context))
+                throw new ChalkableSecurityException();
+            Storage.GradeLevelStorage.Delete(gradeLevels);
+        }
+
+        public void Edit(IList<GradeLevel> gradeLevels)
+        {
+            if (!BaseSecurity.IsDistrict(Context))
+                throw new ChalkableSecurityException();
+            Storage.GradeLevelStorage.Update(gradeLevels);
+        }
+    }
+}
---
+++ Chalkable.StiConnector/Connectors/Model/Reports/GradeVerificationReportParams.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chalkable.StiConnector.Connectors.Model.Reports
+{
+    public class GradeVerificationReportParams
+    {
+        public int AcadSessionId { get; set; }
+        public string Header { get; set; }
+        public string DistrictName { get; set; }
+        public int? TeacherId { get; set; }
+        public int? StaffFilterId { get; set; }
+        public string StartSectionNumber { get; set; }
+        public string EndSectionNumber { get; set; }
+
+        /// <summary>
+        /// SectionSortOptions : 
+        /// FullSectionNumber = 0,
+        ///PeriodSectionNumber = 1,
+        ///PeriodTeacher = 2,
+        ///TeacherSectionNumber = 3,
+        ///TeacherPeriod = 4
+        /// </summary>
+        public int SectionOrder { get; set; }
+        ///<summary>
+        /// StudentSortOptionsEnum
+        ///StudentName = 0,
+        ///GradeLevel = 1,
+        ///IDToPrint = 2
+        ///</summary>
+        public int StudentOrder { get; set; }
+
+        public int IdToPrint { get; set; }
+        public int[] GradingPeriodId { get; set; }
+        public int[] GradedItemId { get; set; }
+
+        ///<summary>
+        /// GradeTypeOptions :
+        ///both = 0,
+        ///alpha = 1,
+        ///numeric = 2
+        ///</summary>
+        public int GradeType { get; set; }
+        public bool IncludeNotes { get; set; }
+        public bool IncludeComments { get; set; }
+        public bool IncludeLegend { get; set; }
+        public bool IncludeSignature { get; set; }
+        public bool IncludeWithdrawn { get; set; }
+        public int NumberToDisplay { get; set; }
+        public int[] StudentIds { get; set; }
+        public int? SectionId { get; set; }
+        public int? StaffId { get; set; }
+    }
+}
