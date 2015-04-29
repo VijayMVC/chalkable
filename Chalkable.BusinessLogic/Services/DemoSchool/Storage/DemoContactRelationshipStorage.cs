742a008e021ef954ab8a66234db3feefd34a404d
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-04-29 09:24:16 -0600

    refactoring DataAccess

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/Storage/DemoContactRelationshipStorage.cs
@@ -0,0 +1,21 @@
+﻿using Chalkable.Common;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool.Storage
+{
+    public class DemoContactRelationshipStorage : BaseDemoIntStorage<ContactRelationship>
+    {
+        public DemoContactRelationshipStorage(DemoStorage storage)
+            : base(storage, x => x.Id)
+        {
+        }
+    }
+
+    public class DemoStudentContactStorage : BaseDemoIntStorage<StudentContact>
+    {
+        public DemoStudentContactStorage(DemoStorage storage) : base(storage, x=> x.ContactRef)
+
+        {
+        }
+    }
+}
