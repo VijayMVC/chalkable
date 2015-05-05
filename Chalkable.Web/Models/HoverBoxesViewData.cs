9369963258780e1958e98d73007759542352f110
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-05-05 18:16:52 +0300

    fixed disciplines on profile

---
+++ Chalkable.Web/Models/HoverBoxesViewData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Chalkable.Web.Models
+{
+    public class HoverBoxesViewData<T>
+    {
+        public string Title { get; set; }
+        public IList<T> Hover { get; set; }
+
+        protected const int MAX_HOVER_LIST_NUMBER = 4;
+    }
+}
