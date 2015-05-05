3e5b969da61d326ba517ea15dfe5edae5f6d95fc
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-05-05 03:36:24 -0600

    implemented discipline calendar for student on server

---
+++ Chalkable.StiConnector/Connectors/Model/InfractionSummary.cs
@@ -0,0 +1,18 @@
+﻿namespace Chalkable.StiConnector.Connectors.Model
+{
+    public class InfractionSummary
+    {
+        /// <summary>
+        /// The number of times the discipline infraction occurred for the student
+        /// </summary>
+        public int Occurrences { get; set; }
+        /// <summary>
+        /// Id of the discipline infraction
+        /// </summary>
+        public int InfractionId { get; set; }
+        /// <summary>
+        /// Id of the student
+        /// </summary>
+        public int StudentId { get; set; }
+    }
+}
