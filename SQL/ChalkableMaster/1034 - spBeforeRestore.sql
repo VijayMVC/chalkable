ab22eee434926c224070f174f60bc548443262ed
Ievgenii Zasoba <geka.zasoba@gmail.com>
2015-05-11 17:25:57 +0300

    Added restore procedures for master

---
+++ SQL/ChalkableMaster/1034 - spBeforeRestore.sql
@@ -0,0 +1,18 @@
+Create Procedure spBeforeRestore
+	@districtId uniqueidentifier
+as
+	
+	ALTER TABLE ApplicationRating NOCHECK CONSTRAINT ALL 
+	ALTER TABLE Fund NOCHECK CONSTRAINT ALL
+	ALTER TABLE FundRequest NOCHECK CONSTRAINT ALL
+	ALTER TABLE FundRequestRoleDistribution NOCHECK CONSTRAINT ALL
+
+	Delete from [SchoolUser] where DistrictRef = @districtId
+	Delete from [UserLoginInfo] where Id in
+		(select id from [User] where DistrictRef = @districtId)
+	Delete from [School] where DistrictRef = @districtId
+	Delete from [User] where DistrictRef = @districtId
+
+
+
+	
