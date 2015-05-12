756914f05b699d534c202f00828067e1f460bef1
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-05-12 08:26:38 -0600

    changed routs in get activityCategories

--- Chalkable.StiConnector/Connectors/ActivityCategoryConnnector.cs
+++ Chalkable.StiConnector/Connectors/ActivityCategoryConnnector.cs
@@ -37,7 +37,8 @@ namespace Chalkable.StiConnector.Connectors
             var nvc = new NameValueCollection();
             for (int i = 0; i < sectionIds.Count; i++)
                 nvc.Add(string.Format("sectionIds[{0}]", i), sectionIds[i].ToString());
-            return Post<IList<ActivityCategory>, int[]>(string.Format("{0}/sections/activities/categories", BaseUrl), sectionIds.ToArray());
+           return Post<IList<ActivityCategory>, int[]>(string.Format("{0}activities/categories", BaseUrl), sectionIds.ToArray());
+            //return Post<IList<ActivityCategory>, int[]>(string.Format("{0}/sections/activities/categories", BaseUrl), sectionIds.ToArray());
         }
 
 
