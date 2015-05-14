caa6b8dbb0116c29bc087fcf80f1c02b0616d1ef
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-05-14 08:22:04 -0600

    fixed CHLK-3971 Teacher > Class profile > Schedule. Error page is shown after clicking the "Schedule"

---
+++ Chalkable.Web/Models/PersonViewDatas/StudentContactViewData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Web.Models.PersonViewDatas
+{
+    public class StudentContactViewData 
+    {
+        public PersonInfoViewData PersonInfo { get; set; }
+        public string RelationshipName { get; set; }
+        public bool IsFamalyMember { get; set; }
+
+        public static IList<StudentContactViewData> Create(IList<StudentContactDetails> studentContacts)
+        {
+            return studentContacts.Select(x => new StudentContactViewData
+                {
+                    PersonInfo = PersonInfoViewData.Create(x.Person),
+                    RelationshipName = x.ContactRelationship.Name,
+                    IsFamalyMember = x.IsFamilyMember
+                }).ToList();
+        } 
+    }
+}
---
+++ Chalkable.Web/app/chlk/models/apps/AppInstallPostData.js
@@ -0,0 +1,25 @@
+REQUIRE('chlk.models.id.AppId');
+NAMESPACE('chlk.models.apps', function () {
+    "use strict";
+    /** @class chlk.models.apps.AppInstallPostData*/
+    CLASS(
+        'AppInstallPostData', [
+            chlk.models.id.AppId, 'appId',
+            String, 'classes',
+            String, 'departments',
+            String, 'gradeLevels',
+            String, 'roles',
+            String, 'submitActionType',
+            String, 'forAll',
+            chlk.models.id.AppInstallGroupId, 'currentPerson',
+
+            Boolean, function isInstallForAll(){
+                return this.getForAll() == 'all';
+            },
+
+            Boolean, function isEmpty(){
+                return this.getClasses() == '' && this.getDepartments() == '' &&
+                    this.getGradeLevels() == '' && this.getRoles() == '' && this.getCurrentPerson().valueOf() == '' && this.getForAll() == '';
+            }
+        ]);
+});
