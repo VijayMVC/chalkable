d132cd613487979bb3f5a405464dad874c546275
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-05-18 17:37:33 +0300

    fixed School Year

--- Chalkable.Web/app/chlk/BaseApp.js
+++ Chalkable.Web/app/chlk/BaseApp.js
@@ -108,7 +108,7 @@ NAMESPACE('chlk', function (){
                 this.saveInSession(session, ChlkSessionConstants.MARKING_PERIOD, chlk.models.schoolYear.MarkingPeriod);
                 this.saveInSession(session, ChlkSessionConstants.MARKING_PERIODS, ArrayOf(chlk.models.schoolYear.MarkingPeriod));
                 this.saveInSession(session, ChlkSessionConstants.GRADING_PERIOD, chlk.models.schoolYear.GradingPeriod);
-                this.saveInSession(session, ChlkSessionConstants.SCHOOL_YEAR, chlk.models.schoolYear.Year, null, ['data']);
+                this.saveInSession(session, ChlkSessionConstants.SCHOOL_YEAR, chlk.models.schoolYear.Year, null);
                 this.saveInSession(session, ChlkSessionConstants.NEXT_MARKING_PERIOD, chlk.models.schoolYear.MarkingPeriod);
                 this.saveInSession(session, ChlkSessionConstants.FINALIZED_CLASS_IDS);
                 this.saveInSession(session, ChlkSessionConstants.CURRENT_CHLK_PERSON, chlk.models.people.User, ChlkSessionConstants.CURRENT_PERSON);
@@ -116,7 +116,7 @@ NAMESPACE('chlk', function (){
                 this.saveInSession(session, 'WEB_SITE_ROOT', null, 'webSiteRoot');
                 this.saveInSession(session, ChlkSessionConstants.AZURE_PICTURE_URL);
                 this.saveInSession(session, ChlkSessionConstants.DEMO_AZURE_PICTURE_URL);
-                this.saveInSession(session, ChlkSessionConstants.SCHOOL_YEAR, chlk.models.id.SchoolYearId, ChlkSessionConstants.CURRENT_SCHOOL_YEAR_ID, ['data', 'id']);
+                this.saveInSession(session, ChlkSessionConstants.SCHOOL_YEAR, chlk.models.id.SchoolYearId, ChlkSessionConstants.CURRENT_SCHOOL_YEAR_ID, ['id']);
                 this.saveInSession(session, ChlkSessionConstants.ATTENDANCE_REASONS, ArrayOf(chlk.models.attendance.AttendanceReason));
                 this.saveInSession(session, ChlkSessionConstants.USER_CLAIMS, ArrayOf(chlk.models.people.Claim));
                 this.saveInSession(session, ChlkSessionConstants.ALPHA_GRADES, ArrayOf(chlk.models.grading.AlphaGrade));
