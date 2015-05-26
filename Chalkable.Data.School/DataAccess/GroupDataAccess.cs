285ce5a79a1d08a55ca9b311a621bd398b3a26f3
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-05-26 06:59:41 -0600

    fixed get groupsdetails

--- Chalkable.Data.School/DataAccess/GroupDataAccess.cs
+++ Chalkable.Data.School/DataAccess/GroupDataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using Chalkable.Data.Common;
 using Chalkable.Data.Common.Orm;
@@ -15,29 +16,35 @@ namespace Chalkable.Data.School.DataAccess
 
         public IList<GroupDetails> GetGroupsDetails(int ownerId)
         {
-            var dbQuery = new DbQuery();
             var groupType = typeof (Group);
             var studentType = typeof (Student);
+            var conds = new AndQueryCondition { { Group.OWNER_REF_FIELD, ownerId } };
+            var groupQuery = Orm.SimpleSelect(groupType.Name, conds);
+            
+            var studentGroupsQuery = new DbQuery();
             var types = new List<Type> {groupType, studentType};
-            dbQuery.Sql
+            studentGroupsQuery.Sql
                 .AppendFormat(Orm.SELECT_FORMAT, Orm.ComplexResultSetQuery(types), groupType.Name)
                 .AppendFormat(Orm.SIMPLE_JOIN_FORMAT, typeof (StudentGroup).Name, StudentGroup.GROUP_REF_FIELD, groupType.Name, Group.ID_FIELD)
                 .AppendFormat(Orm.SIMPLE_JOIN_FORMAT, studentType.Name, Student.ID_FIELD, typeof (StudentGroup).Name, StudentGroup.STUDENT_REF_FIELD);
+            conds.BuildSqlWhere(studentGroupsQuery, groupType.Name);
 
-            new AndQueryCondition {{Group.OWNER_REF_FIELD, ownerId}}.BuildSqlWhere(dbQuery, groupType.Name);
-
-            var studentGroups= ReadMany<StudentGroupComplex>(dbQuery, true).ToList();
-            return studentGroups.GroupBy(x => x.Group.Id).Select(x =>
-                {
-                    var stgroup = x.First();
-                    return new GroupDetails
-                        {
-                            Id = x.Key,
-                            Name = stgroup.Group.Name,
-                            OwnerRef = stgroup.Group.OwnerRef,
-                            Students = x.Select(sg => sg.Student).ToList()
-                        };
-                }).ToList();
+            var dbQuery = new DbQuery(new List<DbQuery> {groupQuery, studentGroupsQuery});
+            return Read(dbQuery, ReadGetGroupDetailsResult);
         }
+
+        private IList<GroupDetails> ReadGetGroupDetailsResult(DbDataReader reader)
+        {
+            var groups = reader.ReadList<Group>();
+            reader.NextResult();
+            var studentGroups = reader.ReadList<StudentGroupComplex>(true);
+            return groups.Select(chlkGroup => new GroupDetails
+                {
+                    Id = chlkGroup.Id, 
+                    Name = chlkGroup.Name, 
+                    OwnerRef = chlkGroup.OwnerRef, 
+                    Students = studentGroups.Where(x => x.Group.Id == chlkGroup.Id).Select(x => x.Student).ToList()
+                }).ToList();
+        } 
     }
 }
