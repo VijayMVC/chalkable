fecfd5d119e569ee4955c390e8c6db4445c569b8
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-05-28 07:01:51 -0600

    dropped SchoolGradeLevel table

--- Chalkable.BusinessLogic/Services/DemoSchool/DemoGradeLevelService.cs
+++ Chalkable.BusinessLogic/Services/DemoSchool/DemoGradeLevelService.cs
@@ -5,18 +5,6 @@ using Chalkable.Data.School.Model;
 
 namespace Chalkable.BusinessLogic.Services.DemoSchool
 {
-    public class DemoSchoolGradeLevelStorage : BaseDemoIntStorage<SchoolGradeLevel>
-    {
-        public DemoSchoolGradeLevelStorage()
-            : base(null, true)
-        {
-        }
-
-        public IList<SchoolGradeLevel> GetAll(int? schoolId)
-        {
-            return data.Where(x => x.Value.SchoolRef == schoolId).Select(x => x.Value).ToList();
-        }
-    }
 
     public class DemoGradeLevelStorage : BaseDemoIntStorage<GradeLevel>
     {
@@ -29,17 +17,14 @@ namespace Chalkable.BusinessLogic.Services.DemoSchool
     public class DemoGradeLevelService : DemoSchoolServiceBase, IGradeLevelService
     {
         private DemoGradeLevelStorage GradeLevelStorage { get; set; }
-        private DemoSchoolGradeLevelStorage SchoolGradeLevelStorage { get; set; }
         public DemoGradeLevelService(IServiceLocatorSchool serviceLocator) : base(serviceLocator)
         {
             GradeLevelStorage = new DemoGradeLevelStorage();
-            SchoolGradeLevelStorage = new DemoSchoolGradeLevelStorage();
         }
 
         public IList<GradeLevel> GetGradeLevels(int? schoolId)
         {
-            var schoolGradeLevels = SchoolGradeLevelStorage.GetAll(schoolId).Select(x => x.GradeLevelRef).ToList();
-            return GradeLevelStorage.GetData().Where(x => schoolGradeLevels.Contains(x.Value.Id)).Select(x => x.Value).ToList();
+            return GradeLevelStorage.GetData().Select(x => x.Value).ToList();
         }
 
         public IList<GradeLevel> GetGradeLevels()
