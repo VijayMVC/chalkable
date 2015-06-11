a1a730b7df26f3a0aa8efb457d7a19b3f5ab9b7b
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-06-11 03:59:11 -0600

    Merge with default

---
+++ Chalkable.BusinessLogic/Services/School/SettingsService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.BusinessLogic.Security;
+using Chalkable.Common.Exceptions;
+using Chalkable.Data.Common;
+using Chalkable.Data.Common.Orm;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.School
+{
+
+    public interface ISettingsService
+    {
+        void AddSettings(IList<DistrictSettings> settings);
+        void Edit(IList<DistrictSettings> settings);
+        DistrictSettings GetSetting(string category, string setting);
+        void Delete(IList<DistrictSettings> settings);
+    }
+
+    public class SettingsService : SchoolServiceBase, ISettingsService
+    {
+        public SettingsService(IServiceLocatorSchool serviceLocator) : base(serviceLocator)
+        {
+        }
+
+        public void AddSettings(IList<DistrictSettings> settings)
+        {
+            BaseSecurity.EnsureSysAdmin(Context);
+            DoUpdate(u => new DataAccessBase<DistrictSettings>(u).Insert(settings));
+        }
+
+        public void Edit(IList<DistrictSettings> settings)
+        {
+            BaseSecurity.EnsureSysAdmin(Context);
+            DoUpdate(u => new DataAccessBase<DistrictSettings>(u).Update(settings));
+        }
+
+        public DistrictSettings GetSetting(string category, string setting)
+        {
+            var conds = new AndQueryCondition
+            {
+                {DistrictSettings.CATEGORY_FIELD, category, ConditionRelation.Equal},
+                {DistrictSettings.SETTING_FIELD, setting, ConditionRelation.Equal},
+            };
+
+            return DoRead(u => new DataAccessBase<DistrictSettings>(u).GetAll(conds).First());
+        }
+
+        public void Delete(IList<DistrictSettings> settings)
+        {
+            BaseSecurity.EnsureSysAdmin(Context);
+            DoUpdate(u => new DataAccessBase<DistrictSettings>(u).Delete(settings));
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/templates/announcement/ShortAnnouncementTpl.js
@@ -0,0 +1,39 @@
+REQUIRE('chlk.templates.ChlkTemplate');
+REQUIRE('chlk.models.announcement.ShortAnnouncementViewData');
+
+NAMESPACE('chlk.templates.announcement', function () {
+
+    /** @class chlk.templates.announcement.ShortAnnouncementTpl*/
+    CLASS(
+        [ria.templates.TemplateBind('~/assets/jade/activities/announcement/AnnouncementAppAttachments.jade')],
+        [ria.templates.ModelBind(chlk.models.announcement.BaseAnnouncementViewData)],
+        'ShortAnnouncementTpl', EXTENDS(chlk.templates.ChlkTemplate), [
+
+            [ria.templates.ModelPropertyBind],
+            String, 'announcementTypeName',
+
+            [ria.templates.ModelPropertyBind],
+            Number, 'order',
+
+            [ria.templates.ModelPropertyBind],
+            String, 'title',
+
+            [ria.templates.ModelPropertyBind],
+            chlk.models.common.ChlkDate, 'expiresDate',
+
+            [ria.templates.ModelPropertyBind],
+            Number, 'avg',
+
+            [ria.templates.ModelPropertyBind],
+            chlk.models.id.AnnouncementId, 'id',
+
+            [ria.templates.ModelPropertyBind],
+            Boolean, 'dropped',
+
+            [ria.templates.ModelPropertyBind],
+            Number, 'maxScore',
+
+            [ria.templates.ModelPropertyBind],
+            Boolean, 'annOwner'
+        ]);
+});
