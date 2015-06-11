836109708a16ff053e1b3dad5da6bdb71d3b7f3b
Andriy Machnew <Andriy Machnew@localhost>
2015-06-11 18:09:09 +0300

    added le columns script for school db

---
+++ Chalkable.Web/Models/StandardSubjectViewData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Web.Models
+{
+    public class StandardSubjectViewData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public static StandardSubjectViewData Create(StandardSubject standardSubject)
+        {
+            return new StandardSubjectViewData
+                {
+                    Id = standardSubject.Id,
+                    Name = standardSubject.Name,
+                    Description = standardSubject.Description
+                };
+        }
+        public static IList<StandardSubjectViewData> Create(IList<StandardSubject> standardSubjects)
+        {
+            return standardSubjects.Select(Create).ToList();
+        } 
+    }
+}
