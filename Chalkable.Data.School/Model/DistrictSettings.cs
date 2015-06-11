c187045b4607711078def6f4d2fa101eaa1dfb8c
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-06-11 09:32:35 -0600

    fixed roleLayout

---
+++ Chalkable.Data.School/Model/DistrictSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Data.Common;
+
+namespace Chalkable.Data.School.Model
+{
+
+        public class DistrictSettings
+        {
+            public const string CATEGORY_FIELD = "Category";
+            public const string SETTING_FIELD = "Setting";
+            
+            [PrimaryKeyFieldAttr]
+            public string Category { get; set; }
+            [PrimaryKeyFieldAttr]
+            public string Setting { get; set; }
+            public string Value { get; set; }
+        }
+
+}
