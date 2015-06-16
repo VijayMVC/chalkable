7372de9bf5a4ced59f44c897536fad34f8d800fa
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-06-16 11:01:38 -0600

    fixed student calendar

---
+++ Chalkable.Data.School/DataAccess/AnnouncementsDataAccess/ClassAnnouncementDataAccess.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Chalkable.Common;
+using Chalkable.Data.Common;
+using Chalkable.Data.Common.Orm;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Data.School.DataAccess.AnnouncementsDataAccess
+{
+    public abstract class ClassAnnouncementDataAccess : AnnouncementDataAccess
+    {
+        protected int? schoolId;
+
+        protected ClassAnnouncementDataAccess(UnitOfWork unitOfWork, int schoolId) : base(unitOfWork)
+        {
+            this.schoolId = schoolId;
+        }
+
+        private const string CREATE_PORCEDURE = "spCreateAnnouncement";
+        private const string REORDER_PROCEDURE = "spReorderAnnouncements";
+
+        private const string CLASS_ANNOUNCEMENT_TYPE_ID_PARAM = "classAnnouncementTypeId";
+        private const string CREATED_PARAM = "created";
+        private const string EXPIRES_PARAM = "expires";
+        private const string PERSON_ID_PARAM = "personId";
+        private const string STATE_PARAM = "state";
+        private const string GRADING_STYLE_PARAM = "gradingStyle";
+        private const string CLASS_ID_PARAM = "classId";
+        
+        private const string SCHOOL_YEAR_ID_PARAM = "schoolYearId";
+        private const string ID_PARAM = "id";
+        private const string FROM_DATE_PARAM = "fromDate";
+        private const string TO_DATE_PARAM = "toDate";
+
+        private const string SCHOOL_ID = "schoolId";
+
+        private const string START_PARAM = "start";
+        private const string COUNT_PARAM = "count";
+
+        protected AnnouncementQueryResult GetAnnouncementsComplex(string procedureName, Dictionary<string, object> parameters, AnnouncementsQuery query)
+        {
+            parameters.Add(ID_PARAM, query.Id);
+            parameters.Add(PERSON_ID_PARAM, query.PersonId);
+            parameters.Add(FROM_DATE_PARAM, query.FromDate);
+            parameters.Add(TO_DATE_PARAM, query.ToDate);
+            parameters.Add(CLASS_ID_PARAM, query.ClassId);
+            parameters.Add(START_PARAM, query.Start);
+            parameters.Add(COUNT_PARAM, query.Count);
+            parameters.Add(SCHOOL_ID, schoolId);
+            using (var reader = ExecuteStoredProcedureReader(procedureName, parameters))
+            {
+                return ReadAnnouncementsQueryResult(reader, query);
+            }
+        }
+        
+        public override AnnouncementDetails Create(int? classAnnouncementTypeId, int? classId, DateTime created, DateTime expiresDate, int personId)
+        {
+            var parameters = new Dictionary<string, object>
+                {
+                    {CLASS_ANNOUNCEMENT_TYPE_ID_PARAM, classAnnouncementTypeId},
+                    {PERSON_ID_PARAM, personId},
+                    {CREATED_PARAM, created},
+                    {EXPIRES_PARAM, expiresDate},
+                    {STATE_PARAM, AnnouncementState.Draft},
+                    {GRADING_STYLE_PARAM, GradingStyleEnum.Numeric100},
+                    {CLASS_ID_PARAM, classId},
+                    {SCHOOL_ID, schoolId}
+                };
+
+            using (var reader = ExecuteStoredProcedureReader(CREATE_PORCEDURE, parameters))
+            {
+                return BuildGetDetailsResult(reader);
+            }
+        }
+
+        public override AnnouncementDetails GetDetails(int id, int callerId, int? roleId)
+        {
+            return GetDetails(id, callerId, roleId, schoolId);
+        }
+        
+        private const string CLASS_ANN_TYPE_PARAM = "classAnnType";
+
+        public override void ReorderAnnouncements(int schoolYearId, int announcementTypeId, int classId)
+        {
+            var parameters = new Dictionary<string, object>
+                {
+                    {SCHOOL_YEAR_ID_PARAM, schoolYearId},
+                    {CLASS_ANN_TYPE_PARAM, announcementTypeId},
+                    {CLASS_ID_PARAM, classId}
+                };
+            using (ExecuteStoredProcedureReader(REORDER_PROCEDURE, parameters))
+            {
+            }
+        }
+        
+        
+        public override IList<AnnouncementComplex> GetByActivitiesIds(IList<int> activitiesIds)
+        {
+            if (activitiesIds == null || activitiesIds.Count == 0) return new List<AnnouncementComplex>();
+            var dbQuery = new DbQuery();
+            dbQuery.Sql.Append(@"select * from vwAnnouncement ");
+            dbQuery.Sql.AppendFormat("where SisActivityId in ({0})", activitiesIds.Select(x => x.ToString(CultureInfo.InvariantCulture)).JoinString(","));
+            return ReadMany<AnnouncementComplex>(dbQuery);
+        }
+
+        public override IList<string> GetLastFieldValues(int personId, int classId, int classAnnouncementType, int count)
+        {
+            var conds = new AndQueryCondition
+                {
+                    {Announcement.CLASS_REF_FIELD, classId},
+                    {Announcement.CLASS_ANNOUNCEMENT_TYPE_REF_FIELD, classAnnouncementType},
+                    {Announcement.SCHOOL_REF_FIELD, schoolId}
+                };
+            var dbQuery = Orm.OrderedSelect(typeof(Announcement).Name, conds, Announcement.ID_FIELD, Orm.OrderType.Desc, count);
+            var anns = ReadMany<Announcement>(dbQuery);
+            if (anns.Count == 0) return new List<string>();
+            return anns.Select(x => x.Content).ToList();
+        }
+
+        public override bool Exists(string title, int classId, DateTime expiresDate, int? excludeAnnouncementId)
+        {
+            var query = new AndQueryCondition
+                {
+                    {Announcement.TITLE_FIELD, title},
+                    {Announcement.CLASS_REF_FIELD, classId},
+                    {Announcement.EXPIRES_FIELD, expiresDate},
+                    {Announcement.SCHOOL_REF_FIELD, schoolId}
+                };
+
+            if (excludeAnnouncementId.HasValue)
+                query.Add(Announcement.ID_FIELD, Announcement.ID_FIELD, excludeAnnouncementId, ConditionRelation.NotEqual);
+
+            return Exists<Announcement>(query);
+        }
+
+        public override bool Exists(int sisActivityId)
+        {
+            return Exists(new List<int> { sisActivityId });
+        }
+        public override bool Exists(IList<int> sisActivitiesIds)
+        {
+            if (sisActivitiesIds != null && sisActivitiesIds.Count > 0)
+            {
+                var dbQuery = new DbQuery();
+                var tableName = "Announcement";
+                var idsString = sisActivitiesIds.Select(x => x.ToString(CultureInfo.InvariantCulture)).JoinString(",");
+                dbQuery.Sql.Append(string.Format(@"select * from [{0}] where [{0}].[{1}] in ({2})"
+                    , tableName, Announcement.SIS_ACTIVITY_ID_FIELD, idsString));
+                return Exists(dbQuery);
+            }
+            return false;
+        }
+        public override bool CanAddStandard(int announcementId)
+        {
+            var dbQuery = new DbQuery();
+            dbQuery.Sql.Append(@"select [Announcement].Id from Announcement 
+                                 join Class on Class.Id = Announcement.ClassRef
+                                 join ClassStandard  on ClassStandard.ClassRef = Class.Id or ClassStandard.ClassRef = Class.CourseRef ");
+            var conds = new AndQueryCondition
+                {
+                    {Announcement.ID_FIELD, announcementId},
+                    {Announcement.SCHOOL_REF_FIELD, schoolId}
+                };
+            conds.BuildSqlWhere(dbQuery, typeof(Announcement).Name);
+            return Exists(dbQuery);
+        }
+
+    }
+}
---
+++ Chalkable.Web/Controllers/CalendarControllers/AnnouncementCalendarController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Chalkable.BusinessLogic.Security;
+using Chalkable.BusinessLogic.Services.DemoSchool.Master;
+using Chalkable.BusinessLogic.Services.School;
+using Chalkable.Common;
+using Chalkable.Common.Exceptions;
+using Chalkable.Data.Common.Enums;
+using Chalkable.Data.Master.Model;
+using Chalkable.Data.School.DataAccess.AnnouncementsDataAccess;
+using Chalkable.Data.School.Model;
+using Chalkable.Web.ActionFilters;
+using Chalkable.Web.Models.AnnouncementsViewData;
+using Chalkable.Web.Models.CalendarsViewData;
+
+namespace Chalkable.Web.Controllers.CalendarControllers
+{
+    [RequireHttps, TraceControllerFilter]
+    public class AnnouncementCalendarController : CalendarController
+    {
+         [AuthorizationFilter("DistrictAdmin, Teacher, Student", true, new[] { AppPermissionType.Announcement })]
+        public ActionResult List(DateTime? date, int? classId, int? personId)
+         {
+             if (!SchoolLocator.Context.PersonId.HasValue)
+                 throw new UnassignedUserException();
+             DateTime start, end;
+             MonthCalendar(ref date, out start, out end);
+             var schoolYearId = GetCurrentSchoolYearId();
+             int? studentId, teacherId;
+             PrepareUsersIdsForCalendar(SchoolLocator, personId, out teacherId, out studentId);
+             var announcements = BaseSecurity.IsDistrictAdmin(Context)
+                    ? SchoolLocator.AnnouncementService.GetAnnouncements(start, end, false, classId, true)
+                    : SchoolLocator.AnnouncementService.GetAdminAnnouncements(null, null, start, end, 0, int.MaxValue, true, studentId);
+             if (personId.HasValue)
+             {
+                 var classes = SchoolLocator.ClassService.GetClasses(schoolYearId, studentId, teacherId);
+                 announcements = announcements.Where(a => classes.Any(c => c.Id == a.ClassRef)).ToList();
+             }
+             if (DemoUserService.IsDemoUser(Context))
+             {
+                 announcements = announcements.Where(x => x.State == AnnouncementState.Created).ToList();
+             }
+             var days = SchoolLocator.CalendarDateService.GetLastDays(schoolYearId, true, start, end);
+             return Json(PrepareMonthCalendar(start, end, date.Value, (dateTime, isCurrentMonth) => 
+                 AnnouncementMonthCalendarViewData.Create(dateTime, isCurrentMonth, announcements, days)));
+         }
+
+         [AuthorizationFilter("DistrictAdmin, Teacher, Student")]
+         public ActionResult ListByDateRange(DateTime? startDate, DateTime? endDate, int? classId)
+         {
+             var query = new AnnouncementsQuery {FromDate = startDate, ToDate = endDate, ClassId = classId};
+             var anns = SchoolLocator.AnnouncementService.GetAnnouncementsComplex(query);
+             return Json(AnnouncementShortViewData.Create(anns));
+         }
+
+         [AuthorizationFilter("DistrictAdmin, Teacher, Student", true, new[] { AppPermissionType.Announcement })]
+         public ActionResult Week(DateTime? date, int? classId, int? personId)
+         {
+
+             var res = BuildDayAnnCalendar(SchoolLocator, date, classId, personId, GetCurrentSchoolYearId());
+             return Json(res, 8);
+         }
+
+         [AuthorizationFilter("DistrictAdmin, Teacher, Student", true, new[] { AppPermissionType.Schedule })]
+         public ActionResult Day(DateTime? date, int? personId)
+         {
+             var res = BuildDayAnnCalendar(SchoolLocator, date, null, personId, GetCurrentSchoolYearId());
+             return Json(res, 8);
+         }
+
+         public static IList<AnnouncementCalendarWeekViewData> BuildDayAnnCalendar(IServiceLocatorSchool locator, DateTime? date,
+             int? classId, int? personId, int schoolYearId)
+         {
+
+             DateTime start, end;
+             WeekCalendar(ref date, out start, out end, locator.Context);
+             int? teacherId, studentId;
+             PrepareUsersIdsForCalendar(locator, personId, out teacherId, out studentId);
+             var res = new List<AnnouncementCalendarWeekViewData>();
+             var announcements = BaseSecurity.IsDistrictAdmin(locator.Context) 
+                 ? locator.AnnouncementService.GetAdminAnnouncements(null, null, start, end, 0, int.MaxValue, true, studentId)
+                 : locator.AnnouncementService.GetAnnouncements(start, end, false, classId, true);
+             var schedule = locator.ClassPeriodService.GetSchedule(teacherId, studentId, classId, start, end);
+             var classes = locator.ClassService.GetClasses(schoolYearId, studentId, teacherId);
+             announcements = announcements.Where(a => a.IsAdminAnnouncement ||  classes.Any(c => c.Id == a.ClassRef)).ToList();
+             for (var d = start; d <= end; d = d.AddDays(1))
+             {
+                 var currentDayAnns = announcements.Where(x => x.Expires.Date == d).ToList();
+                 var daySchedule = schedule.Where(x => x.Day == d).ToList();
+                 var annPeriods = AnnouncementPeriodViewData.Create(daySchedule, currentDayAnns, locator.Context.NowSchoolTime);
+                 res.Add(AnnouncementCalendarWeekViewData.Create(d, annPeriods, currentDayAnns));
+             }
+             return res;
+         }
+
+
+         private static void PrepareUsersIdsForCalendar(IServiceLocatorSchool locator, int? personId, out int? teacherId, out int? studentId)
+         {
+             if (!personId.HasValue)
+             {
+                 teacherId = locator.Context.Role == CoreRoles.TEACHER_ROLE ? locator.Context.PersonId : null;
+                 studentId = locator.Context.Role == CoreRoles.STUDENT_ROLE ? locator.Context.PersonId : null;
+             }
+             else
+             {
+                 var person = locator.PersonService.GetPersonDetails(personId.Value);
+                 teacherId = person.RoleRef == CoreRoles.TEACHER_ROLE.Id ? person.Id : (int?)null;
+                 studentId = person.RoleRef == CoreRoles.STUDENT_ROLE.Id ? person.Id : (int?)null;
+             }
+         }
+        
+    }
+}
