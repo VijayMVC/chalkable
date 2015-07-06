fad51518a735fd9f87e15de4f6d3c2f3ba81e515
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-07-06 18:41:52 +0300

    updated Month Calendar for Lesson Plans

---
+++ Chalkable.Data.School/Model/AnnouncementAssignedAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Chalkable.Data.Common;
+
+namespace Chalkable.Data.School.Model
+{
+    public class AnnouncementAssignedAttribute
+    {
+        public const string ID_FIELD = "Id";
+        public const string ANNOUNCEMENT_REF_FIELD = "AnnouncementRef";
+
+        [IdentityFieldAttr]
+        [PrimaryKeyFieldAttr]
+        public int Id { get; set; }
+        public int AnnouncementRef { get; set; }
+        public int AttributeTypeId { get; set; }
+        public string Uuid { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public bool VisibleForStudents { get; set; }
+
+        [NotDbFieldAttr]
+        public int? SisActivityId { get; set; }
+        [NotDbFieldAttr]
+        public int? SisAttributeId { get; set; }
+
+    }
+}
