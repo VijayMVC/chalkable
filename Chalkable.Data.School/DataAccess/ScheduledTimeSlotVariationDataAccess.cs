c95338e2a2c0cfb6fe420d9d165c216d967cef60
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-07-06 09:47:18 -0600

    fixed get admin annoucnement deatails

---
+++ Chalkable.Data.School/DataAccess/ScheduledTimeSlotVariationDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using Chalkable.Data.Common;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Data.School.DataAccess
+{
+    public class ScheduledTimeSlotVariationDataAccess : DataAccessBase<ScheduledTimeSlotVariation>
+    {
+        public ScheduledTimeSlotVariationDataAccess(UnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
