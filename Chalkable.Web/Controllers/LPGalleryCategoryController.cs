2cbb095d7035cbd32f8b8fdc141b69d34e26b40a
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-07-07 17:16:31 +0300

    fixed links

---
+++ Chalkable.Web/Controllers/LPGalleryCategoryController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using Chalkable.Web.ActionFilters;
+using Chalkable.Web.Models;
+
+namespace Chalkable.Web.Controllers
+{
+    [RequireHttps, TraceControllerFilter]
+    public class LPGalleryCategoryController : ChalkableController
+    {
+        [AuthorizationFilter("Teacher")]
+        public ActionResult CreateCategory(string name)
+        {
+            var res = SchoolLocator.LPGalleryCategoryService.Add(name);
+            return Json(LPGalleryCategoryViewData.Create(res));
+        }
+
+        [AuthorizationFilter("Teacher")]
+        public ActionResult UpdateCategory(int categoryId, string name)
+        {
+            var res = SchoolLocator.LPGalleryCategoryService.Edit(categoryId, name);
+            return Json(LPGalleryCategoryViewData.Create(res));
+        }
+
+        [AuthorizationFilter("Teacher")]
+        public ActionResult DeleteCategory(int categoryId)
+        {
+            SchoolLocator.LPGalleryCategoryService.Delete(categoryId);
+            return Json(true);
+        }
+
+        [AuthorizationFilter("Teacher")]
+        public ActionResult ListCategories()
+        {
+            var res = SchoolLocator.LPGalleryCategoryService.GetList();
+            return Json(LPGalleryCategoryViewData.Create(res));
+        }
+    }
+}
