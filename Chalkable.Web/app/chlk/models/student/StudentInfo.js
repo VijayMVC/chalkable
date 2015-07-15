f3fe557b1d947c3eaebcb3549bf29d32347333fe
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-07-15 19:17:44 +0300

    fixed for CHLK-4098 [58709] Student Info should display Grade Level and is doesn't

--- Chalkable.Web/app/chlk/models/student/StudentInfo.js
+++ Chalkable.Web/app/chlk/models/student/StudentInfo.js
@@ -1,6 +1,7 @@
 REQUIRE('chlk.models.people.User');
 REQUIRE('chlk.models.people.HealthCondition');
 REQUIRE('chlk.models.student.StudentContact');
+REQUIRE('chlk.models.grading.GradeLevel');
 
 NAMESPACE('chlk.models.student', function(){
     "use strict";
@@ -13,9 +14,12 @@ NAMESPACE('chlk.models.student', function(){
 
         ArrayOf(chlk.models.student.StudentContact), 'studentContacts',
 
+        chlk.models.grading.GradeLevel, 'gradeLevel',
+
         OVERRIDE, VOID, function deserialize(raw) {
             BASE(raw);
             this.studentContacts = SJX.fromArrayOfDeserializables(raw.studentcontacts, chlk.models.student.StudentContact);
+            this.gradeLevel = SJX.fromDeserializable(raw.gradelevel, chlk.models.grading.GradeLevel);
         }
     ]);
 });
