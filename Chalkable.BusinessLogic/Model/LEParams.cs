5ec753235132d4e97d2534436e1040cc4c878f26
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-07-20 19:00:56 +0300

    updated groups dialog css

---
+++ Chalkable.BusinessLogic/Model/LEParams.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chalkable.BusinessLogic.Model
+{
+    public class LEParams
+    {
+        public bool LELinkStatus { get; set; }
+        public string LEBaseUrl { get; set; }
+        public bool LEEnabled { get; set; }
+        public bool LESyncComplete { get; set; }
+        public bool IssueLECreditsEnabled { get; set; }
+        public bool LEAccessEnabled { get; set; }
+    }
+}
