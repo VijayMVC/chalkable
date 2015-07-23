41379294797e107098ed330ddccbec9754b4f902
Ievgenii Zasoba <geka.zasoba@gmail.com>
2015-07-23 17:51:36 +0300

    fixed CHLK-4191 Teacher > Creating an item. The error page is shown after attaching a file to an item

---
+++ Chalkable.BusinessLogic/Mapping/ModelMappers/ActivityAssignedAttrToAnnouncementAssignedAttrMapper.cs
@@ -0,0 +1,28 @@
+﻿using Chalkable.Data.School.Model;
+using Chalkable.StiConnector.Connectors.Model;
+
+namespace Chalkable.BusinessLogic.Mapping.ModelMappers
+{
+    public class ActivityAssignedAttrToAnnouncementAssignedAttrMapper : BaseMapper<AnnouncementAssignedAttribute, ActivityAssignedAttribute>
+    {
+        protected override void InnerMap(AnnouncementAssignedAttribute announcementAttribute, ActivityAssignedAttribute activityAttribute)
+        {
+            announcementAttribute.Name = activityAttribute.Name;
+            announcementAttribute.VisibleForStudents = activityAttribute.VisibleInHomePortal;
+            announcementAttribute.Text = activityAttribute.Text;
+            announcementAttribute.SisActivityAssignedAttributeId = activityAttribute.Id;
+            announcementAttribute.AttributeTypeId = activityAttribute.AttributeId;
+            announcementAttribute.SisActivityId = activityAttribute.ActivityId;
+
+            if (activityAttribute.Attachment != null)
+            {
+                announcementAttribute.Uuid = activityAttribute.Attachment.CrocoDocId.HasValue
+                    ? activityAttribute.Attachment.CrocoDocId.ToString()
+                    : "";
+                announcementAttribute.SisAttributeAttachmentId = activityAttribute.Attachment.AttachmentId;
+                announcementAttribute.SisAttachmentMimeType = activityAttribute.Attachment.MimeType;
+                announcementAttribute.SisAttachmentName = activityAttribute.Attachment.Name;
+            }
+        }
+    }
+}
