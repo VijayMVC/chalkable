8a62cc411b6e82c37d07cf5a8e98c2698ae32f2e
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-07-24 15:45:35 +0300

    fixed for CHLK-4175 [59036] Messages will not send

---
+++ Chalkable.BusinessLogic/Services/Master/DbMaintenanceService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Chalkable.BusinessLogic.Security;
+using Chalkable.Data.Master.DataAccess;
+using Chalkable.Data.Master.Model;
+
+namespace Chalkable.BusinessLogic.Services.Master
+{
+    public interface IDbMaintenanceService
+    {
+        void BeforeSisRestore(Guid districtId);
+        IList<RestoreLogItem> AfterSisRestore(Guid districtId);
+    }
+
+    public class DbMaintenanceService : MasterServiceBase, IDbMaintenanceService
+    {
+        public DbMaintenanceService(IServiceLocatorMaster serviceLocator) : base(serviceLocator)
+        {
+        }
+
+        public void BeforeSisRestore(Guid districtId)
+        {
+            BaseSecurity.EnsureSysAdmin(Context);
+            DoUpdate(uow => new DbMaintenanceDataAccess(uow).BeforeSisRestore(districtId));
+        }
+
+        public IList<RestoreLogItem> AfterSisRestore(Guid districtId)
+        {
+            BaseSecurity.EnsureSysAdmin(Context);
+            return DoRead(uow => new DbMaintenanceDataAccess(uow).AfterSisRestore(districtId));
+        }
+    }
+}
