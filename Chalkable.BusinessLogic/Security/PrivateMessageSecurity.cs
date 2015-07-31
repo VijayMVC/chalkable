fd27b427cc6aded6320f7134713e8d531d238419
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-07-31 04:34:38 -0600

    added security check to for edit lesson plan

---
+++ Chalkable.BusinessLogic/Security/PrivateMessageSecurity.cs
@@ -0,0 +1,30 @@
+﻿using Chalkable.BusinessLogic.Services;
+using Chalkable.Common.Exceptions;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Security
+{
+    public  static class PrivateMessageSecurity
+    {
+        public static bool CanMarkMessage(PrivateMessage privateMessage, UserContext context)
+        {
+            return CanSendMessage(context) || privateMessage.ToPersonRef == context.PersonId;
+        }
+        public static bool CanDeleteMessage(PrivateMessage privateMessage, UserContext context)
+        {
+            return CanSendMessage(context) || privateMessage.FromPersonRef == context.PersonId ||
+                   privateMessage.ToPersonRef == context.PersonId;
+        }
+
+        public static bool CanSendMessage(UserContext context)
+        {
+            return BaseSecurity.IsSysAdmin(context) || !context.MessagingDisabled;
+        }
+
+        public static void EnsureMessgingPermission(UserContext context)
+        {
+            if (!CanSendMessage(context))
+                throw new MessagingDisabledException();
+        }
+    }
+}
