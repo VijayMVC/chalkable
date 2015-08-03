78cc690682e54a13063b07755c6537c3a481cc95
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-08-03 17:39:15 +0300

    small fixes for Student Attendance Profile

---
+++ Chalkable.Web/Controllers/AttendanceReasonController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using Chalkable.Data.Common.Enums;
+using Chalkable.Data.Master.Model;
+using Chalkable.Web.ActionFilters;
+using Chalkable.Web.Models.AttendancesViewData;
+
+namespace Chalkable.Web.Controllers
+{
+    [RequireHttps, TraceControllerFilter]
+    public class AttendanceReasonController : ChalkableController
+    {
+        [AuthorizationFilter("SysAdmin, DistrictAdmin, Teacher", true, new[] {AppPermissionType.Attendance})]
+        public ActionResult List()
+        {
+            return Json(AttendanceReasonViewData.Create(SchoolLocator.AttendanceReasonService.List()));
+        }
+
+        public ActionResult GetAllReasons()
+        {
+            return Json(AttendanceReasonDetailsViewData.Create(SchoolLocator.AttendanceReasonService.List(false)));
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/templates/announcement/LessonPlanCategoryTpl.js
@@ -0,0 +1,20 @@
+REQUIRE('chlk.templates.ChlkTemplate');
+REQUIRE('chlk.models.announcement.CategoryViewData');
+
+NAMESPACE('chlk.templates.announcement', function () {
+
+    /** @class chlk.templates.announcement.LessonPlanCategoryTpl*/
+    CLASS(
+        [ria.templates.TemplateBind('~/assets/jade/activities/announcement/LessonPlanCategory.jade')],
+        [ria.templates.ModelBind(chlk.models.announcement.CategoryViewData)],
+        'LessonPlanCategoryTpl', EXTENDS(chlk.templates.ChlkTemplate), [
+            [ria.templates.ModelPropertyBind],
+            Number, 'id',
+
+            [ria.templates.ModelPropertyBind],
+            Number, 'lessonPlansCount',
+
+            [ria.templates.ModelPropertyBind],
+            String, 'name'
+        ])
+});
