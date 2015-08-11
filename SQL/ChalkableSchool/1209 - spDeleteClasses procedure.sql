93f77e460dad5c925b7e1fd6211d266827c3d59d
Ievgenii Zasoba <geka.zasoba@gmail.com>
2015-08-11 16:03:51 +0300

    delete draft class announcement when class is deleting durring the sync

--- SQL/ChalkableSchool/1209 - spDeleteClasses procedure.sql
+++ SQL/ChalkableSchool/1209 - spDeleteClasses procedure.sql
@@ -1,6 +1,19 @@
 Create Procedure spDeleteClasses 
 	@classIds TInt32 readonly
 as	
+	Delete AnnouncementStandard where AnnouncementRef in (Select Id From ClassAnnouncement where ClassRef in (select value from @classIds as ids))
+	Delete AnnouncementApplication where AnnouncementRef in (Select Id From ClassAnnouncement where ClassRef in (select value from @classIds as ids))
+	Delete [Notification] where AnnouncementRef in (Select Id From ClassAnnouncement where ClassRef in (select value from @classIds as ids))
+	Delete AnnouncementAttachment where AnnouncementRef in (Select Id From ClassAnnouncement where ClassRef in (select value from @classIds as ids))
+	Delete AnnouncementQnA where AnnouncementRef in (Select Id From ClassAnnouncement where ClassRef in (select value from @classIds as ids))
+	Delete AnnouncementRecipientData where AnnouncementRef in (Select Id From ClassAnnouncement where ClassRef in (select value from @classIds as ids))
+	Delete AnnouncementAssignedAttribute where AnnouncementRef in (Select Id From ClassAnnouncement where ClassRef in (select value from @classIds as ids))	
+	Delete From AnnouncementApplication where AnnouncementRef in (Select Id From ClassAnnouncement where ClassRef in (select value from @classIds as ids))	
+
+	Delete Announcement where Id in (Select Id From ClassAnnouncement where ClassRef in (select value from @classIds as ids))
+	Delete From ClassAnnouncement where ClassRef in (select value from @classIds as ids)
+
+
 	Delete AnnouncementStandard where AnnouncementRef in (Select Id From LessonPlan where ClassRef in (select value from @classIds as ids))
 	Delete AnnouncementApplication where AnnouncementRef in (Select Id From LessonPlan where ClassRef in (select value from @classIds as ids))
 	Delete [Notification] where AnnouncementRef in (Select Id From LessonPlan where ClassRef in (select value from @classIds as ids))
@@ -8,6 +21,7 @@ as
 	Delete AnnouncementQnA where AnnouncementRef in (Select Id From LessonPlan where ClassRef in (select value from @classIds as ids))
 	Delete AnnouncementRecipientData where AnnouncementRef in (Select Id From LessonPlan where ClassRef in (select value from @classIds as ids))
 	Delete AnnouncementAssignedAttribute where AnnouncementRef in (Select Id From LessonPlan where ClassRef in (select value from @classIds as ids))	
+	Delete From AnnouncementApplication where AnnouncementRef in (Select Id From LessonPlan where ClassRef in (select value from @classIds as ids))	
 
 	Delete Announcement where Id in (Select Id From LessonPlan where ClassRef in (select value from @classIds as ids))
 	Delete From LessonPlan where ClassRef in (select value from @classIds as ids)
