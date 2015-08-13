fc50ee1e2e1335483a9e637ffcbdfbbb2f88a168
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-08-13 09:34:22 -0600

    fixed CHLK-4277 Teacher > Creating an item. The empty file is shown on the item's create after adding

---
+++ Chalkable.Web/Controllers/AttachmentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web;
+using System.Web.Mvc;
+using Chalkable.BusinessLogic.Services;
+using Chalkable.BusinessLogic.Services.School;
+using Chalkable.Common;
+using Chalkable.Common.Exceptions;
+using Chalkable.Common.Web;
+using Chalkable.Web.ActionFilters;
+using Chalkable.Web.ActionResults;
+using Chalkable.Web.Models;
+
+namespace Chalkable.Web.Controllers
+{
+    [RequireHttps, TraceControllerFilter]
+    public class AttachmentController : ChalkableController
+    {
+        [AuthorizationFilter("DistrictAdmin, Teacher, Student")]
+        public ActionResult AttachmentsList(string filter, int? sortType, int? start, int? count)
+        {
+            start = start ?? 0;
+            count = count ?? 10;
+            var attachments = SchoolLocator.AttachementService.GetAttachmentsInfo(filter, (AttachmentSortTypeEnum?) sortType, start.Value, count.Value);
+            return Json(attachments.Transform(x=> AttachmentViewData.Create(x, Context.PersonId.Value)));
+        }
+ 
+    }
+}
---
+++ Chalkable.Web/Models/CommonCoreStandardViewData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Chalkable.Data.Master.Model;
+
+namespace Chalkable.Web.Models
+{
+    public class CommonCoreStandardViewData
+    {
+        public Guid Id { get; set; }
+        public Guid? ParentStandardId { get; set; }
+        public string StandardCode { get; set; }
+        public string Description { get; set; }
+        public Guid StandardCategoryId { get; set; }
+
+        public static CommonCoreStandardViewData Create(CommonCoreStandard commonCoreStandard)
+        {
+            return new CommonCoreStandardViewData
+                {
+                    Id = commonCoreStandard.Id,
+                    ParentStandardId = commonCoreStandard.ParentStandardRef,
+                    Description = commonCoreStandard.Description,
+                    StandardCode = commonCoreStandard.Code,
+                    StandardCategoryId = commonCoreStandard.StandardCategoryRef
+                };
+        }
+
+        public static IList<CommonCoreStandardViewData> Create(IList<CommonCoreStandard> commonCoreStandards)
+        {
+            return commonCoreStandards.Select(Create).ToList();
+        } 
+    }
+}
