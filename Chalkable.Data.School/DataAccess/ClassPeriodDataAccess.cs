73fc8785c2c6e358c7da771f9a57b1ecb77ea3c6
Volodymyr Iatsyshyn <viatsyshyn@emp-game.com>
2015-08-17 18:28:26 +0300

    fixed reference

---
+++ Chalkable.Data.School/DataAccess/ClassPeriodDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Chalkable.Data.Common;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Data.School.DataAccess
+{
+
+    public class ClassPeriodDataAccess : DataAccessBase<ClassPeriod>
+    {
+        public ClassPeriodDataAccess(UnitOfWork unitOfWork)
+            : base(unitOfWork)
+        {
+        }
+
+        public Class CurrentClassForTeacher(int schoolYearId, int personId, DateTime date, int time)
+        {
+            IDictionary<string, object> ps = new Dictionary<string, object>
+            {
+                {"@schoolYearId", schoolYearId},
+                {"@teacherId", personId},
+                {"@date", date},
+                {"@time", time},
+            };
+            using (var reader = ExecuteStoredProcedureReader("spCurrentClassForTeacher", ps))
+            {
+                return reader.ReadOrNull<Class>();
+            }
+        }
+    }
+}
