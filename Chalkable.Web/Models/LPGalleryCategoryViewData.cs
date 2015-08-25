dd9e1e59aa67921650d542455734bd4d344f4a92
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-08-25 04:17:22 -0700

    fixed getEnrollmentStudents

---
+++ Chalkable.Web/Models/LPGalleryCategoryViewData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Web.Models
+{
+    public class LPGalleryCategoryViewData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int OwnerId { get; set; }
+        public int LessonPlansCount { get; set; }
+
+        
+        public static LPGalleryCategoryViewData Create(LPGalleryCategory category)
+        {
+            return new LPGalleryCategoryViewData
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                    OwnerId = category.OwnerRef,
+                    LessonPlansCount = category.LessonPlansCount
+                };
+        }
+
+        public static IList<LPGalleryCategoryViewData> Create(IList<LPGalleryCategory> categories)
+        {
+            return categories.Select(Create).ToList();
+        } 
+    }
+}
