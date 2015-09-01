0202483666092fe205aa288fe5bf79f18b8b2538
Andriy Machnew <amachnew@emp-org.ua>
2015-09-01 16:07:16 +0300

    fixed CHLK-4194 [57864] When selecting out the "Attach Assessments" screen it will shell user to chalkable

---
+++ Chalkable.StiConnector/Connectors/GradingConnector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using Chalkable.StiConnector.Connectors.Model;
+
+namespace Chalkable.StiConnector.Connectors
+{
+    public class GradingConnector : ConnectorBase
+    {
+        public GradingConnector(ConnectorLocator locator)
+            : base(locator)
+        {
+        }
+        public GradingSummaryDashboard GetStudentGradingSummary(int acadSessionId, int studentId)
+        {
+            return Call<GradingSummaryDashboard>(BuildBaseUrl(acadSessionId, studentId) + "summary");
+            
+        }
+
+        public GradingDetailsDashboard GetStudentGradingDetails(int acadSessionId, int studentId, int gradingPeriodId)
+        {
+            var url = BuildBaseUrl(acadSessionId, studentId) + "detail/" + gradingPeriodId;
+            return Call<GradingDetailsDashboard>(url);
+        }
+
+        private string BuildBaseUrl(int acadSessionId, int studentId)
+        {
+            return string.Format("{0}chalkable/{1}/students/{2}/dashboard/grading/", BaseUrl, acadSessionId, studentId);
+        }
+    }
+}
