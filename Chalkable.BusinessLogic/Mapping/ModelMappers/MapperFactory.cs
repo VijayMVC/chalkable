693ad23104c6af0c93271bced69268cfa00cd7d4
viatsyshyn <viatsyshyn@localhost>
2015-09-10 12:46:35 +0300

    TeamCity change in 'Chalkable' project: commit current project settings

---
+++ Chalkable.BusinessLogic/Mapping/ModelMappers/MapperFactory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Chalkable.Common;
+using Chalkable.Common.Exceptions;
+using Chalkable.Data.School.Model;
+using Chalkable.Data.School.Model.Announcements;
+using Chalkable.StiConnector.Connectors.Model;
+
+namespace Chalkable.BusinessLogic.Mapping.ModelMappers
+{
+    public class MapperFactory
+    {
+        private static IDictionary<Pair<Type, Type>, Object> _customMappers;
+
+        static MapperFactory()
+        {
+            BuildMapperDictionary();
+        }
+
+        private static void BuildMapperDictionary()
+        {
+            _customMappers = new Dictionary<Pair<Type, Type>, Object>
+                    {
+                        {new Pair<Type, Type>(typeof(Announcement), typeof(Activity)), new ActivityToAnnouncementMapper()},
+                        {new Pair<Type, Type>(typeof(ClassAnnouncement), typeof(Activity)), new ActivityToClassAnnouncementMapper()},
+                        {new Pair<Type, Type>(typeof(AnnouncementComplex), typeof(Activity)), new ActivityToAnnouncementComplexMapper()},
+                        {new Pair<Type, Type>(typeof(AnnouncementDetails), typeof(Activity)), new ActivityToAnnouncementDetailsMapper()},
+
+                        {new Pair<Type, Type>(typeof(Activity), typeof(Announcement)), new AnnouncementToActivityMapper()},
+                        {new Pair<Type, Type>(typeof(Activity), typeof(ClassAnnouncement)), new ClassAnnouncementToActivityMapper()},
+                        {new Pair<Type, Type>(typeof(Activity), typeof(AnnouncementComplex)), new AnnouncementComplexToActivityMapper()},
+                        {new Pair<Type, Type>(typeof(Activity), typeof(AnnouncementDetails)), new AnnouncementDetailsToActivityMapper()},
+
+                        {new Pair<Type, Type>(typeof(StudentAnnouncement), typeof(Score)), new ScoreToStudentAnnMapper()},
+                        {new Pair<Type, Type>(typeof(StudentAnnouncementDetails), typeof(Score)), new ScoreToStudentAnnMapper()},
+                        {new Pair<Type, Type>(typeof(Score), typeof(StudentAnnouncementDetails)), new StudentAnnouncementToScoreMapper()},
+                        {new Pair<Type, Type>(typeof(Score), typeof(StudentAnnouncement)), new StudentAnnouncementToScoreMapper()},
+                        {new Pair<Type, Type>(typeof(ClassDiscipline), typeof(DisciplineReferral)), new DisciplineReferralToClassDisciplineMapper()},
+                        {new Pair<Type, Type>(typeof(DisciplineReferral), typeof(ClassDiscipline)), new ClassDisciplineToDisciplineReferralMapper()},
+                        {new Pair<Type, Type>(typeof(AnnouncementAssignedAttribute), typeof(ActivityAssignedAttribute)), new ActivityAssignedAttrToAnnouncementAssignedAttrMapper() },
+                        {new Pair<Type, Type>(typeof(ActivityAssignedAttribute), typeof(AnnouncementAssignedAttribute)), new AnnouncementAssignedAttrToActivityAssignedAttrMapper() },
+                    
+                        {new Pair<Type, Type>(typeof(Attachment), typeof(StiAttachment)), new StiAttachmentToAttachmentMapper()},
+                        {new Pair<Type, Type>(typeof(StiAttachment), typeof(Attachment)), new AttachmentToStiAttachmentMapper()}
+                    };
+        }
+        public static IMapper<TReturn, TSource> GetMapper<TReturn, TSource>()
+        {
+            var typesObj = new Pair<Type, Type>(typeof(TReturn), typeof(TSource));
+            if (!_customMappers.ContainsKey(typesObj))
+                throw new ChalkableException("There are no mapper with such source and return types");
+            return _customMappers[typesObj] as IMapper<TReturn, TSource>;
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/templates/setup/ClassroomOptionSetupTpl.js
@@ -0,0 +1,17 @@
+REQUIRE('chlk.models.setup.ClassroomOptionSetupViewData');
+REQUIRE('chlk.templates.common.PageWithClasses');
+
+NAMESPACE('chlk.templates.setup', function () {
+
+    /** @class chlk.templates.setup.ClassroomOptionSetupTpl*/
+    CLASS(
+        [ria.templates.TemplateBind('~/assets/jade/activities/setup/ClassroomOptionSetup.jade')],
+        [ria.templates.ModelBind(chlk.models.setup.ClassroomOptionSetupViewData)],
+        'ClassroomOptionSetupTpl', EXTENDS(chlk.templates.common.PageWithClasses), [
+            [ria.templates.ModelPropertyBind],
+            ArrayOf(chlk.models.grading.GradingScale), 'scales',
+
+            [ria.templates.ModelPropertyBind],
+            chlk.models.grading.ClassroomOptionViewData, 'classroomOptions'
+        ])
+});
