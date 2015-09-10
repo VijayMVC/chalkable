db5162124dfd2930d027796b8b0da901b6913cb4
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-09-10 12:29:25 +0300

    fixed validationEngine path

---
+++ Chalkable.StandardImport/Services/ImportService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.BusinessLogic.Services.Master;
+using Chalkable.StandardImport.Models;
+
+namespace Chalkable.StandardImport.Services
+{
+    public abstract class ImportService
+    {
+        protected IServiceLocatorMaster ServiceLocatorMaster { get; private set; }
+        public CsvContainer CsvContainer { get; protected set; }
+
+        public ImportService(string connectionString)
+        {
+            ServiceLocatorMaster = new StandardImportServiceLocatorMaster(connectionString);
+        }
+
+        public abstract void Import(byte[] data);
+    }
+}
---
+++ Chalkable.StiConnector/SyncModel/SyncModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Chalkable.StiConnector.SyncModel
+{
+    public abstract class SyncModel
+    {
+        public long? SYS_CHANGE_VERSION { get; set; }
+        public long? SYS_CHANGE_CREATION_VERSION { get; set; }
+
+        public abstract int DefaultOrder { get; }
+    }
+}
