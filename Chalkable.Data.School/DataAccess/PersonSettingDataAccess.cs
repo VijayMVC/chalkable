4420628fb323b432ade3074eb2a78bfab179a016
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-09-15 07:13:14 -0700

    fixed saving date value to personSettings

---
+++ Chalkable.Data.School/DataAccess/PersonSettingDataAccess.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Chalkable.Common;
+using Chalkable.Data.Common;
+using Chalkable.Data.Common.Orm;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Data.School.DataAccess
+{
+    public class PersonSettingDataAccess : DataAccessBase<PersonSetting, int>
+    {
+        private const string SP_GET_PERSON_SETTINGS = "spGetPersonSettings";
+        private const string PERSON_ID_PARAM = "personId";
+        private const string KEYS_LIST_PARAM = "keys";
+
+        public PersonSettingDataAccess(UnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public IList<PersonSetting> GetPersonSettings(int personId, IList<string> keys)
+        {
+            var param = new Dictionary<string, object>
+            {
+                {PERSON_ID_PARAM, personId},
+                {KEYS_LIST_PARAM, keys}
+            };
+            using (var reader = ExecuteStoredProcedureReader(SP_GET_PERSON_SETTINGS, param))
+            {
+                return reader.ReadList<PersonSetting>();
+            }
+        }
+
+        public void AddPersonSettings(int personId, IDictionary<string, object> ps)
+        {
+            List<PersonSetting> personSettings = new List<PersonSetting>();
+            foreach (var pair in ps)
+            {
+                if(pair.Value is DateTime)
+                    personSettings.Add(new PersonSetting() {Key = pair.Key, PersonRef = personId, Value = ((DateTime) pair.Value).ToString(Constants.DATE_FORMAT) });
+                else personSettings.Add(new PersonSetting() { Key = pair.Key, PersonRef = personId, Value = pair.Value.ToString() });
+            }
+            Insert(personSettings);
+        }
+    }
+}
---
+++ Chalkable.StiConnector/Connectors/Model/StudentExplorerDashboard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Chalkable.StiConnector.Connectors.Model
+{
+    public class StudentExplorerDashboard
+    {
+        /// <summary>
+        /// List of activities that are due the next 7 days for the student.  These are activities that haven't been scored.
+        /// </summary>
+        public IEnumerable<Activity> Activities { get; set; }
+        /// <summary>
+        /// List of student averages for the grading period
+        /// </summary>
+        public IEnumerable<StudentAverage> Averages { get; set; }
+        /// <summary>
+        /// Id of the GradingPeriod
+        /// </summary>
+        public int? GradingPeriodId { get; set; }
+        /// <summary>
+        /// List of standard averages for the student for the grading period.  
+        /// </summary>
+        public IEnumerable<StandardScore> Standards { get; set; }
+    }
+}
