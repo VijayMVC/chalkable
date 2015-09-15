1b19cf7530430837e44760d2460d597af0011ffb
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-09-15 17:14:57 +0300

    fixed feed filters

---
+++ Chalkable.Web/Tools/PasswordTools.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Chalkable.Common;
+
+namespace Chalkable.Web.Tools
+{
+    static public class PasswordTools
+    {
+        public static bool IsSecurePassword(string newPassword)
+        {
+            if (string.IsNullOrEmpty(newPassword))
+                return false;
+
+            if (newPassword.Length < Settings.MinPasswordLength)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/models/feed/FeedAdmin.js
@@ -0,0 +1,68 @@
+REQUIRE('chlk.models.common.PageWithGrades');
+REQUIRE('chlk.models.announcement.FeedAnnouncementViewData');
+
+NAMESPACE('chlk.models.feed', function () {
+    "use strict";
+
+    var SJX = ria.serialize.SJX;
+
+    /** @class chlk.models.feed.FeedAdmin*/
+    CLASS(
+        'FeedAdmin',
+                EXTENDS(chlk.models.common.PageWithGrades),
+                IMPLEMENTS(ria.serialize.IDeserializable), [
+
+            VOID, function deserialize(raw) {
+                this.items = SJX.fromArrayOfDeserializables(raw.annoucementviewdatas, chlk.models.announcement.FeedAnnouncementViewData);
+                this.importantOnly = SJX.fromValue(raw.importantOnly, Boolean);
+                this.importantCount = SJX.fromValue(raw.importantCount, Number);
+                this.newNotificationCount = SJX.fromValue(raw.newNotificationCount, Number);
+                this.start = SJX.fromValue(raw.start, Number);
+                this.count = SJX.fromValue(raw.count, Number);
+                this.startDate = SJX.fromDeserializable(raw.fromdate, chlk.models.common.ChlkDate);
+                this.endDate = SJX.fromDeserializable(raw.todate, chlk.models.common.ChlkDate);
+                this.lessonPlansOnly = SJX.fromValue(raw.lessonplansonly, Boolean);
+                this.latest = SJX.fromValue(raw.sorttype, Boolean);
+                this.submitType = SJX.fromValue(raw.submitType, String);
+                this.markDoneOption = SJX.fromValue(raw.markDoneOption, Number);
+                this.gradeLevels = SJX.fromValue(raw.gradeLevels, String);
+            },
+
+            [[ArrayOf(chlk.models.announcement.FeedAnnouncementViewData), chlk.models.grading.GradeLevelsForTopBar, Boolean, Number]],
+            function $(items_, gradeLevels_, importantOnly_, newNotificationCount_){
+                BASE(gradeLevels_);
+                if(items_)
+                    this.setItems(items_);
+                if(importantOnly_ !== undefined)
+                    this.setImportantOnly(importantOnly_);
+                if(newNotificationCount_)
+                    this.setNewNotificationCount(newNotificationCount_);
+            },
+
+            ArrayOf(chlk.models.announcement.FeedAnnouncementViewData), 'items',
+
+            Boolean, 'importantOnly',
+
+            Number, 'importantCount',
+
+            Number, 'start',
+
+            Number, 'count',
+
+            Number, 'markDoneOption',
+
+            Number, 'newNotificationCount',
+
+            chlk.models.common.ChlkDate, 'startDate',
+
+            chlk.models.common.ChlkDate, 'endDate',
+
+            Boolean, 'lessonPlansOnly',
+
+            Boolean, 'latest',
+
+            String, 'submitType',
+
+            String, 'gradeLevels'
+        ]);
+});
