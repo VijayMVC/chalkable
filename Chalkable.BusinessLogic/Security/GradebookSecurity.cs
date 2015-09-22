545b6cd359361898f30102c4df1275603208a13c
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-09-22 09:09:11 -0700

    fixed get lesson plan templates

---
+++ Chalkable.BusinessLogic/Security/GradebookSecurity.cs
@@ -0,0 +1,14 @@
+﻿using Chalkable.BusinessLogic.Model;
+using Chalkable.BusinessLogic.Services;
+
+namespace Chalkable.BusinessLogic.Security
+{
+    public class GradebookSecurity
+    {
+        public static bool CanReCalculateGradebook(UserContext context)
+        {
+            return BaseSecurity.HasClaim(ClaimInfo.MAINTAIN_CLASSROOM, context)
+                   || BaseSecurity.HasClaim(ClaimInfo.MAINTAIN_CLASSROOM_ADMIN, context);
+        }
+    }
+}
---
+++ Chalkable.StiImport/Services/SyncModelAdapters/CalendarDayAdapter.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+using Chalkable.Data.School.Model;
+using Chalkable.StiConnector.SyncModel;
+
+namespace Chalkable.StiImport.Services.SyncModelAdapters
+{
+    public class CalendarDayAdapter : SyncModelAdapter<CalendarDay>
+    {
+        public CalendarDayAdapter(AdapterLocator locator) : base(locator)
+        {
+        }
+
+        private Date Selector(CalendarDay x)
+        {
+            return new Date
+            {
+                DayTypeRef = x.DayTypeID,
+                IsSchoolDay = x.InSchool,
+                SchoolYearRef = x.AcadSessionID,
+                BellScheduleRef = x.BellScheduleID,
+                Day = x.Date
+            };
+        }
+
+        protected override void InsertInternal(IList<CalendarDay> entities)
+        {
+            var days = entities.ToList().Select(Selector).ToList();
+            ServiceLocatorSchool.CalendarDateService.Add(days);
+        }
+
+        protected override void UpdateInternal(IList<CalendarDay> entities)
+        {
+            var ds = entities.Select(Selector).ToList();
+            ServiceLocatorSchool.CalendarDateService.Edit(ds);
+        }
+
+        protected override void DeleteInternal(IList<CalendarDay> entities)
+        {
+            var dates = entities.Select(x => new Date
+            {
+                Day = x.Date,
+                SchoolYearRef = x.AcadSessionID
+            }).ToList();
+            ServiceLocatorSchool.CalendarDateService.Delete(dates);
+        }
+    }
+}
