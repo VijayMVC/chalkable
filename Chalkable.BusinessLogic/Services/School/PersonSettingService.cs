3574bdd8b44f6591771cfa9b3252b15a33f408e3
Ievgenii Zasoba <geka.zasoba@gmail.com>
2015-09-30 14:59:59 +0300

    added new column to PersonSetting(SchoolYearRef)

--- Chalkable.BusinessLogic/Services/School/PersonSettingService.cs
+++ Chalkable.BusinessLogic/Services/School/PersonSettingService.cs
@@ -12,9 +12,9 @@ namespace Chalkable.BusinessLogic.Services.School
 {
     public interface IPersonSettingService
     {
-        void SetSettingsForPerson(int personId, IDictionary<string, object> settings);
-        IList<PersonSetting> GetSettingsForPerson(int personId);
-        IList<PersonSetting> GetSettingsForPerson(int personId, IList<string> keys);
+        void SetSettingsForPerson(int personId, int schoolYearId, IDictionary<string, object> settings);
+        IDictionary<string, string> GetSettingsForPerson(int personId, int schoolYearId);
+        IDictionary<string, string> GetSettingsForPerson(int personId, int schoolYearId, IList<string> keys);
     }
 
     class PersonSettingService : SchoolServiceBase, IPersonSettingService
@@ -30,39 +30,46 @@ namespace Chalkable.BusinessLogic.Services.School
                 availableSettings.Add(setting.GetValue(setting).ToString());
         }
         public PersonSettingService(IServiceLocatorSchool serviceLocator) : base(serviceLocator){ }
-
         private bool IsAvailableSettings(IList<string> settings)
         {
             return settings.All(x => availableSettings.Contains(x));
         }
-        public void SetSettingsForPerson(int personId, IDictionary<string, object> settings)
+        public void SetSettingsForPerson(int personId, int schoolYearId, IDictionary<string, object> settings)
         {
             var keys = settings.Keys.ToList();
             if (!IsAvailableSettings(keys))
                 throw new ArgumentException("Not recognized settings");
 
-            var ps = GetSettingsForPerson(personId, keys);
+            var ps = GetSettingsForPerson(personId, schoolYearId, keys);
+            var toSet =
+                ps.Select(
+                    x =>
+                        new PersonSetting()
+                        {
+                            PersonRef = personId,
+                            SchoolYearRef = schoolYearId,
+                            Key = x.Key,
+                            Value = x.Value
+                        }).ToList();
 
-            foreach (var set in ps)
+            foreach (var set in toSet)
             {
                 set.Value = (settings[set.Key] as DateTime?)?
                     .ToString(Constants.DATE_FORMAT, CultureInfo.InvariantCulture) 
                     ?? settings[set.Key]?.ToString();
             }
-            DoUpdate(u => new PersonSettingDataAccess(u).Update(ps));
+            DoUpdate(u => new PersonSettingDataAccess(u).Update(toSet));
 
             settings = settings.Where(s => ps.All(x => x.Key != s.Key)).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
-            DoUpdate(u => new PersonSettingDataAccess(u).AddPersonSettings(personId, settings));
+            DoUpdate(u => new PersonSettingDataAccess(u).AddPersonSettings(personId, schoolYearId, settings));
         }
-
-        public IList<PersonSetting> GetSettingsForPerson(int personId)
+        public IDictionary<string, string> GetSettingsForPerson(int personId, int schoolYearId)
         {
-            return DoRead(u => new PersonSettingDataAccess(u).GetAll(new SimpleQueryCondition("PersonRef", personId, ConditionRelation.Equal)));
+            return DoRead(u => new PersonSettingDataAccess(u).GetPersonSettings(personId, schoolYearId, new List<string>()));
         }
-
-        public IList<PersonSetting> GetSettingsForPerson(int personId, IList<string> keys)
+        public IDictionary<string , string> GetSettingsForPerson(int personId, int schoolYearId, IList<string> keys)
         {
-            return DoRead(u => new PersonSettingDataAccess(u).GetPersonSettings(personId, keys));
+            return DoRead(u => new PersonSettingDataAccess(u).GetPersonSettings(personId, schoolYearId, keys));
         }
     }
 }
