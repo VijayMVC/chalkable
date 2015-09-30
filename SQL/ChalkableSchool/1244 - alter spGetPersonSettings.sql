e89e54e1c870068fdd4ed87a52c9f34707b1895d
Ievgenii Zasoba <geka.zasoba@gmail.com>
2015-09-30 16:45:11 +0300

    alter spGetPersonSettings and added ToSet param to FeedSettings

--- SQL/ChalkableSchool/1244 - alter spGetPersonSettings.sql
+++ SQL/ChalkableSchool/1244 - alter spGetPersonSettings.sql
@@ -1,24 +1,16 @@
-USE [83009FE9-8594-4E33-A09E-1EF4F81D0E8D]
-GO
-
-/****** Object:  StoredProcedure [dbo].[spGetPersonSettings]    Script Date: 9/29/2015 9:54:06 AM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
 Alter Procedure [dbo].[spGetPersonSettings]
 @personId int,
 @schoolYearId int,
 @keys TString ReadOnly
 As
 
+declare @all int = (select count(*) from @keys)
+
 Select [Key], [Value] From PersonSetting 
 Where 
 	PersonRef = @personId
 	And SchoolYearRef = @schoolYearId
-	And ([Key] in(select value from @keys) or not exists(select * from @keys))
+	And ([Key] in(select value from @keys) or @all = 0 )
 
 
 GO
