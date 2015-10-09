036384179a9b3e5cca47586948a3848b552398bb
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-10-09 06:05:30 -0700

    fixed get possible message recipients

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/Master/DemoFundService.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using Chalkable.BusinessLogic.Services.Master;
+using Chalkable.Common;
+using Chalkable.Data.Master.Model;
+using Chalkable.Data.School.Model.ApplicationInstall;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool.Master
+{
+    public class DemoPersonBalance
+    {
+        public decimal Balance { get; set; }
+        public int PersonId { get; set; }
+    }
+    public class DemoPersonBalanceStorage : BaseDemoIntStorage<DemoPersonBalance>
+    {
+        public DemoPersonBalanceStorage()
+            : base(x => x.PersonId, false)
+        {
+        }
+
+        public void AddPersonBalance(DemoPersonBalance balance)
+        {
+            Add(balance);
+        }
+
+        public void UpdatePersonBalance(int personId, decimal balance)
+        {
+            if (data.ContainsKey(personId))
+            {
+                data[personId].Balance = balance;
+            }
+        }
+    }
+    public class DemoFundService : DemoMasterServiceBase, IFundService
+    {
+        private DemoPersonBalanceStorage PersonBalanceStorage { get; set; }
+        public DemoFundService(IServiceLocatorMaster serviceLocator) : base(serviceLocator)
+        {
+            PersonBalanceStorage = new DemoPersonBalanceStorage();
+        }
+
+        public void AddPersonBalance(DemoPersonBalance balance)
+        {
+            PersonBalanceStorage.Add(balance);
+        }
+
+        public decimal GetSchoolReserve(Guid schoolId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetRoleBalance(int roleId, Guid schoolId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FundRequest RequestByPurchaseOrder(Guid? schoolId, Guid? userId, decimal amount, string purchaseOrder, List<Pair<int, decimal>> roleDist, byte[] signature)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FundRequest RequestByPurchaseOrder(Guid? schoolId, Guid? userId, decimal amount, decimal adminAmount, decimal teacherAmount, decimal studentAmount, decimal parentAmount, string purchaseOrder, byte[] signature)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddFundsToRole(Guid schoolId, int roleId, decimal amount, Guid? fundRequestId, string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddFundsToClass(Guid classId, decimal amount, string discription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddFundsToPerson(Guid userId, decimal amount, string discription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<FundRequest> GetFundRequests()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetUserBalance(int userId, bool? privateMoney = null)
+        {
+            return PersonBalanceStorage.GetById(userId).Balance;
+        }
+
+        public void UpdateUserBalance(int userId, decimal newBalance)
+        {
+            PersonBalanceStorage.UpdatePersonBalance(userId, newBalance);
+        }
+
+        public decimal GetClassBalance(Guid classId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Fund AppInstallPersonPayment(Guid appInstallId, decimal amount, DateTime performedDateTime, string descrption)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicationInstallAction> GetSchoolPersonHistory(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetToSchoolPayment(Guid schoolId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetPaymentForApps(Guid schoolId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ApproveReject(Guid fundRequestId, bool isApprove)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/activities/settings/AdminPage.js
@@ -0,0 +1,40 @@
+REQUIRE('chlk.activities.lib.TemplatePage');
+REQUIRE('chlk.templates.settings.AdminMessagingTpl');
+
+NAMESPACE('chlk.activities.settings', function () {
+
+    var timer;
+
+    /** @class chlk.activities.settings.AdminPage */
+    CLASS(
+        [ria.mvc.DomAppendTo('#main')],
+        [chlk.activities.lib.PageClass('profile')],
+        [ria.mvc.TemplateBind(chlk.templates.settings.AdminMessagingTpl)],
+        'AdminPage', EXTENDS(chlk.activities.lib.TemplatePage), [
+            [ria.mvc.DomEventBind('change', '[type=checkbox]')],
+            [[ria.dom.Dom, ria.dom.Event, Object]],
+            VOID, function checkboxSelect(node, event, selected_){
+                clearTimeout(timer);
+
+                timer = setTimeout(function(){
+                    node.parent('form').trigger('submit');
+                }, 2000);
+            },
+
+            [ria.mvc.DomEventBind('change', '.all-students, .teachers-to-students')],
+            [[ria.dom.Dom, ria.dom.Event, Object]],
+            VOID, function mainCheckChange(node, event, selected_){
+                var node2 = node.hasClass('all-students') ? this.dom.find('[name=studenttoclassmessagingonly]') : this.dom.find('[name=teachertoclassmessagingonly]');
+                var checkbox2 = node.hasClass('all-students') ? this.dom.find('[type=checkbox][name=studenttoclassmessagingonly]') : this.dom.find('[type=checkbox][name=teachertoclassmessagingonly]');
+                if(node.checked()){
+                    node2.removeAttr('disabled');
+                    node2.parent('.slide-checkbox').removeAttr('disabled');
+                }
+                else{
+                    node2.setAttr('disabled', 'disabled');
+                    node2.parent('.slide-checkbox').setAttr('disabled', 'disabled');
+                    checkbox2.setValue(false);
+                }
+            }
+        ]);
+});
