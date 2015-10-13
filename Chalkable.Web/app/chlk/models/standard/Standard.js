76a71cfaeca98fe59a2aaf5f8d7e5e33df9c2114
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-10-13 18:25:55 +0300

    added confirm box for CHLK-4389 [59646] Admin portal saves group changes when the user did not select SAVE

---
+++ Chalkable.Web/app/chlk/models/standard/Standard.js
@@ -0,0 +1,85 @@
+REQUIRE('ria.serialize.SJX');
+REQUIRE('ria.serialize.IDeserializable');
+REQUIRE('chlk.models.id.StandardId');
+REQUIRE('chlk.models.id.AnnouncementId');
+REQUIRE('chlk.models.id.StandardSubjectId');
+
+NAMESPACE('chlk.models.standard', function () {
+    "use strict";
+
+    var SJX = ria.serialize.SJX;
+
+    /** @class chlk.models.standard.Standard*/
+    CLASS(UNSAFE,
+        'Standard', IMPLEMENTS(ria.serialize.IDeserializable),  [
+            VOID, function deserialize(raw) {
+                this.name = SJX.fromValue(raw.name, String);
+                this.description = SJX.fromValue(raw.description, String);
+                this.announcementId = SJX.fromValue(raw.announcementid, chlk.models.id.AnnouncementId);
+                this.standardId = SJX.fromValue(raw.standardid, chlk.models.id.StandardId);
+                this.grade = SJX.fromValue(raw.grade, String);
+                this.commonCoreStandardCode = SJX.fromArrayOfValues(raw.ccstandardcodes, String);
+                this.academicBenchmarkId = SJX.fromValue(raw.academicbenchmarkid, String);
+                this.subjectId = SJX.fromValue(raw.standardsubjectid, chlk.models.id.StandardSubjectId);
+            },
+
+            String, 'academicBenchmarkId',
+            String, 'name',
+            String, 'description',
+            chlk.models.id.AnnouncementId, 'announcementId',
+            chlk.models.id.StandardId, 'standardId',
+            String, 'grade',
+            String, 'commonCoreStandardCode',
+            chlk.models.id.StandardSubjectId, 'subjectId',
+
+            function getCommonCoreStandardCode(){
+                return this.commonCoreStandardCode && this.commonCoreStandardCode[0];
+            },
+
+            function getCommonCoreStandardCodes(){
+                return this.commonCoreStandardCode && this.commonCoreStandardCode.join(',');
+            },
+
+            function getCommonCoreStandardCodesArray(){
+                return this.commonCoreStandardCode;
+            },
+
+            String, function displayTitle(){
+                var name = this.getName();
+                if(name && (!name.trim || name.trim() != '')) return name;
+                return this.getDescription();
+            },
+
+            [[chlk.models.id.StandardId, String, String, String]],
+            function $(standardId_, name_, grade_, ccStandardCode_){
+                BASE();
+                if(standardId_)
+                    this.setStandardId(standardId_);
+                if(name_)
+                    this.setName(name_);
+                if(grade_)
+                    this.setGrade(grade_);
+                if(ccStandardCode_)
+                    this.setCommonCoreStandardCode(ccStandardCode_);
+            },
+
+            String, function getUrlComponents(index_) {
+                index_ = index_ != undefined ? index_|0 : 0;
+                return [
+                    'standardId[' + index_ + ']=' + encodeURIComponent(this.getAcademicBenchmarkId() || ''),
+                    'ccStandardCode[' + index_ + ']=' + encodeURIComponent(this.getCommonCoreStandardCode() || ''),
+                    'standardName[' + index_ + ']=' + encodeURIComponent(this.getName() || '')
+                ].join('&');
+
+                /*index_ = index_ != undefined ? index_|0 : 0;
+                var codes = this.getCommonCoreStandardCode().split(',').map(function(item, i, array){
+                    return 'ccStandardCode[' + i + ']=' + encodeURIComponent(array[i] || '')
+                });
+                return codes.concat([
+                    'standardId[' + index_ + ']=' + encodeURIComponent(this.getAcademicBenchmarkId() || ''),
+                    'standardName[' + index_ + ']=' + encodeURIComponent(this.getName() || '')
+                ]).join('&');*/
+            }
+
+        ]);
+});
---
+++ Chalkable.Web/app/chlk/templates/apps/InstallAppDialogTpl.js
@@ -0,0 +1,122 @@
+REQUIRE('chlk.models.apps.AppMarketInstallViewData');
+REQUIRE('chlk.templates.ChlkTemplate');
+
+NAMESPACE('chlk.templates.apps', function () {
+
+    function orderByName(_1, _2) {
+        var a = _1.name, b = _2.name;
+        return a < b ? -1 : a > b ? 1 : 0;
+    }
+
+    /** @class chlk.templates.apps.InstallAppDialogTpl*/
+    CLASS(
+        [ria.templates.TemplateBind('~/assets/jade/activities/apps/app-install-dialog.jade')],
+        [ria.templates.ModelBind(chlk.models.apps.AppMarketInstallViewData)],
+
+
+        'InstallAppDialogTpl', EXTENDS(chlk.templates.ChlkTemplate), [
+            [ria.templates.ModelPropertyBind],
+            chlk.models.apps.AppMarketApplication, 'app',
+
+            [ria.templates.ModelPropertyBind],
+            Boolean, 'alreadyInstalled',
+
+            [ria.templates.ModelPropertyBind],
+            chlk.models.classes.AllSchoolsActiveClasses, 'allClasses',
+
+            [ria.templates.ModelPropertyBind],
+            Boolean, 'fromNewItem',
+
+            [ria.templates.ModelPropertyBind],
+            Boolean, 'fromSuggestedApps',
+
+            Object, function calculateClassesTree_() {
+                var data = this.allClasses, isInstalled = this.isAlreadyInstalled_;
+
+                var schoolsMap = {};
+                data.getSchools()
+                    .map( function (_) { return {
+                        id: _.getId(),
+                        name: _.getName(),
+                        courseTypes: []
+                    }})
+                    .sort(orderByName)
+                    .forEach(function (_) { schoolsMap[_.id.valueOf()] = _; });
+
+                var courseTypesMap = {};
+                data.getCourseTypes()
+                    .map(function (_) {
+                        return {
+                            id: _.getCourseTypeId(),
+                            name: _.getCourseTypeName()
+                        }
+                    })
+                    .forEach(function (_) {
+                        courseTypesMap[_.id.valueOf()] = _;
+                    });
+
+                var schoolCourseTypes = data.getClasses()
+                    .map(function (_) {
+                        return {
+                            id: _.getId(),
+                            name: _.getFullClassName(),
+                            schoolId: _.getSchoolId(),
+                            courseTypeId: _.getCourseTypeId(),
+                            isInstalled: isInstalled(_.getId())
+                        }
+                    })
+                    .sort(orderByName)
+                    .reduce(function (acc, _) {
+                        var schoolCourseTypeId = _.schoolId.valueOf() + '_' + _.courseTypeId.valueOf(),
+                            courseTypeId = _.courseTypeId.valueOf();
+
+                        if (!acc[schoolCourseTypeId]) {
+                            acc[schoolCourseTypeId] = {
+                                id: courseTypesMap[courseTypeId].id,
+                                name: courseTypesMap[courseTypeId].name,
+                                schoolId: _.schoolId,
+                                classes: [_]
+                            }
+                        } else {
+                            acc[schoolCourseTypeId].classes.push(_);
+                        }
+
+                        return acc;
+                    }, {});
+
+                schoolsMap = Object.getOwnPropertyNames(schoolCourseTypes)
+                    .map(function (_) { return schoolCourseTypes[_]})
+                    .reduce(function (acc, _) {
+                        _.isInstalled = _.classes.every(function (_) { return _.isInstalled });
+
+                        acc[_.schoolId.valueOf()].courseTypes.push(_);
+                        return acc
+                    }, schoolsMap);
+
+
+                return Object.getOwnPropertyNames(schoolsMap)
+                    .map(function (_) { return schoolsMap[_]})
+                    .filter(function (_) { return _.courseTypes.length })
+                    .map(function (_) {
+                        _.isInstalled = _.courseTypes.every(function (_) { return _.isInstalled });
+                        return _;
+                    });
+            },
+
+            [[chlk.models.id.ClassId]],
+            Boolean, function isAlreadyInstalled_(classId) {
+                return this.app.getInstalledForGroups().some(function (_) {
+                    return _.grouptype == chlk.models.apps.AppInstallGroupTypeEnum.CLAZZ.valueOf()
+                        && _.id == classId.valueOf()
+                        && _.isinstalled;
+                })
+            },
+
+            Boolean, function isAlreadyInstalledForMe_() {
+                return this.app.getInstalledForGroups().some(function (_) {
+                    return _.grouptype == chlk.models.apps.AppInstallGroupTypeEnum.CURRENT_USER.valueOf()
+                        && _.isinstalled;
+                })
+            }
+        ])
+});
