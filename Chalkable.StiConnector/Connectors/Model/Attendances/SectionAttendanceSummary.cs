f229c0dc270c310c6c7100e700042c760ce2933d
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-10-13 05:37:15 -0700

    fixed CHLK-4424 Teacher > Creating an item. The checkbox "Hide from student Students" doesn't save own state

---
+++ Chalkable.StiConnector/Connectors/Model/Attendances/SectionAttendanceSummary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chalkable.StiConnector.Connectors.Model.Attendances
+{
+    public class SectionAttendanceSummary
+    {
+        public IEnumerable<DailySectionAbsenceSummary> Days { get; set; }
+        public int SectionId { get; set; }
+        public IEnumerable<StudentSectionAbsenceSummary> Students { get; set; }
+    }
+}
