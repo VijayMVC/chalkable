f152446190ab7f37c9a0c76bc12c8cc6b3dc7d1e
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-10-14 03:22:28 -0700

    Created new hot fix branch

---
+++ Chalkable.Common/Exceptions/BlobNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace Chalkable.Common.Exceptions
+{
+    public class BlobNotFoundException : ChalkableException
+    {
+
+        public BlobNotFoundException() : base(ChlkResources.ERR_BLOB_NOT_FOUND)
+        {
+        }
+        public BlobNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
