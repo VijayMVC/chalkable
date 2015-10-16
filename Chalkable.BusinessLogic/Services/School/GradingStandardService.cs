5d63336824a59dee45432fd8e30b498d727634d1
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-10-16 06:06:32 -0700

    fixed CHLK-4426 [60165] Gradebook - Standards are out of order

---
+++ Chalkable.BusinessLogic/Services/School/GradingStandardService.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chalkable.BusinessLogic.Model;
+using Chalkable.BusinessLogic.Security;
+using Chalkable.StiConnector.Connectors.Model;
+
+namespace Chalkable.BusinessLogic.Services.School
+{
+    public interface  IGradingStandardService
+    {
+        Task<IList<GradingStandardInfo>> GetGradingStandards(int classId, int? gradingPeriodId, bool reCalculateStandards = true); 
+        GradingStandardInfo SetGrade(int studentId, int standardId, int classId, int gradingPeriodId, int? alphaGradeId, string note);  
+    }
+
+    public class GradingStandardService : SisConnectedService, IGradingStandardService
+    {
+        public GradingStandardService(IServiceLocatorSchool serviceLocator) : base(serviceLocator)
+        {
+        }
+        public async Task<IList<GradingStandardInfo>> GetGradingStandards(int classId, int? gradingPeriodId, bool reCalculateStandards = true)
+        {
+            if (reCalculateStandards && GradebookSecurity.CanReCalculateGradebook(Context)) 
+                await ConnectorLocator.GradebookConnector.Calculate(classId);
+            var standardScores = ConnectorLocator.StandardScoreConnector.GetStandardScores(classId, null, gradingPeriodId);
+            var standards = ServiceLocator.StandardService.GetStandards(classId, null, null);
+            var res = GradingStandardInfo.Create(standardScores, standards);
+            return res.Where(x => x.Standard.IsActive || x.HasScore).ToList();
+        }
+        public GradingStandardInfo SetGrade(int studentId, int standardId, int classId, int gradingPeriodId, int? alphaGradeId, string note)
+        {
+            var standardScore = new StandardScore
+                {
+                    SectionId = classId,
+                    StudentId = studentId,
+                    StandardId = standardId,
+                    Note = note,
+                    EnteredScoreAlphaGradeId = alphaGradeId,
+                    GradingPeriodId = gradingPeriodId
+                };
+            var res = ConnectorLocator.StandardScoreConnector.Update(classId, studentId, standardId, gradingPeriodId, standardScore);
+            var standard = ServiceLocator.StandardService.GetStandardById(standardId);
+            return GradingStandardInfo.Create(res, standard);
+        }       
+    }
+
+
+
+}
