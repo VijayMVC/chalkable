7a9b13e7f5d2a79ecd625fe9ae39e589497f2e90
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-10-21 08:10:49 -0700

    added access to call UploadAnnouncementAttachment as api method

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/DemoAttendanceMonthService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Chalkable.BusinessLogic.Services.School;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool
+{
+    public class DemoAttendanceMonthStorage : BaseDemoIntStorage<AttendanceMonth>
+    {
+        public DemoAttendanceMonthStorage()
+            : base(x => x.Id)
+        {
+        }
+    }
+
+    public class DemoAttendanceMonthService : DemoSchoolServiceBase, IAttendanceMonthService
+    {
+        private DemoAttendanceMonthStorage AttendanceMonthStorage { get; set; }
+        public DemoAttendanceMonthService(IServiceLocatorSchool serviceLocator) : base(serviceLocator)
+        {
+            AttendanceMonthStorage = new DemoAttendanceMonthStorage();
+        }
+
+        public void Add(IList<AttendanceMonth> attendanceMonths)
+        {
+            AttendanceMonthStorage.Add(attendanceMonths);
+        }
+
+        public void Edit(IList<AttendanceMonth> attendanceMonths)
+        {
+            AttendanceMonthStorage.Update(attendanceMonths);
+        }
+
+        public void Delete(IList<AttendanceMonth> attendanceMonths)
+        {
+            AttendanceMonthStorage.Delete(attendanceMonths);
+        }
+
+        public IList<AttendanceMonth> GetAttendanceMonths(int schoolYearId, DateTime? fromDate = null, DateTime? endDate = null)
+        {
+            var res = AttendanceMonthStorage.GetAll().Where(x=>x.SchoolYearRef == schoolYearId);
+            if (fromDate.HasValue)
+                res = res.Where(x => x.EndDate >= fromDate);
+            if (endDate.HasValue)
+                res = res.Where(x => x.StartDate <= endDate);
+            return res.ToList();
+        }
+    }
+}
---
+++ Chalkable.Web/Models/PersonViewDatas/StudentAppsViewData.cs
@@ -0,0 +1,27 @@
+﻿using Chalkable.Common;
+using Chalkable.Data.School.Model;
+using Chalkable.Web.Models.ApplicationsViewData;
+
+namespace Chalkable.Web.Models.PersonViewDatas
+{
+    public class StudentAppsViewData
+    {
+        public ShortPersonViewData Person { get; set; }
+        public decimal Balance { get; set; }
+        public decimal Reserve { get; set; }
+        public int InstalledAppsCount { get; set; }
+        public PaginatedList<InstalledApplicationViewData> InstalledApps { get; set; }
+
+        public static StudentAppsViewData Create(StudentDetails student, decimal currentBalance, PaginatedList<InstalledApplicationViewData> apps)
+        {
+            return new StudentAppsViewData
+            {
+                Balance = currentBalance,
+                Reserve = 0, // todo : gets this from funds 
+                InstalledApps = apps,
+                InstalledAppsCount = apps.TotalCount,
+                Person = StudentViewData.Create(student)
+            };
+        }
+    }
+}
