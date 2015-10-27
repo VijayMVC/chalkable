406a230fd04fbcb1a0867ec02af740abe8246d44
Ievgenii Zasoba <geka.zasoba@gmail.com>
2015-10-27 12:39:19 +0200

    Fixed spDeleetClasses

---
+++ Chalkable.BusinessLogic/Services/School/TeacherCommentService.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Chalkable.BusinessLogic.Model;
+using Chalkable.BusinessLogic.Security;
+using Chalkable.Common;
+using Chalkable.Common.Exceptions;
+using Chalkable.StiConnector.Connectors.Model;
+
+namespace Chalkable.BusinessLogic.Services.School
+{
+    public interface ITeacherCommentService
+    {
+        TeacherComment AddComment(int teacherId, string comment);
+        TeacherComment EditComment(int commentId, int teacherId, string comment);
+        void DeleteComment(int commentId, int teacherId);
+        void DeleteComments(IList<int> commentsIds, int teacherId);
+        IList<TeacherComment> GetComments(int teacherId);
+        
+    }
+
+    public class TeacherCommentService : SisConnectedService, ITeacherCommentService
+    {
+        public TeacherCommentService(IServiceLocatorSchool serviceLocator) : base(serviceLocator)
+        {
+        }
+
+        public TeacherComment AddComment(int teacherId, string comment)
+        {
+            EnsureIsDistrictAdminOrCurrentTeacher(teacherId);
+            var syId = ServiceLocator.SchoolYearService.GetCurrentSchoolYear().Id;
+            var stiComment = new GradebookCommect {Comment = comment, TeacherId = teacherId};
+            stiComment = ConnectorLocator.SectionCommentConnector.CreateComment(syId, teacherId, stiComment);
+            return stiComment != null ? TeacherComment.Create(stiComment) : null;
+        }
+
+
+        public TeacherComment EditComment(int commentId, int teacherId, string comment)
+        {
+            EnsureIsDistrictAdminOrCurrentTeacher(teacherId);
+            var syId = ServiceLocator.SchoolYearService.GetCurrentSchoolYear().Id;
+            var stiComment = new GradebookCommect {Comment = comment, TeacherId = teacherId, Id = commentId};
+            ConnectorLocator.SectionCommentConnector.UpdateComment(syId, teacherId, stiComment);
+            return TeacherComment.Create(stiComment);
+        }
+
+        public void DeleteComment(int commentId, int teacherId)
+        {
+            DeleteComments(new List<int>{commentId}, teacherId);
+        }
+
+        public void DeleteComments(IList<int> commentsIds, int teacherId)
+        {
+            if (!Context.PersonId.HasValue)
+                throw new UnassignedUserException();
+            if (!BaseSecurity.IsDistrictOrTeacher(Context))
+                throw new ChalkableSecurityException();
+            var syId = ServiceLocator.SchoolYearService.GetCurrentSchoolYear().Id;
+            foreach (var commentId in commentsIds)
+            {
+                ConnectorLocator.SectionCommentConnector.DeleteComment(syId, teacherId, commentId);
+            }
+        }
+
+        public IList<TeacherComment> GetComments(int teacherId)
+        {
+            var syId = ServiceLocator.SchoolYearService.GetCurrentSchoolYear().Id;
+            var stiComments = ConnectorLocator.SectionCommentConnector.GetComments(syId, teacherId) ?? new List<GradebookCommect>();
+            return stiComments.Select(TeacherComment.Create).ToList();
+        }
+
+        private void EnsureIsDistrictAdminOrCurrentTeacher(int teacherId)
+        {
+            if (!(BaseSecurity.IsDistrictAdmin(Context) || (Context.Role == CoreRoles.TEACHER_ROLE && Context.PersonId == teacherId)))
+                throw new ChalkableSecurityException();
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/models/apps/AppMarketDetailsViewData.js
@@ -0,0 +1,42 @@
+REQUIRE('chlk.models.apps.AppMarketBaseViewData');
+
+NAMESPACE('chlk.models.apps', function () {
+    "use strict";
+    /** @class chlk.models.apps.AppMarketDetailsViewData*/
+    CLASS(
+        'AppMarketDetailsViewData', EXTENDS(chlk.models.apps.AppMarketBaseViewData), [
+            chlk.models.apps.AppMarketApplication, 'app',
+            String, 'installBtnTitle',
+            Boolean, 'installed',
+            Boolean, 'fromNewItem',
+            Boolean, 'fromSuggestedApps',
+
+            [[
+                chlk.models.apps.AppMarketApplication,
+                String,
+                ArrayOf(chlk.models.apps.AppCategory),
+                ArrayOf(chlk.models.apps.AppGradeLevel),
+                Number,
+                Boolean,
+                Boolean,
+                Boolean
+            ]],
+            function $(app, installBtnTitle, categories, gradelevels, balance, isInstalled, fromNewItem_, fromSuggestedApps_) {
+                BASE(categories, gradelevels, balance);
+                this.setApp(app);
+                var webSiteLink = app.getDeveloperInfo().getWebSite() || "";
+
+                if (webSiteLink.indexOf("http") == -1){
+                    webSiteLink = "http://" + webSiteLink;
+                    app.getDeveloperInfo().setWebSite(webSiteLink);
+                }
+
+                this.setInstalled(isInstalled);
+                this.setInstallBtnTitle(installBtnTitle);
+                this.setFromNewItem(fromNewItem_ || false);
+                this.setFromSuggestedApps(fromSuggestedApps_ || false);
+            }
+        ]);
+
+
+});
