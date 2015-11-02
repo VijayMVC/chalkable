55ac8cf4fe9a830d113c83e5973bb9938fcafc39
Volodymyr Iatsyshyn <viatsyshyn@emp-game.com>
2015-11-02 20:06:37 +0200

    Implemented file droping

--- Chalkable.Web/app/chlk/controls/FileUploadControl.js
+++ Chalkable.Web/app/chlk/controls/FileUploadControl.js
@@ -16,7 +16,9 @@ NAMESPACE('chlk.controls', function () {
                 var that = this, params = attrs['data-params'] || [],
                     controller = attrs['data-controller'],
                     action = attrs['data-action'],
-                    needFileIndex = attrs.needFileIndex, index = 0;
+                    needFileIndex = attrs.needFileIndex,
+                    index = 0,
+                    dropAreaSelector = attrs.dropAreaSelector;
                 if (controller)
                 {
                     this.context.getDefaultView()
@@ -34,7 +36,43 @@ NAMESPACE('chlk.controls', function () {
                                 state.setParams(p);
                                 state.setPublic(false);
                                 that.context.stateUpdated();
-                            })
+                            });
+
+                            var $dropArea = dropAreaSelector ? node.$.closest(dropAreaSelector) : node.$;
+                            $dropArea
+                                .on('dragenter', function (e) {
+                                    e.stopPropagation();
+                                    e.preventDefault();
+                                    dropAreaSelector && $dropArea.css({'border-style': 'solid'});
+                                })
+                                .on('dragover', function (e) {
+                                    e.stopPropagation();
+                                    e.preventDefault();
+                                })
+                                .on('dragleave', function (e) {
+                                    e.stopPropagation();
+                                    e.preventDefault();
+                                    dropAreaSelector && $dropArea.css({'border-style': ''});
+                                })
+                                .on('drop', function (e) {
+                                    e.preventDefault();
+
+                                    dropAreaSelector && $dropArea.css({'border-style': ''});
+
+                                    var files = e.originalEvent.dataTransfer.files;
+
+                                    var state = that.context.getState();
+                                    var p = params.slice();
+                                    if (needFileIndex)
+                                        p.push(index++);
+                                    p.push(files);
+                                    state.setController(controller);
+                                    state.setAction(action);
+                                    state.setParams(p);
+                                    state.setPublic(false);
+                                    that.context.stateUpdated();
+                                });
+
                         }.bind(this));
                 }
                 return attrs;
