e76725e2180cd5a837cf25f9cd65aa07804ab2ee
Ievgenii Zasoba <geka.zasoba@gmail.com>
2015-11-19 16:48:34 +0200

    Added trigger to fix address sync issue

---
+++ FluentACS/Specs/IdentityProviderSpec.cs
@@ -0,0 +1,19 @@
+﻿namespace FluentACS.Specs
+{
+    public class IdentityProviderSpec
+    {
+        private string displayName;
+
+        public void DisplayName(string name)
+        {
+            Guard.NotNullOrEmpty(() => name, name);
+
+            this.displayName = name;
+        }
+
+        internal string DisplayName()
+        {
+            return this.displayName;
+        }
+    }
+}
