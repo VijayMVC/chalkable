eaeb3d09f5b8ed57c9bccadbae2f27fc6654ec3e
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-12-02 05:33:25 -0800

    fixed feed printing settings

---
+++ Chalkable.Data.School/Model/Announcements/AdminAnnouncement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Chalkable.Data.Common;
+
+namespace Chalkable.Data.School.Model.Announcements
+{
+    public class AdminAnnouncement : Announcement
+    {
+        private const string ADMIN_ANNOUNCEMENT_TYPE_NAME = "Admin Announcement";
+        
+        public const string VW_ADMIN_ANNOUNCEMENT_NAME = "vwAdminAnnouncement";
+        public const string VW_ADMIN_ANNOUNCEMENT_COMPLEX = "vwAdminAnnouncementComplex";
+
+        public const string ADMIN_REF_FIELD = "AdminRef";
+        public const string EXPIRES_FIELD = "Expires";
+        public const string ADMIN_NAME_FIELD = "AdminName";
+
+        public DateTime Expires { get; set; }
+        public int AdminRef { get; set; }
+
+        [NotDbFieldAttr]
+        public string AdminName { get; set; }
+        [NotDbFieldAttr]
+        public string AdminGender { get; set; }
+        [NotDbFieldAttr]
+        public override string AnnouncementTypeName
+        {
+            get { return ADMIN_ANNOUNCEMENT_TYPE_NAME; }
+        }
+        [NotDbFieldAttr]
+        public override AnnouncementTypeEnum Type
+        {
+            get { return AnnouncementTypeEnum.Admin; }
+        }
+        [NotDbFieldAttr]
+        public override int OwnereId
+        {
+            get { return AdminRef; }
+        }
+    }
+}
---
+++ Chalkable.Web/Controllers/SchoolYearController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Chalkable.Data.Common.Enums;
+using Chalkable.Web.ActionFilters;
+using Chalkable.Web.Models;
+
+namespace Chalkable.Web.Controllers
+{
+    [RequireHttps, TraceControllerFilter]
+    public class SchoolYearController : ChalkableController
+    {
+        [AuthorizationFilter("SysAdmin, DistrictAdmin, Teacher, Student", true, new[] { AppPermissionType.Schedule })]
+        public ActionResult List(int? start, int? count)
+        {
+            var res = SchoolLocator.SchoolYearService.GetSchoolYears(start ?? 0, count ?? 10);
+            return Json(res.Transform(SchoolYearViewData.Create));
+        }
+
+        [AuthorizationFilter("SysAdmin, DistrictAdmin, Teacher, Student", true, new[] { AppPermissionType.Schedule })]
+        public ActionResult CurrentSchoolYear()
+        {
+            var res = SchoolLocator.SchoolYearService.GetCurrentSchoolYear();
+            return Json(SchoolYearViewData.Create(res));
+        }
+ }
+}
