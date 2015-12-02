86c0417240fc283b431844b27da29364c0193507
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-12-02 18:23:00 +0200

    Merge with QA_Bugfixing

---
+++ Chalkable.BusinessLogic/Model/FeedSettingsInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Chalkable.Common;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Model
+{
+    public class FeedSettingsInfo
+    {
+        public int? AnnouncementType { get; set; }
+        public bool? SortType { get; set; }
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+        public int? GradingPeriodId { get; set; }
+
+        public FeedSettingsInfo() { }
+
+        public FeedSettingsInfo(IDictionary<string, string> kv)
+        {
+            if (kv.ContainsKey(PersonSetting.FEED_START_DATE))
+                FromDate = DateTimeTools.ParseExactNullable(kv[PersonSetting.FEED_START_DATE]);
+            if (kv.ContainsKey(PersonSetting.FEED_END_DATE))
+                ToDate = DateTimeTools.ParseExactNullable(kv[PersonSetting.FEED_END_DATE]);
+
+            if (kv.ContainsKey(PersonSetting.FEED_GRADING_PERIOD_ID) && !string.IsNullOrWhiteSpace(kv[PersonSetting.FEED_GRADING_PERIOD_ID]))
+                GradingPeriodId = int.Parse(kv[PersonSetting.FEED_GRADING_PERIOD_ID]);
+
+            if (kv.ContainsKey(PersonSetting.FEED_ANNOUNCEMENT_TYPE) && !string.IsNullOrWhiteSpace(kv[PersonSetting.FEED_ANNOUNCEMENT_TYPE]))
+                AnnouncementType = int.Parse(kv[PersonSetting.FEED_ANNOUNCEMENT_TYPE]);
+
+            SortType = GetBoolFromDictionary(PersonSetting.FEED_SORTING, kv);
+        }
+
+        private static bool GetBoolFromDictionary(string key, IDictionary<string, string> keyValue)
+        {
+            return keyValue.ContainsKey(key) && !string.IsNullOrWhiteSpace(keyValue[key]) && bool.Parse(keyValue[key]);
+        }
+
+
+        public IDictionary<string, object> ToDictionary()
+        {
+            return new Dictionary<string, object>
+            {
+                [PersonSetting.FEED_START_DATE] = FromDate,
+                [PersonSetting.FEED_END_DATE] = ToDate,
+                [PersonSetting.FEED_ANNOUNCEMENT_TYPE] = AnnouncementType,
+                [PersonSetting.FEED_GRADING_PERIOD_ID] = GradingPeriodId,
+                [PersonSetting.FEED_SORTING] = SortType
+            };
+        }
+    }
+}
---
+++ Chalkable.Data.Master/Model/ApplicationDistrictOption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Chalkable.Data.Master.Model
+{
+    public class ApplicationDistrictOption
+    {
+        public const string DISTRICT_REF_FIELD = "DistrictRef";
+        public const string APPLICATION_REF_FIELD = "ApplicationRef";
+        public const string BAN_FIELD = "Ban";
+        public Guid ApplicationRef { get; set; }
+	    public Guid DistrictRef { get; set; }
+        public bool Ban { get; set; }
+    }
+}
