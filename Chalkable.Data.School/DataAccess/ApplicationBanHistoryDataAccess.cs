5ed564c4d53d787da78b201be1c2b433b02ea866
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-12-02 15:35:00 +0200

    Checkbox refactoring

---
+++ Chalkable.Data.School/DataAccess/ApplicationBanHistoryDataAccess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Chalkable.Data.Common;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Data.School.DataAccess
+{
+    public class ApplicationBanHistoryDataAccess :DataAccessBase<ApplicationBanHistory, int>
+    {
+        public ApplicationBanHistoryDataAccess(UnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public IList<ApplicationBanHistory> GetApplicationBanHistory(Guid applicationId)
+        {
+            var ps = new Dictionary<string, object> {{"applicationId", applicationId}};
+            return ExecuteStoredProcedureList<ApplicationBanHistory>("spGetApplicationBanHistory", ps);
+        } 
+    }
+}
