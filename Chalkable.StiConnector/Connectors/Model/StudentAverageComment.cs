7fee78edeb5366297d3d0ff309d2ab848911b012
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-12-03 16:01:17 +0200

    fixed for CHLK-4521 [60702] Feed Tools - After selecting a custom date range and then going back to edit that date range the "start" date is missing

---
+++ Chalkable.StiConnector/Connectors/Model/StudentAverageComment.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chalkable.StiConnector.Connectors.Model
+{
+    public class StudentAverageComment
+    {
+        /// <summary>
+        /// Id of the average
+        /// </summary>
+        public int AverageId { get; set; }
+
+        /// <summary>
+        /// Id of the comment (GradingCommentId)
+        /// </summary>
+        public int? CommentId { get; set; }
+
+        /// <summary>
+        /// The code for the comment.  This is the code that prints on the report card.
+        /// </summary>
+        public string CommentCode { get; set; }
+
+        /// <summary>
+        /// The text for the comment.  Example: "Nice Job"
+        /// </summary>
+        public string CommentText { get; set; }
+
+        /// <summary>
+        /// Id of the header (GradingCommentHeaderId)
+        /// </summary>
+        public int HeaderId { get; set; }
+
+        /// <summary>
+        /// The sequence that the grading comment should appear
+        /// </summary>
+        public short HeaderSequence { get; set; }
+
+        /// <summary>
+        /// Text of the grading comment header
+        /// </summary>
+        public string HeaderText { get; set; }
+
+        /// <summary>
+        /// Id of the student
+        /// </summary>
+        public int StudentId { get; set; }
+    }
+}
