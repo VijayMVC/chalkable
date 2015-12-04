ec3ed786124df427c90bfb9a6a28e3ea26ae8c8b
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2015-12-04 04:21:37 -0800

    fixed CHLK-4527 Teacher > Editing an item. The error page is shown after deleting a text from attributes

---
+++ Chalkable.Database.School/Tables/StandardSubject.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[StandardSubject] (
+    [Id]           INT            NOT NULL,
+    [Name]         NVARCHAR (100) NOT NULL,
+    [Description]  NVARCHAR (200) NOT NULL,
+    [AdoptionYear] INT            NULL,
+    [IsActive]     BIT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
