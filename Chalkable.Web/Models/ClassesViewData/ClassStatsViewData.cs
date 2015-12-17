c3cda60d195f386d081c3a321508173ac266ae9c
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-12-17 12:01:28 +0200

    fixed for CHLK-4570 Admin. Classes. The "GradeAvg" column is missing when there are no marks

---
+++ Chalkable.Web/Models/ClassesViewData/ClassStatsViewData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Chalkable.BusinessLogic.Common;
+using Chalkable.BusinessLogic.Model;
+using Chalkable.Common;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.Web.Models.ClassesViewData
+{
+    public class ClassStatsViewData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Guid? DepartmentRef { get; set; }
+        public string PrimaryTeacherDisplayName { get; set; }
+        public int StudentsCount { get; set; }
+        public decimal? AttendancesCount { get; set; }
+        public int? DisciplinesCount { get; set; }
+        public decimal? Average { get; set; }
+
+        public static ClassStatsViewData Create(ClassStatsInfo classDetails)
+        {
+            return new ClassStatsViewData()
+            {
+                Id = classDetails.Id,
+                Name = classDetails.Name,
+                DepartmentRef = classDetails.DepartmentRef,
+
+                PrimaryTeacherDisplayName = classDetails.PrimaryTeacherDisplayName,
+                StudentsCount = classDetails.StudentsCount,
+
+                AttendancesCount = classDetails.AttendancesCount,
+                Average = classDetails.Average,
+                DisciplinesCount = classDetails.DisciplinesCount
+            };
+        }
+
+        public static PaginatedList<ClassStatsViewData> Create(PaginatedList<ClassStatsInfo> classesDetails)
+        {
+            return new PaginatedList<ClassStatsViewData>(classesDetails.Select(Create), classesDetails.PageIndex,
+                classesDetails.PageSize, classesDetails.TotalCount);
+        }
+
+    }
+}
