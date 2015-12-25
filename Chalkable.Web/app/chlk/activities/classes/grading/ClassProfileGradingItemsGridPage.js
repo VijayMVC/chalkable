59efd140f2d0507a7fa5ff4ea24248378173b2c3
Ostap Stasyuk <ostasyuk@emp-game.com>
2015-12-25 14:03:51 +0200

    Grading Class Profile refactoring

--- Chalkable.Web/app/chlk/activities/classes/grading/ClassProfileGradingItemsGridPage.js
+++ Chalkable.Web/app/chlk/activities/classes/grading/ClassProfileGradingItemsGridPage.js
@@ -1,5 +1,5 @@
 REQUIRE('chlk.activities.grading.GradingClassSummaryGridPage');
-REQUIRE('chlk.templates.classes.grading.ClassProfileGradingSummaryGridTpl');
+REQUIRE('chlk.templates.classes.ClassProfileGradingTpl');
 
 NAMESPACE('chlk.activities.classes.grading', function () {
 
@@ -7,7 +7,7 @@ NAMESPACE('chlk.activities.classes.grading', function () {
     CLASS(
         [ria.mvc.DomAppendTo('#main')],
         [chlk.activities.lib.PageClass('profile')],
-        [ria.mvc.TemplateBind(chlk.templates.classes.grading.ClassProfileGradingSummaryGridTpl)],
+        [ria.mvc.TemplateBind(chlk.templates.classes.ClassProfileGradingTpl)],
         [ria.mvc.PartialUpdateRule(chlk.templates.grading.GradingCommentsTpl, chlk.activities.lib.DontShowLoader(), '.grading-comments-list', ria.mvc.PartialUpdateRuleActions.Replace)],
         'ClassProfileGradingItemsGridPage', EXTENDS(chlk.activities.grading.GradingClassSummaryGridPage), [
             OVERRIDE, function prepareGradingModel_(model){
