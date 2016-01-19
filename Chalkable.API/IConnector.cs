0964c45ae74a6e64bef47bb4452ac0c8ad58b4c4
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2016-01-19 06:32:07 -0800

    fixed CHLK-4637 [61148] Admin Summer Release - Teacher Name is "not" sorted by last name first, first name last

---
+++ Chalkable.API/IConnector.cs
@@ -0,0 +1,9 @@
+using System.Threading.Tasks;
+
+namespace Chalkable.API
+{
+    public interface IConnector
+    {
+        Task<T> Call<T>(string endpoint, OnWebRequestIsCreated onCreated = null);
+    }
+}
