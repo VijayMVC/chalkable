c2a4a74c9ddd9f65a730ffe911609559b45f0746
Oleg Yaremko <Oleg Yaremko@localhost>
2016-01-20 14:27:26 +0200

    fixed CHLK-4645 [61164] Class Profile - NOW tab. Teacher Name is not in the correct format.

--- Chalkable.BusinessLogic/Common/NameHelper.cs
+++ Chalkable.BusinessLogic/Common/NameHelper.cs
@@ -14,29 +14,28 @@ namespace Chalkable.BusinessLogic.Common
             return !string.IsNullOrEmpty(gender) && gender.ToLower() == GENDER_MALE ? MR : MS;
         }
 
-
-        private static string StaffDisplayName(string firstName, string lastName, string gender, bool upper)
+        private static string StaffDisplayName(string lastName, string gender, bool upper)
         {
-            var ln = upper ? firstName.ToUpper() + " " + lastName.ToUpper() : firstName + " " + lastName;
+            var ln = upper ? lastName.ToUpper() : lastName;
             return ResolveSalutation(gender) + ln;
         }
         
         public static string FullName(string firstName, string lastName, bool upper = true, string gender = null)
         {
-            var fn = upper ? firstName.ToUpper() : firstName;
-            var ln = upper ? lastName.ToUpper() : lastName;
+            var fn = upper ? firstName.ToUpper() : firstName.CapitalizeFirstLetter();
+            var ln = upper ? lastName.ToUpper() : lastName.CapitalizeFirstLetter();
             var gn = string.IsNullOrWhiteSpace(gender) ? "" : ResolveSalutation(gender);
             return gn + fn + " " + ln;
         }
 
         public static string TeacherDisplayName(this ScheduleItem staff, bool upper = true)
         {
-            return string.IsNullOrEmpty(staff.TeacherLastName) ? null : StaffDisplayName(staff.TeacherFirstName, staff.TeacherLastName, staff.TeacherGender, upper);
+            return string.IsNullOrEmpty(staff.TeacherLastName) ? null : StaffDisplayName(staff.TeacherLastName, staff.TeacherGender, upper);
         }
 
         public static string DisplayName(this Staff staff, bool upper = true)
         {
-            return StaffDisplayName(staff.FirstName, staff.LastName, staff.Gender, upper);
+            return StaffDisplayName(staff.LastName, staff.Gender, upper);
         }
 
         public static string DisplayName(this Student student, bool upper = true)
@@ -47,8 +46,8 @@ namespace Chalkable.BusinessLogic.Common
         public static string DisplayName(this Person person, bool upper = true)
         {
             if (person.RoleRef == CoreRoles.STUDENT_ROLE.Id)
-                return FullName(person.FirstName, person.LastName, upper); 
-            return StaffDisplayName(person.FirstName, person.LastName, person.Gender, upper);
+                return FullName(person.FirstName, person.LastName, upper);
+            return StaffDisplayName(person.LastName, person.Gender, upper);
         }
 
         public static string FullName(this Staff staff, bool upper = true, bool withSalutation = false)
@@ -61,9 +60,9 @@ namespace Chalkable.BusinessLogic.Common
             return FullName(student.FirstName, student.LastName, upper);
         }
 
-        public static string FullName(this Person person, bool upper = true)
+        public static string FullName(this Person person, bool upper = true, bool withSalutation = false)
         {
-            return FullName(person.FirstName, person.LastName, upper);
+            return FullName(person.FirstName, person.LastName, upper, withSalutation ? person.Gender : null);
         }
 
     }
