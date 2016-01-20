e7dcbf0d92ab1ebecfaac0fabad9b4485298f616
Ostap Stasyuk <ostasyuk@emp-game.com>
2016-01-20 11:44:54 +0200

    updated teacher link for Admin Classes

---
+++ Chalkable.Web/app/chlk/controllers/DistrictController.js
@@ -0,0 +1,117 @@
+REQUIRE('chlk.controllers.BaseController');
+
+REQUIRE('chlk.activities.district.DistrictListPage');
+REQUIRE('chlk.activities.district.DistrictDialog');
+REQUIRE('chlk.activities.district.DistrictSummaryPage');
+
+REQUIRE('chlk.models.district.District');
+REQUIRE('chlk.models.id.DistrictId');
+
+REQUIRE('chlk.services.DistrictService');
+REQUIRE('chlk.services.BgTaskService');
+REQUIRE('chlk.services.AdminDistrictService');
+
+NAMESPACE('chlk.controllers', function (){
+
+    /** @class chlk.controllers.DistrictController */
+    CLASS(
+        'DistrictController', EXTENDS(chlk.controllers.BaseController), [
+
+        [ria.mvc.Inject],
+        chlk.services.DistrictService, 'districtService',
+        [ria.mvc.Inject],
+        chlk.services.BgTaskService, 'bgTaskService',
+        [ria.mvc.Inject],
+        chlk.services.AdminDistrictService, 'adminDistrictService',
+
+        [chlk.controllers.SidebarButton('districts')],
+
+        [[Number]],
+        function listAction(pageIndex_) {
+            var result = this.districtService
+                .getDistricts(pageIndex_|0, 100)
+                .attach(this.validateResponse_());
+            return this.PushView(chlk.activities.district.DistrictListPage, result);
+        },
+
+        [[Number]],
+        function listSysAdminAction(pageIndex_) {
+            var result = this.districtService
+                .getDistricts(pageIndex_|0, 100)
+                .attach(this.validateResponse_());
+            return this.PushView(chlk.activities.district.DistrictListPage, result);
+        },
+
+        //TODO: join with list
+        [[Number]],
+        function pageAction(pageIndex) {
+            var result = this.districtService
+                .getDistricts(pageIndex, 100)
+                .attach(this.validateResponse_());
+            return this.UpdateView(chlk.activities.district.DistrictListPage, result);
+        },
+
+        [[chlk.models.id.DistrictId]],
+        function syncAction(id) {
+            var result = this.districtService
+                .syncDistrict(id)
+                .attach(this.validateResponse_());
+            this.ShowMsgBox('Sync task is created', 'fyi.', [{
+                text: Msg.GOT_IT.toUpperCase(),
+                controller: "district",
+                action: "list"
+            }]);
+            return null;
+        },
+
+        [[chlk.models.id.DistrictId]],
+        function deleteAction(id) {
+            var result= this.districtService
+                .removeDistrict(id)
+                .attach(this.validateResponse_())
+                .then(function (data) {
+                    return this.districtService
+                        .getDistricts(0, 100)
+                        .attach(this.validateResponse_());
+                }, this);
+            return this.UpdateView(chlk.activities.district.DistrictListPage, result);
+        },
+
+        [[chlk.models.id.BgTaskId]],
+        function cancelBgTaskAction(id){
+            return this.bgTaskService
+                .cancelTask(id)
+                .attach(this.validateResponse_())
+                .then(function(data){
+                    return this.pageAction(0);
+                }, this);
+        },
+
+        [chlk.controllers.SidebarButton('classes')],
+        function summaryAction(){
+            var result = ria.async.wait([
+                    this.adminDistrictService.getDistrictSummary(),
+                    this.adminDistrictService.getSchoolStatistic()
+                ])
+                .then(function(result){
+                    return new chlk.models.district.DistrictFullSummaryViewData(result[0], new chlk.models.admin.BaseStatisticGridViewData(result[1]));
+                })
+                .attach(this.validateResponse_());
+            return this.PushView(chlk.activities.district.DistrictSummaryPage, result);
+        },
+
+        [chlk.controllers.SidebarButton('classes')],
+        [[chlk.models.admin.BaseStatisticGridViewData]],
+        function schoolsStatisticAction(model){
+            var isFilter = model.getSubmitType() == 'filter';
+            var start = isFilter ? 0 : model.getStart();
+            var result = this.adminDistrictService.getSchoolStatistic(start, model.getFilter())
+                .then(function(model){
+                    return new chlk.models.admin.BaseStatisticGridViewData(model);
+                })
+                .attach(this.validateResponse_());
+            return this.UpdateView(chlk.activities.district.DistrictSummaryPage, result, isFilter ? null : chlk.activities.lib.DontShowLoader());
+        }
+
+    ])
+});
