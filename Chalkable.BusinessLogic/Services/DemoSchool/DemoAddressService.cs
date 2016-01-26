14c642a67baa14bf6c7bb80344ca1658ace19c62
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2016-01-26 15:40:31 +0200

    added person Claims to Person/Me responce. Created PersonClaim model in Chalkable.API

---
+++ Chalkable.BusinessLogic/Services/DemoSchool/DemoAddressService.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Chalkable.BusinessLogic.Services.School;
+using Chalkable.Data.School.Model;
+
+namespace Chalkable.BusinessLogic.Services.DemoSchool
+{
+    public class DemoAddressStorage : BaseDemoIntStorage<Address>
+    {
+        public DemoAddressStorage()
+            : base(x => x.Id)
+        {
+        }
+    }
+
+    public class DemoAddressService : DemoSchoolServiceBase, IAddressService
+    {
+        private DemoAddressStorage AddressStorage { get; set; }
+        public DemoAddressService(IServiceLocatorSchool serviceLocator)
+            : base(serviceLocator)
+        {
+            AddressStorage = new DemoAddressStorage();
+        }
+      
+        public void Add(IList<Address> addresses)
+        {
+            AddressStorage.Add(addresses);
+        }
+
+        public void Edit(IList<Address> addressInfos)
+        {
+            AddressStorage.Update(addressInfos);
+        }
+
+        public void Delete(IList<Address> addresses)
+        {
+            AddressStorage.Delete(addresses);
+            
+        }
+
+        public IList<Address> GetAddress()
+        {
+            return AddressStorage.GetAll();
+        }
+
+        public Address GetAddress(int addressId)
+        {
+            return AddressStorage.GetAll().FirstOrDefault(x => x.Id == addressId);
+        }
+    }
+}
---
+++ Chalkable.StiConnector/VersionHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Chalkable.Common.Exceptions;
+
+namespace Chalkable.StiConnector
+{
+    public static class VersionHelper
+    {
+        public static void ValidateVersionFormat(string version)
+        {
+            if (!IsValidVersionFormat(version))
+                throw new ChalkableException("Invalid version format. Version should have next format 0.0.0.0");
+        }
+
+        private static bool IsValidVersionFormat(this string version)
+        {
+            if (string.IsNullOrWhiteSpace(version)) return false;
+
+            var versionParts = version.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            int number;
+            return versionParts.Length == 4 && versionParts.All(x => int.TryParse(x, out number));
+        }
+
+        public static int CompareVersionTo(string strVersion1, string strVersion2)
+        {
+            var version1 = new Version(strVersion1);
+            var version2 = new Version(strVersion2);
+            return version1.CompareTo(version2);
+        }
+    }
+}
