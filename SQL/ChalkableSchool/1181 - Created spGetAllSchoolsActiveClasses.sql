225228e160e915cd0a1cb8b4f73e488317ca954b
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2016-01-29 20:00:58 +0200

    fixed getLessonplans procedure

---
+++ SQL/ChalkableSchool/1181 - Created spGetAllSchoolsActiveClasses.sql
@@ -0,0 +1,15 @@
+Create Procedure spGetAllSchoolsActiveClasses 
+as
+
+Declare @class TClassDetails
+
+	Insert Into 
+		@class
+	Select 
+		vwAllSchoolsActiveClasses.*, (select count(*) from ClassPerson where ClassRef = Class_Id) as Class_StudentsCount
+	From 
+		vwAllSchoolsActiveClasses
+
+	exec spSelectClassDetails @class
+
+GO
