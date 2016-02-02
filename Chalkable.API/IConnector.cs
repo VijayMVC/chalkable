2aefc0f6452fcec8f12f33435233d7eb4e3d39f6
Volodymyr Iatsyshyn <viatsyshyn@emp-game.com>
2016-02-02 13:05:18 +0200

    added StudyCenter endpoint

--- Chalkable.API/IConnector.cs
+++ Chalkable.API/IConnector.cs
@@ -1,9 +1,10 @@
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Chalkable.API
 {
     public interface IConnector
     {
-        Task<T> Call<T>(string endpoint, OnWebRequestIsCreated onCreated = null);
+        Task<T> Call<T>(string endpoint, OnWebRequestIsCreated onCreated = null, string method = null);
     }
 }
