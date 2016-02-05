3d89414f9b94cf848284277d60c3d4782738dbcc
Oleg Yaremko <Oleg Yaremko@localhost>
2016-02-05 17:56:36 +0200

    fixed CHLK-4698 Teacher. Class profile. Now. Lesson plans aren't shown during viewing, if a teacher isn't an owner of a class

--- Chalkable.BusinessLogic/Services/School/Announcements/LessonPlanService.cs
+++ Chalkable.BusinessLogic/Services/School/Announcements/LessonPlanService.cs
@@ -55,13 +55,16 @@ namespace Chalkable.BusinessLogic.Services.School.Announcements
 
         protected LessonPlanDataAccess CreateLessonPlanDataAccess(UnitOfWork unitOfWork)
         {
-            Trace.Assert(Context.SchoolYearId.HasValue);
-            if (BaseSecurity.IsTeacher(Context))
-                return new LessonPlanForTeacherDataAccess(unitOfWork, Context.SchoolYearId.Value);
+            Trace.Assert(Context.SchoolYearId.HasValue);        
+            if (BaseSecurity.IsDistrictOrTeacher(Context))
+            {
+                if (Context.Claims.HasPermission(ClaimInfo.VIEW_CLASSROOM_ADMIN) || Context.Claims.HasPermission(ClaimInfo.MAINTAIN_CLASSROOM_ADMIN))
+                    return new LessonPlanForAdminDataAccess(unitOfWork, Context.SchoolYearId.Value);
+                if (Context.Claims.HasPermission(ClaimInfo.VIEW_CLASSROOM) || Context.Claims.HasPermission(ClaimInfo.MAINTAIN_CLASSROOM))
+                    return new LessonPlanForTeacherDataAccess(unitOfWork, Context.SchoolYearId.Value);
+            }
             if (Context.Role == CoreRoles.STUDENT_ROLE)
                 return new LessonPlanForStudentDataAccess(unitOfWork, Context.SchoolYearId.Value);
-            if (BaseSecurity.IsDistrictAdmin(Context))
-                return new LessonPlanForAdminDataAccess(unitOfWork, Context.SchoolYearId.Value);
 
             throw new ChalkableException("Not supported role for lesson plan");
         }
