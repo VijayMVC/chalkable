ec737d8d80916be99c5fc0745a8fd06720cbec0c
Igor Zhvanko <igzhva@gmail.com>
2016-02-08 17:13:49 +0200

    fixed CHLK-4693 Admin. Teacher's profile. Schedule. Admin doesn't see items during viewing the schedule of other users

---
+++ Chalkable.Web/Controllers/PersonControllers/PersonController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Web.Mvc;
+using Chalkable.BusinessLogic.Security;
+using Chalkable.Common;
+using Chalkable.Common.Exceptions;
+using Chalkable.Data.Common.Enums;
+using Chalkable.Data.School.Model;
+using Chalkable.Web.ActionFilters;
+using Chalkable.Web.Models.PersonViewDatas;
+
+namespace Chalkable.Web.Controllers.PersonControllers
+{
+    [RequireHttps, TraceControllerFilter]
+    public class PersonController : UserController
+    {
+        [AuthorizationFilter("DistrictAdmin, Teacher, Student", true, new[] { AppPermissionType.User })]
+        public ActionResult Me()
+        {
+            if (!Context.PersonId.HasValue)
+                throw new UnassignedUserException();
+            var person = SchoolLocator.PersonService.GetPersonDetails(Context.PersonId.Value);
+            if (!Context.SchoolYearId.HasValue)
+                throw new ChalkableException("User has no valid school year id");
+            if (!Context.DistrictId.HasValue || !Context.SchoolLocalId.HasValue)
+                throw new UnassignedUserException("User is not assigned to any school");
+
+            var district = MasterLocator.DistrictService.GetByIdOrNull(Context.DistrictId.Value);
+            var school = MasterLocator.SchoolService.GetById(Context.DistrictId.Value, Context.SchoolLocalId.Value);
+            var schoolYear = SchoolLocator.SchoolYearService.GetSchoolYearById(Context.SchoolYearId.Value);
+            return Json(CurrentPersonViewData.Create(person, district, school, schoolYear, Context.Claims));
+        }
+
+        protected PersonScheduleViewData PrepareScheduleData(ShortPersonViewData person)
+        {
+            var mp = SchoolLocator.MarkingPeriodService.GetMarkingPeriodByDate(Context.NowSchoolYearTime.Date);
+            IList<ClassDetails> classes = new List<ClassDetails>();
+            if (mp != null)
+            {
+                if (person.Role.Id == CoreRoles.TEACHER_ROLE.Id)
+                    classes = SchoolLocator.ClassService.GetTeacherClasses(mp.SchoolYearRef, person.Id, mp.Id);
+                else if (person.Role.Id == CoreRoles.STUDENT_ROLE.Id)
+                    classes = SchoolLocator.ClassService.GetStudentClasses(mp.SchoolYearRef, person.Id, mp.Id);
+                else
+                    throw new NotImplementedException();
+            }
+            return PersonScheduleViewData.Create(person, classes);
+        }
+
+        protected PersonInfoViewData GetInfo(int id, Func<PersonDetails, PersonInfoViewData> vdCreator)
+        {
+            if (!CanGetInfo(id))
+                throw new ChalkableSecurityException(ChlkResources.ERR_VIEW_INFO_INVALID_RIGHTS);
+            
+            var person = SchoolLocator.PersonService.GetPersonDetails(id);
+            return vdCreator(person);
+        }
+
+        protected virtual bool CanGetInfo(int personId)
+        {
+            return BaseSecurity.IsDistrictOrTeacher(SchoolLocator.Context) || SchoolLocator.Context.PersonId == personId;
+        }
+
+        [AuthorizationFilter("DistrictAdmin, Teacher, Student")]
+        public ActionResult GetPersons(int? start, int? count, string filter)
+        {
+            var res = SchoolLocator.PersonService.SearchPersons(filter, true, start ?? 0, count ?? 30);
+            return Json(res.Transform(ShortPersonViewData.Create));
+        }
+        
+        [AuthorizationFilter("DistrictAdmin, Teacher, Student")]
+        public ActionResult UpdateInfo(int personId, string email)
+        {
+            if (personId != Context.PersonId)
+                throw new ChalkableSecurityException("User can change email just for himself");
+            Trace.Assert(email != null);
+            string errorMessage;
+            SchoolLocator.PersonService.EditEmailForCurrentUser(email, out errorMessage);
+            if (!string.IsNullOrEmpty(errorMessage))
+                return Json(new ChalkableException(errorMessage));
+
+            var res = GetInfo(personId, PersonInfoViewData.Create);
+            if(Context.Role == CoreRoles.TEACHER_ROLE)
+                res.Email = MasterLocator.UserService.GetUserEmailById(Context.UserId);
+            return Json(res);
+        }
+    }
+}
---
+++ Chalkable.Web/app/chlk/controllers/AccountController.js
@@ -0,0 +1,95 @@
+REQUIRE('chlk.controllers.BaseController');
+REQUIRE('chlk.services.DeveloperService');
+REQUIRE('chlk.services.AccountService');
+REQUIRE('chlk.activities.profile.DeveloperPage');
+REQUIRE('chlk.models.id.SchoolPersonId');
+REQUIRE('chlk.models.developer.DeveloperInfo');
+REQUIRE('chlk.models.account.ChangePassword');
+
+
+NAMESPACE('chlk.controllers', function (){
+
+    /** @class chlk.controllers.AccountController*/
+    CLASS(
+        'AccountController', EXTENDS(chlk.controllers.BaseController), [
+
+
+        [ria.mvc.Inject],
+        chlk.services.DeveloperService, 'developerService',
+
+        [ria.mvc.Inject],
+        chlk.services.AccountService, 'accountService',
+
+        [ria.mvc.ServiceEvent(chlk.services.DeveloperService)],
+        [[String]],
+        VOID, function onUserInfoChange(userName) {
+            this.getContext().getDefaultView().updateUserName(userName);
+        },
+
+        [[chlk.models.account.ChangePassword]],
+        function changePasswordAction(model){
+            this.accountService
+                .changePassword(model.getOldPassword(), model.getNewPassword(), model.getNewPasswordConfirmation())
+                .attach(this.validateResponse_())
+                .then(function(success){
+                    return success
+                        ? this.ShowAlertBox('Password was changed.')
+                        : this.ShowAlertBox('Change password failed.');
+                }, this)
+                .then(function () {
+                    return this.BackgroundNavigate('settings', 'dashboard', []);
+                }, this);
+
+            return null;
+        },
+
+         function logoutAction(){
+             return this.accountService
+                 .logOut()
+                 .attach(this.validateResponse_())
+                 .then(function(res){
+                    if (res.isSuccess()){
+                        location.href = this.getContext().getSession().get('webSiteRoot');
+                        return ria.async.BREAK;
+                    }
+                 }, this);
+         },
+
+        [chlk.controllers.AccessForRoles([
+            chlk.models.common.RoleEnum.DEVELOPER
+        ])],
+        [chlk.controllers.SidebarButton('settings')],
+         function profileDeveloperAction(){
+             var result = this.developerService
+                 .getInfo(this.developerService.getCurrentDeveloperSync().getId())
+                 .attach(this.validateResponse_());
+             return this.PushView(chlk.activities.profile.DeveloperPage, result);
+         },
+
+         [chlk.controllers.AccessForRoles([
+             chlk.models.common.RoleEnum.DEVELOPER
+         ])],
+        [chlk.controllers.SidebarButton('settings')],
+        [[chlk.models.developer.DeveloperInfo]],
+        function profileSaveDeveloperAction(model){
+            this.developerService
+                .saveInfo(
+                    model.getId(),
+                    model.getName(),
+                    model.getWebSite(),
+                    model.getEmail()
+                )
+                .attach(this.validateResponse_())
+                .then(function(res){
+                    return res != null && res.isValidInfo()
+                        ? this.ShowAlertBox('Info saved.')
+                        : this.ShowAlertBox(!res.isValidEmail() ? 'Email already exists' : 'Info save failed.')
+                }, this)
+                .then(function () {
+                    return this.BackgroundNavigate('settings', 'dashboard', []);
+                }, this);
+            return null;
+        }
+
+    ])
+});
