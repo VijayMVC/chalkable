2f877804056e08ab2c19427ba73d4796e8206a1a
Oleg Yaremko <Oleg Yaremko@localhost>
2016-02-10 17:51:08 +0200

    implemented District Admin Settings on server side

---
+++ Chalkable.Api.SampleApp/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Chalkable.Api.SampleApp.Controllers
+{
+    public class StudentController : BaseSampleAppController
+    {
+        public ActionResult Index()
+        {
+            PrepareBaseData(null);
+            return View("App");
+        }
+
+        public ActionResult ViewMode(int announcementApplicationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Practice(string standardId, string commonCoreStandard, string standardName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
