9cebe10ebd85f7ea7a1aa41bba7bb360bc4d5ee6
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2016-02-11 16:17:14 +0200

    fixed upload documant in Chalkable.Api.SampleApp

--- Chalkable.Api.SampleApp/Controllers/DocumentController.cs
+++ Chalkable.Api.SampleApp/Controllers/DocumentController.cs
@@ -9,7 +9,7 @@ namespace Chalkable.Api.SampleApp.Controllers
     public class DocumentController : BaseSampleAppController
     {
         [HttpPost]
-        public async Task<ActionResult> Upload(int announcementId, int announcementType, int? attributeId, string fileName)
+        public async Task<ActionResult> Upload(int announcementId, int announcementType, int? attributeId)
         {
             try
             {
@@ -17,26 +17,26 @@ namespace Chalkable.Api.SampleApp.Controllers
                 string name;
                 if (!GetFileFromRequest(out bin, out name))
                 {
-                    return Json(new Exception("File required"));
+                   throw new Exception("File required");
                 }
                 using (var fileStream = new MemoryStream())
                 {
                     fileStream.Write(bin, 0, bin.Length);
                     fileStream.Seek(0, SeekOrigin.Begin);
                     if (attributeId.HasValue)
-                        await Connector.Announcement.UploadAttributeAttachment(announcementId, announcementType, attributeId.Value, fileName, fileStream);
+                        await Connector.Announcement.UploadAttributeAttachment(announcementId, announcementType, attributeId.Value, name, fileStream);
                     else
-                        await Connector.Announcement.UploadAnnouncementAttachment(announcementId, announcementType, fileName, fileStream);
+                        await Connector.Announcement.UploadAnnouncementAttachment(announcementId, announcementType, name, fileStream);
                 }
             }
             catch (Exception e)
             {
                 if (Request.IsAjaxRequest())
-                    return Json(new { success = false, message = e.ToString() }, JsonRequestBehavior.AllowGet);
+                    return Json(new { success = false, message = e.ToString() }, "text/html");
 
                 throw;
             }
-            return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+            return Json(new { success = true }, "text/html", JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult Index(int announcementId, int announcementType, int? attributeId)
