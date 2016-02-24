e73c0a1c8c1e5fc049e8413def9cb8526b21bd53
Oleg Yaremko <Oleg Yaremko@localhost>
2016-02-24 16:42:56 +0200

    remove classes without school year and teacher in search

--- Chalkable.Database.School/Stored Procedures/spSearchClasses.sql
+++ Chalkable.Database.School/Stored Procedures/spSearchClasses.sql
@@ -6,15 +6,17 @@ as
 declare @class TClassDetails
 
 insert into
-@class
+	@class
 select
-vwClass.*, (select count(*) from ClassPerson where ClassRef = Class_Id) as Class_StudentsCount
+	vwClass.*, (select count(*) from ClassPerson where ClassRef = Class_Id) as Class_StudentsCount
 from
-vwClass
+	vwClass
 where
-(@filter1 is null and @filter2 is null and @filter3 is null) or
-(@filter1 is not null and Class_Name like @filter1 or
-@filter2 is not null and Class_Name like @filter2 or
-@filter3 is not null and Class_Name like @filter3)
+	(@filter1 is null and @filter2 is null and @filter3 is null) or
+	(@filter1 is not null and Class_Name like @filter1 or
+	@filter2 is not null and Class_Name like @filter2 or
+	@filter3 is not null and Class_Name like @filter3) and
+	SchoolYear_Id is not null and
+	Class_PrimaryTeacherRef is not null
 
 exec spSelectClassDetails @class
