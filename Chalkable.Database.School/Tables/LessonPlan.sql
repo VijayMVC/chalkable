1157f81a77e6d98dc95f63f5d5a8bb8a202f4e32
ostasyuk <ostasyuk@localhost>
2016-02-25 16:49:15 +0200

    updated Standards Picker for Assessment (adding for question)

---
+++ Chalkable.Database.School/Tables/LessonPlan.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[LessonPlan] (
+    [Id]                 INT           NOT NULL,
+    [StartDate]          DATETIME2 (7) NULL,
+    [EndDate]            DATETIME2 (7) NULL,
+    [ClassRef]           INT           NOT NULL,
+    [GalleryCategoryRef] INT           NULL,
+    [VisibleForStudent]  BIT           NOT NULL,
+    [SchoolYearRef]      INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_LessonPlan_Announcement] FOREIGN KEY ([Id]) REFERENCES [dbo].[Announcement] ([Id]),
+    CONSTRAINT [FK_LessonPlan_Class] FOREIGN KEY ([ClassRef]) REFERENCES [dbo].[Class] ([Id]),
+    CONSTRAINT [FK_LessonPlan_LPGalleryCategory] FOREIGN KEY ([GalleryCategoryRef]) REFERENCES [dbo].[LPGalleryCategory] ([Id]),
+    CONSTRAINT [FK_LessonPlan_SchoolYear] FOREIGN KEY ([SchoolYearRef]) REFERENCES [dbo].[SchoolYear] ([Id])
+);
+
---
+++ Chalkable.Database.School/Tables/SectionTimeSlotVariation.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[SectionTimeSlotVariation] (
+    [ClassRef]                      INT NOT NULL,
+    [ScheduledTimeSlotVariationRef] INT NOT NULL,
+    CONSTRAINT [PK_SectionTimeSlotVariation] PRIMARY KEY CLUSTERED ([ClassRef] ASC, [ScheduledTimeSlotVariationRef] ASC),
+    CONSTRAINT [FK_SectionTimeSlotVariation_Class] FOREIGN KEY ([ClassRef]) REFERENCES [dbo].[Class] ([Id]),
+    CONSTRAINT [FK_SectionTimeSlotVariation_ScheduledTimeSlotVariation] FOREIGN KEY ([ScheduledTimeSlotVariationRef]) REFERENCES [dbo].[ScheduledTimeSlotVariation] ([Id])
+);
+
