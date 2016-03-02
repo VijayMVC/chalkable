ccea708fb3b9dfae3662dfa9609361a41e7afd02
Taras Kolodiy <tkolodiy@emp-org.ua>
2016-03-02 10:30:18 -0600

    added more automation tests

---
+++ Chalkable.Database.School/User Defined Types/TNotification.sql
@@ -0,0 +1,13 @@
+﻿CREATE TYPE [dbo].[TNotification] AS TABLE (
+    [Type]              INT              NOT NULL,
+    [Message]           NVARCHAR (1024)  NULL,
+    [Shown]             BIT              NOT NULL,
+    [PersonRef]         INT              NOT NULL,
+    [AnnouncementRef]   INT              NULL,
+    [PrivateMessageRef] INT              NULL,
+    [ApplicationRef]    UNIQUEIDENTIFIER NULL,
+    [QuestionPersonRef] INT              NULL,
+    [Created]           DATETIME2 (7)    NOT NULL,
+    [MarkingPeriodRef]  INT              NULL,
+    [WasSend]           BIT              NOT NULL);
+
