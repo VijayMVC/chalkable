7529c16fa3cdc6756cdf38546c8b07091ae8f7cc
Igor Zhvanko <igzhva@gmail.com>
2016-03-07 12:40:15 +0200

    implemented task CHLK-4720 Classes view - change 'Attendance' count to percentage of students in attendance

---
+++ Chalkable.AcademicBenchmarkConnector/Models/RelatedStandard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Chalkable.AcademicBenchmarkConnector.Models
+{
+    public class RelatedStandard
+    {
+        [JsonProperty("data")]
+        public Standard CurrentStandard { get; set; }
+        [JsonProperty("relations")]
+        public StandardRelations Relations { get; set; }
+    }
+
+    public class StandardRelations
+    {
+        [JsonProperty("origin")]
+        public IList<StandardRelation> Origins { get; set; }
+        [JsonProperty("derivative")]
+        public IList<StandardRelation> Derivatives { get; set; }
+        [JsonProperty("related_derivative")]
+        public IList<StandardRelation> RelatedDerivatives { get; set; }
+    }
+
+    public class StandardRelation
+    {
+        //TODO ensure do we need these properties
+        //[JsonProperty("same_concept")]
+        //public string SameConcept { get; set; }
+        //[JsonProperty("same_text")]
+        //public string SameText { get; set; }
+
+        [JsonProperty("standard")]
+        public Standard Standard { get; set; }
+    }
+}
