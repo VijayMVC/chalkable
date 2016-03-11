58550dc25cfae360cb035d4329c8202e150177b4
Igor Zhvanko <igzhva@gmail.com>
2016-03-11 17:19:17 +0200

    fixed Topic model in Chalkable.API

--- Chalkable.API/Models/AcademicBenchmark/Topic.cs
+++ Chalkable.API/Models/AcademicBenchmark/Topic.cs
@@ -1,16 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Chalkable.API.Models.AcademicBenchmark
 {
     public class Topic
     {
         public Guid Id { get; set; }
-        public string Number { get; set; }
         public string Description { get; set; }
-        public string Deepest { get; set; }
         public short Level { get; set; }
-        public string Status { get; set; }
-        public Guid ParentId { get; set; }
+        public Guid? ParentId { get; set; }
         public bool IsDeepest { get; set; }
         public bool IsActive { get; set; }
     }
