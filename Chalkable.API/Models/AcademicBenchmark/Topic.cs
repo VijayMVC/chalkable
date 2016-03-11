9e908e8795d98c67cccb82096277257d9739154b
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2016-03-11 16:41:04 +0200

    fixed topic

--- Chalkable.API/Models/AcademicBenchmark/Topic.cs
+++ Chalkable.API/Models/AcademicBenchmark/Topic.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Chalkable.API.Models.AcademicBenchmark
 {
@@ -15,9 +11,7 @@ namespace Chalkable.API.Models.AcademicBenchmark
         public short Level { get; set; }
         public string Status { get; set; }
         public Guid ParentId { get; set; }
-        public bool IsDeepest => Deepest == "Y";
-        public bool IsActive => Status == "Active";
-        public Authority Authority { get; set; }
-        public Document Document { get; set; }
+        public bool IsDeepest { get; set; }
+        public bool IsActive { get; set; }
     }
 }
