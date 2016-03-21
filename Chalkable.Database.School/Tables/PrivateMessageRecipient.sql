35f4c15432a2879b9ef2e60a29128d6be0d7bc4b
Igor Zhvanko <igzhva@gmail.com>
2016-03-21 18:49:50 +0200

    fixed bad merge

---
+++ Chalkable.Database.School/Tables/PrivateMessageRecipient.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[PrivateMessageRecipient] (
+    [PrivateMessageRef]  INT NOT NULL,
+    [RecipientRef]       INT NOT NULL,
+    [RecipientClassRef]  INT NULL,
+    [Read]               BIT NOT NULL,
+    [DeletedByRecipient] BIT NOT NULL,
+    CONSTRAINT [PK_PrivateMessageRecipient] PRIMARY KEY CLUSTERED ([PrivateMessageRef] ASC, [RecipientRef] ASC),
+    CONSTRAINT [FK_PrivateMessageRecipient_Class] FOREIGN KEY ([RecipientClassRef]) REFERENCES [dbo].[Class] ([Id]),
+    CONSTRAINT [FK_PrivateMessageRecipient_Person] FOREIGN KEY ([RecipientRef]) REFERENCES [dbo].[Person] ([Id]),
+    CONSTRAINT [FK_PrivateMessageRecipient_PrivateMessage] FOREIGN KEY ([PrivateMessageRef]) REFERENCES [dbo].[PrivateMessage] ([Id])
+);
+
