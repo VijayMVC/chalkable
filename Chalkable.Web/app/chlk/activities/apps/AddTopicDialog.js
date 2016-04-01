eda2fd77eaf3ebffc8c190ee70aef97e5429477a
ostasyuk <ostasyuk@localhost>
2016-04-01 17:42:30 +0300

    fixed for CHLK-4769 [61945] Standards Search Feature. Original standard listing does not display after deleting search criteria.

--- Chalkable.Web/app/chlk/activities/apps/AddTopicDialog.js
+++ Chalkable.Web/app/chlk/activities/apps/AddTopicDialog.js
@@ -9,7 +9,7 @@ NAMESPACE('chlk.activities.apps', function(){
         [ria.mvc.DomAppendTo('#chlk-dialogs')],
         [ria.mvc.PartialUpdateRule(chlk.templates.standard.ItemsListTpl, 'list-update', '.browse-items-cnt', ria.mvc.PartialUpdateRuleActions.Replace)],
         [ria.mvc.PartialUpdateRule(chlk.templates.standard.StandardsBreadcrumbTpl, 'add-breadcrumb', '.breadcrumbs-cnt', ria.mvc.PartialUpdateRuleActions.Append)],
-        [ria.mvc.PartialUpdateRule(chlk.templates.standard.StandardsBreadcrumbTpl, 'replace-breadcrumbs', '.breadcrumbs-cnt', ria.mvc.PartialUpdateRuleActions.Replace)],
+        [ria.mvc.PartialUpdateRule(chlk.templates.standard.StandardsBreadcrumbTpl, 'replace-breadcrumbs', '.breadcrumbs-cnt-main', ria.mvc.PartialUpdateRuleActions.Replace)],
         [ria.mvc.PartialUpdateRule(chlk.templates.standard.StandardsMainTableTpl, 'clear-search', '.browse-cnt', ria.mvc.PartialUpdateRuleActions.Replace)],
         [ria.mvc.TemplateBind(chlk.templates.apps.AddTopicDialogTpl)],
 
