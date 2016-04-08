c9f2f8b359da749dcafcf8111bd6eb799d1b696c
Volodymyr Iatsyshyn <viatsyshyn@emp-game.com>
2016-04-08 11:44:05 +0300

    added more details to assert message

---
+++ Chalkable.Database.School/Tables/SchoolPerson.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SchoolPerson] (
+    [SchoolRef] INT NOT NULL,
+    [PersonRef] INT NOT NULL,
+    [RoleRef]   INT NOT NULL,
+    CONSTRAINT [PK_SchoolPerson] PRIMARY KEY CLUSTERED ([SchoolRef] ASC, [PersonRef] ASC),
+    CONSTRAINT [FK_SchoolStaff_PERSON] FOREIGN KEY ([PersonRef]) REFERENCES [dbo].[Person] ([Id]),
+    CONSTRAINT [FK_SchoolStaff_School] FOREIGN KEY ([SchoolRef]) REFERENCES [dbo].[School] ([Id])
+);
+
