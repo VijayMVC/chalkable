7ac59e9700e218f2783ecded2880fb842d8fa511
Volodymyr Iatsyshyn <viatsyshyn@emp-game.com>
2016-04-11 16:10:05 +0300

    fixed sales configuration

---
+++ Chalkable.Web/app/chlk/templates/common/attachments/AttachmentDialogTpl.js
@@ -0,0 +1,27 @@
+REQUIRE('chlk.templates.ChlkTemplate');
+REQUIRE('chlk.models.common.attachments.BaseAttachmentViewData');
+
+NAMESPACE('chlk.templates.common.attachments', function () {
+    /** @class chlk.templates.common.attachments.AttachmentDialogTpl*/
+
+    ASSET('~/assets/jade/common/attachments/attachments-shared.jade')();
+
+    CLASS(
+        [ria.templates.TemplateBind('~/assets/jade/common/attachments/AttachmentDialog.jade')],
+        [ria.templates.ModelBind(chlk.models.common.attachments.BaseAttachmentViewData)],
+        'AttachmentDialogTpl', EXTENDS(chlk.templates.ChlkTemplate), [
+
+            [ria.templates.ModelPropertyBind],
+            ArrayOf(chlk.models.common.attachments.ToolbarButton), 'toolbarButtons',
+
+            [ria.templates.ModelPropertyBind],
+            String, 'url',
+
+            [ria.templates.ModelPropertyBind],
+            chlk.models.attachment.AttachmentTypeEnum, 'type',
+
+            String, function getUrl() {
+                return this.url + (/\?/.test(this.url) ? '&' : '?') + 'apiRoot=' + encodeURIComponent(_GLOBAL.location.origin);
+            }
+        ])
+});
---
+++ SQL/ChalkableMaster/1013 - insert default common core standards.sql
@@ -0,0 +1,6 @@
+insert into CommonCoreStandard
+values ('RL.K',	'Reading: Literature'),
+	('RL.K.1',	'With prompting and support, ask and answer questions about key details in a text.'),
+	('RL.K.2',	'With prompting and support, retell familiar stories, including key details.')
+
+go
