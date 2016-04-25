b5e3a98fbfd7c97a2e9b33e9cf5544df0d740bf5
Taras Kolodiy <tkolodiy@emp-org.ua>
2016-04-25 09:27:09 -0500

    Update posting attendance by teacher

---
+++ Chalkable.AutomatedTests/tests/teacher/test_profiles/test_People_My_Students_Filter_By_Last_Name.py
@@ -0,0 +1,45 @@
+from base_auth_test import *
+
+class TestFeed(BaseAuthedTestCase):
+
+    def test_people_student_sorting_by_last_name(self):
+        self.do_student_sorting_lastname(0)    
+
+    def do_student_sorting_lastname(self, start, count = 1000):
+        empty_list =[]
+        empty_list2 =[]
+        
+        get_needed_page = self.get('/Student/GetStudents.json?myStudentsOnly=true&byLastName=true&start='+str(start)+'&count='+str(count))
+        
+        get_student = get_needed_page['data']
+        totalcount = get_needed_page['totalcount']
+        totalpages = get_needed_page['totalpages']
+        self.assertGreaterEqual(len(get_student), 1, 'Verify that list has at leat 1 student')
+      
+        for item in get_student:
+            p = item['lastname']
+            empty_list.append(p)
+        decoded_list = [x.encode('utf-8') for x in empty_list]
+       
+        only_one_student = decoded_list[0]
+        
+        get_filtered_page = self.get('/Student/GetStudents.json?classId=&filter='+str(only_one_student)+'&myStudentsOnly=true&byLastName=true&start='+str(start)+'&count='+str(count))
+        one_or_two_students = get_filtered_page['data']
+        #print len(one_or_two_students)
+        
+        if len(one_or_two_students) == 1:
+            for item in one_or_two_students:
+                a = item['lastname']
+                self.assertEqual(only_one_student, a, 'Compare two lists of students')
+    
+        if len(one_or_two_students) > 1:
+            for item in one_or_two_students:
+                p = item['lastname']
+                empty_list2.append(p)
+            decoded_list2 = [x.encode('utf-8') for x in empty_list2]  
+            
+            for item in decoded_list2:
+                self.assertEqual(item, only_one_student)
+   
+if __name__ == '__main__':
+    unittest.main()    
---
+++ Chalkable.Database.Master/Tables/BackgroundTask.sql
@@ -0,0 +1,45 @@
+﻿CREATE TABLE [dbo].[BackgroundTask] (
+    [Id]          UNIQUEIDENTIFIER NOT NULL,
+    [DistrictRef] UNIQUEIDENTIFIER NULL,
+    [Type]        INT              NOT NULL,
+    [State]       INT              NOT NULL,
+    [Created]     DATETIME2 (7)    NOT NULL,
+    [Scheduled]   DATETIME2 (7)    NOT NULL,
+    [Started]     DATETIME2 (7)    NULL,
+    [Data]        NVARCHAR (MAX)   NULL,
+    [Completed]   DATETIME2 (7)    NULL,
+    [Domain]      NVARCHAR (256)   NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_BackgroundTask_District] FOREIGN KEY ([DistrictRef]) REFERENCES [dbo].[District] ([Id])
+);
+
+
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_BackgroundTas_Domain]
+    ON [dbo].[BackgroundTask]([Domain] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_BackgroundTask_District]
+    ON [dbo].[BackgroundTask]([DistrictRef] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_BackgroundTask_Scheduled]
+    ON [dbo].[BackgroundTask]([Scheduled] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_BackgroundTask_State]
+    ON [dbo].[BackgroundTask]([State] ASC);
+
+
+GO
+
+
+
+GO
+
+
