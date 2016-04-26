b3ab1465ac61bbd85f80fc63b07ff6f5c4a2c9b0
Igor Zhvanko <igzhva@gmail.com>
2016-04-26 17:55:15 +0300

    fixed CHLK-4831 [40227] New item is prepopulating data I do not want

---
+++ Chalkable.Web/Models/PersonViewDatas/UserViewData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Chalkable.Data.Master.Model;
+
+namespace Chalkable.Web.Models.PersonViewDatas
+{
+    public class UserViewData
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public Guid? DistrictId { get; set; }
+        public int? SisId { get; set; }
+        public string Email { get; set; }
+
+        public static UserViewData Create(User user)
+        {
+            return new UserViewData
+            {
+                Id = user.Id,
+                FullName = user.FullName,
+                DistrictId = user.DistrictRef,
+                SisId = user.SisUserId,
+                Email = user.Login
+            };
+        }
+    }
+}
