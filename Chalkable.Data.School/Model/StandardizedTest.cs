66d12bf4cd0ced9ee0b2abeb0e528978c66489ff
Oleg Yaremko <Oleg Yaremko@localhost>
2016-05-23 12:12:30 +0300

    added models StandardizedTest, StandardizedTestComponent,StandardizedTestScoreType

---
+++ Chalkable.Data.School/Model/StandardizedTest.cs
@@ -0,0 +1,40 @@
+﻿namespace Chalkable.Data.School.Model
+{
+    public class StandardizedTest
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+        public int GradeLevelId { get; set; }
+        public string StateCode { get; set; }
+        public bool DisplayOnTranscript { get; set; }
+        public string SifCode { get; set; } 
+        public string NcesCode { get; set; }
+    }
+
+    public class StandardizedTestComponent
+    {
+         public int Id { get; set; }
+         public int StandardizedTestRef { get; set; }
+         public string Name { get; set; }
+         public bool DisplayOnTranscript { get; set; }
+         public string Code { get; set; }
+         public string StateCode { get; set; }
+         public string SifCode { get; set; }
+         public string NcesCode { get; set; }
+    }
+
+    public class StandardizedTestScoreType
+    {
+        public int Id { get; set; }
+        public int StandardizedTestRef { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Code { get; set; }
+        public string StateCode { get; set; }
+        public string SifCode { get; set; }
+        public string NcesCode { get; set; }
+    }
+}
