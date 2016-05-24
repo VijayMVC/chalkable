52243b40ce6bfd291316ebdfaa5049f1756cbc4f
Oleg Yaremko <Oleg Yaremko@localhost>
2016-05-24 15:57:16 +0300

    provide stored procedure spGetStandardizedTestDetails for GetListOfStandardizedTestDetails

--- Chalkable.BusinessLogic/Services/School/StandardizedTestService.cs
+++ Chalkable.BusinessLogic/Services/School/StandardizedTestService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Chalkable.Data.Common;
+using Chalkable.Data.School.DataAccess;
 using Chalkable.Data.School.Model;
 
 namespace Chalkable.BusinessLogic.Services.School
@@ -75,25 +76,7 @@ namespace Chalkable.BusinessLogic.Services.School
 
         public IList<StandardizedTestDetails> GetListOfStandardizedTestDetails()
         {
-            var standardizedTest = DoRead(u => new DataAccessBase<StandardizedTest>(u).GetAll());
-            var standardizedTestComponent = DoRead(u => new DataAccessBase<StandardizedTestComponent>(u).GetAll());
-            var standardizedTestScoreType = DoRead(u => new DataAccessBase<StandardizedTestScoreType>(u).GetAll());
-
-            return standardizedTest.Select(x => new StandardizedTestDetails
-                {
-                    Id = x.Id,
-                    DisplayName = x.DisplayName,
-                    Name = x.Name,
-                    DisplayOnTranscript = x.DisplayOnTranscript,
-                    StateCode = x.StateCode,
-                    SifCode = x.SifCode,
-                    NcesCode = x.NcesCode,
-                    Description = x.Description,
-                    GradeLevelRef = x.GradeLevelRef,
-                    Code = x.Code,
-                    Components = standardizedTestComponent.Where(stc => x.Id == stc.StandardizedTestRef).ToList(),
-                    ScoreTypes = standardizedTestScoreType.Where(stst => x.Id == stst.StandardizedTestRef).ToList()
-                }).ToList();
+            return DoRead(uow => new StandardizedTestDataAccess(uow).GetListOfStandardizedTestDetails());
         }
     }
 }
