881eb8591b48f7822f2bc8b9da4677cd1569dc51
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2016-05-30 16:54:37 +0300

    fixed copyClassAnnouncements

---
+++ Chalkable.Database.School/User Defined Types/TStudentCustomAlertDetail.sql
@@ -0,0 +1,8 @@
+﻿CREATE TYPE [dbo].[TStudentCustomAlertDetail] AS TABLE(
+	Id INT NOT NULL,
+    CustomAlertDetailId INT,
+    StudentRef INT NOT NULL,
+    SchoolYearRef INT NOT NULL,
+    AlertText VARCHAR(500),
+    CurrentValue VARCHAR(500)
+)
