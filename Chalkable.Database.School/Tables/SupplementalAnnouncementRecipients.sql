cef2e66d5b07221d48ec9f04665e0a77d62fe4f4
Oleg Yaremko <Oleg Yaremko@localhost>
2016-05-31 12:01:46 +0300

    added tables SupplementalAnnouncement and SupplementalAnnouncementRecipients

---
+++ Chalkable.Database.School/Tables/SupplementalAnnouncementRecipients.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[SupplementalAnnouncementRecipients](
+	[SupplementalAnnouncementRef] [int] NOT NULL CONSTRAINT [FK_SupplementalAnnouncementRecipients_SupplementalAnnouncement] FOREIGN KEY REFERENCES [SupplementalAnnouncement] ([Id]),
+	[StudentRef] [int] NOT NULL CONSTRAINT [FK_SupplementalAnnouncementRecipients_Student] FOREIGN KEY REFERENCES [Student] ([Id]),
+	CONSTRAINT [PK_SupplementalAnnouncementRecipients] PRIMARY KEY CLUSTERED ([SupplementalAnnouncementRef] ASC, [StudentRef] ASC)
+)
