6e089b8b5c996bf73f918fab8d45b6b659486f6a
Oleg Yaremko <Oleg Yaremko@localhost>
2016-06-02 17:10:13 +0300

    rename table SupplementalAnnouncementRecipients to SupplementalAnnouncementRecipient

--- Chalkable.Database.School/Tables/SupplementalAnnouncementRecipient.sql
+++ Chalkable.Database.School/Tables/SupplementalAnnouncementRecipient.sql
@@ -1,5 +1,5 @@
-﻿CREATE TABLE [dbo].[SupplementalAnnouncementRecipients](
-	[SupplementalAnnouncementRef] [int] NOT NULL CONSTRAINT [FK_SupplementalAnnouncementRecipients_SupplementalAnnouncement] FOREIGN KEY REFERENCES [SupplementalAnnouncement] ([Id]),
-	[StudentRef] [int] NOT NULL CONSTRAINT [FK_SupplementalAnnouncementRecipients_Student] FOREIGN KEY REFERENCES [Student] ([Id]),
-	CONSTRAINT [PK_SupplementalAnnouncementRecipients] PRIMARY KEY CLUSTERED ([SupplementalAnnouncementRef] ASC, [StudentRef] ASC)
+﻿CREATE TABLE [dbo].[SupplementalAnnouncementRecipient](
+	[SupplementalAnnouncementRef] [int] NOT NULL CONSTRAINT [FK_SupplementalAnnouncementRecipient_SupplementalAnnouncement] FOREIGN KEY REFERENCES [SupplementalAnnouncement] ([Id]),
+	[StudentRef] [int] NOT NULL CONSTRAINT [FK_SupplementalAnnouncementRecipient_Student] FOREIGN KEY REFERENCES [Student] ([Id]),
+	CONSTRAINT [PK_SupplementalAnnouncementRecipient] PRIMARY KEY CLUSTERED ([SupplementalAnnouncementRef] ASC, [StudentRef] ASC)
 )
