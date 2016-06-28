7c0bb5e7d2f23f8dfe91ef27184e190149585c74
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2016-06-28 12:20:00 +0300

    fixed database project

--- Chalkable.Database.School/Scripts/PostDeploy/001_Drop_GalleryCategoryRef_From_LessonPlan.PostDeploy.sql
+++ Chalkable.Database.School/Scripts/PostDeploy/001_Drop_GalleryCategoryRef_From_LessonPlan.PostDeploy.sql
@@ -1,8 +1,4 @@
-﻿If 
-(
-	Object_Id('[dbo].[LessonPlan]') Is Not Null
-	And Exists(Select * From sys.columns Where Name = '[GalleryCategoryRef]') 
-)
+﻿If (Object_Id('[dbo].[LessonPlan]') Is Not Null And Exists(Select * From sys.columns Where Name = '[GalleryCategoryRef]'))
 Begin
 	ALTER TABLE [dbo].[LessonPlan]
 	DROP COLUMN [GalleryCategoryRef]
