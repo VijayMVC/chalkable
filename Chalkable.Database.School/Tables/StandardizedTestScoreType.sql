46163766e0bd39b50d669aab45fdf96ad451995c
Igor Zhvanko <igzhva@gmail.com>
2016-06-28 19:29:52 +0300

    fixed sync issue on qa

--- Chalkable.Database.School/Tables/StandardizedTestScoreType.sql
+++ Chalkable.Database.School/Tables/StandardizedTestScoreType.sql
@@ -2,10 +2,10 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
 	[StandardizedTestRef] INT NOT NULL CONSTRAINT [FK_StandardizedTestScoreType_StandardizedTest] FOREIGN KEY REFERENCES [StandardizedTest] ([Id]),
-    [Name] NVARCHAR(30) NOT NULL, 
+    [Name] NVARCHAR(64) NOT NULL, 
 	[Description] NVARCHAR(MAX) NULL,
-	[Code] NVARCHAR(30) NOT NULL,
-	[StateCode] NVARCHAR(30) NULL, 
-    [SifCode] NVARCHAR(30) NULL, 
-    [NcesCode] NVARCHAR(30) NULL
+	[Code] NVARCHAR(64) NOT NULL,
+	[StateCode] NVARCHAR(64) NULL, 
+    [SifCode] NVARCHAR(64) NULL, 
+    [NcesCode] NVARCHAR(64) NULL
 )
