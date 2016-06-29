294f84cdbeace6aa0a0c0e35b4cb87523818fae5
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2016-06-29 14:13:48 +0300

    Merge with default

--- Chalkable.Database.School/Tables/StandardizedTest.sql
+++ Chalkable.Database.School/Tables/StandardizedTest.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[StandardizedTest]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [Code] NVARCHAR(30) NOT NULL, 
-    [Name] NVARCHAR(30) NOT NULL, 
-    [DisplayName] NVARCHAR(30) NULL, 
+    [Code] NVARCHAR(64) NOT NULL, 
+    [Name] NVARCHAR(64) NOT NULL, 
+    [DisplayName] NVARCHAR(64) NULL, 
     [Description] NVARCHAR(MAX) NULL,
     [GradeLevelRef] INT NULL CONSTRAINT [FK_StandardizedTest_GradeLevel] FOREIGN KEY REFERENCES [GradeLevel] ([Id]), 
-    [StateCode] NVARCHAR(30) NULL, 
+    [StateCode] NVARCHAR(64) NULL, 
     [DisplayOnTranscript] BIT NULL, 
-    [SifCode] NVARCHAR(30) NULL, 
-    [NcesCode] NVARCHAR(30) NULL
+    [SifCode] NVARCHAR(64) NULL, 
+    [NcesCode] NVARCHAR(64) NULL
 )
