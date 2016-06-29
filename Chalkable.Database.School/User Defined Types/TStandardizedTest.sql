3aed3e7a95f6f8e4e8f783b01ca9b49d40c85840
Igor Zhvanko <igzhva@gmail.com>
2016-06-29 13:07:45 +0300

    Merge with default

--- Chalkable.Database.School/User Defined Types/TStandardizedTest.sql
+++ Chalkable.Database.School/User Defined Types/TStandardizedTest.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TYPE [dbo].[TStandardizedTest] AS TABLE
 (
 	[Id] INT NOT NULL, 
-    [Code] NVARCHAR(30) NOT NULL, 
-    [Name] NVARCHAR(30) NOT NULL, 
-    [DisplayName] NVARCHAR(30) NULL, 
+    [Code] NVARCHAR(64) NOT NULL, 
+    [Name] NVARCHAR(64) NOT NULL, 
+    [DisplayName] NVARCHAR(64) NULL, 
     [Description] NVARCHAR(MAX) NULL,
     [GradeLevelRef] INT NULL, 
-    [StateCode] NVARCHAR(30) NULL, 
+    [StateCode] NVARCHAR(64) NULL, 
     [DisplayOnTranscript] BIT NULL, 
-    [SifCode] NVARCHAR(30) NULL, 
-    [NcesCode] NVARCHAR(30) NULL
+    [SifCode] NVARCHAR(64) NULL, 
+    [NcesCode] NVARCHAR(64) NULL
 )
