b0f4dd705a376e5e996dc0f1dc3add58d0d58a1e
ostasyuk <ostasyuk@emp-org.ua>
2016-07-01 10:23:54 +0300

    updated Student Panorama Page

--- Chalkable.Web/app/chlk/models/student/StudentInfo.js
+++ Chalkable.Web/app/chlk/models/student/StudentInfo.js
@@ -3,6 +3,7 @@ REQUIRE('chlk.models.people.HealthCondition');
 REQUIRE('chlk.models.student.StudentContact');
 REQUIRE('chlk.models.grading.GradeLevel');
 REQUIRE('chlk.models.common.NameId');
+REQUIRE('chlk.models.people.EthnicityViewData');
 
 NAMESPACE('chlk.models.student', function(){
     "use strict";
@@ -30,7 +31,10 @@ NAMESPACE('chlk.models.student', function(){
         Number, 'studentNumber',
         chlk.models.common.NameId, 'language',
         chlk.models.common.NameId, 'nationality',
+        chlk.models.common.NameId, 'homeRoom',
         chlk.models.common.ChlkDate, 'originalEnrollmentDate',
+        chlk.models.people.ShortUserInfo, 'counselor',
+        chlk.models.people.EthnicityViewData, 'ethnicity',
 
         OVERRIDE, VOID, function deserialize(raw) {
             BASE(raw);
@@ -45,13 +49,16 @@ NAMESPACE('chlk.models.student', function(){
             this.immigrant = SJX.fromValue(raw.isimmigrant, Boolean);
             this.lep = SJX.fromValue(raw.lep, Boolean);
             this.foreignExchange = SJX.fromValue(raw.isforeignexchange, Boolean);
-            this.stateIDNumber = SJX.fromValue(raw.isforeignexchange, Boolean);
-            this.retained = SJX.fromValue(raw.isretained, Number);
+            this.stateIDNumber = SJX.fromValue(raw.isforeignexchange, Number);
+            this.retained = SJX.fromValue(raw.isretained, Boolean);
             this.alternateStudentNumber = SJX.fromValue(raw.alternatestudentnumber, Number);
             this.studentNumber = SJX.fromValue(raw.studentnumber, Number);
             this.language = SJX.fromDeserializable(raw.language, chlk.models.common.NameId);
+            this.homeRoom = SJX.fromDeserializable(raw.homeroom, chlk.models.common.NameId);
             this.nationality = SJX.fromDeserializable(raw.nationality, chlk.models.common.NameId);
             this.originalEnrollmentDate = SJX.fromDeserializable(raw.originalenrollmentdate, chlk.models.common.ChlkDate);
+            this.counselor = SJX.fromDeserializable(raw.counselor, chlk.models.people.ShortUserInfo);
+            this.ethnicity = SJX.fromDeserializable(raw.ethnicity, chlk.models.people.EthnicityViewData);
         }
     ]);
 });
