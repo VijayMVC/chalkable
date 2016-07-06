c67129037732de5ce05cf89ea0a4558e5064063f
Oleg Yaremko <Oleg Yaremko@localhost>
2016-07-06 18:42:24 +0300

    fixed CHLK-4995 Admin. Gallery. Admin is able to send lesson plans with dates that are beyond grading periods.

--- Chalkable.Web/app/chlk/models/announcement/AnnouncementForm.js
+++ Chalkable.Web/app/chlk/models/announcement/AnnouncementForm.js
@@ -13,21 +13,24 @@ NAMESPACE('chlk.models.announcement', function () {
             Number, 'selectedTypeId',
             String, 'adminRecipientId',
             Array, 'classScheduleDateRanges',
+            chlk.models.schoolYear.Year, 'schoolYear',
             Boolean, 'studyCenterEnabled',
 
-            [[chlk.models.classes.ClassesForTopBar, Boolean]],
-            function $create(classes, isDraft){
+            [[chlk.models.classes.ClassesForTopBar, Boolean, chlk.models.schoolYear.Year]],
+            function $create(classes, isDraft, schoolYear){
                 BASE();
                 if(classes){
                     this.setTopData(classes);
                 }
                 this.setIsDraft(isDraft || false);
+                this.schoolYear = schoolYear;
             },
 
-            [[chlk.models.announcement.FeedAnnouncementViewData]],
-            function $createFromAnnouncement(announcement){
+            [[chlk.models.announcement.FeedAnnouncementViewData, chlk.models.schoolYear.Year]],
+            function $createFromAnnouncement(announcement, schoolYear){
                 BASE();
                 this.setAnnouncement(announcement);
+                this.schoolYear = schoolYear;
             }
         ]);
 });
