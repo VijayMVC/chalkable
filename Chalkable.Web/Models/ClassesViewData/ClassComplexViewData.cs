c25d80bc8b1f1f1b190c400e9ee75f3e997b126c
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2016-07-11 20:53:28 +0300

    Merge with default

--- Chalkable.Web/Models/ClassesViewData/ClassComplexViewData.cs
+++ Chalkable.Web/Models/ClassesViewData/ClassComplexViewData.cs
@@ -8,9 +8,7 @@ namespace Chalkable.Web.Models.ClassesViewData
     public class ClassComplexViewData : ClassViewData
     {
         public Room Room { get; set; }
-
         public IList<string> Periods { get; set; }
-
         public IList<string> DayTypes { get; set; }
 
         public ClassComplexViewData(ClassDetails classComplex) : base(classComplex)
@@ -25,7 +23,7 @@ namespace Chalkable.Web.Models.ClassesViewData
         {
             return classDetails.Select(x => new ClassComplexViewData(x)
             {
-                Room = rooms.FirstOrDefault(r => r.Id == x.RoomRef.Value),
+                Room = x.RoomRef.HasValue ? rooms.FirstOrDefault(r => r.Id == x.RoomRef.Value) : null,
                 Periods = x.ClassPeriods?.Select(cp => cp.Period.Name).Distinct().ToList(),
                 DayTypes = dayTypes.Where(dt => x.ClassPeriods.Select(cp => cp.DayTypeRef).Contains(dt.Id)).Select(dq => dq.Name).Distinct().ToList()
         }).ToList();
