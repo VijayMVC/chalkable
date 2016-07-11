e9ec480df7bb861cc1cc4ade83020ac30f27cc6b
Igor Zhvanko <igzhva@gmail.com>
2016-07-11 14:13:20 +0300

    fixed CHLK-5066

---
+++ Chalkable.Database.School/Tables/AlternateScore.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[AlternateScore] (
+    [Id]                    INT            NOT NULL,
+    [Name]                  NVARCHAR (3)   NOT NULL,
+    [Description]           NVARCHAR (255) NOT NULL,
+    [IncludeInAverage]      BIT            NOT NULL,
+    [PercentOfMaximumScore] DECIMAL (6, 2) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
