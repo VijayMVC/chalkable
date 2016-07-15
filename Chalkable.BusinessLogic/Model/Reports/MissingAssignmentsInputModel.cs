a251a612187226c3da85d5b5f08a55806e08fa5c
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2016-07-15 19:16:38 +0300

    fixed student explorer

---
+++ Chalkable.BusinessLogic/Model/Reports/MissingAssignmentsInputModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Chalkable.Common;
+
+namespace Chalkable.BusinessLogic.Model.Reports
+{
+    public class MissingAssignmentsInputModel : BaseReportInputModel
+    {
+        public IntList AlternateScoreIds { get; set; }
+        public bool AlternateScoresOnly { get; set; }
+        public bool ConsiderZerosAsMissingGrades { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public bool IncludeWithdrawn { get; set; }
+        public bool OnePerPage { get; set; }
+        public int OrderBy { get; set; }
+        public bool SuppressStudentName { get; set; }
+    }
+}
