060a6b2e34ce8eb568b30a70b360564ca04bd505
Taras Kolodiy <tkolodiy@emp-org.ua>
2016-09-12 10:13:06 -0500

    Update. Admin. Feed

--- Chalkable.AutomatedTests/tests/teacher/teacher_selected_class_attendance/loading_the_main_page_of_attendance.py
+++ Chalkable.AutomatedTests/tests/teacher/teacher_selected_class_attendance/loading_the_main_page_of_attendance.py
@@ -3,7 +3,7 @@ from base_auth_test import *
 class TestFeed(BaseAuthedTestCase):
     def test_attendance(self):
         get_attendance_summary = self.get('/Attendance/AttendanceSummary.json?')
-        get_not_taken_attendance_classes= self.get('/NotTakenAttendanceClasses.json?')
+        get_not_taken_attendance_classes= self.get('/Attendance/NotTakenAttendanceClasses.json?')
 
 if __name__ == '__main__':
     unittest.main()
