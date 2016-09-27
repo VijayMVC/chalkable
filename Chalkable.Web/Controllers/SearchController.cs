6d89dbfa896d8b2d19716de995a23cb1bd641ec8
Yura Andrusyshyn <yandrusyshyn@emp.org.ua>
2016-09-27 11:04:09 +0300

    refactor search on server side

--- Chalkable.Web/Controllers/SearchController.cs
+++ Chalkable.Web/Controllers/SearchController.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web.Mvc;
 using Chalkable.BusinessLogic.Model;
+using Chalkable.BusinessLogic.Security;
+using Chalkable.BusinessLogic.Services.School;
 using Chalkable.Data.Common.Enums;
 using Chalkable.Data.School.Model;
 using Chalkable.Web.ActionFilters;
@@ -16,78 +21,48 @@ namespace Chalkable.Web.Controllers
         [AuthorizationFilter("DistrictAdmin, Teacher, Student", true, new[] { AppPermissionType.User })]  // - System Admin
         public ActionResult Search(string query, IntList includedSearchType)
         {
-            IDictionary<SearchTypeEnum, object> searchRes = new Dictionary<SearchTypeEnum, object>();
+            Trace.Assert(Context.PersonId.HasValue);
 
-            int? teacherId = null;
-            if (Context.Role == CoreRoles.TEACHER_ROLE && !Context.Claims.HasPermission(ClaimInfo.VIEW_STUDENT))
-                teacherId = Context.PersonId;
-
-            int? studentId = null;
-            if (Context.Role == CoreRoles.STUDENT_ROLE)
-                studentId = Context.PersonId;
-
-            query = query.ToLower();
-            var personList = new PersonList
+            IDictionary<SearchTypeEnum, Func<string, object>> searchActionRegister = new Dictionary <SearchTypeEnum, Func<string, object>>
             {
-                Students = new List<StudentSchoolsInfo>(),
-                Staffs = new List<Staff>()
+                {SearchTypeEnum.Student, SearchStudents},
+                {SearchTypeEnum.Staff, SearchStaffs},
+                {SearchTypeEnum.Announcements, SchoolLocator.AnnouncementFetchService.GetAnnouncementsByFilter},
+                {SearchTypeEnum.Attachments, SearchAttachments},
+                {SearchTypeEnum.Classes, SchoolLocator.ClassService.SearchClasses}
             };
+            if(Context.Role == CoreRoles.DISTRICT_ADMIN_ROLE)
+                searchActionRegister.Add(SearchTypeEnum.Group, q => SchoolLocator.GroupService.GetGroups(Context.PersonId.Value, q));
 
-            if (includedSearchType != null)
+            IDictionary<SearchTypeEnum, object> searchRes = new Dictionary<SearchTypeEnum, object>();
+            query = query.ToLower();
+            foreach (var keyAction in searchActionRegister.Where(keyAction => IsTypeIncluded(includedSearchType, keyAction.Key)))
             {
-                if (includedSearchType.Any(x => x == (int) SearchTypeEnum.Persons))
-                {
-                    personList.Students = SchoolLocator.StudentService.SearchStudents(Context.SchoolYearId.Value, null, null, null, null, teacherId, studentId, query, true, 0, 15, null, true);
-                    personList.Staffs = SchoolLocator.StaffService.SearchStaff(Context.SchoolYearId.Value, null, studentId, query, true, 0, 15);
-                }
-
-                if (includedSearchType.Any(x => x == (int) SearchTypeEnum.Student))
-                    personList.Students = SchoolLocator.StudentService.SearchStudents(Context.SchoolYearId.Value, null, null, null, null, teacherId, studentId, query, true, 0, 15, null, true);
-
-                if (includedSearchType.Any(x => x == (int) SearchTypeEnum.Student))
-                    personList.Staffs = SchoolLocator.StaffService.SearchStaff(Context.SchoolYearId.Value, null, studentId, query, true, 0, 15);
-
-                if (
-                    includedSearchType.Any( x => x == (int) SearchTypeEnum.Persons || x == (int) SearchTypeEnum.Student || x == (int) SearchTypeEnum.Staff))
-                    searchRes.Add(SearchTypeEnum.Persons, personList);
-
-                if (includedSearchType.Any(x => x == (int) SearchTypeEnum.Announcements))
-                    searchRes.Add(SearchTypeEnum.Announcements, SchoolLocator.AnnouncementFetchService.GetAnnouncementsByFilter(query));
-
-                if (includedSearchType.Any(x => x == (int) SearchTypeEnum.Announcements))
-                {
-                    var attachments = SchoolLocator.AnnouncementAttachmentService.GetAnnouncementAttachments(query);
-                    searchRes.Add(SearchTypeEnum.Attachments, SchoolLocator.AnnouncementAttachmentService.TransformToAttachmentsInfo(attachments, null));
-                }
-
-                if (includedSearchType.Any(x => x == (int) SearchTypeEnum.Announcements))
-                    searchRes.Add(SearchTypeEnum.Classes, SchoolLocator.ClassService.SearchClasses(query));
-
-                if (includedSearchType.Any(x => x == (int) SearchTypeEnum.Group) && Context.Role == CoreRoles.DISTRICT_ADMIN_ROLE)
-                    searchRes.Add(SearchTypeEnum.Group, SchoolLocator.GroupService.GetGroups(Context.PersonId.Value, query));
-                
+                searchRes.Add(keyAction.Key, keyAction.Value(query));
             }
-            else
-            {
-                personList.Students = SchoolLocator.StudentService.SearchStudents(Context.SchoolYearId.Value, null, null, null, null, teacherId, studentId, query, true, 0, 15, null, true);
-                personList.Staffs = SchoolLocator.StaffService.SearchStaff(Context.SchoolYearId.Value, null, studentId, query, true, 0, 15);
-                searchRes.Add(SearchTypeEnum.Persons, personList);
-                searchRes.Add(SearchTypeEnum.Announcements, SchoolLocator.AnnouncementFetchService.GetAnnouncementsByFilter(query));
-                var attachments = SchoolLocator.AnnouncementAttachmentService.GetAnnouncementAttachments(query);
-                searchRes.Add(SearchTypeEnum.Attachments, SchoolLocator.AnnouncementAttachmentService.TransformToAttachmentsInfo(attachments, null));
-                searchRes.Add(SearchTypeEnum.Classes, SchoolLocator.ClassService.SearchClasses(query));
-                if (Context.Role == CoreRoles.DISTRICT_ADMIN_ROLE)
-                    searchRes.Add(SearchTypeEnum.Group, SchoolLocator.GroupService.GetGroups(Context.PersonId.Value, query));
-            }
-           
             return Json(SearchViewData.Create(searchRes));
         }
 
-
-        public class PersonList
+        private static bool IsTypeIncluded(IList<int> includedSearchTypes, SearchTypeEnum type)
         {
-            public IList<StudentSchoolsInfo> Students { get; set; }
-            public IList<Staff> Staffs { get; set; }
+            return includedSearchTypes == null || includedSearchTypes.Count == 0 || includedSearchTypes.Contains((int) type);
         }
+
+        private PaginatedList<StudentSchoolsInfo> SearchStudents(string query)
+        {
+            var teacherId = BaseSecurity.IsTeacher(Context) && !Context.Claims.HasPermission(ClaimInfo.VIEW_STUDENT) ? Context.PersonId : null;
+            var studentId = Context.Role == CoreRoles.STUDENT_ROLE ? Context.PersonId : null;
+            return SchoolLocator.StudentService.SearchStudents(Context.SchoolYearId.Value, null, null, null, null, teacherId, studentId, query, true, 0, 15, null, true);
+        }
+        private PaginatedList<Staff> SearchStaffs(string query)
+        {
+            var studentId = Context.Role == CoreRoles.STUDENT_ROLE ? Context.PersonId : null;
+            return SchoolLocator.StaffService.SearchStaff(Context.SchoolYearId.Value, null, studentId, query, true, 0, 15);
+        } 
+        private IList<AnnouncementAttachmentInfo> SearchAttachments(string query)
+        {
+            var attachments = SchoolLocator.AnnouncementAttachmentService.GetAnnouncementAttachments(query);
+            return SchoolLocator.AnnouncementAttachmentService.TransformToAttachmentsInfo(attachments, null);
+        } 
     }
 }
