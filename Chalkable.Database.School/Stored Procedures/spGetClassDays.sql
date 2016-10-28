1acb2c9d23e17e6469811e5d035edbdf3034e5fc
Igor Zhvanko <igzhva@gmail.com>
2016-10-28 13:19:57 +0300

    CHLK-5445 Teacher. The error page is shown after clicking the "Discard" button during creating items

--- Chalkable.Database.School/Stored Procedures/spGetClassDays.sql
+++ Chalkable.Database.School/Stored Procedures/spGetClassDays.sql
@@ -4,7 +4,12 @@ As
 
 Declare @schoolYearId int;
 Set		@schoolYearId = (Select SchoolYearRef From Class Where Id = @classId);
-Declare @markingPeriodId int = (Select MarkingPeriodRef From MarkingPeriodClass Where ClassRef = @classId)
+Declare @markingPeriodId int = (
+	Select top 1 MarkingPeriodRef 
+	From MarkingPeriodClass Join MarkingPeriod
+		On MarkingPeriodClass.MarkingPeriodRef = MarkingPeriod.Id
+	Where ClassRef = @classId Order By MarkingPeriod.EndDate Desc
+)
 Declare @markingPeriodStartDate datetime2 = (Select StartDate From MarkingPeriod Where Id = @markingPeriodId)
 Declare @markingPeriodEndDate datetime2 = (Select EndDate From MarkingPeriod Where Id = @markingPeriodId)
 
