e5ff9d0a3393f4a69ccf02658e1d70140e7e3034
Taras Kolodiy <tkolodiy@emp-org.ua>
2016-12-14 08:40:46 -0600

    Feed. Marking items. Refactoring.

--- Chalkable.AutomatedTests/tests/teacher/Feed/ToDo/test_1_mark_done_all_items_due_before_today_older_than_30_days.py
+++ Chalkable.AutomatedTests/tests/teacher/Feed/ToDo/test_1_mark_done_all_items_due_before_today_older_than_30_days.py
@@ -12,12 +12,12 @@ class TestMarkItems_All_DueBeforeToday_OlderThan30days(BaseTestCase):
         self.teacher.post_json('/Announcement/UnDone.json?', data={'option': 3})
 
         # get a current time
-        self.current_time = time.strftime('%Y-%m-%d')
+        self.current_time = datetime.now()
 
         # get a date needed for filtering
-        date = datetime.strptime(self.current_time, "%Y-%m-%d")
+        date = datetime.now()
         end_date = date.today() - timedelta(days=30)
-        self.current_date_minus_30 = end_date.strftime("%Y-%m-%d")
+        self.current_date_minus_30 = end_date
 
     def internal_(self, option, start, count):
         self.teacher.post_json('/Announcement/Done.json?', data={'option': option})
@@ -31,21 +31,22 @@ class TestMarkItems_All_DueBeforeToday_OlderThan30days(BaseTestCase):
 
         def get_item_date(one_item):
             if one_item['type'] == 1:
-                return one_item['classannouncementdata']['expiresdate']
+                return datetime.date(datetime.strptime(one_item['classannouncementdata']['expiresdate'], '%Y-%m-%d'))
             if one_item['type'] == 4:
-                return one_item['supplementalannouncementdata']['expiresdate']
+                return datetime.date(
+                    datetime.strptime(one_item['supplementalannouncementdata']['expiresdate'], '%Y-%m-%d'))
 
         def get_lesson_plan_end_date(one_item):
             if one_item['type'] == 3:
-                return one_item['lessonplandata']['enddate']
+                return datetime.date(datetime.strptime(one_item['lessonplandata']['enddate'], '%Y-%m-%d'))
 
         def current_time_or_time_minus_30(inner_option):
             if inner_option == 2:
                 inner_current_time = self.current_date_minus_30
-                return inner_current_time
+                return datetime.date(inner_current_time)
             if inner_option == 1:
                 inner_current_time = self.current_time
-                return inner_current_time
+                return datetime.date(inner_current_time)
 
         for item in dictionary_verify_annoucementviewdatas_all:
             if item['type'] == 1 or item['type'] == 4:
--- Chalkable.AutomatedTests/tests/teacher/Feed/ToDo/test_2_custom_range_mark_done_all_items_due_before_today_older_than_30_days.py
+++ Chalkable.AutomatedTests/tests/teacher/Feed/ToDo/test_2_custom_range_mark_done_all_items_due_before_today_older_than_30_days.py
@@ -13,12 +13,12 @@ class TestCustomRangeMarkItems(BaseTestCase):
         self.teacher.post_json('/Announcement/UnDone.json?', data={'option': 3})
 
         # get a current time
-        self.current_time = time.strftime('%Y-%m-%d')
+        self.current_time = datetime.now()
 
         # get a date needed for filtering
-        date = datetime.strptime(self.current_time, "%Y-%m-%d")
+        date = datetime.now()
         end_date = date.today() - timedelta(days=30)
-        self.current_date_minus_30 = end_date.strftime("%Y-%m-%d")
+        self.current_date_minus_30 = end_date
 
         # getting grading periods
         list_for_start_date = []
@@ -27,41 +27,36 @@ class TestCustomRangeMarkItems(BaseTestCase):
         get_first_period = get_grading_periods['data']
         if len(get_first_period) > 0:
             for item in get_first_period:
-                self.startdate_2 = item['startdate']
-                list_for_start_date.append(self.startdate_2)
-                self.enddate_2 = item['enddate']
-                list_for_end_date.append(self.enddate_2)
+                list_for_start_date.append(datetime.strptime(item['startdate'], '%Y-%m-%d'))
+                list_for_end_date.append(datetime.strptime(item['enddate'] , '%Y-%m-%d'))
 
-        decoded_list_1 = [x.encode('utf-8') for x in list_for_start_date]
-        decoded_list_2 = [x.encode('utf-8') for x in list_for_end_date]
+        list_for_start_date.sort()
+        list_for_end_date.sort()
 
         # getting a start of the first grading period and an end of the last grading period
-        self.start_of_grading_period = decoded_list_1[0]
-        self.end_of_grading_period = decoded_list_2[-1]
+        self.start_of_grading_period = list_for_start_date[0]
+        self.end_of_grading_period = list_for_end_date[-1]
 
-        start_gr_period_in_date_time = datetime.strptime(self.start_of_grading_period, "%Y-%m-%d")
-        start_gr_period_plus_10 = start_gr_period_in_date_time.date() + timedelta(days=10)
+        #start_gr_period_in_date_time = datetime.strptime(self.start_of_grading_period, "%Y-%m-%d")
+        start_gr_period_plus_10 = self.start_of_grading_period + timedelta(days=10)
 
-        end_gr_period_in_date_time = datetime.strptime(self.end_of_grading_period, "%Y-%m-%d")
-        end_gr_period_minus_10 = end_gr_period_in_date_time.date() - timedelta(days=10)
-        self.current_date_plus2_10 = start_gr_period_plus_10.strftime("%Y-%m-%d")
-        self.current_date_minus2_10 = end_gr_period_minus_10.strftime("%Y-%m-%d")
+        #end_gr_period_in_date_time = datetime.strptime(self.end_of_grading_period, "%Y-%m-%d")
+        end_gr_period_minus_10 =  self.end_of_grading_period - timedelta(days=10)
 
-        self.random_date_string_format = self.random_date(self.current_date_plus2_10, self.current_date_minus2_10)
+        self.random_date_string_format = self.random_date(start_gr_period_plus_10.strftime("%Y-%m-%d"), end_gr_period_minus_10.strftime("%Y-%m-%d"))
 
         self.random_date_string_format_correct_format = datetime.strptime(self.random_date_string_format,
                                                                           "%m-%d-%Y").strftime("%Y-%m-%d")
 
         random_date_date_time_format = datetime.strptime(self.random_date_string_format_correct_format, "%Y-%m-%d")
-        random_date_minus_10 = random_date_date_time_format.date() - timedelta(days=10)
-        random_date_plus_10 = random_date_date_time_format.date() + timedelta(days=10)
-        self.current_date_minus_10 = random_date_minus_10.strftime("%Y-%m-%d")
-        self.current_date_plus_10 = random_date_plus_10.strftime("%Y-%m-%d")
+
+        self.random_date_minus_10 = random_date_date_time_format.date() - timedelta(days=10)
+        self.random_date_plus_10 = random_date_date_time_format.date() + timedelta(days=10)
 
         # filter: custom range, earliest
         self.teacher.post_json('/Feed/SetSettings.json?', data={'sortType': '0',
-                                                                'fromDate': self.current_date_minus_10,
-                                                                'toDate': self.current_date_plus_10})
+                                                                'fromDate': self.random_date_minus_10.strftime("%Y-%m-%d"),
+                                                                'toDate': self.random_date_plus_10.strftime("%Y-%m-%d")})
 
     def internal_(self, option):
         def list_items_json_unicode(start, count):
@@ -72,25 +67,25 @@ class TestCustomRangeMarkItems(BaseTestCase):
 
         def get_lesson_plan_start_date(one_item):
             if one_item['type'] == 3:
-                return one_item['lessonplandata']['startdate']
+                return datetime.date(datetime.strptime(one_item['lessonplandata']['startdate'], '%Y-%m-%d'))
 
         def get_lesson_plan_end_date(one_item):
             if one_item['type'] == 3:
-                return one_item['lessonplandata']['enddate']
+                return datetime.date(datetime.strptime(one_item['lessonplandata']['enddate'], '%Y-%m-%d'))
 
         def get_item_date(one_item):
             if one_item['type'] == 1:
-                return one_item['classannouncementdata']['expiresdate']
+                return datetime.date(datetime.strptime(one_item['classannouncementdata']['expiresdate'], '%Y-%m-%d'))
             if one_item['type'] == 4:
-                return one_item['supplementalannouncementdata']['expiresdate']
+                return datetime.date(datetime.strptime(one_item['supplementalannouncementdata']['expiresdate'], '%Y-%m-%d'))
 
         def current_time_or_time_minus_30(inner_option):
             if inner_option == 2:
                 inner_current_time = self.current_date_minus_30
-                return inner_current_time
+                return datetime.date(inner_current_time)
             if inner_option == 1:
                 inner_current_time = self.current_time
-                return inner_current_time
+                return datetime.date(inner_current_time)
 
         # marking items
         self.teacher.post_json('/Announcement/Done.json?', data={'option': option})
@@ -103,14 +98,14 @@ class TestCustomRangeMarkItems(BaseTestCase):
 
             def get_item_date2(one_item):
                 if one_item['type'] == 1:
-                    return one_item['classannouncementdata']['expiresdate']
+                    return datetime.date(datetime.strptime(one_item['classannouncementdata']['expiresdate'], '%Y-%m-%d'))
                 if one_item['type'] == 4:
-                    return one_item['supplementalannouncementdata']['expiresdate']
+                    return datetime.date(datetime.strptime(one_item['supplementalannouncementdata']['expiresdate'], '%Y-%m-%d'))
                 if one_item['type'] == 3:
-                    return one_item['lessonplandata']['startdate']
+                    return datetime.date(datetime.strptime(one_item['lessonplandata']['startdate'], '%Y-%m-%d'))
 
             for item in list_items_json_unicode(0, 2500):
-                self.assertTrue((get_item_date2(item) < self.current_date_minus_10 or (get_item_date(item) > self.current_date_plus_10),
+                self.assertTrue(((get_item_date2(item) < self.random_date_minus_10) or (get_item_date2(item) > self.random_date_plus_10),
                                      'Items are not marked as complete ' + str(item["id"])))
 
         if option == 1 or option == 2:
@@ -123,8 +118,8 @@ class TestCustomRangeMarkItems(BaseTestCase):
 
                 if item['type'] == 3:
                     self.assertTrue(
-                        (get_lesson_plan_start_date(item) >= self.current_date_minus_10 and get_lesson_plan_end_date(
-                            item) >= current_time_or_time_minus_30(option)),
+                        ((get_lesson_plan_start_date(item) >= self.random_date_minus_10) and (get_lesson_plan_end_date(
+                            item) >= current_time_or_time_minus_30(option))),
                         'This lesson plan have not to be in the list ' + str(item["id"]))
 
     def test_mark_done_all_items(self):
